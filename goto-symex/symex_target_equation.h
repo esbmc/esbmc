40baba9d3b3b93321914cf439d3434cdc88e85ea
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-03-21 12:41:22 +0000

    Implement operator< for SSA_stept

--- goto-symex/symex_target_equation.h
+++ goto-symex/symex_target_equation.h
@@ -134,6 +134,8 @@ public:
     void output(
       const namespacet &ns,
       std::ostream &out) const;
+
+    bool operator<(const SSA_stept s2) const;
   };
   
   unsigned count_ignored_SSA_steps() const
