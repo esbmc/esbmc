4350d615135a92f097dc76e294b04fc5038a6dbf
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-04-18 13:03:34 +0000

    Ensure that the proposition updates squirted into goto program are of bool type

--- esbmc/parseoptions.cpp
+++ esbmc/parseoptions.cpp
@@ -884,7 +884,8 @@ void cbmc_parseoptionst::add_monitor_exprs(goto_programt::targett insn, goto_pro
   std::set<std::pair<std::string, exprt> >::const_iterator trig_it;
   for (trig_it = triggered.begin(); trig_it != triggered.end(); trig_it++) {
     std::string prop_name = "c::" + trig_it->first + "_status";
-    new_insn.code = code_assignt(symbol_exprt(prop_name), trig_it->second);
+    new_insn.code = code_assignt(symbol_exprt(prop_name, typet("bool")), trig_it->second);
+
     // new_insn location field not set - I believe it gets numbered later.
     insn_list.insert(insn, new_insn);
   }
