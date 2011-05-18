1e91c12e1466d907d70d32cff3202f9d0e7f06c5
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-05-18 16:22:57 +0000

    Convert string.c STRING_ABSTRACTION switchyness to pairs of functions.
    IE, for strcmp generate a normal strcmp, and a strcmp_strabs, one
    containing no string-abstracted routines, and the other with them.

--- ansi-c/library/string.c
+++ ansi-c/library/string.c
@@ -1,45 +1,59 @@
 #include <string.h>
 #include <stdlib.h>
 
+#include "intrinsics.h"
+
 char *strcpy(char *dst, const char *src)
 {
   __ESBMC_HIDE:;
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  size_t i;
+  for(i=0; src[i]!=0; i++)
+    dst[i]=src[i];
+  return dst;
+}
+
+char *strcpy_strabs(char *dst, const char *src)
+{
+  __ESBMC_HIDE:;
   __ESBMC_assert(__ESBMC_is_zero_string(src), "strcpy zero-termination of 2nd argument");
   __ESBMC_assert(__ESBMC_buffer_size(dst)>__ESBMC_zero_string_length(src), "strcpy buffer overflow");
   dst[__ESBMC_zero_string_length(src)]=0;
   __ESBMC_is_zero_string(dst)=1;
   __ESBMC_zero_string_length(dst)=__ESBMC_zero_string_length(src);
-  #else
-  size_t i;
-  for(i=0; src[i]!=0; i++)
-    dst[i]=src[i];
-  #endif
   return dst;
 }
 
 char *strncpy(char *dst, const char *src, size_t n)
 {
   __ESBMC_HIDE:;
-  #ifdef __ESBMC_STRING_ABSTRACTION
-  __ESBMC_assert(__ESBMC_is_zero_string(src), "strncpy zero-termination of 2nd argument");
-  __ESBMC_assert(__ESBMC_buffer_size(dst)>=n, "strncpy buffer overflow");
-  __ESBMC_is_zero_string(dst)=__ESBMC_zero_string_length(src)<n;
-  __ESBMC_zero_string_length(dst)=__ESBMC_zero_string_length(src);
-  #else
   size_t i=0;
   for( ; i<n && src[i]!=0; i++)
     dst[i]=src[i];
   for( ; i<n ; i++)
     dst[i]=0;
-  #endif
+  return dst;
+}
+
+char *strncpy_strabs(char *dst, const char *src, size_t n)
+{
+  __ESBMC_HIDE:;
+  __ESBMC_assert(__ESBMC_is_zero_string(src), "strncpy zero-termination of 2nd argument");
+  __ESBMC_assert(__ESBMC_buffer_size(dst)>=n, "strncpy buffer overflow");
+  __ESBMC_is_zero_string(dst)=__ESBMC_zero_string_length(src)<n;
+  __ESBMC_zero_string_length(dst)=__ESBMC_zero_string_length(src);
   return dst;
 }
 
 char *strcat(char *dst, const char *src)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  // XXX - this has no body if string abstraction option is not enabled
+  return dst;
+}
+
+char *strcat_strabs(char *dst, const char *src)
+{
+  __ESBMC_HIDE:
   size_t new_size;
   __ESBMC_assert(__ESBMC_is_zero_string(dst), "strcat zero-termination of 1st argument");
   __ESBMC_assert(__ESBMC_is_zero_string(src), "strcat zero-termination of 2nd argument");
@@ -53,14 +67,19 @@ char *strcat(char *dst, const char *src)
   dst[new_size - 1]=0;
   __ESBMC_is_zero_string(dst)=1;
   __ESBMC_zero_string_length(dst)=new_size;
-  #endif
   return dst;
 }
 
 char *strncat(char *dst, const char *src, size_t n)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  // XXX - this has no body if string abstraction option isn't enabled
+  return dst;
+}
+
+char *strncat_strabs(char *dst, const char *src, size_t n)
+{
+  __ESBMC_HIDE:
   size_t additional, new_size;
   __ESBMC_assert(__ESBMC_is_zero_string(dst), "strncat zero-termination of 1st argument");
   __ESBMC_assert(__ESBMC_is_zero_string(src) || __ESBMC_buffer_size(src)>=n, "strncat zero-termination of 2nd argument");
@@ -75,7 +94,6 @@ char *strncat(char *dst, const char *src, size_t n)
   dst[dest_len + i] = 0;
   __ESBMC_is_zero_string(dst)=1;
   __ESBMC_zero_string_length(dst)=new_size;
-  #endif
   return dst;
 }
 
@@ -84,11 +102,18 @@ int strcmp(const char *s1, const char *s2)
   __ESBMC_HIDE:;
   int retval;
   if(s1!=0 && s1==s2) return 0;
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  // XXX - this does nothing useful if string abstraction isn't defined
+  return retval;
+}
+
+int strcmp_strabs(const char *s1, const char *s2)
+{
+  __ESBMC_HIDE:;
+  int retval;
+  if(s1!=0 && s1==s2) return 0;
   __ESBMC_assert(__ESBMC_is_zero_string(s1), "strcmp zero-termination of 1st argument");
   __ESBMC_assert(__ESBMC_is_zero_string(s2), "strcmp zero-termination of 2nd argument");
   if(__ESBMC_zero_string_length(s1) != __ESBMC_zero_string_length(s1)) __ESBMC_assume(retval!=0);
-  #endif
   return retval;
 }
 
@@ -96,24 +121,32 @@ int strncmp(const char *s1, const char *s2, size_t n)
 {
   __ESBMC_HIDE:
   if(s1!=0 && s1==s2) return 0;
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  // XXX - does nothing useful if string abstraction option not on
+  return 0;
+}
+
+int strncmp_strabs(const char *s1, const char *s2, size_t n)
+{
+  if(s1!=0 && s1==s2) return 0;
   __ESBMC_assert(__ESBMC_is_zero_string(s1) || __ESBMC_buffer_size(s1)>=n, "strncmp zero-termination of 1st argument");
   __ESBMC_assert(__ESBMC_is_zero_string(s1) || __ESBMC_buffer_size(s2)>=n, "strncmp zero-termination of 2nd argument");
-  #else
-  #endif
+  // XXX - doesn't do anything useful /even if/ string abs is on
+  return 0;
 }
 
 size_t strlen(const char *s)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
-  //__ESBMC_assert(__ESBMC_is_zero_string(s), "strlen zero-termination");
-  return __ESBMC_zero_string_length(s);
-  #else
   size_t len=0;
   while(s[len]!=0) len++;
   return len;
-  #endif
+}
+
+size_t strlen_strabs(const char *s)
+{
+  __ESBMC_HIDE:
+  //__ESBMC_assert(__ESBMC_is_zero_string(s), "strlen zero-termination");
+  return __ESBMC_zero_string_length(s);
 }
 
 char *strdup(const char *str)
@@ -123,9 +156,19 @@ char *strdup(const char *str)
   bufsz=(strlen(str)+1)*sizeof(char);
   char *cpy=malloc(bufsz);
   if(cpy==((void *)0)) return 0;
-  #ifdef __ESBMC_STRING_ABSTRACTION
   __ESBMC_assume(__ESBMC_buffer_size(cpy)==bufsz);
-  #endif
+  cpy=strcpy(cpy, str);
+  return cpy;
+}
+
+char *strdup_strabs(const char *str)
+{
+  __ESBMC_HIDE:;
+  size_t bufsz;
+  bufsz=(strlen(str)+1)*sizeof(char);
+  char *cpy=malloc(bufsz);
+  if(cpy==((void *)0)) return 0;
+  __ESBMC_assume(__ESBMC_buffer_size(cpy)==bufsz);
   cpy=strcpy(cpy, str);
   return cpy;
 }
@@ -133,7 +176,13 @@ char *strdup(const char *str)
 void *memcpy(void *dst, const void *src, size_t n)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  for(size_t i=0; i<n ; i++) dst[i]=src[i];
+  return dst;
+}
+
+void *memcpy_strabs(void *dst, const void *src, size_t n)
+{
+  __ESBMC_HIDE:
   //__ESBMC_assert(__ESBMC_buffer_size(src)>=n, "memcpy buffer overflow");
   //__ESBMC_assert(__ESBMC_buffer_size(dst)>=n, "memcpy buffer overflow");
   //  for(size_t i=0; i<n ; i++) dst[i]=src[i];
@@ -146,16 +195,20 @@ void *memcpy(void *dst, const void *src, size_t n)
   else if(!(__ESBMC_is_zero_string(dst) &&
             n <= __ESBMC_zero_string_length(dst)))
     __ESBMC_is_zero_string(dst)=0;
-  #else
-  for(size_t i=0; i<n ; i++) dst[i]=src[i];
-  #endif
   return dst;
 }
 
 void *memset(void *s, int c, size_t n)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  char *sp=s;
+  for(size_t i=0; i<n ; i++) sp[i]=c;
+  return s;
+}
+
+void *memset_strabs(void *s, int c, size_t n)
+{
+  __ESBMC_HIDE:
   char *sp=s;
   for(size_t i=0; i<n ; i++) {sp[i]=c;}
 #if 0
@@ -175,17 +228,26 @@ void *memset(void *s, int c, size_t n)
   else
     __ESBMC_is_zero_string(s)=0;
 #endif
-  #else
-  char *sp=s;
-  for(size_t i=0; i<n ; i++) sp[i]=c;
-  #endif
   return s;
 }
 
 void *memmove(void *dest, const void *src, size_t n)
 {
   __ESBMC_HIDE:
-  #ifdef __ESBMC_STRING_ABSTRACTION
+  if (dest-src >= n)
+  {
+    for(size_t i=0; i<n ; i++) dest[i]=src[i];
+  }
+  else
+  {
+    for(size_t i=n; i>0 ; i--) dest[i-1]=src[i-1];
+  }
+  return dest;
+}
+
+void *memmove_strabs(void *dest, const void *src, size_t n)
+{
+  __ESBMC_HIDE:
   __ESBMC_assert(__ESBMC_buffer_size(src)>=n, "memmove buffer overflow");
   // dst = src (with overlap allowed)
   if(__ESBMC_is_zero_string(src) &&
@@ -196,27 +258,12 @@ void *memmove(void *dest, const void *src, size_t n)
   }
   else
     __ESBMC_is_zero_string(dest)=0;
-  #else
-  if (dest-src >= n)
-  {
-    for(size_t i=0; i<n ; i++) dest[i]=src[i];
-  }
-  else
-  {
-    for(size_t i=n; i>0 ; i--) dest[i-1]=src[i-1];
-  }
-  #endif
   return dest;
 }
 
 int memcmp(const void *s1, const void *s2, size_t n)
 {
   __ESBMC_HIDE:;
-  int res;
-  #ifdef __ESBMC_STRING_ABSTRACTION
-  __ESBMC_assert(__ESBMC_buffer_size(s1)>=n, "memcmp buffer overflow of 1st argument");
-  __ESBMC_assert(__ESBMC_buffer_size(s2)>=n, "memcmp buffer overflow of 2nd argument");
-  #else
   const unsigned char *sc1=s1, *sc2=s2;
   for(; n!=0; n--)
   {
@@ -224,6 +271,15 @@ int memcmp(const void *s1, const void *s2, size_t n)
     if (res != 0)
       return res;
   }
-  #endif
+  return res;
+}
+
+int memcmp_strabs(const void *s1, const void *s2, size_t n)
+{
+  __ESBMC_HIDE:;
+  int res;
+  __ESBMC_assert(__ESBMC_buffer_size(s1)>=n, "memcmp buffer overflow of 1st argument");
+  __ESBMC_assert(__ESBMC_buffer_size(s2)>=n, "memcmp buffer overflow of 2nd argument");
+  // XXX - memcmp doesn't do anything here when strabs is enabled
   return res;
 }
