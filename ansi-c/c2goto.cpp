35a0244d8b153d9d03223edaff5807dc63c9ed31
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-05-18 16:23:08 +0000

    Add basics of library-to-goto-object converting program.

---
+++ ansi-c/c2goto.cpp
@@ -0,0 +1,34 @@
+#include <goto-programs/goto_convert_functions.h>
+#include <langapi/language_ui.h>
+#include <ui_message.h>
+#include <parseoptions.h>
+
+class c2goto_parseopt : public parseoptions_baset, public language_uit
+{
+  public:
+  c2goto_parseopt(int argc, const char **argv):
+    parseoptions_baset("", argc, argv),
+    language_uit(cmdline)
+  {
+  }
+
+  int doit() {
+    goto_functionst goto_functions;
+
+    if (parse()) return true;
+    if (typecheck()) return true;
+    if (final()) return true;
+
+    clear_parse();
+
+    goto_convert(context, optionst(), goto_functions, ui_message_handler);
+
+    return 0;
+  }
+};
+
+int main(int argc, const char **argv)
+{
+  c2goto_parseopt parseopt(argc, argv);
+  return parseopt.main();
+}
