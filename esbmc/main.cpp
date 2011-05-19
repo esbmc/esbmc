7a9dcc7c8aa8ae87e52be048612ead9b748d08c2
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-05-19 16:04:11 +0000

    Support optional cpp/specc/php languages for esbmc

--- esbmc/main.cpp
+++ esbmc/main.cpp
@@ -37,4 +37,17 @@ int main(int argc, const char **argv)
 }
 
 const mode_table_et mode_table[] =
-{ LANGAPI_HAVE_MODE_C, LANGAPI_HAVE_MODE_CVC, LANGAPI_HAVE_MODE_END };
+{
+  LANGAPI_HAVE_MODE_C,
+  LANGAPI_HAVE_MODE_CVC,
+#ifdef USE_CPP
+  LANGAPI_HAVE_MODE_CPP,
+#endif
+#ifdef USE_SPECC
+  LANGAPI_HAVE_MODE_SPECC,
+#endif
+#ifdef USE_PHP
+  LANGAPI_HAVE_MODE_PHP,
+#endif
+  LANGAPI_HAVE_MODE_END
+};
