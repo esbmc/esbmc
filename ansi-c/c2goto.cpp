f8a70d1c1713192333f9b2b377b7a3eb754c946d
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-05-19 14:40:50 +0000

    Enable 16/32/64 options for c2goto, feed into global "config".

--- ansi-c/c2goto.cpp
+++ ansi-c/c2goto.cpp
@@ -2,18 +2,21 @@
 #include <langapi/language_ui.h>
 #include <ui_message.h>
 #include <parseoptions.h>
+#include <util/config.h>
 
 class c2goto_parseopt : public parseoptions_baset, public language_uit
 {
   public:
   c2goto_parseopt(int argc, const char **argv):
-    parseoptions_baset("", argc, argv),
+    parseoptions_baset("(16)(32)(64)", argc, argv),
     language_uit(cmdline)
   {
   }
 
   int doit() {
     goto_functionst goto_functions;
+
+    config.set(cmdline);
 
     if (parse()) return true;
     if (typecheck()) return true;
