3be5ba05a7baa82ef43b31e2d457c56031687d9a
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-05-21 13:49:53 +0000

    Don't use dl-lib routines to locate c library symbols.
    Two reasons: it seems that dlopen doesn't allow us to open our self
    binary, possibly because we're an executable, not a shared object.
    Other reason is that the binary object symbols get discarded,
    probably because nothing links against them. They're also likely to
    disappear when the binary gets stripped.
    
    So, this way we link against those symbols and determine what clib to
    use nondynamically. However this means that we can't use
    add_cprover_library when there's no cprover library.

--- ansi-c/cprover_library.cpp
+++ ansi-c/cprover_library.cpp
@@ -9,7 +9,6 @@ Author: Daniel Kroening, kroening@kroening.com
 extern "C" {
 #include <stdio.h>
 #include <stdlib.h>
-#include <dlfcn.h>
 }
 
 #include <sstream>
@@ -23,17 +22,21 @@ extern "C" {
 #include "cprover_library.h"
 #include "ansi_c_language.h"
 
-/*******************************************************************\
+extern void *_binary_clib16_goto_size;
+extern void *_binary_clib32_goto_size;
+extern void *_binary_clib64_goto_size;
+extern void *_binary_clib16_goto_start;
+extern void *_binary_clib32_goto_start;
+extern void *_binary_clib64_goto_start;
+extern void *_binary_clib16_goto_end;
+extern void *_binary_clib32_goto_end;
+extern void *_binary_clib64_goto_end;
 
-Function: add_cprover_library
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
+void *clib_ptrs[3][3] = {
+{ _binary_clib16_goto_size, _binary_clib16_goto_start, _binary_clib16_goto_end},
+{ _binary_clib32_goto_size, _binary_clib32_goto_start, _binary_clib32_goto_end},
+{ _binary_clib64_goto_size, _binary_clib64_goto_start, _binary_clib64_goto_end},
+};
 
 void add_cprover_library(
   contextt &context,
@@ -43,46 +46,42 @@ void add_cprover_library(
   goto_functionst goto_functions;
   ansi_c_languaget ansi_c_language;
   char symname_buffer[256];
-  void *self, *start;
   FILE *f;
-  char *filename;
+  void **this_clib_ptrs;
   unsigned long size;
+  int fd;
 
   if(config.ansi_c.lib==configt::ansi_ct::LIB_NONE)
     return;
 
-  self = dlopen(NULL, 0);
-  if (self == NULL) {
-    std::cerr << "Could not open self linker handle with dlopen" << std::endl;
+  if (config.ansi_c.int_width == 16) {
+    this_clib_ptrs = &clib_ptrs[0][0];
+  } else if (config.ansi_c.int_width == 32) {
+    this_clib_ptrs = &clib_ptrs[1][0];
+  } else if (config.ansi_c.int_width == 64) {
+    this_clib_ptrs = &clib_ptrs[2][0];
+  } else {
+    std::cerr << "No c library for bitwidth " << config.ansi_c.int_width << std::endl;
     abort();
   }
 
-  sprintf(symname_buffer, "_binary_clib%d_goto_start", config.ansi_c.int_width);
-  start = dlsym(self, symname_buffer);
-  if (start == NULL) {
-    std::cerr << "Could not locate internal C library" << std::endl;
-    abort();
-  }
-
-  sprintf(symname_buffer, "_binary_clib%d_goto_size", config.ansi_c.int_width);
-  size = (unsigned long)dlsym(self, symname_buffer);
+  size = (unsigned long)this_clib_ptrs[0];
   if (size == 0) {
     std::cerr << "error: Zero-lengthed internal C library" << std::endl;
     abort();
   }
 
-  dlclose(self);
-
   sprintf(symname_buffer, "ESBMC_XXXXXX");
-  filename = mktemp(symname_buffer);
-  f = fopen(filename, "w");
-  if (fwrite(start, size, 1, f) != 1) {
+  fd = mkstemp(symname_buffer);
+  close(fd);
+  f = fopen(symname_buffer, "w");
+  if (fwrite(this_clib_ptrs[1], size, 1, f) != 1) {
     std::cerr << "Couldn't manipulate internal C library" << std::endl;
     abort();
   }
   fclose(f);
 
-  std::ifstream infile(filename);
+  std::ifstream infile(symname_buffer);
   read_goto_binary(infile, new_ctx, goto_functions, message_handler);
 
   ansi_c_language.merge_context(
