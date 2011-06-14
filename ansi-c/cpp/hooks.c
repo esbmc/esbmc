33be29e7c5ccf429e2f481d975a03f4b63718446
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-06-14 17:00:10 +0000

    Lay out the form of record for which headers we'll be hooking.

--- ansi-c/cpp/hooks.c
+++ ansi-c/cpp/hooks.c
@@ -2,6 +2,16 @@
 
 #include <string.h>
 
+struct hooked_header {
+	const char *basename;
+	void *textstart;
+	void *textend;
+};
+
+struct hooked_header headers[] = {
+{ "stddef.h",		NULL,		NULL }
+};
+
 int
 handle_hooked_header(usch *name)
 {
