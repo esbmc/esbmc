dbd44a39a986bcc6d33fc162d80aa23fd735dece
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-06-14 17:00:52 +0000

    Further adjust header object symbols types.
    Silences some compiler warnings. The actual _type_ of these symbols is
    dodgy as they're not actual symbols.

--- ansi-c/headers.h
+++ ansi-c/headers.h
@@ -1,5 +1,5 @@
 /* This file defines pointers to the locations in ESBMC of headers-as-object
  * files */
 
-extern char *_binary_stddef_c_start;
-extern char *_binary_stddef_c_end;
+extern char _binary_stddef_c_start;
+extern char _binary_stddef_c_end;
