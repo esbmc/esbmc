66022a95ef6e39da5d6fc797fa1a506ddb585811
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-06-15 01:59:01 +0000

    Add hook for stdarg

--- ansi-c/cpp/hooks.c
+++ ansi-c/cpp/hooks.c
@@ -13,6 +13,8 @@ struct hooked_header {
 struct hooked_header headers[] = {
 { "stddef.h",		&_binary_stddef_c_start,	&_binary_stddef_c_end },
 /* stddef.h contains a variety of compiler-specific functions */
+{ "stdarg.h",		&_binary_stdarg_c_start,	&_binary_stdarg_c_end},
+/* contains va_start and similar functionality */
 { NULL, NULL, NULL}
 };
 
---
+++ ansi-c/headers/stdarg.h
@@ -0,0 +1,7 @@
+/* Define standard macros; esbmc currently copes with gcc internal forms,
+ * so we'll just replicate those */
+
+#define va_start(v,l)   __builtin_va_start(v,l)
+#define va_end(v)       __builtin_va_end(v)
+#define va_arg(v,l)     __builtin_va_arg(v,l)
+#define va_copy(d,s)    __builtin_va_copy(d,s)
