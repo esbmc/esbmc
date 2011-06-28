0257bf2fa8f151ce339d0ef92faab52ee9bab75b
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-06-28 16:12:54 +0000

    Handle "NULL" turning up in constant value strings

--- solvers/z3/z3_conv.cpp
+++ solvers/z3/z3_conv.cpp
@@ -3784,6 +3784,11 @@ bool z3_convt::convert_constant(const exprt &expr, Z3_ast &bv)
     // value will not actually be interpreted as number by below code
     value = expr.get_string("value");
   }
+  else if (expr.type().id() == "pointer" && expr.get_string("value") == "NULL")
+  {
+    // Uuugghhhh. Match what happens if we were to feed this to binary2integer.
+    value = "0";
+  }
   else if (is_signed(expr.type()))
   {
     value = integer2string(binary2integer(expr.get_string("value"), true),10);
