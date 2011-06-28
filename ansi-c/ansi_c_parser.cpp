89b1cd2396df39e74403573984ec292736262322
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-06-28 20:40:44 +0000

    Don't make a symbol declaration its own type
    IE, the type of an "assumption" variable was becoming "assumption".
    Ouch.

--- ansi-c/ansi_c_parser.cpp
+++ ansi-c/ansi_c_parser.cpp
@@ -129,6 +129,7 @@ void ansi_c_parsert::convert_declarator(
   } else if (declarator.id() == "symbol") {
     insert_subtype(declarator, type);
     identifier = declarator;
+    declarator = declarator.add("subtype");
   } else {
     // Some un-named value: a prototype parameter perhaps.
     identifier.make_nil();
