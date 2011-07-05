07c0aa3ed1c96e9d8e9bbcce6109b5b33770dae8
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2011-07-05 14:11:20 +0000

    Remove smtlang dir
    Presumably this is a parser for the SMT-LIB syntax. I don't know what
    purpose it serves, but it seems complete and working.
    
    However it isn't being used by ESBMC, and it doesn't sound like it will
    be any time soon (how does one model check SMT?), so off it goes. If
    needs be, it can be resurrected in the future.

--- smtlang/smt_finalizer.cpp
+++
@@ -1,9 +0,0 @@
-/*******************************************************************\
-
-Module: SMT-LIB Builtin Logics, Finalizer Base Class 
-
-Author: CM Wintersteiger
-
-\*******************************************************************/
-
-#include "smt_finalizer.h"
