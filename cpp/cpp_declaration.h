62ab37504ed1a6fb9cc8b2b6de483e9a9448f832
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-01-10 16:24:06 -0400

    Backport some methods on cpp_declaration.h from CBMC 4.0
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- cpp/cpp_declaration.h
+++ cpp/cpp_declaration.h
@@ -14,59 +14,102 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 #include "cpp_declarator.h"
 #include "cpp_storage_spec.h"
 #include "cpp_member_spec.h"
+#include "cpp_template_type.h"
+#include "cpp_template_args.h"
 
 class cpp_declarationt:public exprt
 {
 public:
   typedef std::vector<cpp_declaratort> declaratorst;
 
-  cpp_declarationt():exprt("cpp-declaration")
+  inline cpp_declarationt():exprt("cpp-declaration")
   {
   }
   
-  bool is_template() const
+  inline bool is_constructor() const
+  {
+    return type().id()=="constructor";
+  }
+
+  inline bool is_destructor() const
+  {
+    return type().id()=="destructor";
+  }
+
+  inline bool is_template() const
   {
     return get_bool("is_template");
   }
   
-  const declaratorst &declarators() const
+  inline bool is_template_class() const
+  {
+    return is_template() &&
+           type().id()=="struct" &&
+           declarators().empty();
+  }
+
+  inline const declaratorst &declarators() const
   {
     return (const declaratorst &)operands();
   }
 
-  declaratorst &declarators()
+  inline declaratorst &declarators()
   {
     return (declaratorst &)operands();
   }
   
-  const cpp_storage_spect &storage_spec() const
+  inline const cpp_storage_spect &storage_spec() const
   {
-    return (const cpp_storage_spect &)find("storage_spec");
+    return static_cast<const cpp_storage_spect &>(
+      find("storage_spec"));
   }
 
-  cpp_storage_spect &storage_spec()
+  inline cpp_storage_spect &storage_spec()
   {
-    return (cpp_storage_spect &)add("storage_spec");
+    return static_cast<cpp_storage_spect &>(
+      add("storage_spec"));
   }
 
-  const cpp_member_spect &member_spec() const
+  inline const cpp_member_spect &member_spec() const
   {
-    return (const cpp_member_spect &)find("member_spec");
+    return static_cast<const cpp_member_spect &>(
+      find("member_spec"));
   }
 
-  cpp_member_spect &member_spec()
+  inline cpp_member_spect &member_spec()
   {
-    return (cpp_member_spect &)add("member_spec");
+    return static_cast<cpp_member_spect &>(
+      add("member_spec"));
   }
 
-  typet &template_type()
+  inline template_typet &template_type()
   {
-    return (typet &)add("template_type");
+    return static_cast<template_typet &>(add("template_type"));
   }
 
-  const typet &template_type() const
+  inline const template_typet &template_type() const
   {
-    return (const typet &)find("template_type");
+    return static_cast<const template_typet &>(find("template_type"));
+  }
+
+  inline cpp_template_args_non_tct &partial_specialization_args()
+  {
+    return static_cast<cpp_template_args_non_tct &>(add("partial_specialization_args"));
+  }
+
+  inline const cpp_template_args_non_tct &partial_specialization_args() const
+  {
+    return static_cast<const cpp_template_args_non_tct &>(find("partial_specialization_args"));
+  }
+
+  inline void set_specialization_of(const irep_idt &id)
+  {
+    set("specialization_of", id);
+  }
+
+  inline irep_idt get_specialization_of() const
+  {
+    return get("specialization_of");
   }
 
   void output(std::ostream &out) const;
@@ -75,13 +118,13 @@ public:
 extern inline cpp_declarationt &to_cpp_declaration(irept &irep)
 {
   assert(irep.id()=="cpp-declaration");
-  return (cpp_declarationt &)irep;
+  return static_cast<cpp_declarationt &>(irep);
 }
 
 extern inline const cpp_declarationt &to_cpp_declaration(const irept &irep)
 {
   assert(irep.id()=="cpp-declaration");
-  return (const cpp_declarationt &)irep;
+  return static_cast<const cpp_declarationt &>(irep);
 }
 
 #endif
