c969d933d05c04388a866c8b2cf552563b1f064a
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-01-11 16:55:07 -0400

    Backport template_map.h/cpp from CBMC 4.0
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- cpp/template_map.cpp
+++ cpp/template_map.cpp
@@ -25,7 +25,7 @@ void template_mapt::apply(typet &type) const
   if(type.id()=="array")
   {
     apply(type.subtype());
-    apply((exprt &)type.add("size"));
+    apply(static_cast<exprt &>(type.add("size")));
   }
   else if(type.id()=="pointer")
   {
@@ -38,7 +38,7 @@ void template_mapt::apply(typet &type) const
 
     Forall_irep(it, components)
     {
-      typet &subtype=(typet &)it->find("type");
+      typet &subtype=static_cast<typet &>(it->add("type"));
       apply(subtype);
     }
   }
@@ -55,14 +55,14 @@ void template_mapt::apply(typet &type) const
   }
   else if(type.id()=="code")
   {
-    apply((typet &)type.add("return_type"));
+    apply(static_cast<typet &>(type.add("return_type")));
 
     irept::subt &arguments=type.add("arguments").get_sub();
 
     Forall_irep(it, arguments)
     {
       if(it->id()=="argument")
-        apply((typet &)(it->add("type")));
+        apply(static_cast<typet &>(it->add("type")));
     }
   }
   else if(type.id()=="merged_type")
@@ -106,7 +106,7 @@ void template_mapt::apply(exprt &expr) const
 
 /*******************************************************************\
 
-Function: template_mapt::apply
+Function: template_mapt::lookup
 
   Inputs:
 
@@ -128,6 +128,52 @@ exprt template_mapt::lookup(const irep_idt &identifier) const
     return e;
   }
 
+  expr_mapt::const_iterator e_it=
+    expr_map.find(identifier);
+
+  if(e_it!=expr_map.end())
+    return e_it->second;
+
+  return static_cast<const exprt &>(get_nil_irep());
+}
+
+/*******************************************************************\
+
+Function: template_mapt::lookup_type
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+typet template_mapt::lookup_type(const irep_idt &identifier) const
+{
+  type_mapt::const_iterator t_it=
+    type_map.find(identifier);
+
+  if(t_it!=type_map.end())
+    return t_it->second;
+
+  return static_cast<const typet &>(get_nil_irep());
+}
+
+/*******************************************************************\
+
+Function: template_mapt::lookup_expr
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+exprt template_mapt::lookup_expr(const irep_idt &identifier) const
+{
   expr_mapt::const_iterator e_it=
     expr_map.find(identifier);
 
@@ -162,3 +208,178 @@ void template_mapt::print(std::ostream &out) const
     out << it->first << " = " << it->second.pretty() << std::endl;
 }
 
+/*******************************************************************\
+
+Function: template_mapt::build
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void template_mapt::build(
+  const template_typet &template_type,
+  const cpp_template_args_tct &template_args)
+{
+  const template_typet::parameterst &template_parameters=
+    template_type.parameters();
+
+  cpp_template_args_tct::argumentst instance=
+    template_args.arguments();
+
+  template_typet::parameterst::const_iterator t_it=
+    template_parameters.begin();
+
+  if(instance.size()<template_parameters.size())
+  {
+    // check for default parameters
+    for(unsigned i = instance.size();
+        i < template_parameters.size();
+        i++)
+    {
+      const exprt &param=template_parameters[i];
+
+      exprt value = static_cast<const exprt &>(param.find("#default"));
+
+      if(value.is_not_nil())
+        instance.push_back(value);
+      else
+        break;
+    }
+  }
+
+  // these should have been typechecked before
+  assert(instance.size()==template_parameters.size());
+
+  for(cpp_template_args_tct::argumentst::const_iterator
+      i_it=instance.begin();
+      i_it!=instance.end();
+      i_it++, t_it++)
+  {
+    assert(t_it!=template_parameters.end());
+    set(*t_it, *i_it);
+  }
+}
+
+/*******************************************************************\
+
+Function: template_mapt::set
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void template_mapt::set(
+  const template_parametert &parameter,
+  const exprt &value)
+{
+  if(parameter.id()=="type")
+  {
+    if(parameter.id()!="type")
+      assert(false); // typechecked before!
+
+    typet tmp=value.type();
+
+    irep_idt identifier=parameter.type().get("identifier");
+    type_map[identifier]=tmp;
+  }
+  else
+  {
+    // must be non-type
+
+    if(value.id()=="type")
+      assert(false); // typechecked before!
+
+    irep_idt identifier=parameter.get("identifier");
+    expr_map[identifier]=value;
+  }
+}
+
+/*******************************************************************\
+
+Function: template_mapt::build_unassigned
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+void template_mapt::build_unassigned(
+  const template_typet &template_type)
+{
+  const template_typet::parameterst &template_parameters=
+    template_type.parameters();
+
+  for(template_typet::parameterst::const_iterator
+      t_it=template_parameters.begin();
+      t_it!=template_parameters.end();
+      t_it++)
+  {
+    const exprt &t=*t_it;
+
+    if(t.id()=="type")
+    {
+      typet tmp("unassigned");
+      tmp.set("identifier", t.type().get("identifier"));
+      tmp.location()=t.location();
+      type_map[t.type().get("identifier")]=tmp;
+    }
+    else
+    {
+      exprt tmp("unassigned", t.type());
+      tmp.set("identifier", t.get("identifier"));
+      tmp.location()=t.location();
+      expr_map[t.get("identifier")]=tmp;
+    }
+  }
+}
+
+/*******************************************************************\
+
+Function: template_mapt::build_template_args
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+cpp_template_args_tct template_mapt::build_template_args(
+  const template_typet &template_type) const
+{
+  const template_typet::parameterst &template_parameters=
+    template_type.parameters();
+
+  cpp_template_args_tct template_args;
+  template_args.arguments().resize(template_parameters.size());
+
+  for(unsigned i=0; i<template_parameters.size(); i++)
+  {
+    const exprt &t=template_parameters[i];
+
+    if(t.id()=="type")
+    {
+      template_args.arguments()[i]=
+        exprt("type", lookup_type(t.type().get("identifier")));
+    }
+    else
+    {
+      template_args.arguments()[i]=
+        lookup_expr(t.get("identifier"));
+    }
+  }
+
+  return template_args;
+}
--- cpp/template_map.h
+++ cpp/template_map.h
@@ -14,9 +14,13 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 
 #include <expr.h>
 
+#include "cpp_template_type.h"
+#include "cpp_template_args.h"
+
 class template_mapt
 {
 public:
+  // this maps template parameters to their instantiated value
   typedef std::map<irep_idt, typet> type_mapt;
   typedef std::map<irep_idt, exprt> expr_mapt;
   type_mapt type_map;
@@ -32,6 +36,8 @@ public:
   }
 
   exprt lookup(const irep_idt &identifier) const;
+  typet lookup_type(const irep_idt &identifier) const;
+  exprt lookup_expr(const irep_idt &identifier) const;
 
   void print(std::ostream &out) const;
 
@@ -40,21 +46,41 @@ public:
     type_map.clear();
     expr_map.clear();
   }
+
+  void set(
+    const template_parametert &parameter,
+    const exprt &value);
+
+  void build(
+    const template_typet &template_type,
+    const cpp_template_args_tct &template_args);
+
+  void build_unassigned(
+    const template_typet &template_type);
+
+  cpp_template_args_tct build_template_args(
+    const template_typet &template_type) const;
 };
 
-struct cpp_saved_template_mapt
+class cpp_saved_template_mapt
 {
-    cpp_saved_template_mapt(template_mapt& map):
-        old_map(map),map(map){}
-    ~cpp_saved_template_mapt()
-    { map.swap(old_map); }
+public:
+  cpp_saved_template_mapt(template_mapt &map):
+    old_map(map), map(map)
+  {
+  }
 
-    void restore()
-    { map = old_map;}
+  ~cpp_saved_template_mapt()
+  {
+    #if 0
+    std::cout << "RESTORING TEMPLATE MAP\n";
+    #endif
+    map.swap(old_map);
+  }
 
-    private:
-    template_mapt old_map;
-    template_mapt& map;
+private:
+  template_mapt old_map;
+  template_mapt &map;
 };
 
 #endif
