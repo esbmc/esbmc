64009caa9cd20c791ddb7bc4d19b5bb442bf0990
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-01-12 12:00:41 -0400

    Backport cpp_typecheck_type.cpp from CBMC 4.0
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- cpp/cpp_typecheck_type.cpp
+++ cpp/cpp_typecheck_type.cpp
@@ -7,7 +7,7 @@ Author: Daniel Kroening, kroening@cs.cmu.edu
 \*******************************************************************/
 
 #include <location.h>
-#include <simplify_expr_class.h>
+
 #include <ansi-c/c_qualifiers.h>
 
 #include "cpp_typecheck.h"
@@ -65,10 +65,12 @@ void cpp_typecheckt::typecheck_type(typet &type)
     if(symbol_expr.id()!="type")
     {
       err_location(type);
-      str << "error: expected type" << std::endl;
+      str << "error: expected type";
       throw 0;
     }
-    type = symbol_expr.type();
+
+    type=symbol_expr.type();
+    assert(type.is_not_nil());
 
     if(type.get_bool("#constant"))
       qualifiers.is_constant = true;
@@ -88,41 +90,40 @@ void cpp_typecheckt::typecheck_type(typet &type)
     // Check if it is a pointer-to-member
     if(type.find("to-member").is_not_nil())
     {
-      // Must point to a method
-      if(type.subtype().id()!= "code")
+      // these can point either to data members or member functions
+      // of a class
+
+      typet &class_object=static_cast<typet &>(type.add("to-member"));
+
+      if(class_object.id()=="cpp-name")
       {
-        err_location(type);
-        str << "pointer-to-member musst point to a method: "
-            << type.subtype().pretty() << std::endl;
-        throw 0;
+        assert(class_object.get_sub().back().id()=="::");
+        class_object.get_sub().pop_back();
       }
 
-      typet &member=static_cast<typet &>(type.add("to-member"));
+      typecheck_type(class_object);
 
-      if(member.id()=="cpp-name")
+      // there may be arguments if this is a pointer to member function
+      if(type.subtype().id()=="code")
       {
-        assert(member.get_sub().back().id()=="::");
-        member.get_sub().pop_back();
-      }
-
-      typecheck_type(member);
-
-      irept::subt &args=type.subtype().add("arguments").get_sub();
-      if(args.empty() || args.front().get("#base_name")!="this")
-      {
-        // Add 'this' to the arguments
-        exprt a0("argument");
-        a0.set("#base_name","this");
-        a0.type().id("pointer");
-        a0.type().subtype() = member;
-        args.insert(args.begin(),a0);
+        irept::subt &args=type.subtype().add("arguments").get_sub();
+        if(args.empty() ||
+           args.front().get("#base_name")!="this")
+        {
+          // Add 'this' to the arguments
+          exprt a0("argument");
+          a0.set("#base_name", "this");
+          a0.type().id("pointer");
+          a0.type().subtype() = class_object;
+          args.insert(args.begin(),a0);
+        }
       }
     }
 
     // now do qualifier
     if(type.find("#qualifier").is_not_nil())
     {
-      typet &t=(typet &)type.add("#qualifier");
+      typet &t=static_cast<typet &>(type.add("#qualifier"));
       cpp_convert_plain_type(t);
       c_qualifierst q(t);
       q.write(type);
@@ -132,26 +133,12 @@ void cpp_typecheckt::typecheck_type(typet &type)
   }
   else if(type.id()=="array")
   {
-    exprt &size_expr=static_cast<exprt &>(type.add("size"));
+    exprt &size_expr=to_array_type(type).size();
 
     if(size_expr.is_nil())
       type.id("incomplete_array");
     else
-    {
       typecheck_expr(size_expr);
-
-      simplify_exprt expr_simplifier;
-      expr_simplifier.simplify(size_expr);
-
-      if(size_expr.id() != "constant" &&
-         size_expr.id() != "infinity")
-      {
-        err_location(type);
-        str << "failed to determine size of array: " <<
-          expr2cpp(size_expr,context) << std::endl;
-        throw 0;
-      }
-    }
 
     typecheck_type(type.subtype());
 
@@ -211,15 +198,56 @@ void cpp_typecheckt::typecheck_type(typet &type)
   else if(type.id()=="cpp-template-type")
   {
   }
+  else if(type.id()=="typeof")
+  {
+    exprt e=static_cast<const exprt &>(type.find("expr"));
+
+    if(e.is_nil())
+    {
+      typet tmp_type=
+        static_cast<const typet &>(type.find("sizeof-type"));
+
+      if(tmp_type.id()=="cpp-name")
+      {
+        // this may be ambiguous -- it can be either a type or
+        // an expression
+
+        cpp_typecheck_fargst fargs;
+
+        exprt symbol_expr=resolve(
+          to_cpp_name(static_cast<const irept &>(tmp_type)),
+          cpp_typecheck_resolvet::BOTH,
+          fargs);
+
+        type=symbol_expr.type();
+      }
+      else
+      {
+        typecheck_type(tmp_type);
+        type=tmp_type;
+      }
+    }
+    else
+    {
+      typecheck_expr(e);
+      type=e.type();
+    }
+  }
   #ifdef CPP_SYSTEMC_EXTENSION
   else if(type.id() == "verilogbv")
   {
   }
   #endif
+  else if(type.id()=="unassigned")
+  {
+    // ignore, for template argument guessing
+  }
   else
   {
     err_location(type);
-    str << "unexpected type: " << type.pretty() << std::endl;
+    str << "unexpected type: " << type.pretty();
     throw 0;
   }
+
+  assert(type.is_not_nil());
 }
