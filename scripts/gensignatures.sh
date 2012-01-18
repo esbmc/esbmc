1388d73852528fb25e621108726d930af10f2dc1
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-01-18 23:19:56 +0000

    Script to generate a list of tarball signatures

---
+++ scripts/gensignatures.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+stat .git > /dev/null 2>/dev/null
+if test $? != 0; then
+  echo "Please run gensignatures.sh in the root dir of ESBMC"
+  exit 1
+fi
+
+tgzlist=`ls .release/*.tgz`
+
+# Generate checksums of each tgz
+for file in $tgzlist;
+do
+  md5sum=`md5sum $file | cut "--delim= " -f 1`
+  shasum=`shasum $file | cut "--delim= " -f 1`
+  sha2sum=`sha256sum $file | cut "--delim= " -f 1`
+
+  echo "File $file checksums"
+  echo "MD5:    $md5sum"
+  echo "SHA:    $shasum"
+  echo "SHA256: $sha2sum"
+done
