19548b9222c8bac888df90ea0173c5aee0a0b293
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-01-28 13:03:54 +0000

    Don't fetch numerals from non numeral ASTs
    Looks like this is the way to detect whether or not a piece of AST has
    actually been resolved to a value, or whether it's not been fully
    resolved because it isn't part of the model.

--- solvers/z3/z3_get.cpp
+++ solvers/z3/z3_get.cpp
@@ -316,18 +316,24 @@ z3_convt::bv_get_rec(
 #endif
   } else if (type.id() == "signedbv" || type.id() == "unsignedbv") {
     unsigned width;
+    if (Z3_get_ast_kind(z3_ctx, bv) != Z3_NUMERAL_AST)
+      return nil_exprt();
     std::string value = Z3_get_numeral_string(z3_ctx, bv);
     boolbv_get_width(type, width);
     constant_exprt value_expr(type);
     value_expr.set_value(integer2binary(string2integer(value), width));
     return value_expr;
   } else if (type.id() == "fixedbv" && int_encoding) {
+    if (Z3_get_ast_kind(z3_ctx, bv) != Z3_NUMERAL_AST)
+      return nil_exprt();
     std::string value = Z3_get_numeral_string(z3_ctx, bv);
     get_type_width(type, width);
     constant_exprt value_expr(type);
     value_expr.set_value(get_fixed_point(width, value));
     return value_expr;
   } else if (type.id() == "c_enum") {
+    if (Z3_get_ast_kind(z3_ctx, bv) != Z3_NUMERAL_AST)
+      return nil_exprt();
     std::string value = Z3_get_numeral_string(z3_ctx, bv);
     constant_exprt value_expr(type);
     value_expr.set_value(value);
