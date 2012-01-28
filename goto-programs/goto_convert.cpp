f1f40c22dd80ad68b2002ced80ff39cdf1a1b2b1
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-01-28 06:52:06 +0000

    Force types in code to become resolved
    In a vastly annoying development, various types from C aren't linked up
    to their real type during conversion. i.e., they're stored as
    c::tag-somename, where somename is a typedef name, or forward dec.
    
    This proceeds to explode later on during solving, when we don't have the
    namespace handy. Specifically when working out the width of atype.

--- goto-programs/goto_convert.cpp
+++ goto-programs/goto_convert.cpp
@@ -21,6 +21,25 @@ Author: Daniel Kroening, kroening@kroening.com
 #include "remove_skip.h"
 #include "destructor.h"
 
+static void
+link_up_type_names(irept &irep, const namespacet &ns)
+{
+
+  if (irep.find(exprt::i_type) != get_nil_irep()) {
+    if (irep.find("type").id() == "symbol") {
+      typet newtype = ns.follow((typet&)irep.find("type"));
+      irep.add("type") = newtype;
+    }
+  }
+
+  Forall_irep(it, irep.get_sub())
+    link_up_type_names(*it, ns);
+  Forall_named_irep(it, irep.get_named_sub())
+    link_up_type_names(it->second, ns);
+
+  return;
+}
+
 /*******************************************************************\
 
 Function: goto_convertt::finish_gotos
@@ -870,6 +889,9 @@ void goto_convertt::convert_assign(
   else
   {
     remove_sideeffects(rhs, dest);
+
+    link_up_type_names(lhs, ns);
+    link_up_type_names(rhs, ns);
 
     if(lhs.id()=="typecast")
     {
