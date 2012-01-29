5df8ec06c2bbcd2a05a67840beb7c8b839905123
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-01-29 02:17:44 +0000

    Whitespace
    Uncrustify fouls this up, unfortunately

--- solvers/z3/z3_prop.h
+++ solvers/z3/z3_prop.h
@@ -61,13 +61,13 @@ public:
   friend class z3_convt;
 
 private:
-	z3_capi z3_api;
-	Z3_context z3_ctx;
-	typedef std::map<std::string, Z3_ast> map_prop_varst;
-	map_prop_varst map_prop_vars;
-	std::list<Z3_ast> assumpt;
-    bool store_assumptions;
-    bool smtlib;
+  z3_capi z3_api;
+  Z3_context z3_ctx;
+  typedef std::map<std::string, Z3_ast> map_prop_varst;
+  map_prop_varst map_prop_vars;
+  std::list<Z3_ast> assumpt;
+  bool store_assumptions;
+  bool smtlib;
 
 protected:
   unsigned _no_variables;
