bac6f928b6f39c691d8aeb3db1e395b9d6cc8c57
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-02-04 05:06:03 +0000

    Run execution_state.h through uncrustify

--- goto-symex/execution_state.h
+++ goto-symex/execution_state.h
@@ -1,9 +1,9 @@
 /*******************************************************************\
 
-Module:
+   Module:
 
-Author: Ben YIU, yspb1g08@ecs.soton.ac.uk
-		Lucas Cordeiro, lcc08r@ecs.soton.ac.uk
+   Author: Ben YIU, yspb1g08@ecs.soton.ac.uk Lucas Cordeiro,
+     lcc08r@ecs.soton.ac.uk
 
 \*******************************************************************/
 
@@ -28,266 +28,284 @@ class reachability_treet;
 class execution_statet
 {
 
-public:
-	execution_statet(const goto_functionst &goto_functions,
-                const namespacet &ns, const reachability_treet *art,
-                goto_symex_statet::level2t &l2, bool _is_schedule):
-		owning_rt(art),
-		_state_level2(l2),
-                _target(ns),
-                _goto_functions(goto_functions)
-	{
+  public:
+  execution_statet(const goto_functionst &goto_functions, const namespacet &ns,
+                   const reachability_treet *art, goto_symex_statet::level2t &l2,
+                   bool _is_schedule) :
+    owning_rt(art),
+    _state_level2(l2),
+    _target(ns),
+    _goto_functions(goto_functions)
+  {
 
-          // XXXjmorse - C++s static initialization order trainwreck means
-          // we can't initialize the id -> serializer map statically. Instead,
-          // manually inspect and initialize. This is not thread safe.
-          if (!execution_statet::expr_id_map_initialized) {
-            execution_statet::expr_id_map_initialized = true;
-            execution_statet::expr_id_map = init_expr_id_map();
-          }
-
-		is_schedule = _is_schedule;
-		reexecute_instruction = true;
-		reexecute_atomic = false;
-		_CS_number = 0;
-		_TS_number = 0;
-		node_id = 0;
-		guard_execution = "execution_statet::\\guard_exec";
-		guard_thread = "execution_statet::\\trdsel";
-
-		goto_functionst::function_mapt::const_iterator it=
-				goto_functions.function_map.find("main");
-		if(it==goto_functions.function_map.end())
-			throw "main symbol not found; please set an entry point";
-
-		_goto_program =&(it->second.body);
-
-		add_thread((*_goto_program).instructions.begin(),(*_goto_program).instructions.end(), _goto_program);
-		_active_thread = 0;
-		_last_active_thread = 0;
-		generating_new_threads = 0;
-		node_count=0;
-		nondet_count = 0;
-		dynamic_counter = 0;
-		_DFS_traversed.reserve(1);
-		_DFS_traversed[0] = false;
-
-		str_state = string_container.take_state_snapshot();
-	};
-
-	execution_statet(const execution_statet &ex) :
-		owning_rt(ex.owning_rt),
-		_state_level2(ex._state_level2),
-		_target(ex._target),
-		_goto_functions(ex._goto_functions)
-	{
-		*this = ex;
-
-		// Don't copy string state in this copy constructor - instead
-		// take another snapshot to represent what string state was
-		// like when we began the exploration this execution_statet will
-		// perform.
-		str_state = string_container.take_state_snapshot();
-
-		// Regenerate threads state using new objects _state_level2 ref
-		_threads_state.clear();
-		std::vector<goto_symex_statet>::const_iterator it;
-		for(it = ex._threads_state.begin(); it != ex._threads_state.end(); it++) {
-			goto_symex_statet state(*it, _state_level2);
-			_threads_state.push_back(state);
-		}
-
-	}
-
-	execution_statet& operator=(const execution_statet &ex)
-	{
-		is_schedule = ex.is_schedule;
-		_threads_state = ex._threads_state;
-		_atomic_numbers = ex._atomic_numbers;
-		_DFS_traversed = ex._DFS_traversed;
-		_exprs = ex._exprs;
-		generating_new_threads = ex.generating_new_threads;
-		last_global_expr = ex.last_global_expr;
-		_exprs_read_write = ex._exprs_read_write;
-		last_global_read_write = ex.last_global_read_write;
-		_last_active_thread = ex._last_active_thread;
-		_state_level2 = ex._state_level2;
-		_active_thread = ex._active_thread;
-		guard_execution = ex.guard_execution;
-		guard_thread = ex.guard_thread;
-		_parent_guard_identifier = ex._parent_guard_identifier;
-		reexecute_instruction = ex.reexecute_instruction;
-		reexecute_atomic = ex.reexecute_atomic;
-		nondet_count = ex.nondet_count;
-		dynamic_counter = ex.dynamic_counter;
-		node_id = ex.node_id;
-		parent_node_id = ex.parent_node_id;
-
-		_goto_program = ex._goto_program;
-		_CS_number = ex._CS_number;
-		_TS_number = ex._TS_number;
-		return *this;
-	}
-
-	virtual ~execution_statet() {
-		// Free all name strings and suchlike we generated on this run
-		// and no longer require
-		// But, not if we're running with --schedule, as we'll need all
-		// that information later.
-		if (!is_schedule)
-			string_container.restore_state_snapshot(str_state);
-	};
-
-    // Types
-
-    typedef std::string (*serialise_fxn)(execution_statet &ex_state, const exprt &rhs);
-    typedef std::map<const irep_idt, serialise_fxn> expr_id_map_t;
-
-    // Methods
-
-    /* number of context switches we've performed to reach this state */
-    void increment_context_switch()
-    {
-      _CS_number++;
+    // XXXjmorse - C++s static initialization order trainwreck means
+    // we can't initialize the id -> serializer map statically. Instead,
+    // manually inspect and initialize. This is not thread safe.
+    if (!execution_statet::expr_id_map_initialized) {
+      execution_statet::expr_id_map_initialized = true;
+      execution_statet::expr_id_map = init_expr_id_map();
     }
 
-    void increment_time_slice()
-    {
-      _TS_number++;
+    is_schedule = _is_schedule;
+    reexecute_instruction = true;
+    reexecute_atomic = false;
+    _CS_number = 0;
+    _TS_number = 0;
+    node_id = 0;
+    guard_execution = "execution_statet::\\guard_exec";
+    guard_thread = "execution_statet::\\trdsel";
+
+    goto_functionst::function_mapt::const_iterator it =
+      goto_functions.function_map.find("main");
+    if (it == goto_functions.function_map.end())
+      throw "main symbol not found; please set an entry point";
+
+    _goto_program = &(it->second.body);
+
+    add_thread(
+      (*_goto_program).instructions.begin(),
+      (*_goto_program).instructions.end(), _goto_program);
+    _active_thread = 0;
+    _last_active_thread = 0;
+    generating_new_threads = 0;
+    node_count = 0;
+    nondet_count = 0;
+    dynamic_counter = 0;
+    _DFS_traversed.reserve(1);
+    _DFS_traversed[0] = false;
+
+    str_state = string_container.take_state_snapshot();
+  };
+
+  execution_statet(const execution_statet &ex) :
+    owning_rt(ex.owning_rt),
+    _state_level2(ex._state_level2),
+    _target(ex._target),
+    _goto_functions(ex._goto_functions)
+  {
+    *this = ex;
+
+    // Don't copy string state in this copy constructor - instead
+    // take another snapshot to represent what string state was
+    // like when we began the exploration this execution_statet will
+    // perform.
+    str_state = string_container.take_state_snapshot();
+
+    // Regenerate threads state using new objects _state_level2 ref
+    _threads_state.clear();
+    std::vector<goto_symex_statet>::const_iterator it;
+    for (it = ex._threads_state.begin(); it != ex._threads_state.end(); it++) {
+      goto_symex_statet state(*it, _state_level2);
+      _threads_state.push_back(state);
     }
 
-    void reset_time_slice()
-    {
-      _TS_number=0;
-    }
+  }
 
-    int get_context_switch()
-    {
-      return _CS_number;
-    }
+  execution_statet&
+  operator=(const execution_statet &ex)
+  {
+    is_schedule = ex.is_schedule;
+    _threads_state = ex._threads_state;
+    _atomic_numbers = ex._atomic_numbers;
+    _DFS_traversed = ex._DFS_traversed;
+    _exprs = ex._exprs;
+    generating_new_threads = ex.generating_new_threads;
+    last_global_expr = ex.last_global_expr;
+    _exprs_read_write = ex._exprs_read_write;
+    last_global_read_write = ex.last_global_read_write;
+    _last_active_thread = ex._last_active_thread;
+    _state_level2 = ex._state_level2;
+    _active_thread = ex._active_thread;
+    guard_execution = ex.guard_execution;
+    guard_thread = ex.guard_thread;
+    _parent_guard_identifier = ex._parent_guard_identifier;
+    reexecute_instruction = ex.reexecute_instruction;
+    reexecute_atomic = ex.reexecute_atomic;
+    nondet_count = ex.nondet_count;
+    dynamic_counter = ex.dynamic_counter;
+    node_id = ex.node_id;
+    parent_node_id = ex.parent_node_id;
 
-    int get_time_slice()
-    {
-      return _TS_number;
-    }
+    _goto_program = ex._goto_program;
+    _CS_number = ex._CS_number;
+    _TS_number = ex._TS_number;
+    return *this;
+  }
 
-    void reset_DFS_traversed()
-    {
-   	  for(unsigned int i=0;i<_threads_state.size();i++)
-		_DFS_traversed.at(i) = false;
-    }
+  virtual
+  ~execution_statet() {
+    // Free all name strings and suchlike we generated on this run
+    // and no longer require
+    // But, not if we're running with --schedule, as we'll need all
+    // that information later.
+    if (!is_schedule)
+      string_container.restore_state_snapshot(str_state);
+  };
 
-    void recover_global_state(const namespacet &ns, symex_targett &target);
+  // Types
 
-    irep_idt get_guard_identifier();
-    irep_idt get_guard_identifier_base();
-    void set_parent_guard(const irep_idt & parent_guard);
-    bool all_threads_ended();
-	goto_symex_statet & get_active_state();
-	const goto_symex_statet & get_active_state() const;
-	unsigned int get_active_state_number() { return _active_thread; }
-	unsigned int get_active_atomic_number();
-	void increment_active_atomic_number();
-	void decrement_active_atomic_number();
-    void set_state_guard(unsigned int i, const exprt & guard);
-	void set_active_state(unsigned int i);
-    void execute_guard(const namespacet & ns, symex_targett &target);
+  typedef std::string (*serialise_fxn)(execution_statet &ex_state,
+                                       const exprt &rhs);
+  typedef std::map<const irep_idt, serialise_fxn> expr_id_map_t;
 
-	void add_thread(goto_programt::const_targett start, goto_programt::const_targett end, const goto_programt *prog);
-	void add_thread(goto_symex_statet & state);
-    void end_thread(const namespacet &ns, symex_targett &target);
-    /* Presumably this does the same as read_globals, see below */
-    unsigned int get_expr_write_globals(const namespacet &ns, const exprt & expr);
-    /* This takes the given expression, and for all constituent parts looks
-     * through the identifiers that it touches and checks to see whether or
-     * not they're globals. Counts them; also puts them in the
-     * _exprs_read_write implicitly as reads: my eyes are on fire. */
-    unsigned int get_expr_read_globals(const namespacet &ns, const exprt & expr);
+  // Methods
 
-    //void deadlock_detection(const namespacet &ns, symex_targett &target);
-    void increament_trds_in_run(const namespacet &ns, symex_targett &target);
-    void update_trds_count(const namespacet &ns, symex_targett &target);
-    //void update_trds_status(const namespacet &ns, symex_targett &target);
+  /* number of context switches we've performed to reach this state */
+  void
+  increment_context_switch()
+  {
+    _CS_number++;
+  }
 
-    crypto_hash generate_hash(void) const;
-    crypto_hash update_hash_for_assignment(const exprt &rhs);
-    std::string serialise_expr(const exprt &rhs);
+  void
+  increment_time_slice()
+  {
+    _TS_number++;
+  }
 
-    void print_stack_traces(const namespacet &ns, unsigned int indent = 0) const;
+  void
+  reset_time_slice()
+  {
+    _TS_number = 0;
+  }
 
-private:
-    void decreament_trds_in_run(const namespacet &ns, symex_targett &target);
-    const symbolt& lookup(const namespacet &ns, const irep_idt &identifier)  const;
-    bool is_in_lookup(const namespacet &ns, const irep_idt &identifier) const;
+  int
+  get_context_switch()
+  {
+    return _CS_number;
+  }
 
-    // Object state
+  int
+  get_time_slice()
+  {
+    return _TS_number;
+  }
 
-public:
+  void
+  reset_DFS_traversed()
+  {
+    for (unsigned int i = 0; i < _threads_state.size(); i++)
+      _DFS_traversed.at(i) = false;
+  }
 
-    const reachability_treet *owning_rt;
+  void recover_global_state(const namespacet &ns, symex_targett &target);
 
-    /* jmorse - Set of current thread states, indexed by threads id number*/
-    std::vector<goto_symex_statet> _threads_state;
+  irep_idt get_guard_identifier();
+  irep_idt get_guard_identifier_base();
+  void set_parent_guard(const irep_idt & parent_guard);
+  bool all_threads_ended();
+  goto_symex_statet & get_active_state();
+  const goto_symex_statet & get_active_state() const;
+  unsigned int
+  get_active_state_number() {
+    return _active_thread;
+  }
+  unsigned int get_active_atomic_number();
+  void increment_active_atomic_number();
+  void decrement_active_atomic_number();
+  void set_state_guard(unsigned int i, const exprt & guard);
+  void set_active_state(unsigned int i);
+  void execute_guard(const namespacet & ns, symex_targett &target);
 
-    /* jmorse - appears to just be a flag indicating whether we're currently
-     * in an atomic section */
-    std::vector<unsigned int> _atomic_numbers;
+  void add_thread(goto_programt::const_targett start,
+                  goto_programt::const_targett end,
+                  const goto_programt *prog);
+  void add_thread(goto_symex_statet & state);
+  void end_thread(const namespacet &ns, symex_targett &target);
+  /* Presumably this does the same as read_globals, see below */
+  unsigned int get_expr_write_globals(const namespacet &ns, const exprt & expr);
+  /* This takes the given expression, and for all constituent parts looks through
+     the identifiers that it touches and checks to see whether or not they're
+     globals. Counts them; also puts them in the
+   * _exprs_read_write implicitly as reads: my eyes are on fire. */
+  unsigned int get_expr_read_globals(const namespacet &ns, const exprt & expr);
 
-    /* jmorse - Depth first search? */
-    std::vector<bool> _DFS_traversed;
+  //void deadlock_detection(const namespacet &ns, symex_targett &target);
+  void increament_trds_in_run(const namespacet &ns, symex_targett &target);
+  void update_trds_count(const namespacet &ns, symex_targett &target);
+  //void update_trds_status(const namespacet &ns, symex_targett &target);
 
-    /* jmorse - a set of expressions, one for each active thread, showing
-     * where each thread is at? generate_states_base. */
-    std::vector<exprt> _exprs;
+  crypto_hash generate_hash(void) const;
+  crypto_hash update_hash_for_assignment(const exprt &rhs);
+  std::string serialise_expr(const exprt &rhs);
 
-    int generating_new_threads;
-    /* jmorse - Presumably the last expr to be executed */
-    exprt last_global_expr;
+  void print_stack_traces(const namespacet &ns, unsigned int indent = 0) const;
 
-    /* jmorse - a set of operations (irep_idts; identifiers?) that presumably
-     * occur at the top of each state. indexed by thread id no. So, it's the
-     * set of most recent reads/writes of thread? */
-    std::vector<read_write_set> _exprs_read_write;
+  private:
+  void decreament_trds_in_run(const namespacet &ns, symex_targett &target);
+  const symbolt& lookup(const namespacet &ns, const irep_idt &identifier)  const;
+  bool is_in_lookup(const namespacet &ns, const irep_idt &identifier) const;
 
-    /* jmorse - what the name says */
-    read_write_set last_global_read_write;
+  // Object state
 
-    unsigned int _last_active_thread;
-    goto_symex_statet::level2t _state_level2;
-    unsigned int _active_thread;
+  public:
 
-    irep_idt guard_execution;
-    irep_idt guard_thread;
-    irep_idt _parent_guard_identifier;
+  const reachability_treet *owning_rt;
 
-    // Is the "--schedule" option enabled?
-    bool is_schedule;
+  /* jmorse - Set of current thread states, indexed by threads id number*/
+  std::vector<goto_symex_statet> _threads_state;
 
-    bool reexecute_instruction; // temporarily disable context switch for the thread inherited from the last active thread
-    bool reexecute_atomic; // temporarily disable context switch for the thread inherited from the last active thread
+  /* jmorse - appears to just be a flag indicating whether we're currently in an
+     atomic section */
+  std::vector<unsigned int> _atomic_numbers;
 
-    int _TS_number;
+  /* jmorse - Depth first search? */
+  std::vector<bool> _DFS_traversed;
 
-    unsigned nondet_count;
-    unsigned dynamic_counter;
+  /* jmorse - a set of expressions, one for each active thread, showing where each
+     thread is at? generate_states_base. */
+  std::vector<exprt> _exprs;
 
-    unsigned int node_id;
-    unsigned int parent_node_id;
-    symex_target_equationt _target;
+  int generating_new_threads;
+  /* jmorse - Presumably the last expr to be executed */
+  exprt last_global_expr;
 
-private:
-    const goto_functionst &_goto_functions;
-    const goto_programt *_goto_program;
-    int _CS_number;
-    string_containert::str_snapshot str_state;
+  /* jmorse - a set of operations (irep_idts; identifiers?) that presumably occur
+     at the top of each state. indexed by thread id no. So, it's the set of most
+     recent reads/writes of thread? */
+  std::vector<read_write_set> _exprs_read_write;
 
-    // Static stuff:
+  /* jmorse - what the name says */
+  read_write_set last_global_read_write;
 
-public:
-    static expr_id_map_t init_expr_id_map();
-    static bool expr_id_map_initialized;
-    static expr_id_map_t expr_id_map;
-    static unsigned int node_count;
+  unsigned int _last_active_thread;
+  goto_symex_statet::level2t _state_level2;
+  unsigned int _active_thread;
+
+  irep_idt guard_execution;
+  irep_idt guard_thread;
+  irep_idt _parent_guard_identifier;
+
+  // Is the "--schedule" option enabled?
+  bool is_schedule;
+
+  bool reexecute_instruction;     // temporarily disable context switch for the
+                                  // thread inherited from the last active thread
+  bool reexecute_atomic;     // temporarily disable context switch for the thread
+                             // inherited from the last active thread
+
+  int _TS_number;
+
+  unsigned nondet_count;
+  unsigned dynamic_counter;
+
+  unsigned int node_id;
+  unsigned int parent_node_id;
+  symex_target_equationt _target;
+
+  private:
+  const goto_functionst &_goto_functions;
+  const goto_programt *_goto_program;
+  int _CS_number;
+  string_containert::str_snapshot str_state;
+
+  // Static stuff:
+
+  public:
+  static expr_id_map_t init_expr_id_map();
+  static bool expr_id_map_initialized;
+  static expr_id_map_t expr_id_map;
+  static unsigned int node_count;
 };
 
 #endif /* EXECUTION_STATE_H_ */
