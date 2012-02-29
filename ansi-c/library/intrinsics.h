3ed92caa1cbf17d042a31db0002cae2ad95c46b2
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-02-29 13:55:43 +0000

    Declare get_thread_state intrinsic; and its flags

--- ansi-c/library/intrinsics.h
+++ ansi-c/library/intrinsics.h
@@ -27,3 +27,6 @@ typedef void *(*__ESBMC_thread_start_func_type)(void *);
 unsigned int __ESBMC_spawn_thread(void (*)(void));
 unsigned int __ESBMC_get_thread_id(void);
 void __ESBMC_terminate_thread(void);
+
+#define __ESBMC_thread_flag_running               1
+unsigned long int __ESBMC_get_thread_state(unsigned long int tid);
