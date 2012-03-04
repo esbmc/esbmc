f125eb6a0b4d3c25d88ef09c4511bf5e91d2d922
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-04 10:40:15 +0000

    Run uncrustify over symex main.

--- goto-symex/symex_main.cpp
+++ goto-symex/symex_main.cpp
@@ -1,9 +1,9 @@
 /*******************************************************************\
 
-Module: Symbolic Execution
+   Module: Symbolic Execution
 
-Author: Daniel Kroening, kroening@kroening.com
-		Lucas Cordeiro, lcc08r@ecs.soton.ac.uk
+   Author: Daniel Kroening, kroening@kroening.com Lucas Cordeiro,
+     lcc08r@ecs.soton.ac.uk
 
 \*******************************************************************/
 
@@ -28,10 +28,9 @@ Author: Daniel Kroening, kroening@kroening.com
 #include <simplify_expr.h>
 #include "config.h"
 
-void goto_symext::claim(
-  const exprt &claim_expr,
-  const std::string &msg,
-  statet &state) {
+void
+goto_symext::claim(
+  const exprt &claim_expr, const std::string &msg, statet &state) {
 
   total_claims++;
 
@@ -43,8 +42,8 @@ void goto_symext::claim(
     do_simplify(expr);
 
   if (expr.is_true() &&
-    !options.get_bool_option("all-assertions"))
-  return;
+      !options.get_bool_option("all-assertions"))
+    return;
 
   state.guard.guard_expr(expr);
 
@@ -70,9 +69,9 @@ goto_symext::get_symex_result(void)
   return new goto_symext::symex_resultt(target, total_claims, remaining_claims);
 }
 
-void goto_symext::symex_step(
-        const goto_functionst &goto_functions,
-        reachability_treet & art) {
+void
+goto_symext::symex_step(
+  const goto_functionst &goto_functions, reachability_treet & art) {
 
   execution_statet &ex_state = art.get_cur_state();
   statet &state = ex_state.get_active_state();
@@ -85,177 +84,177 @@ void goto_symext::symex_step(
 
   // depth exceeded?
   {
-      unsigned max_depth = atoi(options.get_option("depth").c_str());
-      if (max_depth != 0 && state.depth > max_depth)
-          state.guard.add(false_exprt());
-      state.depth++;
+    unsigned max_depth = atoi(options.get_option("depth").c_str());
+    if (max_depth != 0 && state.depth > max_depth)
+      state.guard.add(false_exprt());
+    state.depth++;
   }
 
-    // actually do instruction
-    switch (instruction.type) {
-        case SKIP:
-            // really ignore
-            state.source.pc++;
-            break;
-        case END_FUNCTION:
-                symex_end_of_function(state);
+  // actually do instruction
+  switch (instruction.type) {
+  case SKIP:
+    // really ignore
+    state.source.pc++;
+    break;
 
-                // Potentially skip to run another function ptr target; if not,
-                // continue
-                if (!run_next_function_ptr_target(goto_functions, state,
-                                                  false))
-                  state.source.pc++;
-            break;
-        case LOCATION:
-            target->location(state.guard, state.source);
-            state.source.pc++;
-            break;
-        case GOTO:
-        {
-            exprt tmp(instruction.guard);
-            replace_dynamic_allocation(state, tmp);
-            replace_nondet(tmp);
-            dereference(tmp, state, false);
+  case END_FUNCTION:
+    symex_end_of_function(state);
 
-           symex_goto(art.get_cur_state().get_active_state(), tmp);
-        }
-            break;
-        case ASSUME:
-            if (!state.guard.is_false()) {
-                exprt tmp(instruction.guard);
-                replace_dynamic_allocation(state, tmp);
-                replace_nondet(tmp);
-                dereference(tmp, state, false);
+    // Potentially skip to run another function ptr target; if not,
+    // continue
+    if (!run_next_function_ptr_target(goto_functions, state, false))
+      state.source.pc++;
+    break;
 
-                exprt tmp1 = tmp;
-                state.rename(tmp, ns);
+  case LOCATION:
+    target->location(state.guard, state.source);
+    state.source.pc++;
+    break;
 
-                do_simplify(tmp);
-                if (!tmp.is_true())
-                {
-                    exprt tmp2 = tmp;
-                    state.guard.guard_expr(tmp2);
+  case GOTO:
+  {
+    exprt tmp(instruction.guard);
+    replace_dynamic_allocation(state, tmp);
+    replace_nondet(tmp);
+    dereference(tmp, state, false);
 
-                    assume(tmp2, state);
+    symex_goto(art.get_cur_state().get_active_state(), tmp);
+  }
+  break;
 
-                    // we also add it to the state guard
-                    state.guard.add(tmp);
-                }
-            }
-            state.source.pc++;
-            break;
+  case ASSUME:
+    if (!state.guard.is_false()) {
+      exprt tmp(instruction.guard);
+      replace_dynamic_allocation(state, tmp);
+      replace_nondet(tmp);
+      dereference(tmp, state, false);
 
-        case ASSERT:
-            if (!state.guard.is_false()) {
-                if (!options.get_bool_option("no-assertions") ||
-                        !state.source.pc->location.user_provided()
-                        || options.get_bool_option("deadlock-check")) {
+      exprt tmp1 = tmp;
+      state.rename(tmp, ns);
 
-                    std::string msg = state.source.pc->location.comment().as_string();
-                    if (msg == "") msg = "assertion";
-                    exprt tmp(instruction.guard);
+      do_simplify(tmp);
+      if (!tmp.is_true()) {
+	exprt tmp2 = tmp;
+	state.guard.guard_expr(tmp2);
 
-                    replace_dynamic_allocation(state, tmp);
-                    replace_nondet(tmp);
-                    dereference(tmp, state, false);
+	assume(tmp2, state);
 
-                    claim(tmp, msg, state);
-                }
-            }
-            state.source.pc++;
-            break;
-
-        case RETURN:
-        	 if(!state.guard.is_false()) {
-                         const code_returnt &code =
-                           to_code_return(instruction.code);
-                         code_assignt assign;
-                         if (make_return_assignment(state, assign, code))
-                           goto_symext::symex_assign(state, assign);
-                         symex_return(state);
-                 }
-
-            state.source.pc++;
-            break;
-
-        case ASSIGN:
-            if (!state.guard.is_false()) {
-                codet deref_code=instruction.code;
-                replace_dynamic_allocation(state, deref_code);
-                replace_nondet(deref_code);
-                assert(deref_code.operands().size()==2);
-
-                dereference(deref_code.op0(), state, true);
-                dereference(deref_code.op1(), state, false);
-
-                symex_assign(state, deref_code);
-            }
-            state.source.pc++;
-            break;
-        case FUNCTION_CALL:
-            if (!state.guard.is_false())
-            {
-                code_function_callt deref_code =
-                        to_code_function_call(instruction.code);
-
-                replace_dynamic_allocation(state, deref_code);
-                replace_nondet(deref_code);
-
-                if (deref_code.lhs().is_not_nil()) {
-                    dereference(deref_code.lhs(), state, true);
-                }
-
-                Forall_expr(it, deref_code.arguments()) {
-                    dereference(*it, state, false);
-                }
-
-                if (has_prefix(deref_code.function().identifier().as_string(),
-                               "c::__ESBMC")) {
-                  state.source.pc++;
-                  run_intrinsic(deref_code, art,
-                                deref_code.function().identifier().as_string());
-                  return;
-                }
-
-                symex_function_call(goto_functions, state, deref_code);
-            }
-            else
-            {
-                state.source.pc++;
-            }
-            break;
-
-        case OTHER:
-            if (!state.guard.is_false()) {
-                codet deref_code(instruction.code);
-                const irep_idt &statement = deref_code.get_statement();
-                if (statement == "cpp_delete" ||
-                        statement == "cpp_delete[]" ||
-                        statement == "free" ||
-                        statement == "printf") {
-                    replace_dynamic_allocation(state, deref_code);
-                    replace_nondet(deref_code);
-                    dereference(deref_code, state, false);
-                }
-
-                symex_other(state);
-            }
-            state.source.pc++;
-            break;
-
-        default:
-            std::cerr << "GOTO instruction type " << instruction.type;
-            std::cerr << " not handled in goto_symext::symex_step" << std::endl;
-            abort();
+	// we also add it to the state guard
+	state.guard.add(tmp);
+      }
     }
+    state.source.pc++;
+    break;
+
+  case ASSERT:
+    if (!state.guard.is_false()) {
+      if (!options.get_bool_option("no-assertions") ||
+          !state.source.pc->location.user_provided()
+          || options.get_bool_option("deadlock-check")) {
+
+	std::string msg = state.source.pc->location.comment().as_string();
+	if (msg == "") msg = "assertion";
+	exprt tmp(instruction.guard);
+
+	replace_dynamic_allocation(state, tmp);
+	replace_nondet(tmp);
+	dereference(tmp, state, false);
+
+	claim(tmp, msg, state);
+      }
+    }
+    state.source.pc++;
+    break;
+
+  case RETURN:
+    if (!state.guard.is_false()) {
+      const code_returnt &code =
+        to_code_return(instruction.code);
+      code_assignt assign;
+      if (make_return_assignment(state, assign, code))
+	goto_symext::symex_assign(state, assign);
+      symex_return(state);
+    }
+
+    state.source.pc++;
+    break;
+
+  case ASSIGN:
+    if (!state.guard.is_false()) {
+      codet deref_code = instruction.code;
+      replace_dynamic_allocation(state, deref_code);
+      replace_nondet(deref_code);
+      assert(deref_code.operands().size() == 2);
+
+      dereference(deref_code.op0(), state, true);
+      dereference(deref_code.op1(), state, false);
+
+      symex_assign(state, deref_code);
+    }
+    state.source.pc++;
+    break;
+
+  case FUNCTION_CALL:
+    if (!state.guard.is_false()) {
+      code_function_callt deref_code =
+        to_code_function_call(instruction.code);
+
+      replace_dynamic_allocation(state, deref_code);
+      replace_nondet(deref_code);
+
+      if (deref_code.lhs().is_not_nil()) {
+	dereference(deref_code.lhs(), state, true);
+      }
+
+      Forall_expr(it, deref_code.arguments()) {
+	dereference(*it, state, false);
+      }
+
+      if (has_prefix(deref_code.function().identifier().as_string(),
+                     "c::__ESBMC")) {
+	state.source.pc++;
+	run_intrinsic(deref_code, art,
+	              deref_code.function().identifier().as_string());
+	return;
+      }
+
+      symex_function_call(goto_functions, state, deref_code);
+    } else   {
+      state.source.pc++;
+    }
+    break;
+
+  case OTHER:
+    if (!state.guard.is_false()) {
+      codet deref_code(instruction.code);
+      const irep_idt &statement = deref_code.get_statement();
+      if (statement == "cpp_delete" ||
+          statement == "cpp_delete[]" ||
+          statement == "free" ||
+          statement == "printf") {
+	replace_dynamic_allocation(state, deref_code);
+	replace_nondet(deref_code);
+	dereference(deref_code, state, false);
+      }
+
+      symex_other(state);
+    }
+    state.source.pc++;
+    break;
+
+  default:
+    std::cerr << "GOTO instruction type " << instruction.type;
+    std::cerr << " not handled in goto_symext::symex_step" << std::endl;
+    abort();
+  }
 }
 
 void
 goto_symext::run_intrinsic(code_function_callt &call, reachability_treet &art,
-                           const std::string symname)
+  const std::string symname)
 {
 
-  if(symname == "c::__ESBMC_yield") {
+  if (symname == "c::__ESBMC_yield") {
     intrinsic_yield(art);
   } else if (symname == "c::__ESBMC_switch_to") {
     intrinsic_switch_to(call, art);
@@ -274,7 +273,9 @@ goto_symext::run_intrinsic(code_function_callt &call, reachability_treet &art,
   } else {
     std::cerr << "Function call to non-intrinsic prefixed with __ESBMC (fatal)";
     std::cerr << std::endl << "The name in question: " << symname << std::endl;
-    std::cerr << "(NB: the C spec reserves the __ prefix for the compiler and environment)" << std::endl;
+    std::cerr <<
+    "(NB: the C spec reserves the __ prefix for the compiler and environment)"
+              << std::endl;
     abort();
   }
 
