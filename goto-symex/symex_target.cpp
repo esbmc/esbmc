b9b2397a353a0f692433d23a78ef95286e419d45
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-04 10:46:07 +0000

    Kill a docuheader

--- goto-symex/symex_target.cpp
+++ goto-symex/symex_target.cpp
@@ -8,18 +8,6 @@ Author: Daniel Kroening, kroening@kroening.com
 
 #include "symex_target.h"
 
-/*******************************************************************\
-
-Function: operator <
-
-  Inputs:
-
- Outputs:
-
- Purpose:
-
-\*******************************************************************/
-
 bool operator < (const symex_targett::sourcet &a, const symex_targett::sourcet &b)
 {
   if(a.thread_nr < b.thread_nr) return true;
