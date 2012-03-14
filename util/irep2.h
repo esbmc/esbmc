15440b48caa9c03c51275e3c1f20c378ce30de80
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-14 20:26:51 +0000

    Add documentation.
    There is now more documentation here than code.

--- util/irep2.h
+++ util/irep2.h
@@ -2,17 +2,29 @@
 /** Base class for all types */
 class type2t
 {
+  /** Enumeration identifying each sort of type.
+   *  The idea being that we might (for whatever reason) at runtime need to fall
+   *  back onto identifying a type through just one field, for some reason. It's
+   *  also highly useful for debugging */
   enum type_ids {
   };
+
+  /** Instance of type_ids recording this types type. */
   type_ids type_id;
 };
 
 /** Base class for all expressions */
 class expr2t
 {
+  /** Enumeration identifying each sort of expr.
+   *  The idea being to permit runtime identification of a type, for debugging or
+   *  otherwise. See type2t::type_ids. */
   enum expr_ids {
   };
+
+  /** Instance of expr_ids recording tihs exprs type. */
   expr_ids expr_id;
 
+  /** Type of this expr. All exprs have a type. */
   type2t type;
 };
