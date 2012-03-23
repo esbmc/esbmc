eaa565637d84cdd373731ca82b5a1ce98ba5c0c1
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-23 23:48:34 +0000

    Split migrate_irep into type and expr portions

--- util/migrate.h
+++ util/migrate.h
@@ -1,4 +1,6 @@
 #include "std_expr.h"
+#include "std_types.h"
 #include "irep2.h"
 
-bool migrate_irep(exprt &expr, expr2tc &new_expr);
+bool migrate_type(typet &type, type2tc &new_type);
+bool migrate_expr(exprt &expr, expr2tc &new_expr);
