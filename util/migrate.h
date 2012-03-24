79d58244810b43c8c6555c0da3032f088c982719
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-24 00:13:34 +0000

    Constify migreate functions

--- util/migrate.h
+++ util/migrate.h
@@ -2,5 +2,5 @@
 #include "std_types.h"
 #include "irep2.h"
 
-bool migrate_type(typet &type, type2tc &new_type);
-bool migrate_expr(exprt &expr, expr2tc &new_expr);
+bool migrate_type(const typet &type, type2tc &new_type);
+bool migrate_expr(const exprt &expr, expr2tc &new_expr);
