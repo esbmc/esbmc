974b5698daa8ed6649eedcf8c0638a3719b4a10c
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-24 00:09:00 +0000

    Add skeleton irep2.cpp expr2t base class methods.
    Crucially here, we're defining convert_smt to bounce straight over to
    the prop_convt object with no further consideration.

--- util/irep2.cpp
+++ util/irep2.cpp
@@ -1,1 +1,10 @@
 #include "irep2.h"
+
+#include <solvers/prop/prop_conv.h>
+
+expr2t::expr2t(const type2tc _type, expr_ids id)
+  : expr_id(id), type(_type)
+{
+}
+
+void expr2t::convert_smt(prop_convt &obj) const { obj.convert_smt_expr(*this); }
