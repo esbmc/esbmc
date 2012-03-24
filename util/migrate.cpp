6cda522b82b8c847c1f0cce7e933317297e2b183
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-03-24 00:13:50 +0000

    Implement barebones migration functions.
    This is just a dummy conversion routine for types, and a conversion
    routine for the simplest expression type I could find a symbol. In all
    cirucmstances, we return true if we actually converted something, and
    initialize the passed in expr2tc reference if that's the case.

--- util/migrate.cpp
+++ util/migrate.cpp
@@ -1,3 +1,28 @@
 #include "migrate.h"
 
 // File for old irep -> new irep conversions.
+
+
+bool
+migrate_type(const typet &type, type2tc &new_type_ref)
+{
+
+  return false;
+}
+
+bool
+migrate_expr(const exprt &expr, expr2tc &new_expr_ref)
+{
+  type2tc type;
+
+  if (expr.id() == "symbol") {
+    if (!migrate_type(expr.type(), type))
+      return false;
+
+    expr2t *new_expr = new symbol2t(type, expr.identifier().as_string());
+    new_expr_ref = expr2tc(new_expr);
+    return true;
+  } else {
+    return false;
+  }
+}
