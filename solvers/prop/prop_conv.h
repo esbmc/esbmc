14fb619185ebe46027d6e6678b08ef6017bec092
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-04-20 02:16:46 +0100

    Commit a bit of shl code I forgot to stage Guurrg.

--- solvers/prop/prop_conv.h
+++ solvers/prop/prop_conv.h
@@ -141,6 +141,7 @@ public:
   virtual void convert_smt_expr(const mul2t &mul, void *&arg) = 0;
   virtual void convert_smt_expr(const div2t &mul, void *&arg) = 0;
   virtual void convert_smt_expr(const modulus2t &mod, void *&arg) = 0;
+  virtual void convert_smt_expr(const shl2t &shl, void *&arg) = 0;
 };
 
 #endif
