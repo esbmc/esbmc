ad1cd5b85e8d40ead8929ea7d81f4e180658c6f3
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-04-22 12:17:53 -0400

    updating test case sum03_base_case
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/k-induction/sum03_base_case/main.c
+++ regression/k-induction/sum03_base_case/main.c
@@ -1,16 +1,21 @@
 #define a (2)
+int nondet_int();
 unsigned int nondet_uint();
-struct sumt {
-  int sn;
-} sum;
+_Bool nondet_bool();
 
 int main() { 
-  int i, n=nondet_uint(), sn=0;
-  for(i=1; i<=n; i++) {
+  int sn=0;
+  unsigned int loop1=0, n1=nondet_uint();
+  unsigned int i=0;
+
+  //for(i=1; i<=n; i++) {
+  while(loop1<n1){
+    loop1++;
      	if (i<10) //first case
-    sum.sn = sum.sn + a; //second case OK
+    sn = sn + a; //second case OK
     //if (i==4) sn=-10; //third case OK
+    i++;
   }
   //__ESBMC_assume(i>n);
-  assert(sum.sn==n*a || sum.sn == 0);
+  assert(sn==a || sn == 0);
 }
