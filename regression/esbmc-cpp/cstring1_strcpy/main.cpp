fa5a943d44e75de909709e20346ce2db102aa390
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-04-23 14:17:30 -0400

    updating test case cstring1_strcpy
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc-cpp/cstring1_strcpy/main.cpp
+++ regression/esbmc-cpp/cstring1_strcpy/main.cpp
@@ -10,5 +10,7 @@ int main ()
   strcpy (str2,str1);
   strcpy (str3,"copy successful");
   std::cout << "\nstr1: " << str1 << "\nstr2: " << str2 << "\nstr3: " << str3 << std::endl;
+  assert(strcmp(str1,str2)==0);
+  assert(strcmp(str3,"copy successful")==0);
   return 0;
 }
