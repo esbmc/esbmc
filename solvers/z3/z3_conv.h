ee2e043b355c3e7dd7c3b985fc72526e20cd1249
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-04-24 15:40:53 +0100

    Remove useless commented code.

--- solvers/z3/z3_conv.h
+++ solvers/z3/z3_conv.h
@@ -235,7 +235,6 @@ private:
   z3_capi z3_api;
 
   bool int_encoding, ignoring_expr, equivalence_checking;
-  //Z3_ast assumptions[Z3_UNSAT_CORE_LIMIT];
   std::list<Z3_ast> assumptions;
   std::string filename;
 
