9326c5eb6d0c887d0a25c20578a444e343eb4de2
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-05-03 18:45:34 +0100

    Take another shot at base simplify method.
    So, just modifying oneself is in fact a bad plan, just in case we're
    reference counted. So instead, return a new copy of ourselves.
    
    This might not actually be required... what we really want to be able to
    do is have our object detatched/cloned/whatever if we're going to
    simplify it, and there are other things using it. However, this is a
    recipie for pain.

--- util/simplify_expr2.cpp
+++ util/simplify_expr2.cpp
@@ -3,23 +3,40 @@
 expr2t *
 expr2t::do_simplify(void)
 {
-  bool res = false;
   // Base simplify - the obj being simplified has no function that simplifies
   // itself. So, fetch a vector of operands and apply simplification to anything
-  // further down.
+  // further down. If something gets changed, then we need to clone this expr
+  // and return a new one, as we've been modified.
+  bool changed = false;
   std::vector<expr2tc*> operands;
+  std::vector<expr2t*> newoperands;
+
   list_operands(operands);
   for (std::vector<expr2tc *>::iterator it = operands.begin();
        it != operands.end(); it++) {
     expr2t *tmp = (**it).get()->do_simplify();
-    if (tmp == NULL) {
-      ; // No modification
-    } else {
-      // We've been returned a new irep to use. Assign into this objects
-      // expr2tc field, which will also cause the previous value to be
-      // de-referenced.
-      **it = expr2tc(tmp);
-    }
+    newoperands.push_back(tmp);
+    if (tmp != NULL)
+      changed = true;
   }
-  return false;
+
+  if (changed == false)
+    return NULL;
+
+  // An operand has been changed; clone ourselves and update.
+  expr2t *new_us = clone_raw();
+  std::vector<expr2tc*> clonedoperands;
+  new_us->list_operands(clonedoperands);
+  assert(clonedoperands.size() == newoperands.size());
+
+  std::vector<expr2t *>::iterator it2 = newoperands.begin();
+  for (std::vector<expr2tc *>::iterator it = clonedoperands.begin();
+       it != clonedoperands.end(); it++, it2++) {
+    if ((*it2) == NULL)
+      continue; // No change in operand;
+    else
+      **it = expr2tc(*it2); // Operand changed; overwrite with new one.
+  }
+
+  return new_us;
 }
