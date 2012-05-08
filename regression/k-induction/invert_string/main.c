61f14e6ef6bed65ad5270ab125b6639edf5a1f7a
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-05-08 13:38:56 -0400

    modifying test case inver_string
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/k-induction/invert_string/main.c
+++ regression/k-induction/invert_string/main.c
@@ -1,33 +1,30 @@
-//#include <stdio.h>
-//#include <string.h>
-//#define MAX 10
-int nondet_int();
 char nondet_char();
 unsigned int nondet_uint();
-int MAX = nondet_uint();
+unsigned int max = nondet_uint();
 
 int main() {
-    char str1[MAX], str2[MAX];
-    int cont, i, j;
-    cont = 0;
+    __ESBMC_assume(max>0 && max<6);
+    char str1[max], str2[max];
+    //unsigned int i, j;
+    int i, j;
     
-    for (i=0; i<MAX; i++) {
+    for (i=0; i<max; i++) {
         str1[i]=nondet_char();
     }
-	str1[MAX-1]= '\0';
+
+    str1[max-1]= '\0';
 
     j = 0;
     
     // Copia str1 inversa para str2
-    for (i = MAX - 1; i >= 0; i--) {
+    for (i = max - 1; i >= 0; i--) {
         str2[j] = str1[i];
         j++;
     }
-	//__ESBMC_assume(i<0);
-	j = MAX-1;
-    for (i=0; i<MAX; i++) {
+
+    j = max-1;
+    for (i=0; i<max; i++) {
       assert(str1[i] == str2[j]);
-	  j--;
-    }
-    
+      j--;
+    }   
 }
