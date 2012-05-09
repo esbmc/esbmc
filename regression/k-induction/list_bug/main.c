dd92f09defb54969fd147c8c8339ce5d59498d13
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-05-09 02:37:18 -0400

    adding test case list_bug
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/k-induction/list_bug/main.c
@@ -0,0 +1,39 @@
+#include<stdlib.h>
+typedef struct list {
+	int key;
+	struct list *next;
+} mlist;
+
+mlist *head;
+
+mlist* search_list(mlist *l, int k){
+  l = head;
+  while(l!=NULL && l->key!=k) {
+    l = l->next;
+  }
+  return l;
+}
+
+int insert_list(mlist *l, int k){
+  l = (mlist*)malloc(sizeof(mlist));
+  l->key = k;
+  if (head==NULL) {
+    l->next = NULL;
+  } else {
+    l->key = k;
+    l->next = head;
+  }
+  head = l;	
+  return 0;	
+}
+
+int main(void){
+  int i;
+  mlist *mylist, *temp;
+  insert_list(mylist,2);
+  insert_list(mylist,5);
+  insert_list(mylist,1);
+  insert_list(mylist,3);
+  temp = search_list(head,2);
+  assert(temp->key==1);
+}
