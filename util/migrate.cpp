e8316a00d5a9e1acc6bbd057a36f1088a7f46ad6
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-05-10 18:00:55 +0100

    Add migration for expression code statements.

--- util/migrate.cpp
+++ util/migrate.cpp
@@ -989,6 +989,11 @@ migrate_expr(const exprt &expr, expr2tc &new_expr_ref)
       ops.push_back(tmp_op);
     }
     new_expr_ref = expr2tc(new code_printf2t(ops));
+  } else if (expr.id() == irept::id_code && expr.statement() == "expression") {
+    assert(expr.operands().size() == 1);
+    expr2tc theop;
+    migrate_expr(expr.op0(), theop);
+    new_expr_ref = expr2tc(new code_expression2t(theop));
   } else {
     expr.dump();
     throw new std::string("migrate expr failed");
@@ -1768,6 +1773,15 @@ migrate_expr_back(const expr2tc &ref)
     forall_exprs(it, ref2.operands) {
       codeexpr.operands().push_back(migrate_expr_back(*it));
     }
+    return codeexpr;
+  }
+  case expr2t::code_expression_id:
+  {
+    const code_expression2t &ref2 = to_code_expression2t(ref);
+    exprt codeexpr("code", code_typet());
+    codeexpr.statement(irep_idt("expression"));
+    exprt op0 = migrate_expr_back(ref2.operand);
+    codeexpr.copy_to_operands(op0);
     return codeexpr;
   }
   default:
