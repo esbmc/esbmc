0ea57394fab35acc882ae1c697224c3e41879213
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-05-10 19:17:54 -0400

    updating in test descriptions and test cases
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-cpp/string_at_6_bug/main.cpp
+++ regression/esbmc-cpp/string_at_6_bug/main.cpp
@@ -11,16 +11,22 @@ int main ()
   int i;
   str1 = string("Test string: x");
   for( i = 0 ; i < 5 ; i++ )
-  	str2 = str1.at(i);
+  	str2 += str1.at(i);
+	cout << "Test ==" << str2 << endl;
 
   for( i = 0 ; i < 6 ; i++ )
-  	str3 = str1.at(6 - i);  
+  	str3 += str1.at(6 - i);  
+
+	cout << "Test s ==" << str3 << endl;
   
   for( i = 0 ; i < 7 ; i++ )
-  	str4 = str1.at(7 - i);
+  	str4 += str1.at(7 - i);
+
+
+	cout << "Test st ==" << str4 << endl;
   
-  assert( (str2 != "Test ") || (str3 != "Test s") || (str4 != "Test st") );
-  
+  assert( (str2 != "Test ") && (str3 != "Test s") && (str4 != "Test st") );
+
   cout << str3  << endl;
   return 0;
 }
--- regression/esbmc-cpp/string_length_2/main.cpp
+++ regression/esbmc-cpp/string_length_2/main.cpp
@@ -5,5 +5,5 @@ using namespace std;
 int main(){
 	string str1;
 	str1 = string("Test");
-	assert(!str1.length() == 4);
+	assert(str1.length() == 4);
 }
