b736ae46213ab4ef082649ba502dbb28a022d7f2
Hendrio M. Marques <hendriomm@gmail.com>
2012-05-14 16:11:05 -0400

    added capacity, copy, data and erase tests
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-cpp/string_erase_3_bug/main.cpp
@@ -0,0 +1,19 @@
+//TEST FAILS
+// string::erase
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  string str ("This is an example phrase.");
+  string::iterator it;
+
+  // erase used in the same order as described above:
+  str.erase (10,8);
+  assert(str != "This is an phrase.");
+  cout << str << endl;        // "This is an phrase."
+
+  return 0;
+}
---
+++ regression/esbmc-cpp/string_erase_final_bug/main.cpp
@@ -0,0 +1,27 @@
+//TEST FAILS
+// string::erase
+#include <iostream>
+#include <string>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  string str ("This is an example phrase.");
+  string::iterator it;
+
+  // erase used in the same order as described above:
+  str.erase (10,8);
+  assert(str != "This is an phrase.");
+  cout << str << endl;        // "This is an phrase."
+
+  it=str.begin()+9;
+  str.erase (it);
+  assert(str != "This is a phrase.");
+  cout << str << endl;        // "This is a phrase."
+
+  str.erase (str.begin()+5, str.end()-7);
+    assert(str != "This phrase.");
+  cout << str << endl;        // "This phrase."
+  return 0;
+}
