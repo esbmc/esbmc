65fe8a870e8ed09d6c36b3d61efd9c9df93ac592
Hendrio M. Marques <hendriomm@gmail.com>
2012-05-14 14:31:20 -0400

    added string capacity tests
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-cpp/string_capacity_3/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <iostream>
+#include <cassert>
+
+using namespace std;
+
+int main(){
+	string str;
+	str = "Test string";
+	assert(str.capacity() >= 11);
+	cout << str.capacity();
+	str += ": x";
+	assert(str.capacity() >= 13);
+	cout << str.capacity();
+	str.assign("New test string: xyzabcdefg");
+	assert(str.capacity() >= 26);
+	cout << str.capacity();
+}
