e8e430cb76a842baf0a91f5558ce6f40f0b2ef83
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-05-15 17:48:46 -0400

    adding new test case terminator_02
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/k-induction/terminator_02/main.c
@@ -0,0 +1,27 @@
+int nondet_int();
+_Bool nondet_bool();
+
+main()
+{
+  int x=nondet_int();
+  int y=nondet_int();
+  int z=nondet_int();
+
+  while(x<100 && 100<z) 
+  {
+    _Bool tmp=nondet_bool();
+    if (tmp)
+   {
+     x++;
+   }
+   else
+   {
+     x--;
+     z--;
+   }
+  }                       
+    
+  //  assert(array[0]>=menor);    
+}
+
+
