eb3ee064f578f54d98146875a9339f27fc24c008
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-05-15 15:20:26 +0100

    Implement array_name for irep2.
    Should probably become a member of the class in the future.

--- util/array_name.cpp
+++ util/array_name.cpp
@@ -44,3 +44,24 @@ std::string array_name(
   return "array";
 }
 
+std::string array_name(
+  const namespacet &ns,
+  const expr2tc &expr)
+{
+  if (is_index2t(expr))
+  {
+    return array_name(ns, to_index2t(expr).source_value) + "[]";
+  }
+  else if (is_symbol2t(expr))
+  {
+    const symbolt &symbol=ns.lookup(to_symbol2t(expr).name);
+    return "array `"+id2string(symbol.base_name)+"'";
+  }
+  else if (is_constant_string2t(expr))
+  {
+    return "string";
+  }
+
+  return "array";
+}
+
