dc984e9c255c13fe090e76d733d5e4ad2f8509e8
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-05-23 13:32:06 -0400

    fix the test case string_assign_7_bug
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-string/string_assign_7_bug/main.cpp
+++ regression/esbmc-string/string_assign_7_bug/main.cpp
@@ -9,13 +9,13 @@ using namespace std;
 int main ()
 {
   string str;
-  string base="The quick brown fox jumps over a lazy dog.";
+  string base="The quick brown fox";
 
   // used in the same order as described above:
 
   str.assign(base);
   cout << str << endl;
-  assert(str != "The quick brown fox jumps over a lazy dog.");
+  assert(str != "The quick brown fox");
   
   return 0;
 }
