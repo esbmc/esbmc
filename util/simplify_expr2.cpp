f28759fccf43daa14ca0672a5ce2562d005f7f08
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-05-26 17:45:17 +0100

    Don't simplify negative index offsets.
    As it's invalid; it also correctly triggers an assertion failure when
    fetching the ulong.

--- util/simplify_expr2.cpp
+++ util/simplify_expr2.cpp
@@ -704,8 +704,8 @@ index2t::do_simplify(bool second __attribute__((unused))) const
     // Index might be greater than the constant array size. This means we can't
     // simplify it, and the user might be eaten by an assertion failure in the
     // model. We don't have to think about this now though.
-    unsigned long the_idx = idx.as_ulong();
-    if (the_idx >= arr.datatype_members.size())
+    long the_idx = idx.as_long();
+    if (the_idx >= arr.datatype_members.size() || the_idx < 0)
       return expr2tc();
 
     return arr.datatype_members[the_idx];
