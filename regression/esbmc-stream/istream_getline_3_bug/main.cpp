386dbcf085ca34470a4a856661860609bfa8a21f
Hendrio M. Marques <hendriomm@gmail.com>
2012-05-29 18:14:47 -0400

    fixing istream::getline character bounds
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-stream/istream_getline_3_bug/main.cpp
+++ regression/esbmc-stream/istream_getline_3_bug/main.cpp
@@ -9,10 +9,10 @@ int main () {
 
   cout << "Enter your name: ";
   cin.getline (name,256);
-  assert(!((int)cin.gcount() >= 0 && (int)cin.gcount() < 256));
+  assert(!((int)cin.gcount() >= 0 && (int)cin.gcount() <= 256));
   cout << "Enter your favourite movie: ";
   cin.getline (title,256);
-  assert(!((int)cin.gcount() >= 0 && (int)cin.gcount() < 256));
+  assert(!((int)cin.gcount() >= 0 && (int)cin.gcount() <=256));
   cout << name << "'s favourite movie is " << title;
 
   return 0;
