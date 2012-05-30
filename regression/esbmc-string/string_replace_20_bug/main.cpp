2bf6e174ae00d551498053e3e54a44220102a3d8
Hendrio M. Marques <hendriomm@gmail.com>
2012-05-30 19:18:42 -0400

    fixing string::replace (iterator) tests
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-string/string_replace_20_bug/main.cpp
+++ regression/esbmc-string/string_replace_20_bug/main.cpp
@@ -13,6 +13,21 @@ int main ()
   string str3="sample phrase";
   string str4="useful.";
 
+  string str=base;                // "this is a test string."
+  str.replace(9,5,str2);          // "this is an example string."
+  assert(str != "this is an example string.");
+  
+  str.replace(19,6,str3,7,6);     // "this is an example phrase."
+  assert(str != "this is an example phrase.");
+  
+  str.replace(8,10,"just all",6); // "this is just a phrase."
+  assert(str != "this is just a phrase.");
+  
+  str.replace(8,6,"a short");     // "this is a short phrase."
+  assert(str != "this is a short phrase.");
+  
+  str.replace(22,1,3,'!');        // "this is a short phrase!!!"
+  assert(str != "this is a short phrase!!!");
 
   // Using iterators:                      0123456789*123456789*
   string::iterator it = str.begin();   //  ^
