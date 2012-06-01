d2a0d46a30318f41ca680ac0fc492ad35aad3caf
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-06-01 13:11:22 -0400

    fixing the test case string_substr_4_bug
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-string/string_substr_4_bug/main.cpp
+++ regression/esbmc-string/string_substr_4_bug/main.cpp
@@ -9,17 +9,17 @@ using namespace std;
 
 int main ()
 {
-  string str="We think in generalities, but we live in details.";
+  string str="We ge, live.";
                              // quoting Alfred N. Whitehead
   string str2, str3;
   int pos;
 
-  str2 = str.substr (12,12); // "generalities"
+  str2 = str.substr (3,2); // "generalities"
 
   pos = str.find("live");    // position of "live" in str
   str3 = str.substr (pos);   // get from "live" to the end
 
   cout << str2 << ' ' << str3 << endl;
-  assert((str2 != "generalities")&&(str3 != "live in details"));
+  assert((str2 == "generalities")&&(str3 != "live in details"));
   return 0;
 }
