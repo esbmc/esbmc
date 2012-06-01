fbbdd61836f6830287fd29662d33bcb847cb035e
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-06-01 12:32:50 -0400

    fixing the test case string_find_2
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-string/string_find_2/main.cpp
+++ regression/esbmc-string/string_find_2/main.cpp
@@ -6,18 +6,18 @@ using namespace std;
 
 int main ()
 {
-  string str ("There are two needles in this haystack with needles.");
-  string str2 ("needle");
+  string str ("Ttwo need hneed.");
+  string str2 ("need");
   size_t found;
 
   // different member versions of find in the same order as above:
   found=str.find(str2);
-  assert(int(found)==14);
+  assert(int(found)==5);
   if (found!=string::npos)
     cout << "first 'needle' found at: " << int(found) << endl;
 
-  found=str.find("needles are small",found+1,6);
-  assert(int(found)==44);
+  found=str.find("needles are small",found+1,4);
+  assert(int(found)==11);
   if (found!=string::npos)
     cout << "second 'needle' found at: " << int(found) << endl;
 
