594d15b57ff97eb34ac56b8b61ee7ac9c0c5b741
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-06-05 16:31:23 -0400

    fixing the test case string_find_last_of_1
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-string/string_find_last_of_1/main.cpp
+++ regression/esbmc-string/string_find_last_of_1/main.cpp
@@ -11,15 +11,19 @@ int main ()
   
   size_t found;
   
-  cout << "Splitting: " << str1 << endl;
+  cout << "Splitting one: " << str1 << endl;
+  cout << "Splitting two: " << str2 << endl;
+
   found=str1.find_last_of("/\\");
   assert(int(found) == 8);
+  
   cout << " folder: " << str1.substr(0,found) << endl;
   cout << " file: " << str1.substr(found+1) << endl;
-  
   cout << "Splitting: " << str2 << endl;
+  
   found=str2.find_last_of("/\\");
   assert(int(found) == 10);
+
   cout << " folder: " << str2.substr(0,found) << endl;
   cout << " file: " << str2.substr(found+1) << endl;
   
