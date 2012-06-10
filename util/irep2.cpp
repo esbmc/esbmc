7495b8ee2a202738c96e19fd80d539e91f4250ed
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-06-10 22:33:18 +0100

    bitnor2t -> new template goo.

--- util/irep2.cpp
+++ util/irep2.cpp
@@ -1962,6 +1962,8 @@ std::string bitxor2t::field_names [esbmct::num_type_fields]  =
 { "side_1", "side_2", "", ""};
 std::string bitnand2t::field_names [esbmct::num_type_fields]  =
 { "side_1", "side_2", "", ""};
+std::string bitnor2t::field_names [esbmct::num_type_fields]  =
+{ "side_1", "side_2", "", ""};
 
 // Explicit template instanciations
 
@@ -2073,5 +2075,8 @@ template class esbmct::expr_methods<bitxor2t, bit_2ops,
     expr2tc, bit_2ops, &bit_2ops::side_1,
     expr2tc, bit_2ops, &bit_2ops::side_2>;
 template class esbmct::expr_methods<bitnand2t, bit_2ops,
+    expr2tc, bit_2ops, &bit_2ops::side_1,
+    expr2tc, bit_2ops, &bit_2ops::side_2>;
+template class esbmct::expr_methods<bitnor2t, bit_2ops,
     expr2tc, bit_2ops, &bit_2ops::side_1,
     expr2tc, bit_2ops, &bit_2ops::side_2>;
--- util/irep2.h
+++ util/irep2.h
@@ -1247,6 +1247,7 @@ class bitand2t;
 class bitor2t;
 class bitxor2t;
 class bitnand2t;
+class bitnor2t;
 
 // Data definitions.
 
@@ -1535,6 +1536,10 @@ typedef esbmct::expr_methods<bitnand2t, bit_2ops,
         expr2tc, bit_2ops, &bit_2ops::side_1,
         expr2tc, bit_2ops, &bit_2ops::side_2>
         bitnand_expr_methods;
+typedef esbmct::expr_methods<bitnor2t, bit_2ops,
+        expr2tc, bit_2ops, &bit_2ops::side_1,
+        expr2tc, bit_2ops, &bit_2ops::side_2>
+        bitnor_expr_methods;
 
 /** Constant integer class. Records a constant integer of an arbitary
  *  precision */
@@ -1898,20 +1903,18 @@ public:
   static std::string field_names[esbmct::num_type_fields];
 };
 
-class bitnor2t : public esbmct::expr<bitnor2t, esbmct::expr2tc_side_1,
-                                             esbmct::expr2tc_side_2>
+class bitnor2t : public bitnor_expr_methods
 {
 public:
-  bitnor2t(const type2tc &type, const expr2tc &v1, const expr2tc &v2)
-    : esbmct::expr<bitnor2t, esbmct::expr2tc_side_1, esbmct::expr2tc_side_2>
-      (type, bitnor_id, v1, v2) {}
+  bitnor2t(const type2tc &t, const expr2tc &s1, const expr2tc &s2)
+  : bitnor_expr_methods(t, bitnor_id, s1, s2) {}
   bitnor2t(const bitnor2t &ref)
-    : esbmct::expr<bitnor2t, esbmct::expr2tc_side_1, esbmct::expr2tc_side_2>
-      (ref) {}
+  : bitnor_expr_methods(ref) {}
+
   virtual expr2tc do_simplify(bool second) const;
+
+  static std::string field_names[esbmct::num_type_fields];
 };
-template class esbmct::expr<bitnor2t, esbmct::expr2tc_side_1,
-                                    esbmct::expr2tc_side_2>;
 
 class bitnxor2t : public esbmct::expr<bitnxor2t, esbmct::expr2tc_side_1,
                                                esbmct::expr2tc_side_2>
