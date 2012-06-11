1ed7fef681f15dffa1d5e11e4a6bdb64c2fba9cd
Bruno Savino <bruno.judiss@gmail.com>
2012-06-11 16:50:47 -0400

    fix esbmc-stream test cases
    Signed-off-by: Bruno Savino <bruno.judiss@gmail.com>

--- regression/esbmc-stream/istream_operator>>_char/main.cpp
+++ regression/esbmc-stream/istream_operator>>_char/main.cpp
@@ -1,19 +1,20 @@
 // example on extraction
 #include <iostream>
 #include <cassert>
+
 using namespace std;
 
 int main () {
   char n;
   char n1[256];
   string n2;
-  
+
   cout << "Enter character: ";
   cin >> n;
   assert((int)cin.gcount() >= 0);
   cout << "Enter a string: ";
-  cin >> n1;            // manipulator
-//  assert(cin.flags() & ios::hex == iostream::hex);
+  cin >> hex >> n1;            // manipulator
+  assert(cin.flags() & ios::hex == iostream::hex);
   cout << "Enter another string: ";
   cin >> n2;            // manipulator
   
--- regression/esbmc-stream/ostream_constructor_1/main.cpp
+++ regression/esbmc-stream/ostream_constructor_1/main.cpp
@@ -8,10 +8,10 @@ int main () {
   filebuf fb;
   fb.open ("test",ios::out);
   ostream os(&fb);
-  assert(os.is_open());
+//  assert(os.is_open());
   os << "Test sentence\n";
   
   fb.close();
-  assert(!(os.is_open()));
+//  assert(!(os.is_open()));
   return 0;
 }
