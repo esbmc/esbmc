403604d3ae7f53fd75c08fcad4f62c6dfef80e51
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-06-27 15:43:13 +0100

    Add string container wrapper that hashes.
    For storing hash maps of strings.
    
    This is because I'm not convinced by reading the value set code that it
    can't be easily turned into not indexing by strings without a
    significant refactor. So, actually do store as strings, but don't use
    irep_idt; instead store as a hash-mapped string wrapper.

--- util/string_hash.h
+++ util/string_hash.h
@@ -19,4 +19,30 @@ struct string_hash
   size_t operator()(const std::string &s) const { return hash_string(s); }
 };
 
+class string_wrapper {
+public:
+  string_wrapper(const std::string &str)
+    : the_string(str), hash(hash_string(str)) { }
+
+  std::string the_string;
+  size_t hash;
+};
+
+class string_wrap_hash {
+public:
+  size_t operator()(const string_wrapper &s) const { return s.hash; }
+  size_t operator()(const string_wrapper &s1, const string_wrapper &s2) const
+  {
+    if (s1.hash < s2.hash)
+      return true;
+    else if (s1.hash > s2.hash)
+      return false;
+
+    // Hashes match,
+    if (s1.the_string < s2.the_string)
+      return true;
+    return false;
+  }
+};
+
 #endif
