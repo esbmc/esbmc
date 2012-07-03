aa6b2cbd277af9eb4c06ed0d8623cc5f9112688e
Hendrio M. Marques <hendriomm@gmail.com>
2012-07-03 16:04:20 -0400

    including stringstream::operator<< for int values
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-stream/sstream_str_int/main.cpp
+++ regression/esbmc-stream/sstream_str_int/main.cpp
@@ -8,15 +8,15 @@ using namespace std;
 int main () {
 
   stringstream oss;
-  int val1 = 1532;
+  int val1 = -1532;
   unsigned int val2 = 433;
   
   oss << val1;
-  assert(oss.str() == "1532");
+  assert(oss.str() == "-1532");
   
   
   oss << val2;
-  assert(oss.str() == "1532433");
+  assert(oss.str() == "-1532433");
   
   return 0;
 }
