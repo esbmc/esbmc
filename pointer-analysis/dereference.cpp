99cd9634cedbf695d5ca2d1f0a0a5aeba6f08fdb
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-07-11 16:29:17 -0400

    fix the 'memory not applicable' problem in the method memory_model_bytes
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- pointer-analysis/dereference.cpp
+++ pointer-analysis/dereference.cpp
@@ -324,6 +324,9 @@ void dereferencet::build_reference_to(
   exprt &pointer_guard,
   const guardt &guard)
 {
+  const typet &dereference_type=
+    ns.follow(deref_expr.type()).subtype();
+
   value.make_nil();
   pointer_guard.make_false();
 
@@ -472,6 +475,133 @@ void dereferencet::build_reference_to(
   }
   else
   {
+#if 0
+    // something generic -- really has to be a symbol
+    address_of_exprt object_pointer(object);
+
+    if(o.offset().is_zero())
+    {
+      equality_exprt equality(deref_expr, object_pointer);
+
+      if(ns.follow(equality.lhs().type())!=ns.follow(equality.rhs().type()))
+        equality.lhs().make_typecast(equality.rhs().type());
+
+      pointer_guard=equality;
+    }
+    else
+    {
+      pointer_guard=exprt("same-object", bool_typet());
+      pointer_guard.copy_to_operands(deref_expr, object_pointer);
+    }
+
+    value=object;
+
+    guardt tmp_guard(guard);
+    tmp_guard.add(pointer_guard);
+
+    valid_check(object, tmp_guard, mode);
+
+    exprt offset;
+
+
+    const typet &object_type=ns.follow(object.type());
+    exprt root_object=o.root_object();
+    const typet &root_object_type=ns.follow(root_object.type());
+
+    if(dereference_type_compare(value, dereference_type) &&
+       o.offset().is_zero())
+    {
+      // The simplest case: types match, and offset is zero!
+      // This is great, we are almost done.
+      value=object;
+
+      if(object_type!=ns.follow(dereference_type))
+        value.make_typecast(dereference_type);
+      return ;
+    }
+    else if(root_object_type.id()=="array" &&
+            dereference_type_compare(root_object, dereference_type))
+    {
+      // We have an array with a subtype that matches
+      // the dereferencing type.
+      // We will require well-alignedness!
+
+      exprt offset;
+
+      // this should work as the object is essentially the root object
+      if(o.offset().is_constant())
+        offset=o.offset();
+      else
+        offset=unary_exprt("pointer_offset", deref_expr, index_type());
+
+      exprt adjusted_offset;
+
+      // are we doing a byte?
+      mp_integer element_size=
+        pointer_offset_size(dereference_type);
+
+      if(element_size==1)
+      {
+        // no need to adjust offset
+        adjusted_offset=offset;
+      }
+      else
+      {
+        exprt element_size_expr=
+          from_integer(element_size, offset.type());
+
+        adjusted_offset=binary_exprt(
+          offset, exprt::div, element_size_expr, offset.type());
+
+        // TODO: need to assert well-alignedness
+      }
+
+      index_exprt index_expr=
+        index_exprt(root_object, adjusted_offset, root_object_type.subtype());
+
+      bounds_check(index_expr, guard);
+
+      value=index_expr;
+
+      if(ns.follow(value.type())!=ns.follow(dereference_type))
+        value.make_typecast(dereference_type);
+
+      return ;
+    }
+    else
+    {
+      // we extract something from the root object
+      value=o.root_object();
+
+      // this is relative to the root object
+      exprt offset=
+        unary_exprt("pointer_offset", deref_expr, index_type());
+
+      if(memory_model(value, dereference_type, tmp_guard, offset))
+      {
+        // ok, done
+      }
+      else
+      {
+        if(!options.get_bool_option("no-pointer-check"))
+        {
+          std::string msg="memory model not applicable (got `";
+          msg+=from_type(ns, "", value.type());
+          msg+="', expected `";
+          msg+=from_type(ns, "", dereference_type);
+          msg+="')";
+
+          dereference_callback.dereference_failure(
+            "pointer dereference",
+            msg, tmp_guard);
+        }
+
+        return ; // give up, no way that this is ok
+      }
+    }
+#endif
+
+#if 1
     value=object;
 
     exprt object_pointer("address_of", pointer_typet());
@@ -513,13 +643,8 @@ void dereferencet::build_reference_to(
         if(!options.get_bool_option("no-pointer-check"))
         {
           //nec: ex29
-		  std::string name = value.type().get("name").as_string();
-		  if(name == "cpp::std::struct.ofstream" ||
-				  name == "cpp::std::struct.fstream" ||
-				  name == "cpp::std::struct.ifstream")
-			 return ;
-          if (value.type().subtype().id()=="empty"
-        		  || type.subtype().id()=="empty")
+          if (value.type().subtype().id()=="empty" ||
+        		  type.subtype().id()=="empty")
             return;
           std::string msg="memory model not applicable (got `";
           msg+=from_type(ns, "", value.type());
@@ -561,6 +686,7 @@ void dereferencet::build_reference_to(
         }
       }
     }
+#endif
   }
 }
 
@@ -837,7 +963,13 @@ bool dereferencet::memory_model_bytes(
   const guardt &guard,
   exprt &new_offset)
 {
+#if 1
+  //std::cout << "value.pretty(): " << value.pretty() << std::endl;
+  //std::cout << "new_offset.pretty(): " << new_offset.pretty() << std::endl;
+  //std::cout << "to_type.id(): " << to_type.id() << std::endl;
+
   const typet from_type=value.type();
+  exprt result;
 
   // we won't try to convert to/from code
   if(from_type.is_code() || to_type.is_code())
@@ -907,7 +1039,172 @@ bool dereferencet::memory_model_bytes(
 
     return true;
   }
+  else if (to_type.id()=="struct")
+  {
+    if (ns.follow(from_type).id()=="array" &&
+       pointer_offset_size(ns.follow(from_type).subtype())==1 &&
+       pointer_offset_size(to_type)==1 &&
+       is_a_bv_type(ns.follow(from_type).subtype()) &&
+       is_a_bv_type(to_type))
+    {
+      // yes, can use 'index'
+      result=index_exprt(value, new_offset, ns.follow(from_type).subtype());
+
+      // possibly need to convert
+      if(!base_type_eq(result.type(), to_type, ns))
+        result.make_typecast(to_type);
+      }
+      else
+      {
+        // no, use 'byte_extract'
+        result=exprt(byte_extract_id(), to_type);
+        result.copy_to_operands(value, new_offset);
+      }
+
+    value=result;
+
+    // are we within the bounds?
+    if(!options.get_bool_option("no-pointer-check"))
+    {
+      // upper bound
+      {
+        mp_integer from_width=pointer_offset_size(from_type);
+        mp_integer to_width=pointer_offset_size(to_type);
+
+        exprt bound=from_integer(from_width-to_width, new_offset.type());
+
+        binary_relation_exprt
+          offset_upper_bound(new_offset, exprt::i_gt, bound);
+
+        guardt tmp_guard(guard);
+        tmp_guard.add(offset_upper_bound);
+        dereference_callback.dereference_failure(
+          "pointer dereference",
+          "object upper bound", tmp_guard);
+      }
+
+      // lower bound is easy
+      if(!new_offset.is_zero())
+      {
+        binary_relation_exprt
+          offset_lower_bound(new_offset, exprt::i_lt,
+                           gen_zero(new_offset.type()));
+
+        guardt tmp_guard(guard);
+        tmp_guard.add(offset_lower_bound);
+        dereference_callback.dereference_failure(
+        "pointer dereference",
+        "object lower bound", tmp_guard);
+      }
+    }
+    return true;
+  }
 
   return false;
+#endif
+#if 0
+  const typet from_type=value.type();
+
+  // We simply refuse to convert to/from code.
+  if(from_type.id()=="code" || to_type.id()=="code")
+    return false;
+
+  // We won't do this without a committment to an endianness.
+  if(config.ansi_c.endianess==configt::ansi_ct::NO_ENDIANESS)
+    return false;
+
+  // But everything else we will try!
+  // We just rely on byte_extract to do the job!
+
+  exprt result;
+
+  // See if we have an array of bytes already,
+  // and we want something byte-sized.
+  if(ns.follow(from_type).id()=="array" &&
+     pointer_offset_size(ns.follow(from_type).subtype())==1 &&
+     pointer_offset_size(to_type)==1 &&
+     is_a_bv_type(ns.follow(from_type).subtype()) &&
+     is_a_bv_type(to_type))
+  {
+    // yes, can use 'index'
+    result=index_exprt(value, new_offset, ns.follow(from_type).subtype());
+
+    // possibly need to convert
+    if(!base_type_eq(result.type(), to_type, ns))
+      result.make_typecast(to_type);
+    assert(0);
+  }
+  else
+  {
+    // no, use 'byte_extract'
+    result=exprt(byte_extract_id(), to_type);
+    result.copy_to_operands(value, new_offset);
+  }
+
+  value=result;
+
+  // are we within the bounds?
+  if(!options.get_bool_option("no-pointer-check"))
+  {
+    // upper bound
+    {
+      mp_integer from_width=pointer_offset_size(from_type);
+      mp_integer to_width=pointer_offset_size(to_type);
+
+      exprt bound=from_integer(from_width-to_width, new_offset.type());
+
+      binary_relation_exprt
+        offset_upper_bound(new_offset, exprt::i_gt, bound);
+
+      guardt tmp_guard(guard);
+      tmp_guard.add(offset_upper_bound);
+      dereference_callback.dereference_failure(
+        "pointer dereference",
+        "object upper bound", tmp_guard);
+    }
+
+    // lower bound is easy
+    if(!new_offset.is_zero())
+    {
+      binary_relation_exprt
+        offset_lower_bound(new_offset, exprt::i_lt,
+                           gen_zero(new_offset.type()));
+
+      guardt tmp_guard(guard);
+      tmp_guard.add(offset_lower_bound);
+      dereference_callback.dereference_failure(
+        "pointer dereference",
+        "object lower bound", tmp_guard);
+    }
+  }
+  return true;
+#endif
 }
 
+
+/*******************************************************************\
+
+Function: dereferencet::byte_extract_id
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+irep_idt dereferencet::byte_extract_id()
+{
+  switch(config.ansi_c.endianess)
+  {
+  case configt::ansi_ct::IS_LITTLE_ENDIAN:
+    return "byte_extract_little_endian";
+
+  case configt::ansi_ct::IS_BIG_ENDIAN:
+    return "byte_extract_big_endian";
+
+  default:
+    assert(false);
+  }
+}
