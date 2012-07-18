635746953e3ab291ba8a5556fca25a6f496a3995
Hendrio M. Marques <hendriomm@gmail.com>
2012-07-18 17:51:37 -0400

    creating new test case suite: esbmc-polymorphism
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-polymorphism/polymorphism_common_error_3/main.cpp
@@ -0,0 +1,42 @@
+//treat with a base class object like a derived class object
+//it can generate a compilation error
+#include <iostream>
+using namespace std;
+
+class Shape{
+  public:
+    virtual int area (void);
+    virtual void printArea(void);
+
+};
+
+class CPolygon {
+  protected:
+    int width, height;
+  public:
+    void set_values (int a, int b)
+      { width=a; height=b; }
+    virtual int area (void) =0;
+    void printarea (void)
+      { cout << this->area() << endl; }
+  };
+
+class CRectangle: public CPolygon {
+  public:
+    int area (void)
+      { return (width * height); }
+    int randomVariable;
+  };
+
+class CTriangle: public CPolygon {
+  public:
+    int area (void)
+      { return (width * height / 2); }
+    int randomVariable2;
+  };
+
+int main () {
+  Shape *sh = new Shape;
+  
+  return 0;
+}
