ea5f8e2e630d3d3f817bb477afe008ef2248f830
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-07-25 20:04:06 -0400

    add new test case line_break
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc-stream/line_break/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Linha p deve seguir o formato: \"p cnf num_of_vars num_of_clause \(num_of_vars e num_of_clauses devem ser > 1)\"" << endl;
+  return 0;
+}
+
