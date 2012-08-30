4cf3bfdf54efb5c3b17f5d2ede15f7bd803f0a80
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-08-30 13:54:21 -0400

    Fix some tests descriptions
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/esbmc-try_catch/try-catch_14/main.cpp
+++ regression/esbmc-try_catch/try-catch_14/main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 int main (void) {
   vector<int> myvector(10);
   try {
-	 myvector.at(0)=1;
+    myvector.at(0)=1;
     myvector.at(20)=100;      // vector::at throws an out-of-range
   }
   catch (out_of_range& oor) {
-	 assert(myvector.at(0)==2);
+    assert(myvector.at(0)==2);
     cerr << "Out of Range error: " << oor.what() << endl;
   }
   return 0;
