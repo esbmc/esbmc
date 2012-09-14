d91578065b593ec069f75391b9588cf84c1f6a47
Herbert O. Rocha <herberthb12@gmail.com>
2012-09-14 15:39:50 -0400

    update test.desc and test.pl for esbmc-vector
    Signed-off-by: Herbert O. Rocha <herberthb12@gmail.com>

--- regression/esbmc-cpp/esbmc-vector/test.pl
+++ regression/esbmc-cpp/esbmc-vector/test.pl
@@ -1,139 +1,299 @@
 #!/usr/bin/perl
-
-use subs;
-use strict;
-use warnings;
-
 # test.pl
 #
 # runs a test and check its output
+
+use subs;
+use strict;
+#use warnings;
+no warnings;
+use XML::LibXML;
+use vars qw(%tags);
+
+#------------------------------------------------------
+# Generate the xml file with result the tests 
+my $doc = XML::LibXML::Document->new('1.0', 'utf-8');
+my $root = $doc->createElement("all-results");
+# Insert time and date
+my $time_and_date_run_test = "date_time_run";	
+my $second_root = $doc->createElement( $time_and_date_run_test );	#add tag
+$root->addChild( $second_root ); #add root
+#insert value in the node
+#$second_root->appendTextNode("Sep 13, 2012 13:54:35");
+$second_root->appendTextNode(get_date_time());
+$root->appendChild($second_root);
+#------------------------------------------------------
+
 my $llvm = 0;
 my $testdesc = "test.desc";
 
+
+# ---------------------------------------
+# run esbmc
 sub run($$$) {
-  my ($input, $options, $output) = @_;
-  my $cmd = "esbmc $options $input >$output 2>&1";
+	my ($input, $options, $output) = @_;
+	
+	# for XML file
+	#------------------------------
+	$tags{"item_04_esbmc-option"} = "esbmc ".$options." ".$input;
+	#------------------------------
+	
+	my $cmd = "esbmc $options $input >$output 2>&1";  
+	
+	print LOG "Running $cmd\n";
 
-  print LOG "Running $cmd\n";
-  system $cmd;
-  my $exit_value = $? >> 8;
-  my $signal_num = $? & 127;
-  my $dumped_core = $? & 128;
-  my $failed = 0;
+	system $cmd;
 
-  print LOG "  Exit: $exit_value\n";
-  print LOG "  Signal: $signal_num\n";
-  print LOG "  Core: $dumped_core\n";
+	my $exit_value = $? >> 8;
+	my $signal_num = $? & 127;
+	my $dumped_core = $? & 128;
+	my $failed = 0;
+	
+	# for XML file
+	#------------------------------
+	$tags{"item_06_exit-signal"} = $exit_value;
+	$tags{"item_07_signal-num"} = $signal_num;
+	$tags{"item_08_core-signal"} = $dumped_core;
+	#------------------------------
 
-  if($signal_num != 0) {
-    $failed = 1;
-    print "Killed by signal $signal_num";
-    if($dumped_core) {
-      print " (code dumped)";
-    }
-  }
+	print LOG "  Exit: $exit_value\n";
+	print LOG "  Signal: $signal_num\n";
+	print LOG "  Core: $dumped_core\n";
+	
+	if($signal_num != 0) {
+		$failed = 1;
+		print "Killed by signal $signal_num";
+		if($dumped_core) {
+			print " (code dumped)";
+		}
+	}
 
-  if ($signal_num == 2) {
-    # Interrupted by ^C: we should exit too
-    print "Halting tests on interrupt";
-    exit 1;
-  }
+	if ($signal_num == 2) {
+		# Interrupted by ^C: we should exit too
+		print "Halting tests on interrupt";
+		exit 1;
+	}
 
-  system "echo EXIT=$exit_value >>$output";
-  system "echo SIGNAL=$signal_num >>$output";
+	system "echo EXIT=$exit_value >>$output";
+	system "echo SIGNAL=$signal_num >>$output";
 
-  return $failed;
+	return $failed;
 }
 
-sub load($) {
-  my ($fname) = @_;
-  
-  open FILE, "<$fname";
-  my @data = <FILE>;
-  close FILE;
 
-  chomp @data;
-  return @data;
+sub load($) {
+	my ($fname) = @_;
+	my @data = ();
+
+	### old read
+	# open FILE, "<$fname";
+	# my @data = <FILE>;
+	# close FILE;
+	### old read
+
+	# Parse to xml
+	my $parser = XML::LibXML->new;
+	my $doc    = $parser->parse_file($fname);	
+	my $ter = "";
+
+	for my $item ($doc->findnodes('//test-case')) {	      
+		push(@data,$item->findvalue('item_04_file_C_to_test'));    
+		push(@data,$item->findvalue('item_05_option_to_run_esbmc'));    
+		push(@data,$item->findvalue('item_06_expected_result'));
+		# for XML file
+		#------------------------------
+		$tags{"item_11_summary_test_case"} = $item->findvalue('item_03_summary');
+		#------------------------------
+		
+	}	
+	
+	
+
+	chomp @data;  
+	return @data;
 }
 
 sub test($$) {
-  my ($name, $test) = @_;
-  
-  my ($input, $options, @results) = load($test);
+	my ($name, $test) = @_;
 
-  my $output = $input;
-  $output =~ s/\.cpp $/.out/;
-  $output =~ s/\.c $/.out/;
+	my ($input, $options, @results) = load($test);
 
-  if($output eq $input) {
-    print("Error in test file -- $test\n");
-    return 1;
-  }
+	my $output = $input;
+	$output =~ s/\.cpp.*$/.out/;
+	$output =~ s/\.c.*$/.out/;
 
-  print LOG "Test '$name'\n";
-  print LOG "  Input: $input\n";
-  print LOG "  Output: $output\n";
-  print LOG "  Options: $options\n";
-  print LOG "  Results:\n";
-  foreach my $result (@results) {
-    print LOG "    $result\n";
-  }
-
-  my $failed = run($input, $options, $output);
-
-  if(!$failed) {
-    print LOG "Execution [OK]\n";
-    my $included = 1;
-    foreach my $result (@results) {
-      if($result eq "--") {
-	$included = !$included;
-      } else {
-	my $r;
-	system "grep '$result' '$output' >/dev/null";
-	$r = ($included ? $? != 0 : $? == 0);
-	if($r) {
-	  print LOG "$result [FAILED]\n";
-	  $failed = 1;
-	} else {
-	  print LOG "$result [OK]\n";
+	if($output eq $input) {
+		print("Error in test file -- $test\n");
+		return 1;
 	}
-      }
-    }
-  } else {
-    print LOG "Execution [FAILED]\n";
-  }
+	
+	# for XML file
+	#------------------------------
+	$tags{"item_01_test-name"} = $name;
+	$tags{"item_02_input-code"} = $input;
+	$tags{"item_03_output-code"} = $output;	
+	#------------------------------
 
-  print LOG "\n";
+	print LOG "Test '$name'\n";
+	print LOG "  Input: $input\n";
+	print LOG "  Output: $output\n";
+	print LOG "  Options: $options\n";
+	print LOG "  Results:\n";
+	
+	my $result;
+	foreach my $result (@results) {
+		print LOG "    $result\n";
+		# for XML file	
+		#------------------------------
+		if($result =~ m/SUCCESSFUL/){			
+			$tags{"item_09_expected-result"} = "[SUCCESSFUL]";
+		}else{			
+			$tags{"item_09_expected-result"} = "[FAILED]";
+		}		
+		#------------------------------
+		
+	}
+	
 
-  return $failed;
+	my $failed = run($input, $options, $output);
+
+	if(!$failed) {
+		# for XML file
+		#------------------------------
+		$tags{"item_05_execution-status"} = "[OKAY]";
+		#------------------------------
+		
+		print LOG "Execution [OK]\n";
+		my $included = 1;
+		foreach my $result (@results) {			
+			if($result eq "--") {
+				$included = !$included;
+			} else {
+				my $r;				
+				system "grep '$result' '$output' >/dev/null";
+				
+				# for XML file
+				#------------------------------
+				if($failed){
+					$tags{"item_10_actual-result"} = "[FAILED]";
+				}else{
+					$tags{"item_10_actual-result"} = "[SUCCESSFUL]";
+				}
+				#------------------------------					
+				
+				$r = ($included ? $? != 0 : $? == 0);				
+				if($r) {					
+					# expected result is NOT egual to actual result
+					print LOG "$result [FAILED]\n";					
+					$failed = 1;
+					
+					# for XML file	
+					#------------------------------
+					if(not($result =~ m/SUCCESSFUL/)){			
+						$tags{"item_10_actual-result"} = "[SUCCESSFUL]";
+					}else{			
+						$tags{"item_10_actual-result"} = "[FAILED]";
+					}		
+					#------------------------------					
+				} else {
+					print LOG "$result [OK]\n";
+					
+					# expected result is egual to actual result
+					# for XML file	
+					#------------------------------
+					if($result =~ m/SUCCESSFUL/){			
+						$tags{"item_10_actual-result"} = "[SUCCESSFUL]";
+					}else{			
+						$tags{"item_10_actual-result"} = "[FAILED]";
+					}		
+					#------------------------------					
+				}
+			}
+		}
+	} else {
+		# for XML file
+		#------------------------------
+		$tags{"item_05_execution-status"} = "[FAILED]";
+		#------------------------------
+		print LOG "Execution [FAILED]\n";
+	}
+
+	print LOG "\n";	
+	
+	# for XML -> generate all nodes on xml structure
+	#------------------------------------------------------
+	my $name_xml = "";
+	my $tag = "";
+	my $value = "";
+	
+	# Generate node sort
+	#$count_test_run = $count_test_run + 1;
+	#my $node_name = "run-test_ID_".$count_test_run;
+	
+	my $node_name = "run-test";
+	my $third_root = $doc->createElement( $node_name );
+	$root->addChild( $third_root );
+	
+	
+	for $name_xml (sort(keys %tags)) {
+		
+		$tag = $doc->createElement($name_xml);		
+		$value = $tags{$name_xml};
+		$tag->appendTextNode($value);
+		$third_root->appendChild($tag);		
+	}	
+	
+	#------------------------------------------------------
+	
+	return $failed;
 }
 
 sub dirs() {
-  my @list;
+	my @list;
 
-  opendir CWD, ".";
-  @list = grep { !/^\./ && -d "$_" && !/CVS/ } readdir CWD;
-  closedir CWD;
+	opendir CWD, ".";
+	@list = grep { !/^\./ && -d "$_" && !/CVS/ } readdir CWD;
+	closedir CWD;
 
-  @list = sort @list;
+	@list = sort @list;
 
-  return @list;
+	return @list;
 }
 
+#get date and time 
+sub get_date_time(){
+	my $time = time;    # or any other epoch timestamp
+	my $get_time_date = "";
+	my @months = ("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec");
+	my ($sec, $min, $hour, $day,$month,$year) = (localtime($time))[0,1,2,3,4,5];
+	# You can use 'gmtime' for GMT/UTC dates instead of 'localtime'
+	$get_time_date = $months[$month]." ".$day.", ".($year+1900)." ".$hour.":".$min.":".$sec;	
+	return $get_time_date;
+}
+# End functions
+# -------------------------------------------------------------------
+
+
+# Main
+# ------------------------------------------------------------------
 if(@ARGV != 0) {
-  if (@ARGV[0] eq "--llvm") {
+	if ($ARGV[0] eq "--llvm") {
 	 $llvm = 1;
-     $testdesc = "testllvm.desc";
-  } else {
-    print "Usage:\n";
-    print "  test.pl\n";
-    print "  test.pl --llvm\n";
-    exit 1;
-  }
+	 $testdesc = "testllvm.desc";
+	} else {
+	print "Usage:\n";
+	print "  test.pl\n";
+	print "  test.pl --llvm\n";
+	exit 1;
+	}
 } 
 
-open LOG,">tests.log";
 
+#Log run
+open LOG,">tests.log";
+#--------
+
+#Loading test cases
 print "Loading\n";
 my @tests = dirs();
 my $count = @tests;
@@ -142,22 +302,27 @@ if($count == 1) {
 } else {
   print "  $count tests found\n";
 }
+
 print "\n";
 my $failures = 0;
 print "Running tests\n";
-foreach my $test (@tests) {
-  print "  Running $test";
+foreach my $test (@tests) {		
+	
+	print "  Running $test";
 
-  chdir $test;
-  my $failed = test($test, $testdesc);
-  chdir "..";
+	chdir $test;
+					# PATH, file.desc
+	my $failed = test($test, $testdesc);
+	#
+	chdir "..";
 
-  if($failed) {
-    $failures++;
-    print "  [FAILED]\n";
-  } else {
-    print "  [OK]\n";
-  }
+	#count fails
+	if($failed) {
+		$failures++;
+		print "  [FAILED]\n";
+	} else {
+		print "  [OK]\n";
+	}
 }
 print "\n";
 
@@ -172,5 +337,17 @@ if($failures == 0) {
   }
 }
 
+#--------
 close LOG;
 
+#--------------------------------------------------
+# close xml file
+$doc->setDocumentElement($root);
+# Save the resulting XML file
+open(FILE_XML , ">test_log.xml") or die "Could not possible to write xml log file!";
+print FILE_XML $doc->toString(1);
+close FILE_XML;
+
+
+
+exit $failures;
