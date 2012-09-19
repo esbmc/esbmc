1107d3751c7872743535f6e05e438c45a0bc268d
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-09-19 11:33:10 -0400

    Update test case
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/esbmc-cpp/esbmc-try_catch/try-catch_39/main.cpp
+++ regression/esbmc-cpp/esbmc-try_catch/try-catch_39/main.cpp
@@ -2,13 +2,15 @@
 
 struct Base {};
 struct Base1 {};
-struct Derived : Base, Base1 {};
+struct Base2 {};
+struct Derived : Base2, Base, Base1  {};
 
 int main()
 {
   try {
     throw Derived();
   }
+  catch(Base2) {  }
   catch(Base1) {  }
   catch(Base) { assert(0); }
   return 0;
