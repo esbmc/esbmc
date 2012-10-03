b94a392e0970c65e65583d808c7857af21243504
Lucas Cordeiro <lucasccordeiro@gmail.com>
2012-10-03 13:05:07 -0400

    update the test case ch22_12
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc-cpp/esbmc-cpp/ch22_12/main.cpp
+++ regression/esbmc-cpp/esbmc-cpp/ch22_12/main.cpp
@@ -42,15 +42,23 @@ int main()
         a=1; b=1;
         assert((a xor b)==0);
 
+        a=2; b=3;
+        assert(a and_eq b);
+        a=0; b=0;
+        assert((a and_eq b)==0);
+
+        a=2; b=3;
+        assert(a or_eq b);
+        a=0; b=0;
+        assert((a or_eq b)==0);
+
+        a=2; b=3;
+        assert(a xor_eq b);
+        a=0; b=0;
+        assert((a xor_eq b)==0);
+
 #if 0
        << "\n     not a: " << ( not a )
-       << "\na not_eq b: " << ( a not_eq b )
-       << "\na bitand b: " << ( a bitand b )
-       << "\na bit_or b: " << ( a bitor b )
-       << "\n   a xor b: " << ( a xor b )
-       << "\n   compl a: " << ( compl a )
-       << "\na and_eq b: " << ( a and_eq b )
-       << "\n a or_eq b: " << ( a or_eq b )
        << "\na xor_eq b: " << ( a xor_eq b ) << endl;
 #endif	
    return 0;
