f92c58b7f0e42703bc7af157913f52dfba6e3b29
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2012-10-18 19:09:52 +0100

    Fix allocated_obj a little.
    Everything has to be public; also we have to store the symbol as an
    exprt because, although the lhs of an alloc is always a symbol, it
    doesn't come in as that type.

--- goto-symex/goto_symex.h
+++ goto-symex/goto_symex.h
@@ -55,10 +55,11 @@ public:
 public:
   /** Records for dynamically allocated blobs of memory. */
   class allocated_obj {
+  public:
     allocated_obj(const exprt &s, const guardt &g)
       : obj(s), alloc_guard(g) { }
     /** Symbol identifying the pointer that was allocated. Must have ptr type */
-    symbol_exprt obj;
+    exprt obj;
     /** Guard when allocation occured. */
     guardt alloc_guard;
   };
