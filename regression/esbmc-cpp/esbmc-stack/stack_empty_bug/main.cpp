6ffc382279c8ad514edb1ae24b8ca712490834bf
Hendrio M. Marques <hendriomm@gmail.com>
2012-10-22 19:10:22 -0400

    new test case suite: esbmc-stack
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-cpp/esbmc-stack/stack_empty_bug/main.cpp
@@ -0,0 +1,23 @@
+// stack::empty
+#include <iostream>
+#include <stack>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  stack<int> mystack;
+  int sum (0);
+
+  for (int i=1;i<=10;i++) mystack.push(i);
+
+  while (!mystack.empty())
+  {
+     sum += mystack.top();
+     mystack.pop();
+  }
+  assert(!mystack.empty());
+  cout << "total: " << sum << endl;
+  
+  return 0;
+}
