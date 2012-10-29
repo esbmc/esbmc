d738f05c3d43a93c6e0578aec16a823e91a950ed
Mauro de Freitas <maurokenny@gmail.com>
2012-10-29 16:23:16 -0400

    Code style changes
    Signed-off-by: Mauro de Freitas <maurokenny@gmail.com>

--- regression/esbmc-cpp/esbmc-inheritance/dynamic_cast_simple/main.cpp
+++ regression/esbmc-cpp/esbmc-inheritance/dynamic_cast_simple/main.cpp
@@ -17,9 +17,8 @@ class CRectangle: public CPolygon {
   public:
   CRectangle(int w, int h)
   {
-	width = w;
-	height = h;
-
+    width = w;
+    height = h;
   }
     int area (void)
       { return (width * height); }
@@ -29,25 +28,24 @@ class CTriangle: public CPolygon {
   public:
   CTriangle(int w, int h)
   {
-	width = w;
-	height = h;
-
+    width = w;
+    height = h;
   }
     int area (void)
       { return ((width * height) / 2); }
   };
 
 int main () {
-	CPolygon* polygons;
+  CPolygon* polygons;
 
-	polygons = new CRectangle(20,30);
+  polygons = new CRectangle(20,30);
 
-	CTriangle* trin = dynamic_cast <CTriangle *> (polygons);
-	if (trin != 0)
-	{
-		trin->set_values(10, 10);
-		assert(trin->area() == 300);
-	}
+  CTriangle* trin = dynamic_cast <CTriangle *> (polygons);
+  if (trin != 0)
+  {
+    trin->set_values(10, 10);
+    assert(trin->area() == 300);
+  }
 
-	return 0;
+  return 0;
 }
