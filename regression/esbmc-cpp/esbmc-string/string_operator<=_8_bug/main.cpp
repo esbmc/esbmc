2ccda3b2da80b658a7bb8b5aee95a37649980486
Hendrio M. Marques <hendriomm@gmail.com>
2012-11-16 17:59:53 -0400

    new alterations in some test cases: esbmc-string
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-cpp/esbmc-string/string_operator<=_8_bug/main.cpp
+++ regression/esbmc-cpp/esbmc-string/string_operator<=_8_bug/main.cpp
@@ -7,7 +7,7 @@ int main(){
   string str1, str2, str3, str4;
   str1 = string("AAAA");
   str2 = string(str1, 3);
-  str3 = string("AA", 6);
+  str3 = string("AA", 2);
   str4 = string('A',12);
-  assert( (str2 <= str1)&&(str3 < str1)&&(str4 <= str1) );
+  assert( (str1 <= str2)&&(str3 < str1)&&(str4 <= str1) );
 }
