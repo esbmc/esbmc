f9de3b42533d0279257a02ef31ec16be13b5f9a8
Hendrio M. Marques <hendriomm@gmail.com>
2012-11-16 13:59:55 -0400

    new test case suite: esbmc-map
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-cpp/esbmc-map/map_begin/main.cpp
@@ -0,0 +1,31 @@
+// map::begin/end
+#include <iostream>
+#include <map>
+#include <cassert>
+using namespace std;
+
+int main ()
+{
+  map<char,int> mymap;
+  map<char,int>::iterator it;
+
+  mymap['b'] = 100;
+  mymap['a'] = 200;
+  mymap['c'] = 300;
+  it = mymap.begin();
+ 
+  assert((*it).first == 'a');
+  assert((*it).second == 200);
+  it++;
+  assert((*it).first == 'b');
+  assert((*it).second == 100);
+  it++;
+  assert((*it).first == 'c');
+  assert((*it).second == 300);
+  
+  // show content:
+  for ( it=mymap.begin() ; it != mymap.end(); it++ )
+    cout << (*it).first << " => " << (*it).second << endl;
+
+  return 0;
+}
