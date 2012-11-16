6e755cc9ffe10b3053456ecca9bce40354b4867b
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-11-16 18:20:15 -0400

    New test case
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ regression/esbmc-cpp/esbmc-cpp/template_function_specialization/main.cpp
@@ -0,0 +1,22 @@
+
+template < class T >
+T teste(T value1, T value2)
+{
+   return value1;
+}
+
+template < >
+int teste(int value1, int value2)
+{
+   return value2;
+}
+
+int main()
+{
+  double y = teste(1.0, 2.0);
+  assert(y==1.0);
+
+  int x = teste(1,2);
+  assert(x==2);
+}
+
