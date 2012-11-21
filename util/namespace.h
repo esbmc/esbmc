613b9dfa03518ac0e0aed5f34403da158fd6e6ea
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2012-11-21 08:57:03 -0400

    Code style
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- util/namespace.h
+++ util/namespace.h
@@ -23,40 +23,40 @@ public:
       throw "identifier "+id2string(name)+" not found";
     return *symbol;
   }
-  
+
   const symbolt &lookup(const irept &irep) const
   {
     return lookup(irep.identifier());
   }
-  
+
   virtual ~namespacet()
   {
   }
-   
+
   virtual bool lookup(const irep_idt &name, const symbolt *&symbol) const;
   void follow_symbol(irept &irep) const;
   void follow_macros(exprt &expr) const;
-  
+
   const typet &follow(const typet &src) const;
-  
+
   namespacet(const contextt &_context)
   { context1=&_context; context2=NULL; }
-   
+
   namespacet(const contextt &_context1, const contextt &_context2)
   { context1=&_context1; context2=&_context2; }
-  
+
   namespacet(const contextt *_context1, const contextt *_context2)
   { context1=_context1; context2=_context2; }
-   
+
   unsigned get_max(const std::string &prefix) const;
-  
+
   const contextt &get_context() const
   {
     return *context1;
   }
-  
+
  protected:
   const contextt *context1, *context2;
 };
- 
+
 #endif
