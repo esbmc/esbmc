90e4a146febaaa70e319ea00e7cc4886d3a0edc6
Mauro de Freitas <maurokenny@gmail.com>
2012-11-26 14:47:35 -0400

    Updated script to create testllvm.desc from test.desc automatically
    Signed-off-by: Mauro de Freitas <maurokenny@gmail.com>

--- regression/esbmc-cpp/convert_llbmc.py
+++ regression/esbmc-cpp/convert_llbmc.py
@@ -1,73 +1,95 @@
 #!/bin/python
+#############################
+# Script to create new file testllvm.desc from test.desc
+# If the file does not exist, it will creat it
+##############################
 import os
 import xml.etree.ElementTree as ET
 import sys
+from copy import copy
 from sys import argv
-#from os.path import expanduser
-#home = expanduser("~")
-
 
 def show_info(path):
     #changing path
     os.chdir(path)
+    #printing path
     print "#############################################"
     print path
     print "#############################################"
-    tree = ET.parse("test.desc")
+    #try to open test.desc
+    try:
+   	tree = ET.parse("test.desc")
+    except IOError as e:
+        sys.exit("Could not open test.desc")
+
+    #XML file
     root = tree.getroot()
 
     print "---> TEST.DESC:"
     #getting the options from file test.desc
     opt = root.find("item_05_option_to_run_esbmc").text
     expected = root.find("item_06_expected_result");
-    print "---> " + opt
-    print "---> " + expected.text
     unw = opt.split()
-    print unw
+    #print unw
     pos = unw.index("--unwind") #unwind option
     if (pos+1) < len(unw):
        unw_value = unw[pos+1] #value
-       print unw_value
+       print "unwind: " + unw_value
+    else:
+       sys.exit("Parameter unwind error") 
 
-    #testllvm.desc
-    tree = ET.parse("testllvm.desc")
-    root = tree.getroot()
+    ########### testllvm.desc ###############
+    #try to open testllvm.desc
+    existsll = True
+    try:
+        tree = ET.parse("testllvm.desc")
+        root = tree.getroot()
+    except IOError as e:
+    	existsll = False
+    print "---> testllvm.desc:"
+    print "---> Checking testllvm.desc" 
+    llbmc_opt = root.find("item_05_option_to_run_esbmc")
+    if not existsll: #file does not exist
+        print "---> testllvm.desc does not exist, creating..." 
+        llbmc_file = root.find("item_04_file_C_to_test")
+        fileName, fileExtension = os.path.splitext(llbmc_file.text)
+        llbmc_file.text = fileName + ".bc"
+        
+        llbmc_opt.text = "--ignore-missing-function-bodies --max-loop-iterations= --no-max-loop-iterations-checks"
+    else:
+        #expected
+        expected_llbmc = root.find("item_06_expected_result");
+        expected_llbmc.text = expected.text
 
-    print "---> TEST.DESC:"
     #getting the options from file test.desc
-    llbmc_item = root.find("item_05_option_to_run_esbmc") 
-    llbmc_opt = llbmc_item.text
-    expected_llbmc = root.find("item_06_expected_result");
-    print "---> " + llbmc_opt
-    print "---> " + expected_llbmc.text
-    print llbmc_opt
-    llbmc_s = llbmc_opt.split()
+    #max-loop
+    llbmc_s = llbmc_opt.text.split()
     llbmc_s[1] = "--max-loop-iterations=" + str(unw_value)
     changed = " ".join(llbmc_s)
-    llbmc_item.text = changed
-
-    expected_llbmc.text = expected.text
-    tree.write('testllvm.desc',encoding="UTF-8")
-    
-    os.system("echo '' >> testllvm.desc")
+    llbmc_opt.text = changed
 
     
-
-
-   # raw_input("Press ENTER to next")
+    print "---> Writing testllvm.desc"
+    tree.write('testllvm.desc',encoding="utf-8",xml_declaration=True)
+    #adding new empty line    
+    print
+    
+    try:
+        open("testllvm.desc","a").write("\n")
+    except :
+        pass
+#    raw_input("Press ENTER to next")
 
 def main():
     path = argv[1];
-    print path    
     listing = os.listdir(path)
-    listing.sort()
-#    raw_input()
+    listing.sort() #sort files
+    os.chdir(path)
     for infile in listing:
-        if os.path.isdir(path+infile):
-          #print infile
-          show_info(path+infile)
-    
+        if os.path.isdir(infile):
+          show_info(infile)
+          os.chdir("..")
+          
 if __name__ == "__main__":
     main()
-
 
