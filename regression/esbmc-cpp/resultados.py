d81488180a8ea13a45cc8dfa8777d285666c4d05
Mauro de Freitas <maurokenny@gmail.com>
2012-12-05 16:39:38 -0400

    Changed script resultados.py to catch crash suite results
    Signed-off-by: Mauro de Freitas <maurokenny@gmail.com>

--- regression/esbmc-cpp/resultados.py
+++ regression/esbmc-cpp/resultados.py
@@ -21,6 +21,7 @@ fai   = 0
 fsuc  = 0
 fneg  = 0
 total = 0
+crash = 0
 
 def disp_resul():
     """
@@ -30,30 +31,40 @@ def disp_resul():
     print "Verification Fail: ", fai
     print "False positive:", fsuc
     print "False negative:", fneg 
+    print "Crashed:", crash
     print "Total: ", total 
 
 def resultados(ite_ex, ite_ac):
     """
     Check the result. Sucess, Fail, false sucess and false fail
     """
-    global suc, fai, fsuc, fneg, total
-    if ite_ex.text == ite_ac.text == "[SUCCESSFUL]":
-      suc+=1
-    elif ite_ex.text == ite_ac.text == "[FAILED]":
-      fai+=1
-    elif ite_ex.text == "[FAILED]":
+    global suc, fai, fsuc, fneg, total, crash
+    if ite_ac.text == "[ABORTED]":
+      crash+=1
+      #print "CRASH"
+    elif ite_ex.text == ite_ac.text:
+      if ite_ac.text == "[SUCCESSFUL]":
+        suc+=1
+        #print "SUCCESSFUL"
+      elif ite_ac.text in ["[FAILED]", "[CONVERSION_ERROR]", "[PARSING_ERROR]"]:
+        fai+=1
+        #print "FAILED"
+    elif ite_ex.text == "[FAILED]" and ite_ac.text == "[SUCCESSFUL]":
       fsuc+=1
+      #print "FALSO POSITIVO"
     else:
       fneg+=1
+      #print "FALSO NEGATIVO"
     
     total+=1
+    #print
 
 def show_info(path):
     """
     Parse tree
     """
-    global suc, fai, fsuc, fneg, total
-    suc = fai = fsuc = fneg = total = 0
+    global suc, fai, fsuc, fneg, total, crash
+    suc = fai = fsuc = fneg = total = crash = 0
     os.chdir(path)
     print "##### Directory: " + path
     try:
@@ -67,10 +78,11 @@ def show_info(path):
     for res in root.findall('run-test'):
       ite_ex = res.find('item_09_expected-result')
       ite_ac = res.find('item_10_actual-result')
+      #print res.find('item_01_test-name').text
+      #print ite_ex.text
+      #print ite_ac.text
       if ite_ex != None and ite_ac != None:
         resultados(ite_ex, ite_ac)
-        
-        
       else:
         f.write('Error file: ')
         f.write(res.find('item_01_test-name').text + '\n') 
