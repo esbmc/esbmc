8a6eb7a655814d91963a77c50c71282460e1c608
Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>
2012-12-19 16:11:31 -0400

    upgrading in the test case list_merge-2
    Signed-off-by: Felipe Rodrigues Monteiro Sousa <rms.felipe@gmail.com>

--- regression/esbmc-cpp/esbmc-list/list_merge-2/main.cpp
+++ regression/esbmc-cpp/esbmc-list/list_merge-2/main.cpp
@@ -12,34 +12,20 @@ bool mycomparison (double first, double second)
 int main ()
 {
   list<double> first, second;
-  double mydoubles[] = {1.4, 2.2, 2.9, 3.1, 3.7, 7.1};
-  double md2[] = {1.4, 2.2, 2.9, 2.1, 3.1, 3.7, 7.1};
-  list<double> merged; 
-  merged.assign(mydoubles,mydoubles+6);
+  double md2[] = {1.4, 2.2, 2.1, 3.1};
+  list<double> merged;
   
   first.push_back (3.1);
   first.push_back (2.2);
-  first.push_back (2.9);
 
-  second.push_back (3.7);
-  second.push_back (7.1);
   second.push_back (1.4);
 
   first.sort();
-  second.sort();
-
-  first.merge(second);
-  assert(first == merged);
   
   second.push_back (2.1);
-  merged.assign(md2,md2+7);
+  merged.assign(md2,md2+4);
   first.merge(second,mycomparison);
   assert(merged == first);
-
-  cout << "first contains:";
-  for (list<double>::iterator it=first.begin(); it!=first.end(); ++it)
-    cout << " " << *it;
-  cout << endl;
 
   return 0;
 }
