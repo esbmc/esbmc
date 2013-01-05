411644816e62f11af6d91b342242c424a68922bc
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-01-05 15:54:16 +0000

    Move another file to using new-irep-containers

--- goto-symex/dynamic_allocation.cpp
+++ goto-symex/dynamic_allocation.cpp
@@ -29,29 +29,27 @@ void goto_symext::default_replace_dynamic_allocation(expr2tc &expr)
     // replace with CPROVER_alloc[POINTER_OBJECT(...)]
     const valid_object2t &obj = to_valid_object2t(expr);
 
-    expr2tc obj_expr(new pointer_object2t(uint_type2(), obj.value));
+    pointer_object2tc obj_expr(uint_type2(), obj.value);
 
     exprt alloc_array=symbol_expr(ns.lookup(valid_ptr_arr_name));
     expr2tc alloc_arr_2;
     migrate_expr(alloc_array, alloc_arr_2);
 
-    expr2tc index_expr = expr2tc(new index2t(get_bool_type(),
-                                              alloc_arr_2, obj_expr));
+    index2tc index_expr(get_bool_type(), alloc_arr_2, obj_expr);
     expr = index_expr;
   }
   else if (is_invalid_pointer2t(expr))
   {
     const invalid_pointer2t &ptr = to_invalid_pointer2t(expr);
 
-    expr2tc obj_expr(new pointer_object2t(uint_type2(), ptr.ptr_obj));
+    pointer_object2tc obj_expr(uint_type2(), ptr.ptr_obj);
 
     exprt alloc_array=symbol_expr(ns.lookup(valid_ptr_arr_name));
     expr2tc alloc_arr_2;
     migrate_expr(alloc_array, alloc_arr_2);
 
-    expr2tc index_expr = expr2tc(new index2t(get_bool_type(),
-                                             alloc_arr_2, obj_expr));
-    expr2tc notindex = expr2tc(new not2t(index_expr));
+    index2tc index_expr(get_bool_type(), alloc_arr_2, obj_expr);
+    not2tc notindex(index_expr);
 
     // XXXjmorse - currently we don't correctly track the fact that stack
     // objects change validity as the program progresses, and the solver is
@@ -64,18 +62,18 @@ void goto_symext::default_replace_dynamic_allocation(expr2tc &expr)
     expr2tc sym_2;
     migrate_expr(sym, sym_2);
 
-    expr2tc ptr_obj = expr2tc(new pointer_object2t(int_type2(), ptr.ptr_obj));
-    expr2tc is_dyn = expr2tc(new index2t(get_bool_type(), sym_2, ptr_obj));
+    pointer_object2tc ptr_obj(int_type2(), ptr.ptr_obj);
+    index2tc is_dyn(get_bool_type(), sym_2, ptr_obj);
 
     // Catch free pointers: don't allow anything to be pointer object 1, the
     // invalid pointer.
     type2tc ptr_type = type2tc(new pointer_type2t(type2tc(new empty_type2t())));
-    expr2tc invalid_object = expr2tc(new symbol2t(ptr_type, "INVALID"));
-    expr2tc isinvalid = expr2tc(new equality2t(ptr.ptr_obj, invalid_object));
-    expr2tc notinvalid = expr2tc(new not2t(isinvalid));
+    symbol2tc invalid_object(ptr_type, "INVALID");
+    equality2tc isinvalid(ptr.ptr_obj, invalid_object);
+    not2tc notinvalid(isinvalid);
 
-    expr2tc is_not_bad_ptr = expr2tc(new and2t(notindex, is_dyn));
-    expr2tc is_valid_ptr = expr2tc(new or2t(is_not_bad_ptr, isinvalid));
+    and2tc is_not_bad_ptr(notindex, is_dyn);
+    or2tc is_valid_ptr(is_not_bad_ptr, isinvalid);
 
     expr = is_valid_ptr;
   }
@@ -84,14 +82,13 @@ void goto_symext::default_replace_dynamic_allocation(expr2tc &expr)
     // replace with CPROVER_alloc[POINTER_OBJECT(...)]
     const deallocated_obj2t &obj = to_deallocated_obj2t(expr);
 
-    expr2tc obj_expr = expr2tc(new pointer_object2t(uint_type2(), obj.value));
+    pointer_object2tc obj_expr(uint_type2(), obj.value);
 
     exprt alloc_array=symbol_expr(ns.lookup(deallocd_arr_name));
     expr2tc alloc_arr_2;
     migrate_expr(alloc_array, alloc_arr_2);
 
-    expr2tc index_expr = expr2tc(new index2t(get_bool_type(),
-                                             alloc_arr_2, obj_expr));
+    index2tc index_expr(get_bool_type(), alloc_arr_2, obj_expr);
     expr = index_expr;
   }
   else if (is_dynamic_size2t(expr))
@@ -100,14 +97,13 @@ void goto_symext::default_replace_dynamic_allocation(expr2tc &expr)
     //nec: ex37.c
     const dynamic_size2t &size = to_dynamic_size2t(expr);
 
-    expr2tc obj_expr = expr2tc(new pointer_object2t(int_type2(), size.value));
+    pointer_object2tc obj_expr(int_type2(), size.value);
 
     exprt alloc_array=symbol_expr(ns.lookup(alloc_size_arr_name));
     expr2tc alloc_arr_2;
     migrate_expr(alloc_array, alloc_arr_2);
 
-    expr2tc index_expr = expr2tc(new index2t(uint_type2(), alloc_arr_2,
-                                             obj_expr));
+    index2tc index_expr(uint_type2(), alloc_arr_2, obj_expr);
     expr = index_expr;
   }
 }
