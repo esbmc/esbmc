24c4dea0e6f4bf01c22a8a5d38dbe44e4e5616a4
Hendrio M. Marques <hendriomm@gmail.com>
2013-01-14 18:25:59 -0400

    test case alterations
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-cpp/esbmc-algorithm/algorithm107/main.cpp
+++ regression/esbmc-cpp/esbmc-algorithm/algorithm107/main.cpp
@@ -8,18 +8,19 @@ using namespace std;
 bool myfunction (int i,int j) { return (i<j); }
 
 int main () {
-  int myints[] = {1,2,3,4,5,4,3,2,1};
-  int myints1[] = {1,1,2,2,3,3,4,4,5};
-  vector<int> v(myints,myints+9);                         // 1 2 3 4 5 4 3 2 1
+  int myints[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+  int myints1[] = {1,2,3,4,5,6,7,8,9,10,11,12};
+  vector<int> v(myints,myints+11);                         // 1 2 3 4 5 4 3 2 1
   vector<int>::iterator it;
 
   // using default comparison:
-  v.assign(myints1,myints1+9);                      //  1 1 2 2 3 3 4 4 5
+  v.assign(myints1,myints1+11);                      //  1 1 2 2 3 3 4 4 5
 
   cout << "looking for a 3... ";
-  if (binary_search (v.begin(), v.end(), 3))
-    cout << "found!\n"; else cout << "not found.\n";
-
+  //if (binary_search (v.begin(), v.end(), 3))
+    //cout << "found!\n"; else cout << "not found.\n";
+  assert(!binary_search (v.begin(), v.end(), 3));
+/*
   // using myfunction as comp:
   sort (v.begin(), v.end(), myfunction);
   for(it = v.begin();it < v.end();it++)
@@ -29,6 +30,6 @@ int main () {
   cout << "looking for a 6... ";
   if (binary_search (v.begin(), v.end(), 6, myfunction))
     cout << "found!\n"; else cout << "not found.\n";
-
+*/
   return 0;
 }
