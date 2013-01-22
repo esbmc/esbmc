2f5c1708a9a2c7e11e87094609fb976d262958b0
Hendrio M. Marques <hendriomm@gmail.com>
2013-01-22 14:09:29 -0400

    test cases simplification (algorithm)
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-cpp/esbmc-algorithm/algorithm109/main.cpp
+++ regression/esbmc-cpp/esbmc-algorithm/algorithm109/main.cpp
@@ -7,15 +7,17 @@ using namespace std;
 
 int main () {
   int first[] = {5,10,15,20,25};
-  int second[] = {50,40,30,20,10};
-  vector<int> v(10);
+  int second[] = {10,20,30,40,50};
+  vector<int> v;
   vector<int>::iterator it;
+  v.assign(first,first+5);
+  v.insert(v.end(),second, second+5);
 
-  sort (first,first+5);
-  sort (second,second+5);
+//  sort (first,first+5);
+//  sort (second,second+5);
 
-  copy (first,first+5,v.begin());
-  copy (second,second+5,v.begin()+5);
+//  copy (first,first+5,v.begin());
+//  copy (second,second+5,v.begin()+5);
 
   inplace_merge (v.begin(),v.begin()+5,v.end());
   assert(v[6] != 25);
