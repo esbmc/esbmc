83a0add3f7a2957e839f491fb384c5d50789da97
Hendrio M. Marques <hendriomm@gmail.com>
2013-01-22 17:38:24 -0400

    new case tests created from older ones in esbmc-algorithm directory
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

---
+++ regression/esbmc-cpp/esbmc-algorithm/algorithm86_fill/main.cpp
@@ -0,0 +1,24 @@
+// fill algorithm example
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <cassert>
+using namespace std;
+
+int main () {
+  vector<int> myvector (8);                       // myvector: 0 0 0 0 0 0 0 0
+
+//  fill (myvector.begin(),myvector.begin()+4,5);   // myvector: 5 5 5 5 0 0 0 0
+//  assert(myvector[3] != 5);
+  
+  fill (myvector.begin()+3,myvector.end()-2,8);   // myvector: 5 5 5 8 8 8 0 0
+
+  assert(myvector[3] != 8);
+  cout << "myvector contains:";
+  for (vector<int>::iterator it=myvector.begin(); it!=myvector.end(); ++it)
+    cout << " " << *it;
+
+  cout << endl;
+ 
+  return 0;
+}
