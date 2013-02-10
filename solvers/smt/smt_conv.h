f67026abb70e126086e88ccb7953f9534ef21b7a
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-02-10 22:51:46 +0000

    Add some pointless nonterminal AST nodes.

--- solvers/smt/smt_conv.h
+++ solvers/smt/smt_conv.h
@@ -20,6 +20,13 @@ enum smt_func_kind {
   SMT_FUNC_SYMBOL,
 
   // Nonterminals
+  SMT_FUNC_ADD,
+  SMT_FUNC_BVADD,
+  SMT_FUNC_SUB,
+  SMT_FUNC_BVSUB,
+  SMT_FUNC_MUL,
+  SMT_FUNC_DIV,
+  SMT_FUNC_BVDIV,
 };
 
 class smt_ast {
