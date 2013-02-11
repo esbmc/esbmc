10765e0d84119052eeb5b6cb5bddc1b7c9f03900
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-02-11 04:05:14 +0000

    Slightly simplify Z3 func conversion.
    Thinking about it, seeing how the z3++ layer takes care of splitting
    between bv and int operations here, we can just fall through for a
    couple of labels.

--- solvers/z3/z3_conv.cpp
+++ solvers/z3/z3_conv.cpp
@@ -2967,15 +2967,12 @@ z3_convt::mk_func_app(const smt_sort *s __attribute__((unused)), smt_func_kind k
   // the solver, then that's absolutely fine.
   switch (k) {
   case SMT_FUNC_ADD:
-    return new z3_smt_ast(mk_add(asts[0]->e, asts[1]->e), temp);
   case SMT_FUNC_BVADD:
     return new z3_smt_ast(mk_add(asts[0]->e, asts[1]->e), temp);
   case SMT_FUNC_SUB:
-    return new z3_smt_ast(mk_sub(asts[0]->e, asts[1]->e), temp);
   case SMT_FUNC_BVSUB:
     return new z3_smt_ast(mk_sub(asts[0]->e, asts[1]->e), temp);
   case SMT_FUNC_MUL:
-    return new z3_smt_ast((asts[0]->e * asts[1]->e), temp);
   case SMT_FUNC_BVMUL:
     return new z3_smt_ast((asts[0]->e * asts[1]->e), temp);
   case SMT_FUNC_HACKS:
