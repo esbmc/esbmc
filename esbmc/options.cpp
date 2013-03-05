a32eabdc851be3edb17ccdade670b1122279e133
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-03-05 02:22:25 +0000

    Actually, smtlib solver prog is a string option.
    Not a switch.

--- esbmc/options.cpp
+++ esbmc/options.cpp
@@ -119,7 +119,7 @@ const struct opt_templ esbmc_options[] = {
 { 0,	"smt-thread-guard",	switc,		""	},
 { 0,	"smt-symex-guard",	switc,		""	},
 { 0,	"symex-ssa-trace",	switc,		""	},
-{ 0,    "smtlib-solver-prog",   switc,          ""      },
+{ 0,    "smtlib-solver-prog",   string,         ""      },
 { 0,    "smtlib",               switc,          ""      },
 { '?',	"",			switc,		""	},
 { 'h',	"",			switc,		""	},
