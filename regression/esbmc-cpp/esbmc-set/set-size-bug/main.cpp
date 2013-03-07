dec3044babbab8efb577a4eb64b44817e92e4c21
Hendrio M. Marques <hendriomm@gmail.com>
2013-03-07 16:21:35 -0400

    changing some case tests in esbmc-set directory
    Signed-off-by: Hendrio M. Marques <hendriomm@gmail.com>

--- regression/esbmc-cpp/esbmc-set/set-size-bug/main.cpp
+++ regression/esbmc-cpp/esbmc-set/set-size-bug/main.cpp
@@ -5,17 +5,11 @@ using namespace std;
 
 int main ()
 {
-  set<int> myints;
-  cout << "0. size: " << (int) myints.size() << endl;
-  assert(myints.size() == 0);
-  for (int i=0; i<10; i++) myints.insert(i);
-  cout << "1. size: " << (int) myints.size() << endl;
-  assert(myints.size() == 10);
-  myints.insert (100);
-  cout << "2. size: " << (int) myints.size() << endl;
-  assert(myints.size() == 11);
-  myints.erase(5);
-  cout << "3. size: " << (int) myints.size() << endl;
-  assert(myints.size() > 10);
+  int b[10] = {0,1,2,3,4,5,6,7,8,9};
+  set<int> myints(b,b+10);
+  assert(myints.size() != 10);
+  
+  
+
   return 0;
 }
