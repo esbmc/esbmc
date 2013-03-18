05b88d031f27dbcc5b5e0b257b40e73f1b15e170
Lucas Cordeiro <lucasccordeiro@gmail.com>
2013-03-18 00:06:43 -0400

    update test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/digital_filter_03/main.c
+++ regression/esbmc/digital_filter_03/main.c
@@ -9,9 +9,7 @@ float max = 1.0f; float min = -1.0f;
 int xsize = 6;
 
 int main(void) {
-//  overflow(a,b,k,l, Na, Nb, max, min, xsize);
-//  limitCycle(a,b,k,l, Na, Nb, max, min, xsize);
-  timing(a,b,k,l, Na, Nb, max, min, xsize);
+  check_filter_timing(a,b,k,l, Na, Nb, max, min, xsize);
   return 0;
 }
 
