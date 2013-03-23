29f367cb8689c8e176963f2149ee316b47e7f0f4
Lucas Cordeiro <lucasccordeiro@gmail.com>
2013-03-23 12:48:08 -0400

    update the test case count_down_02
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/k-induction/count_down_02/main.c
+++ regression/k-induction/count_down_02/main.c
@@ -5,7 +5,7 @@ int main() {
   int x=nondet_int();
   __ESBMC_assume(x>0);
   while(x>0) {
-//    __ESBMC_assume(fail!=1);
+    __ESBMC_assume(fail!=1);
     if(fail==1) {
       x= -1;
 //      fail=2;
