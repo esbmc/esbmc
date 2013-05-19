1037bf9f84cfe4395889f7360dd8e0b56f37b8c2
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-05-19 18:20:15 +0100

    Fix a massive fixedbv-comparison fail.
    At the time when I produced some code that was implementing relations, I
    figured that integers would be the only things that made their way past.
    So to detect signed comparisons, I checked for signedbv types.
    
    Of course, fixedbvs / other non-integer types are signed too. So fix
    this to model that.

--- solvers/z3/z3_conv.cpp
+++ solvers/z3/z3_conv.cpp
@@ -1057,7 +1057,8 @@ z3_convt::convert_rel(const expr2tc &side1, const expr2tc &side2,
     convert_bv(cast, args[1]);
   }
 
-  output = convert(args[0], args[1], !is_signedbv_type(side1));
+  bool is_unsigned = is_unsignedbv_type(side1);
+  output = convert(args[0], args[1], is_unsigned);
 }
 
 void
