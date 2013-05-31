808bf2b5edb6b0d667bc8523890969fb22888cd1
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-05-31 14:17:46 +0100

    Fix value-set assigning superclass to subclass.
    That is, when we assign the value-sets of the members of a superclass to
    a subclass, this is sort-of-valid (i.e., no-ones proved it's incorrect
    to me yet). However if we iterate over the subclasses fields, we end up
    extracting nonexistant members out of the superclass, which is bad.

--- pointer-analysis/value_set.cpp
+++ pointer-analysis/value_set.cpp
@@ -632,11 +632,15 @@ void value_sett::assign(
   
   if (is_struct_type(lhs_type) || is_union_type(lhs_type))
   {
-    const std::vector<type2tc> &members = (is_struct_type(lhs_type))
-      ? to_struct_type(lhs_type).members : to_union_type(lhs_type).members;
-    const std::vector<irep_idt> &member_names = (is_struct_type(lhs_type))
-      ? to_struct_type(lhs_type).member_names
-      : to_union_type(lhs_type).member_names;
+    // Assign the values of all members of the rhs thing to the lhs. It's
+    // sort-of-valid for the right hand side to be a superclass of the subclass,
+    // in which case there are some fields not common between them, so we
+    // iterate over the superclasses members.
+    const std::vector<type2tc> &members = (is_struct_type(rhs->type))
+      ? to_struct_type(rhs->type).members : to_union_type(rhs->type).members;
+    const std::vector<irep_idt> &member_names = (is_struct_type(rhs->type))
+      ? to_struct_type(rhs->type).member_names
+      : to_union_type(rhs->type).member_names;
     
     unsigned int i = 0;
     for (std::vector<type2tc>::const_iterator c_it = members.begin();
