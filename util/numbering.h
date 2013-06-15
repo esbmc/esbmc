ced139bf393e5b88418708e1b948865b5c350ae2
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-06-15 05:17:59 +0100

    Implement erase methods for object numberers.

--- util/numbering.h
+++ util/numbering.h
@@ -21,9 +21,14 @@ template <typename T>
 class numbering
 {
 public:
+  numbering()
+  {
+    next_obj_num = 0;
+  }
+
   unsigned number(const T &a)
   {
-    unsigned int num = numbers.size();
+    unsigned int num = next_obj_num++;
     std::pair<typename numberst::const_iterator, bool> result=
       numbers.insert(
       std::pair<T, unsigned>
@@ -60,20 +65,37 @@ public:
     return vec.size();
   }
 
+  void erase(unsigned int num)
+  {
+    // Precondition: this number actually exists.
+    assert(vec.find(num) != vec.end());
+
+    const T &ref = vec[num];
+    numbers.erase(ref);
+    vec.erase(num);
+    return;
+  }
+
 protected:
   typedef std::map<T, unsigned> numberst;
   typedef hash_map_cont<unsigned, T, std::hash<unsigned> > vectort;
   numberst numbers;  
   vectort vec;
+  unsigned int next_obj_num;
 };
 
 template <typename T, class hash_fkt>
 class hash_numbering
 {
 public:
+  hash_numbering()
+  {
+    next_obj_num = 0;
+  }
+
   unsigned number(const T &a)
   {
-    unsigned int num = numbers.size();
+    unsigned int num = next_obj_num++;
     std::pair<typename numberst::const_iterator, bool> result=
       numbers.insert(
       std::pair<T, unsigned>
@@ -115,11 +137,20 @@ public:
     return vec[i];
   }
 
+  void erase(unsigned int num)
+  {
+    assert(vec.find(num) != vec.end());
+    const T &ref = vec[num];
+    numbers.erase(ref);
+    vec.erase(num);
+  }
+
 protected:
   typedef hash_map_cont<T, unsigned, hash_fkt> numberst;
   typedef hash_map_cont<unsigned, T, std::hash<unsigned> > vectort;
   numberst numbers;  
   vectort vec;
+  unsigned int next_obj_num;
 };
 
 #endif
