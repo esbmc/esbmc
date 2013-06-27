748a572717874ec2f547653d9d9cb7d93b18e2d6
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2013-06-27 11:38:02 -0400

    Classes for each kinduction's step
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- esbmc/kinduction_parallel.cpp
+++ esbmc/kinduction_parallel.cpp
@@ -8,12 +8,45 @@
 #include "kinduction_parallel.h"
 
 kinduction_thread::kinduction_thread()
- : Thread()
+  : Thread(),
+    k(0)
+{ }
+
+/* Base case class implementation */
+
+base_case::base_case()
+  : kinduction_thread()
 {
 
 }
 
-void kinduction_thread::run()
+void base_case::run()
+{
+
+}
+
+/* Forward condition class implementation */
+
+forward_condition::forward_condition()
+  : kinduction_thread()
+{
+
+}
+
+void forward_condition::run()
+{
+
+}
+
+/* Inductive step class implementation */
+
+inductive_step::inductive_step()
+  : kinduction_thread()
+{
+
+}
+
+void inductive_step::run()
 {
 
 }
--- esbmc/kinduction_parallel.h
+++ esbmc/kinduction_parallel.h
@@ -10,11 +10,34 @@
 
 #include "thread.h"
 
+// This will be the base class for k induction threads
 class kinduction_thread : public Thread
 {
   public:
     kinduction_thread();
 
+  protected:
+    unsigned int k;
+};
+
+class base_case : public kinduction_thread
+{
+  public:
+    base_case();
+    virtual void run();
+};
+
+class forward_condition : public kinduction_thread
+{
+  public:
+    forward_condition();
+    virtual void run();
+};
+
+class inductive_step : public kinduction_thread
+{
+  public:
+    inductive_step();
     virtual void run();
 };
 
