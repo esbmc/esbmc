4c05ce132a8cff0e8297143b9b7753765c5b9702
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-07-06 23:57:51 +0100

    Instigate AND.

--- solvers/minisat/minisat_conv.cpp
+++ solvers/minisat/minisat_conv.cpp
@@ -711,6 +711,12 @@ minisat_convt::mk_func_app(const smt_sort *ressort __attribute__((unused)),
                                    args[2]->bv[i]));
     break;
   }
+  case SMT_FUNC_AND:
+  {
+    result = new minisat_smt_ast(ressort);
+    result->bv.push_back(land(args[0]->bv[0], args[1]->bv[0]));
+    break;
+  }
   case SMT_FUNC_BVADD:
   {
     literalt carry_in = const_literal(false);
