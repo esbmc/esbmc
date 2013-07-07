8684e43da7b5714a48f8851464546f3ec425d758
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-07-07 08:59:18 +0100

    Implement fixedbv fetching properly.

--- solvers/minisat/minisat_conv.cpp
+++ solvers/minisat/minisat_conv.cpp
@@ -696,9 +696,21 @@ minisat_convt::get(const expr2tc &expr)
   }
   case type2t::unsignedbv_id:
   case type2t::signedbv_id:
-  case type2t::fixedbv_id:
   {
     return get_bv(expr->type, value);
+  }
+  case type2t::fixedbv_id:
+  {
+    expr2tc tmp = get_bv(expr->type, value);
+    const constant_int2t &intval = to_constant_int2t(tmp);
+    uint64_t val = intval.constant_value.to_ulong();
+    std::stringstream ss;
+    ss << val;
+    constant_exprt value_expr(migrate_type_back(expr->type));
+    value_expr.set_value(get_fixed_point(expr->type->get_width(), ss.str()));
+    fixedbvt fbv;
+    fbv.from_expr(value_expr);
+    return constant_fixedbv2tc(expr->type, fbv);
   }
   case type2t::array_id:
   {
