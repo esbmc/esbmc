0905ae398943c6bb678ecc4eb2d652d20a7c4816
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-07-16 14:13:27 +0100

    Implement mathsat BV concatenation

--- solvers/mathsat/mathsat_conv.cpp
+++ solvers/mathsat/mathsat_conv.cpp
@@ -90,6 +90,9 @@ mathsat_convt::mk_func_app(const smt_sort *s, smt_func_kind k,
   case SMT_FUNC_AND:
     r = msat_make_and(env, args[0]->t, args[1]->t);
     break;
+  case SMT_FUNC_CONCAT:
+    r = msat_make_bv_concat(env, args[0]->t, args[1]->t);
+    break;
   case SMT_FUNC_BVADD:
     r = msat_make_bv_plus(env, args[0]->t, args[1]->t);
     break;
