3d4a3cdc11bd5547c5b1b33bcd49adf55031fefb
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-07-17 20:49:36 +0100

    CVC functions boilerplate.

--- solvers/cvc/cvc_conv.cpp
+++ solvers/cvc/cvc_conv.cpp
@@ -1,5 +1,11 @@
 #include "cvc_conv.h"
 
+prop_convt *
+create_new_cvc_solver(bool int_encoding, bool is_cpp, const namespacet &ns)
+{
+    return new cvc_convt(is_cpp, int_encoding, ns);
+}
+
 cvc_convt::cvc_convt(bool is_cpp, bool int_encoding, const namespacet &ns)
    : smt_convt(true, int_encoding, ns, is_cpp, false, true, false)
 {
@@ -7,6 +13,106 @@ cvc_convt::cvc_convt(bool is_cpp, bool int_encoding, const namespacet &ns)
 }
 
 cvc_convt::~cvc_convt()
+{
+  abort();
+}
+
+prop_convt::resultt
+cvc_convt::dec_solve()
+{
+  abort();
+}
+
+expr2tc
+cvc_convt::get(const expr2tc &expr __attribute__((unused)))
+{
+  abort();
+}
+
+tvt
+cvc_convt::l_get(literalt l __attribute__((unused)))
+{
+  abort();
+}
+
+const std::string
+cvc_convt::solver_text()
+{
+  abort();
+}
+
+void
+cvc_convt::assert_lit(const literalt &l __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_func_app(const smt_sort *s __attribute__((unused)), smt_func_kind k __attribute__((unused)),
+                             const smt_ast * const *args __attribute__((unused)),
+                             unsigned int numargs __attribute__((unused)))
+{
+  abort();
+}
+
+smt_sort *
+cvc_convt::mk_sort(const smt_sort_kind k __attribute__((unused)), ...)
+{
+  abort();
+}
+
+literalt
+cvc_convt::mk_lit(const smt_ast *s __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_smt_int(const mp_integer &theint __attribute__((unused)), bool sign __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_smt_real(const std::string &str __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_smt_bvint(const mp_integer &theint __attribute__((unused)), bool sign __attribute__((unused)),
+                              unsigned int w __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_smt_bool(bool val __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_smt_symbol(const std::string &name __attribute__((unused)), const smt_sort *s __attribute__((unused)))
+{
+  abort();
+}
+
+smt_sort *
+cvc_convt::mk_struct_sort(const type2tc &type __attribute__((unused)))
+{
+  abort();
+}
+
+smt_sort *
+cvc_convt::mk_union_sort(const type2tc &type __attribute__((unused)))
+{
+  abort();
+}
+
+smt_ast *
+cvc_convt::mk_extract(const smt_ast *a __attribute__((unused)), unsigned int high __attribute__((unused)),
+                            unsigned int low __attribute__((unused)), const smt_sort *s __attribute__((unused)))
 {
   abort();
 }
--- solvers/cvc/cvc_conv.h
+++ solvers/cvc/cvc_conv.h
@@ -8,6 +8,29 @@ class cvc_convt : public smt_convt
 public:
   cvc_convt(bool is_cpp, bool int_encoding, const namespacet &ns);
   ~cvc_convt();
+
+  virtual resultt dec_solve();
+  virtual expr2tc get(const expr2tc &expr);
+  virtual tvt l_get(literalt l);
+  virtual const std::string solver_text();
+
+  virtual void assert_lit(const literalt &l);
+
+  virtual smt_ast *mk_func_app(const smt_sort *s, smt_func_kind k,
+                               const smt_ast * const *args,
+                               unsigned int numargs);
+  virtual smt_sort *mk_sort(const smt_sort_kind k, ...);
+  virtual literalt mk_lit(const smt_ast *s);
+  virtual smt_ast *mk_smt_int(const mp_integer &theint, bool sign);
+  virtual smt_ast *mk_smt_real(const std::string &str);
+  virtual smt_ast *mk_smt_bvint(const mp_integer &theint, bool sign,
+                                unsigned int w);
+  virtual smt_ast *mk_smt_bool(bool val);
+  virtual smt_ast *mk_smt_symbol(const std::string &name, const smt_sort *s);
+  virtual smt_sort *mk_struct_sort(const type2tc &type);
+  virtual smt_sort *mk_union_sort(const type2tc &type);
+  virtual smt_ast *mk_extract(const smt_ast *a, unsigned int high,
+                              unsigned int low, const smt_sort *s);
 };
 
 #endif /* _ESBMC_SOLVERS_CVC_CVC_CONV_H_ */
