67968f417e465064676399a23838fec634542bdd
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-07-27 17:05:15 +0100

    Remember to pump data to stdout

--- ansi-c/headers/flail.sh
+++ ansi-c/headers/flail.sh
@@ -2,8 +2,7 @@
 
 symname=$1
 
-thefile=`mktemp /tmp/ESBMCbuildXXXXXX`
-echo "char $1 [] = {" >> $thefile
-cat $2 | od -v -t d1 stddef.h | sed -E 's_^[0-9]+( +)?__' | sed -E 's_ +_ _g' | sed 's_ _,_g' | sed -E 's_([0-9])$_\1,_g' >> $thefile
-echo "};" >> $thefile
-mv $thefile $3
+echo "char $1 [] = {"
+cat $2 | od -v -t d1 stddef.h | sed -E 's_^[0-9]+( +)?__' | sed -E 's_ +_ _g' | sed 's_ _,_g' | sed -E 's_([0-9])$_\1,_g'
+echo "};"
+echo "unsigned int $1_size = sizeof($1);"
