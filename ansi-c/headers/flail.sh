4b490aeb57f155e6ca6b22e2c320890bef2548e7
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-08-14 17:22:47 +0100

    Fix the C library -> object file conversion.
    The purpose of this code is to convert a binary file into an object
    file with a symbol that we can use to read it in-process. That means
    pumping it through 'od', mashing the output, and compiling it.
    However, it turns out that the format string chosen before ('d1')
    prints negative signs ('-') occasionally, as it's printing signed
    characters. That led to some format muckups, which then exploded and
    led to the C library not coming out as what went in.
    
    So fix that by printing characters as unsigned chars.

--- ansi-c/headers/flail.sh
+++ ansi-c/headers/flail.sh
@@ -3,6 +3,6 @@
 symname=$1
 
 echo "char $1 [] = {"
-cat $2 | od -v -t d1 $2 | sed -E 's_^[0-9]+( +)?__' | sed -E 's_ +_ _g' | sed 's_ _,_g' | sed -E 's_([0-9])$_\1,_g'
+cat $2 | od -v -t u1 $2 | sed -E 's_^[0-9]+( +)?__' | sed -E 's_ +_ _g' | sed 's_ _,_g' | sed -E 's_([0-9])$_\1,_g'
 echo "};"
 echo "unsigned int $1_size = sizeof($1);"
