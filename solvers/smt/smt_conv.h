f0c640c1069b46c9b69ddde28df1bbeb645c269e
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-09-09 13:14:52 +0100

    Detect tuple array asts through all dimensions.

--- solvers/smt/smt_conv.h
+++ solvers/smt/smt_conv.h
@@ -151,7 +151,19 @@ public:
 };
 
 #define is_tuple_ast_type(x) (is_structure_type(x) || is_pointer_type(x))
-#define is_tuple_array_ast_type(x) (is_array_type(x) && (is_structure_type(to_array_type(x).subtype) || is_pointer_type(to_array_type(x).subtype)))
+
+inline bool is_tuple_array_ast_type(const type2tc &t)
+{
+  if (!is_array_type(t))
+    return false;
+
+  const array_type2t &arr_type = to_array_type(t);
+  type2tc range = arr_type.subtype;
+  while (is_array_type(range))
+    range = to_array_type(range).subtype;
+
+  return is_tuple_ast_type(range);
+}
 
 class smt_ast {
   // Mostly opaque class for storing whatever data the backend solver feels
