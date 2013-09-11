8e6d27ec9688e169b980e2790c86d76aa2dae5c5
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2013-09-11 17:14:33 +0100

    Multiple inclusion protection for migrate.h

--- util/migrate.h
+++ util/migrate.h
@@ -1,3 +1,6 @@
+#ifndef _ESBMC_UTIL_MIGRATE_H_
+#define _ESBMC_UTIL_MIGRATE_H_
+
 #include "irep2.h"
 
 #include "std_expr.h"
@@ -12,3 +15,5 @@ void migrate_type(const typet &type, type2tc &new_type);
 void migrate_expr(const exprt &expr, expr2tc &new_expr);
 typet migrate_type_back(const type2tc &ref);
 exprt migrate_expr_back(const expr2tc &ref);
+
+#endif /* _ESBMC_UTIL_MIGRATE_H_ */
