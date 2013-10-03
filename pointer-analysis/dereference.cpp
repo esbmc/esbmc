2477904f0d215c1b11fc2622e98b4d49d61e9222
Jeremy Morse <jmorse+git@studentrobotics.org>
2013-10-03 19:18:30 +0100

    Pointer-sized bv's are not compatible with ptrs.
    Apparently.

--- pointer-analysis/dereference.cpp
+++ pointer-analysis/dereference.cpp
@@ -940,7 +940,7 @@ dereferencet::construct_from_dyn_offset(expr2tc &value, const expr2tc &offset,
 
       // That's going to come out as a bitvector;
       if (type != accuml->type) {
-        assert(base_type_eq(type, accuml->type, ns));
+        assert(base_type_eq(type, accuml->type, ns) || is_pointer_type(type));
         accuml = typecast2tc(type, accuml);
       }
 
