a1613e47a58549043f49e626748d7be23dc0a3a4
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2013-10-10 11:49:38 -0400

    Test cases from denis
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ regression/esbmc-cpp/Denis-template-tests/call1/main.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+//Origin: harinath@cs.umn.edu
+//PR c++/10804
+// G++ was not emiting the function foo.
+
+// { dg-do run }
+
+
+template<class T>
+struct A
+{
+  A() { const void (*a)() = foo; }
+  static const void foo() {}
+};
+int main(int argc, char *argv[])
+{
+  A<int> a;
+}
