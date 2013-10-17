068fbea2d038827303ea601219c37f664e940b9f
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2013-10-17 09:11:52 -0400

    Cleanup
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- esbmc/kinduction_parallel.h
+++ esbmc/kinduction_parallel.h
@@ -8,12 +8,22 @@
 #ifndef K_INDUCTION_PARALLEL_H_
 #define K_INDUCTION_PARALLEL_H_
 
-#include "thread.h"
+#include <signal.h>
+#include <sys/wait.h>
 
 #include <goto-programs/goto_functions.h>
+
 #include "bmc.h"
 
 const unsigned int MAX_STEPS=50;
+
+enum STEP { BASE_CASE, FORWARD_CONDITION, INDUCTIVE_STEP };
+
+struct resultt
+{
+  STEP step;
+  short result;
+};
 
 class base_caset
 {
@@ -57,31 +67,4 @@ class inductive_stept
     goto_functionst &_goto_functions;
 };
 
-class safe_queues
-{
-  public:
-    static safe_queues *get_instance();
-
-    enum STEP { BASE_CASE, FORWARD_CONDITION, INDUCTIVE_STEP };
-
-    void update_queue(STEP s, unsigned int k, int res);
-    void update_finished(STEP s, bool finished);
-
-  private:
-    safe_queues();
-    ~safe_queues();
-
-    short bc_queue[MAX_STEPS];
-    short fc_queue[MAX_STEPS];
-    short is_queue[MAX_STEPS];
-
-    pthread_mutex_t _mutex;
-
-    static safe_queues* instance;
-
-    bool bc_finished, fc_finished, is_finished;
-
-    safe_queues(safe_queues const &);
-    void operator=(safe_queues const &);
-};
 #endif /* K_INDUCTION_PARALLEL_H_ */
