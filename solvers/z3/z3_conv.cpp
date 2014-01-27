7bfbcf21fa3574fe968f0d2d2dcae7c6af3c9c76
Jeremy Morse <jcmm106@ecs.soton.ac.uk>
2014-01-27 15:51:54 +0000

    Fix a misinterpretation of byte extract offsets.
    Bits != bytes.

--- solvers/z3/z3_conv.cpp
+++ solvers/z3/z3_conv.cpp
@@ -1680,6 +1680,7 @@ z3_convt::convert_smt_expr(const byte_extract2t &data, void *_bv)
     z3::expr src, o;
     convert_bv(source, src);
     convert_bv(offs, o);
+    o = o * ctx.esbmc_int_val(8);
     output = z3::to_expr(ctx, Z3_mk_bvlshr(ctx, src, o));
     output = z3::to_expr(ctx, Z3_mk_extract(z3_ctx, 7, 0, output));
     return;
