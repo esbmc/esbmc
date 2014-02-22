f261d836015cb82b79d40d0c1426111b9ea28ba5
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2014-02-22 14:51:04 -0400

    Changed script to read cpp files
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/k-induction/test.pl
+++ regression/k-induction/test.pl
@@ -65,7 +65,7 @@ sub test($$) {
   my ($input, $options, @results) = load($test);
 
   my $output = $input;
-  $output =~ s/\.c$/.out/;
+  $output =~ s/\.cpp$/.out/;
 
   if($output eq $input) {
     print("Error in test file -- $test\n");
