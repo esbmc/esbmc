2c0e8769521a7a7460d1eb6f8c6d6112d1326753
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-02-28 14:20:14 +0000

    Fix a minor brain fail.
    A line I deleted here had a side effect (the iterator being incremented)
    that I missed. Correctly increment the iterator to point at the 2nd
    element in the solver response.

--- solvers/smtlib/smtlib_conv.cpp
+++ solvers/smtlib/smtlib_conv.cpp
@@ -452,6 +452,7 @@ smtlib_convt::get_array_elem (const smt_ast *array, uint64_t index,
   assert(response.sexpr_list.size() == 2 && "Expected 2 operands in "
          "valuation_pair_list from smtlib solver");
   std::list<sexpr>::iterator it = response.sexpr_list.begin();
+  it++; // Echo of what we selected
   sexpr &respval = *it++;
 
   // Attempt to read an integer.
