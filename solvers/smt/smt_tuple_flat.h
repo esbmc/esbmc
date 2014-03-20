85c7717655f092091a363ee04912bdd50c672d66
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-03-20 13:07:20 +0000

    Duplicate class decl for tuple flatteners

--- solvers/smt/smt_tuple_flat.h
+++ solvers/smt/smt_tuple_flat.h
@@ -193,3 +193,30 @@ public:
   virtual smt_astt select(smt_convt *ctx, const expr2tc &idx) const;
   virtual smt_astt project(smt_convt *ctx, unsigned int elem) const;
 };
+
+class smt_tuple_sym_flattener : public tuple_iface
+{
+public:
+  smt_tuple_sym_flattener(smt_convt *_ctx, const namespacet &_ns)
+    : tuple_iface(), ctx(_ctx), ns(_ns) { }
+
+  virtual smt_sortt mk_struct_sort(const type2tc &type);
+  virtual smt_sortt mk_union_sort(const type2tc &type);
+  virtual smt_astt tuple_create(const expr2tc &structdef);
+  virtual smt_astt union_create(const expr2tc &unidef);
+  virtual smt_astt tuple_fresh(smt_sortt s, std::string name = "");
+  virtual smt_astt tuple_array_of(const expr2tc &init_value,
+                                            unsigned long domain_width);
+  virtual smt_astt mk_tuple_symbol(const expr2tc &expr);
+  virtual smt_astt mk_tuple_array_symbol(const expr2tc &expr);
+  virtual expr2tc tuple_get(const expr2tc &expr);
+
+  expr2tc tuple_get_rec(tuple_node_smt_astt tuple);
+  virtual smt_astt tuple_array_create(const type2tc &array_type,
+                                            smt_astt *input_args,
+                                            bool const_array,
+                                            smt_sortt domain);
+
+  smt_convt *ctx;
+  const namespacet &ns;
+};
