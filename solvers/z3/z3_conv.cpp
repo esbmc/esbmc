3c40d937a3f69c7a5a6f1948b7b55b34f516f6a6
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-03-20 17:56:10 +0000

    Avoid a conversion loop when making tuple arrays

--- solvers/z3/z3_conv.cpp
+++ solvers/z3/z3_conv.cpp
@@ -350,8 +350,11 @@ z3_convt::convert_type(const type2tc &type, z3::sort &sort)
   {
     // Because of crazy domain sort rewriting, pass this via all the other smt
     // processing code.
-    const z3_smt_sort *array_type = z3_sort_downcast(convert_sort(type));
-    sort = array_type->s;
+    const array_type2t &arr = to_array_type(type);
+    unsigned int domain_width = calculate_array_domain_width(arr);
+    smt_sortt domain = mk_sort(SMT_SORT_BV, domain_width, false);
+    smt_sortt range = convert_sort(arr.subtype);
+    sort = z3_sort_downcast(mk_sort(SMT_SORT_ARRAY, domain, range))->s;
     break;
   }
   case type2t::unsignedbv_id:
