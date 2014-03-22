fc810cd5cb6e7582f638adfe9f6743a0379e30b8
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-03-22 00:18:46 +0000

    Add boilerplate for yices conversion class
    Disable strict compile instead of adding attribute unused to every
    single flaming parameter.

---
+++ solvers/yices/yices_conv.cpp
@@ -0,0 +1,133 @@
+#include "yices_conv.h"
+
+yices_convt::yices_convt(bool int_encoding, const namespacet &ns, bool is_cpp)
+  : smt_convt(int_encoding, ns, is_cpp), array_iface(false, false)
+{
+  yices_init();
+}
+
+smt_convt::resultt
+yices_convt::dec_solve()
+{
+  abort();
+}
+
+tvt
+yices_convt::l_get(smt_astt l)
+{
+  abort();
+}
+
+const std::string
+yices_convt::solver_text()
+{
+  abort();
+}
+
+void
+yices_convt::assert_ast(smt_astt a)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_func_app(smt_sortt s, smt_func_kind k,
+                             smt_astt const *args,
+                             unsigned int numargs)
+{
+  abort();
+}
+
+smt_sortt
+yices_convt::mk_sort(const smt_sort_kind k, ...)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_smt_int(const mp_integer &theint, bool sign)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_smt_real(const std::string &str)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_smt_bvint(const mp_integer &theint, bool sign,
+                              unsigned int w)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_smt_bool(bool val)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_smt_symbol(const std::string &name, smt_sortt s)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_array_symbol(const std::string &name, smt_sortt s)
+{
+  abort();
+}
+
+smt_sortt
+yices_convt::mk_struct_sort(const type2tc &type)
+{
+  abort();
+}
+
+smt_sortt
+yices_convt::mk_union_sort(const type2tc &type)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::mk_extract(smt_astt a, unsigned int high,
+                            unsigned int low, smt_sortt s)
+{
+  abort();
+}
+
+smt_astt
+yices_convt::convert_array_of(const expr2tc &init_val,
+                                unsigned long domain_width)
+{
+  abort();
+}
+
+void
+yices_convt::add_array_constraints_for_solving()
+{
+  abort();
+}
+
+expr2tc
+yices_convt::get_bool(smt_astt a)
+{
+  abort();
+}
+
+expr2tc
+yices_convt::get_bv(const type2tc &t, smt_astt a)
+{
+  abort();
+}
+
+expr2tc
+yices_convt::get_array_elem(smt_astt array, uint64_t index,
+                       const type2tc &subtype)
+{
+  abort();
+}
---
+++ solvers/yices/yices_conv.h
@@ -0,0 +1,47 @@
+#ifndef _ESBMC_SOLVERS_YICES_YICES_CONV_H_
+#define _ESBMC_SOLVERS_YICES_YICES_CONV_H_
+
+#include <solvers/smt/smt_conv.h>
+
+#include <yices.h>
+
+class yices_convt : public smt_convt, public array_iface
+{
+public:
+  yices_convt(bool int_encoding, const namespacet &ns, bool is_cpp);
+
+  virtual resultt dec_solve();
+  virtual tvt l_get(const smt_ast *l);
+  virtual const std::string solver_text();
+
+  virtual void assert_ast(const smt_ast *a);
+
+  virtual smt_astt mk_func_app(const smt_sort *s, smt_func_kind k,
+                               const smt_ast * const *args,
+                               unsigned int numargs);
+  virtual smt_sortt mk_sort(const smt_sort_kind k, ...);
+  virtual smt_astt mk_smt_int(const mp_integer &theint, bool sign);
+  virtual smt_astt mk_smt_real(const std::string &str);
+  virtual smt_astt mk_smt_bvint(const mp_integer &theint, bool sign,
+                                unsigned int w);
+  virtual smt_astt mk_smt_bool(bool val);
+  virtual smt_astt mk_smt_symbol(const std::string &name, const smt_sort *s);
+  virtual smt_astt mk_array_symbol(const std::string &name, const smt_sort *s);
+  virtual smt_sortt mk_struct_sort(const type2tc &type);
+  virtual smt_sortt mk_union_sort(const type2tc &type);
+  virtual smt_astt mk_extract(const smt_ast *a, unsigned int high,
+                              unsigned int low, const smt_sort *s);
+
+  smt_astt convert_array_of(const expr2tc &init_val,
+                                  unsigned long domain_width);
+
+  virtual void add_array_constraints_for_solving();
+
+  expr2tc get_bool(smt_astt a);
+  expr2tc get_bv(const type2tc &t, smt_astt a);
+  expr2tc get_array_elem(smt_astt array, uint64_t index,
+                         const type2tc &subtype);
+
+};
+
+#endif /* _ESBMC_SOLVERS_YICES_YICES_CONV_H_ */
