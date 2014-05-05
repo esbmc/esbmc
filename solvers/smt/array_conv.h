6a2a0819eb9f11a80a938ad35fc78db05d8117c5
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-05-05 11:07:07 +0100

    Allow array creation to take an array sort
    When constructing arrays, if we create an array of structs, we end up
    calling the solvers mk_sort with a flattener's struct sort. Which
    (should) immediately croak.
    
    Work around this case by providing the array sort from the caller,
    outside of the usual array api. This is the only place that that sort is
    created, as mk_array_symbol takes a sort. All other decisions based on
    whether something has come from the solver or a flattener should be
    resolved through the ast methods.

--- solvers/smt/array_conv.h
+++ solvers/smt/array_conv.h
@@ -79,6 +79,8 @@ public:
   void add_array_constraints_for_solving(void);
 
   // Heavy lifters
+  virtual const smt_ast *convert_array_of_wsort(
+      smt_astt init_val, unsigned long domain_width, smt_sortt arr_sort);
   unsigned int new_array_id(void);
   void convert_array_assign(const array_ast *src, smt_astt sym);
   const smt_ast *mk_select(const array_ast *array, const expr2tc &idx,
