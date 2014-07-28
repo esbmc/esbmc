0419e217bd26d3f96a497aacc0417018ab6f23a2
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-07-28 23:49:16 +0100

    Purge some intermediate files

--- scripts/autopurge.sh
+++ scripts/autopurge.sh
@@ -24,5 +24,7 @@ rm config.status 2>/dev/null
 rm stamp-h1 2>/dev/null
 rm ac_config.h 2>/dev/null
 rm libtool 2>/dev/null
+rm scripts/ac_config.in~ 2>/dev/null
+
 
 find . | grep Makefile.in | grep -v regression | xargs rm 2>/dev/null
