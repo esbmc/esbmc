b7b21350b17ed73c8eb8d1b47b832ecf47096fa4
Lucas Cordeiro <lucasccordeiro@gmail.com>
2014-11-21 16:00:55 -0400

    update test spec
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc-pt/11_scale2/main.c
+++ regression/esbmc-pt/11_scale2/main.c
@@ -1,11 +1,13 @@
 // This example is runned first through the preprocessor 
 // to replace the N symbol with a constant.
 #include <pthread.h> 
+#define __N__ 1
+
 int g, g1;
 
 void thr() {
-  glb_init(g>0);
-  glb_init(g1==0);
+  __ESBMC_assume(g>0);
+  __ESBMC_assume(g1==0);
   while (g1 < g) {
     g1 = g1 + 1;
   }
