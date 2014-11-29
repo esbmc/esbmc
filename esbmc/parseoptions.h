de6de245f3b113aff88c33f7e3e0b5334e8819e4
Jeremy Morse <jmorse+git@studentrobotics.org>
2014-11-29 15:20:12 +0000

    Add decl for parseoptions parse_clang
    Which I missed before, sorry
    
    Conflicts:
    	esbmc/parseoptions.h

--- esbmc/parseoptions.h
+++ esbmc/parseoptions.h
@@ -66,6 +66,8 @@ protected:
   void add_monitor_exprs(goto_programt::targett insn, goto_programt::instructionst &insn_list, std::map<std::string, std::pair<std::set<std::string>, expr2tc> >monitors);
 
   void print_ileave_points(namespacet &ns, goto_functionst &goto_functions);
+
+  int parse_clang();
 };
 
 #endif
