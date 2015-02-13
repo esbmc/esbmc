ab362c60d620abfd9d28a23fa5d796d8246a20e1
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2015-02-13 14:55:42 +0000

    New test case
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ regression/esbmc-cpp/esbmc-cbmc/Templates39/main.cpp
@@ -0,0 +1,31 @@
+#include<cassert>
+
+// define Z<>
+template <typename T>
+class Z
+{
+public:
+  typename T::f some;
+};
+
+// Forward declaration of FF
+class FF;
+
+// make an instance of Z<FF>
+typedef Z<FF> my_Z;
+
+// Declare FF
+class FF
+{
+public:
+  typedef int f;
+};
+
+// trigger elaboration of Z<FF>
+my_Z z;
+
+int main()
+{
+  assert(z.some==0);
+  return 0;
+}
