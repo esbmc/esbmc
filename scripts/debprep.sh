4891560ea1805de93d7bfcb038c578858c9bf5df
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-02-28 13:27:14 +0000

    Further mangle debprep script
    Simply create a temporary dir in tmp, format it appropriately, and
    produce a source distribution tarball.

--- scripts/debprep.sh
+++ scripts/debprep.sh
@@ -1,9 +1,35 @@
-#!/bin/bash
+#!/bin/bash -e
+
+VER=1.24.99
 
 if test ! -e .git; then
   echo "Please run from ESBMC root dir";
   exit 1
 fi
 
-scripts/autoboot.sh
-ln -s scripts/debian debian
+tmpfile=`mktemp /tmp/esbmc_build_XXXXXX`
+tmpdir=`mktemp -d /tmp/esbmc_build_XXXXXX`
+
+function fin () {
+  rm -rf $tmpdir
+  if rm $tmpfile 2>/dev/null; then
+    echo "" > /dev/null;
+  fi
+}
+
+trap fin EXIT
+
+rm $tmpfile
+TARDIR=$tmpdir/esbmc-${VER}
+mkdir $TARDIR
+
+git archive -o $tmpfile HEAD
+tar -xf $tmpfile -C $TARDIR
+rm $tmpfile
+
+# Ahem
+mkdir $TARDIR/.git
+
+sh -c "cd $TARDIR; scripts/autoboot.sh"
+here=`pwd`
+sh -c "cd $tmpdir; tar -cjf $here/esbmc_${VER}.orig.tar.bz2 esbmc-${VER}"
