88ac867ad6223e00072f7dfa42d322e1d4c8d04e
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2015-03-09 17:15:40 +0000

    Update test case
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/k-induction/dirkex_unsafe/main.c
+++ regression/k-induction/dirkex_unsafe/main.c
@@ -1,11 +1,13 @@
+_Bool __VERIFIER_nondet_bool();
+
 int main() {
 	
 	unsigned int x1 = 0, x2 = 0; int s = 1;
 	
-	while (nondet()) {
+	while (__VERIFIER_nondet_bool()) {
 		if (s == 1) x1++;
 		else if (s == 2) x2++;
-		
+
 		s++;
 		if (s == 5) s = 1;
 	}
