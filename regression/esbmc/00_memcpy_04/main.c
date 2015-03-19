b23fdb7664728758c19a6b7a4361d07e7d2c0ea5
Lucas Cordeiro <lcordeiro@localhost.localdomain>
2015-03-19 12:41:31 -0300

    update test case 00_memcpy_04
    Signed-off-by: Lucas Cordeiro <lcordeiro@localhost.localdomain>

--- regression/esbmc/00_memcpy_04/main.c
+++ regression/esbmc/00_memcpy_04/main.c
@@ -6,7 +6,7 @@ int main() {
   double *dev_a = (double*)malloc(sizeof(double));
   double a = 1;
   memcpy(dev_a,&a,sizeof(double));
-  assert(dev_a[0]==1);
+  assert(dev_a[0]==2);
   return 0;
 }
 
