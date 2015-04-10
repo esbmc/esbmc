75e719f317c1bf6c01d27f929f9abf6d468cd71a
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-04-11 00:34:39 +0100

    Revert "Remove 'time' C library implementation"
    This reverts commit 114c8c5c63f17ec47f7ffc85fd8873073b166d4f.

---
+++ ansi-c/library/time.c
@@ -0,0 +1,15 @@
+#ifdef _MINGW
+#define __CRT__NO_INLINE
+#define __declspec /* hacks */
+#endif
+
+#include <time.h>
+#undef time
+
+time_t time(time_t *tloc)
+{
+  time_t res;
+  if(!tloc) *tloc=res;
+  return res;
+}
+
