ae71b87c38521dd4ea7c126ea567a5e68bfd41c2
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2015-06-03 18:36:47 +0100

    New method to save loop functions
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- goto-programs/goto_unwind.cpp
+++ goto-programs/goto_unwind.cpp
@@ -10,9 +10,56 @@
 #include "goto_unwind.h"
 
 void goto_unwind(
-  goto_functionst& goto_functions __attribute__((unused)),
-  const namespacet& ns __attribute__((unused)),
-  message_handlert& message_handler __attribute__((unused)))
+  goto_functionst& goto_functions,
+  const namespacet& ns,
+  message_handlert& message_handler)
+{
+  Forall_goto_functions(it, goto_functions)
+    if(it->second.body_available)
+      goto_unwindt(it->second, ns, message_handler);
+}
+
+void goto_unwindt::goto_unwind_rec()
 {
 
+}
+
+void goto_unwindt::find_function_loops()
+{
+  for(goto_programt::instructionst::const_iterator
+      it=goto_function.body.instructions.begin();
+      it!=goto_function.body.instructions.end();
+      it++)
+  {
+    // We found a loop, let's record its instructions
+    if (it->is_backwards_goto())
+    {
+      assert(it->targets.size() == 1);
+      create_function_loop(
+        (*it->targets.begin())->location_number, it->location_number);
+    }
+  }
+}
+
+void goto_unwindt::create_function_loop(
+  unsigned int loop_head,
+  unsigned int loop_exit)
+{
+  std::cout << "loop_head: " << loop_head << std::endl;
+  std::cout << "loop_exit: " << loop_exit << std::endl;
+
+  goto_programt::instructionst::iterator
+    it=goto_function.body.instructions.begin();
+
+  // Find loop head
+  while (it->location_number != loop_head) it++;
+
+  goto_programt loop_body;
+
+  while (it->location_number != loop_exit)
+  {
+    goto_programt::targett new_instruction=loop_body.add_instruction();
+    *new_instruction=*it;
+    ++it;
+  }
 }
--- goto-programs/goto_unwind.h
+++ goto-programs/goto_unwind.h
@@ -20,4 +20,38 @@ void goto_unwind(
   const namespacet &ns,
   message_handlert &message_handler);
 
+class goto_unwindt:public message_streamt
+{
+public:
+  goto_unwindt(
+    goto_functiont &_goto_function,
+    const namespacet &_ns,
+    message_handlert &_message_handler):
+    message_streamt(_message_handler),
+    goto_function(_goto_function),
+    ns(_ns)
+  {
+    // Find loops
+    find_function_loops();
+
+    // unwind loops
+    goto_unwind_rec();
+  }
+
+protected:
+  goto_functiont &goto_function;
+  const namespacet &ns;
+
+  typedef unsigned int loop_head;
+  typedef unsigned int loop_exit;
+  typedef std::map<loop_head, loop_exit> loops;
+
+  loops function_loops;
+
+  void find_function_loops();
+  void create_function_loop(unsigned int loop_head, unsigned int loop_exit);
+
+  void goto_unwind_rec();
+};
+
 #endif /* GOTO_PROGRAMS_GOTO_UNWIND_H_ */
