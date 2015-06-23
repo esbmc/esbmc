618b4cb9ef220edc9d63cd45f7fa44f0cafe0e28
Jeremy Morse <jmorse+git@studentrobotics.org>
2015-06-23 16:39:04 +0100

    Fix inclusion of config.h
    These appear to have been the victim of an overly broad .gitignore,
    followed by the export to a public repo not picking them up. Whoops.

---
+++ util/config.cpp
@@ -0,0 +1,319 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#include <iostream>
+
+#include <irep2.h>
+
+#include "config.h"
+
+configt config;
+
+void configt::ansi_ct::set_16()
+{
+  int_width=2*8;
+  long_int_width=4*8;
+  char_width=1*8;
+  short_int_width=2*8;
+  long_long_int_width=8*8;
+  pointer_width=4*8;
+  pointer_diff_width=4*8;
+  single_width=4*8;
+  double_width=8*8;
+  long_double_width=8*8;
+  char_is_unsigned=false;
+  word_size=16;
+  wchar_t_width=2*8;
+  alignment=2;
+}
+
+void configt::ansi_ct::set_32()
+{
+  int_width=4*8;
+  long_int_width=4*8;
+  char_width=1*8;
+  short_int_width=2*8;
+  long_long_int_width=8*8;
+  pointer_width=4*8;
+  pointer_diff_width=4*8;
+  single_width=4*8;
+  double_width=8*8;
+  long_double_width=8*8;
+  char_is_unsigned=false;
+  word_size=32;
+  wchar_t_width=2*8;
+  alignment=4;
+
+  // Configure exprs we use as long / uints etc for this mode.
+  const_cast<constant_int2tc&>(zero_ulong) = zero_u32;
+  const_cast<constant_int2tc&>(one_ulong) = one_u32;
+  const_cast<constant_int2tc&>(zero_long) = zero_32;
+  const_cast<constant_int2tc&>(one_long) = one_32;
+}
+
+void configt::ansi_ct::set_64()
+{
+  int_width=4*8;
+  long_int_width=8*8;
+  char_width=1*8;
+  short_int_width=2*8;
+  long_long_int_width=8*8;
+  pointer_width=8*8;
+  pointer_diff_width=8*8;
+  single_width=4*8;
+  double_width=8*8;
+  long_double_width=8*8;
+  char_is_unsigned=false;
+  word_size=64;
+  wchar_t_width=2*8;
+  alignment=4;
+
+  // Configure exprs we use as long / uints etc for this mode.
+  const_cast<constant_int2tc&>(zero_ulong) = zero_u64;
+  const_cast<constant_int2tc&>(one_ulong) = one_u64;
+  const_cast<constant_int2tc&>(zero_long) = zero_64;
+  const_cast<constant_int2tc&>(one_long) = one_64;
+}
+
+bool configt::set(const cmdlinet &cmdline)
+{
+  // defaults
+  ansi_c.set_32();
+
+  #ifdef HAVE_FLOATBV
+  ansi_c.use_fixed_for_float=false;
+  #else
+  ansi_c.use_fixed_for_float=true;
+  #endif
+
+  ansi_c.endianess=ansi_ct::NO_ENDIANESS;
+  ansi_c.os=ansi_ct::NO_OS;
+  ansi_c.lib=configt::ansi_ct::LIB_NONE;
+
+  if(cmdline.isset("16"))
+    ansi_c.set_16();
+
+  if(cmdline.isset("32"))
+    ansi_c.set_32();
+
+  if(cmdline.isset("64"))
+    ansi_c.set_64();
+
+  if(cmdline.isset("function"))
+    main=cmdline.getval("function");
+
+  if(cmdline.isset('D'))
+    ansi_c.defines=cmdline.get_values('D');
+
+  if(cmdline.isset('I'))
+    ansi_c.include_paths=cmdline.get_values('I');
+
+  if(cmdline.isset("floatbv"))
+    ansi_c.use_fixed_for_float=false;
+
+  if(cmdline.isset("fixedbv"))
+    ansi_c.use_fixed_for_float=true;
+
+  if(cmdline.isset("i386-linux"))
+  {
+    ansi_c.os=configt::ansi_ct::OS_I386_LINUX;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+  }
+
+  if(cmdline.isset("i386-win32"))
+  {
+    ansi_c.os=configt::ansi_ct::OS_WIN32;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+  }
+
+  if(cmdline.isset("i386-macos"))
+  {
+    ansi_c.os=configt::ansi_ct::OS_I386_MACOS;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+  }
+
+  if(cmdline.isset("ppc-macos"))
+  {
+    ansi_c.os=configt::ansi_ct::OS_PPC_MACOS;
+    ansi_c.endianess=configt::ansi_ct::IS_BIG_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+  }
+
+  if(cmdline.isset("no-arch"))
+  {
+    ansi_c.os=configt::ansi_ct::NO_OS;
+    ansi_c.endianess=configt::ansi_ct::NO_ENDIANESS;
+    ansi_c.lib=configt::ansi_ct::LIB_NONE;
+  }
+  else if(ansi_c.os==configt::ansi_ct::NO_OS)
+  {
+    // this is the default
+    #ifdef _WIN32
+    ansi_c.os=configt::ansi_ct::OS_WIN32;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+    #else
+    #ifdef __APPLE__
+    ansi_c.os=configt::ansi_ct::OS_I386_MACOS;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+    #else
+    ansi_c.os=configt::ansi_ct::OS_I386_LINUX;
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+    ansi_c.lib=configt::ansi_ct::LIB_FULL;
+    #endif
+    #endif
+  }
+
+  if(cmdline.isset("string-abstraction"))
+    ansi_c.string_abstraction=true;
+  else
+    ansi_c.string_abstraction=false;
+
+  if(cmdline.isset("no-lock-check"))
+    ansi_c.lock_check=false;
+  else
+	ansi_c.lock_check=true;
+
+  if(cmdline.isset("deadlock-check"))
+    ansi_c.deadlock_check=true;
+  else
+    ansi_c.deadlock_check=false;
+
+  if(cmdline.isset("no-library"))
+    ansi_c.lib=configt::ansi_ct::LIB_NONE;
+
+  if(cmdline.isset("little-endian"))
+    ansi_c.endianess=configt::ansi_ct::IS_LITTLE_ENDIAN;
+
+  if(cmdline.isset("big-endian"))
+    ansi_c.endianess=configt::ansi_ct::IS_BIG_ENDIAN;
+
+  if(cmdline.isset("little-endian") &&
+     cmdline.isset("big-endian")) {
+      std::cerr << "Can't set both little and big endian modes" << std::endl;
+      return true;
+  }
+
+  if(cmdline.isset("unsigned-char"))
+    ansi_c.char_is_unsigned=true;
+
+  return false;
+}
+
+std::string configt::this_architecture()
+{
+  std::string this_arch;
+
+  // following http://wiki.debian.org/ArchitectureSpecificsMemo
+
+  #ifdef __alpha__
+  this_arch="alpha";
+  #elif __armel__
+  this_arch="armel";
+  #elif __aarch64__
+  this_arch="arm64";
+  #elif __arm__
+    #ifdef __ARM_PCS_VFP
+    this_arch="armhf"; // variant of arm with hard float
+    #else
+    this_arch="arm";
+    #endif
+  #elif __mipsel__
+    #if _MIPS_SIM==_ABIO32
+    this_arch="mipsel";
+    #elif _MIPS_SIM==_ABIN32
+    this_arch="mipsn32el";
+    #else
+    this_arch="mips64el";
+    #endif
+  #elif __mips__
+    #if _MIPS_SIM==_ABIO32
+    this_arch="mips";
+    #elif _MIPS_SIM==_ABIN32
+    this_arch="mipsn32";
+    #else
+    this_arch="mips64";
+    #endif
+  #elif __powerpc__
+    #if defined(__ppc64__) || defined(__PPC64__) || defined(__powerpc64__) || defined(__POWERPC64__)
+      #ifdef __LITTLE_ENDIAN__
+      this_arch="ppc64le";
+      #else
+      this_arch="ppc64";
+      #endif
+    #else
+    this_arch="powerpc";
+    #endif
+  #elif __sparc__
+    #ifdef __arch64__
+    this_arch="sparc64";
+    #else
+    this_arch="sparc";
+    #endif
+  #elif __ia64__
+  this_arch="ia64";
+  #elif __s390x__
+  this_arch="s390x";
+  #elif __s390__
+  this_arch="s390";
+  #elif __x86_64__
+    #ifdef __ILP32__
+    this_arch="x32"; // variant of x86_64 with 32-bit pointers
+    #else
+    this_arch="x86_64";
+    #endif
+  #elif __i386__
+  this_arch="i386";
+  #elif _WIN64
+  this_arch="x86_64";
+  #elif _WIN32
+  this_arch="i386";
+  #else
+  // something new and unknown!
+  this_arch="unknown";
+  #endif
+
+  return this_arch;
+}
+
+/*******************************************************************\
+
+Function: configt::ansi_ct::this_operating_system
+
+  Inputs:
+
+ Outputs:
+
+ Purpose:
+
+\*******************************************************************/
+
+std::string configt::this_operating_system()
+{
+  std::string this_os;
+
+  #ifdef _WIN32
+  this_os="windows";
+  #elif __APPLE__
+  this_os="macos";
+  #elif __FreeBSD__
+  this_os="freebsd";
+  #elif __linux__
+  this_os="linux";
+  #elif __SVR4
+  this_os="solaris";
+  #else
+  this_os="unknown";
+  #endif
+
+  return this_os;
+}
---
+++ util/config.h
@@ -0,0 +1,74 @@
+/*******************************************************************\
+
+Module:
+
+Author: Daniel Kroening, kroening@kroening.com
+
+\*******************************************************************/
+
+#ifndef CPROVER_UTIL_CONFIG_H
+#define CPROVER_UTIL_CONFIG_H
+
+#include "cmdline.h"
+#include "options.h"
+
+class configt
+{
+public:
+  struct ansi_ct
+  {
+    // for ANSI-C
+    unsigned int_width;
+    unsigned long_int_width;
+    unsigned char_width;
+    unsigned short_int_width;
+    unsigned long_long_int_width;
+    unsigned pointer_width;
+    unsigned single_width;
+    unsigned double_width;
+    unsigned long_double_width;
+    unsigned pointer_diff_width;
+    unsigned word_size;
+    unsigned wchar_t_width;
+
+    bool char_is_unsigned;
+    bool use_fixed_for_float;
+
+    void set_16();
+    void set_32();
+    void set_64();
+
+    // alignment (in structs) measured in bytes
+    unsigned alignment;
+
+    typedef enum { NO_ENDIANESS, IS_LITTLE_ENDIAN, IS_BIG_ENDIAN } endianesst;
+    endianesst endianess;
+
+    typedef enum { NO_OS, OS_I386_LINUX, OS_I386_MACOS, OS_PPC_MACOS, OS_WIN32 } ost;
+    ost os;
+
+    std::list<std::string> defines;
+    std::list<std::string> undefines;
+    std::list<std::string> preprocessor_options;
+    std::list<std::string> include_paths;
+
+    typedef enum { LIB_NONE, LIB_FULL } libt;
+    libt lib;
+    bool string_abstraction;
+    bool deadlock_check;
+    bool lock_check;
+  } ansi_c;
+
+  std::string main;
+
+  bool set(const cmdlinet &cmdline);
+
+  optionst options;
+
+  static std::string this_architecture();
+  static std::string this_operating_system();
+};
+
+extern configt config;
+
+#endif
