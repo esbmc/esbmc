acd5630f258e3b7cd4f762d38723a09842bed8c2
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2015-07-23 18:33:59 +0100

    Revert "Declare methods for clang->ESBMC exprs conversion"
    This reverts commit 5f491fdb1b9bee896d91c581f9ce7d4b8c8116af.

--- util/migrate.h
+++ util/migrate.h
@@ -10,8 +10,6 @@
 #include "std_expr.h"
 #include "std_types.h"
 
-#include <clang/AST/Type.h>
-
 // Don't ask
 class namespacet;
 extern namespacet *migrate_namespace_lookup;
@@ -23,8 +21,5 @@ void migrate_type(const typet &type, type2tc &new_type,
 void migrate_expr(const exprt &expr, expr2tc &new_expr);
 typet migrate_type_back(const type2tc &ref);
 exprt migrate_expr_back(const expr2tc &ref);
-
-void migrate_type(const clang::Type &type, type2tc &new_type);
-void migrate_expr(const clang::Expr &expr, type2tc &new_expr);
 
 #endif /* _ESBMC_UTIL_MIGRATE_H_ */
