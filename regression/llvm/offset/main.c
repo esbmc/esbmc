683af5036520d52e405b40e8a2a7488826e13b1a
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2015-11-18 17:36:00 +0000

    Update test.desc
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/llvm/offset/main.c
+++ regression/llvm/offset/main.c
@@ -1,4 +1,4 @@
-//#include <assert.h>
+#include <assert.h>
 
 struct S {
   float f;
@@ -17,7 +17,7 @@ int main()
 
   assert(__builtin_offsetof(struct abc1, a) == 0); // always, because there's no padding before a.
   assert(__builtin_offsetof(struct abc1, b) == 4); // here, on my system
-//  assert(__builtin_offsetof(union abc2, a) == __builtin_offsetof(union abc2, b)); // (members overlap)
+  assert(__builtin_offsetof(union abc2, a) == __builtin_offsetof(union abc2, b)); // (members overlap)
 
   assert(__builtin_offsetof(struct T, s[2].d) == 48);
   return 0;
