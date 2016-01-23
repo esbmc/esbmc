83aee11bec9d335762bea9cad7fc8b95486d3036
Lucas Cordeiro <lcordeiro@localhost.localdomain>
2016-01-23 20:49:59 -0200

    implement MPOR  to identify accesses to different positions in (shared) arrays. For more information, see our paper Verifying CUDA Programs using SMT-Based Context-Bounded Model Checking at http://esbmc.org/gpu
    Signed-off-by: Lucas Cordeiro <lcordeiro@localhost.localdomain>

--- src/goto-symex/execution_state.cpp
+++ src/goto-symex/execution_state.cpp
@@ -26,6 +26,8 @@
 #include "config.h"
 
 unsigned int execution_statet::node_count = 0;
+std::map<expr2tc, std::list<unsigned int>> vars_map;
+std::map<expr2tc, bool> is_global;
 
 execution_statet::execution_statet(const goto_functionst &goto_functions,
                                    const namespacet &ns,
@@ -199,6 +201,7 @@ execution_statet::operator=(const execution_statet &ex)
 execution_statet::~execution_statet()
 {
   delete state_level2;
+  vars_map.clear();
 };
 
 void
@@ -649,7 +652,48 @@ execution_statet::get_expr_globals(const namespacet &ns, const expr2tc &expr,
         name == "c::__ESBMC_is_dynamic") {
       return;
     } else if ((symbol->static_lifetime || symbol->type.is_dynamic_set())) {
-      globals_list.insert(expr);
+    	std::list<unsigned int> threadId_list;
+    	std::map<expr2tc,std::list<unsigned int>>::iterator it_find;
+    	it_find = vars_map.find(expr);
+
+    	//the expression was accessed in another interleaving
+    	if (it_find != vars_map.end()){
+
+    		threadId_list = it_find->second;
+    		threadId_list.push_back(get_active_state().top().level1.thread_id);
+
+    		vars_map.insert(std::pair<expr2tc, std::list<unsigned int>>(expr,threadId_list));
+
+    		std::list<unsigned int>::iterator it_list;
+    		for(it_list=threadId_list.begin(); it_list!=threadId_list.end(); ++it_list){
+
+    			//find if some thread access the same expression
+    			if(*it_list!=get_active_state().top().level1.thread_id){
+    				globals_list.insert(expr);
+    				is_global.insert(std::pair<expr2tc, bool>(expr,true));
+    			}
+    			//expression was not accessed by other thread
+    			else{
+    				std::map<expr2tc, bool>::iterator its_global;
+    				its_global = is_global.find(expr);
+    				//expression was defined as global in another interleaving
+    				if (its_global != is_global.end()){
+    					globals_list.insert(expr);
+    				}
+    			}
+    		}
+    		//first access of expression
+    	}else{
+    		std::map<expr2tc, bool>::iterator its_global;
+    		its_global = is_global.find(expr);
+    		if (its_global != is_global.end()){
+    			globals_list.insert(expr);
+    		}else{
+    			threadId_list.push_back(get_active_state().top().level1.thread_id);
+    			vars_map.insert(std::pair<expr2tc, std::list<unsigned int>>(expr,threadId_list));
+    			globals_list.insert(expr);
+    		}
+    	}
     } else {
       return;
     }
