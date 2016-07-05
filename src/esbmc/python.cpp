6a8b6b88e174886e870dfc998b5310310ef105a7
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-05 01:05:58 +0100

    Install list of namespaced types in python

--- src/esbmc/python.cpp
+++ src/esbmc/python.cpp
@@ -6,6 +6,9 @@
 #include <boost/python.hpp>
 using namespace boost::python;
 
+class dummy_expr_class { };
+class dummy_type_class { };
+
 BOOST_PYTHON_MODULE(esbmc)
 {
   // Use boost preprocessing iteration to enumerate all irep classes and
@@ -13,8 +16,21 @@ BOOST_PYTHON_MODULE(esbmc)
   // so that it can actually be typechecked, but that will require:
   //  * A boost set of ireps to exist
   //  * expr_id's to be registered into a template like irep_methods2.
-#define _ESBMC_IREP2_MPL_IREP_SET(r, data, elem) BOOST_PP_CAT(elem,2t)::build_python_class(expr2t::BOOST_PP_CAT(elem,_id));
-BOOST_PP_LIST_FOR_EACH(_ESBMC_IREP2_MPL_IREP_SET, foo, ESBMC_LIST_OF_EXPRS)
+
+  // Namespace into types and exprs.
+  {
+    scope types = class_<dummy_type_class>("type");
+
+#define _ESBMC_IREP2_MPL_TYPE_SET(r, data, elem) BOOST_PP_CAT(elem,_type2t)::build_python_class(type2t::BOOST_PP_CAT(elem,_id));
+BOOST_PP_LIST_FOR_EACH(_ESBMC_IREP2_MPL_TYPE_SET, foo, ESBMC_LIST_OF_TYPES)
+  }
+
+  {
+    scope types = class_<dummy_expr_class>("expr");
+
+#define _ESBMC_EXPR2_MPL_EXPR_SET(r, data, elem) BOOST_PP_CAT(elem,2t)::build_python_class(expr2t::BOOST_PP_CAT(elem,_id));
+BOOST_PP_LIST_FOR_EACH(_ESBMC_EXPR2_MPL_EXPR_SET, foo, ESBMC_LIST_OF_EXPRS)
+  }
 }
 
 // Include these other things that are special to the esbmc binary:
