9b5788eb0af7cd2dd079b867e176de9b4bde9110
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-12 23:03:44 +0100

    Test for existance of base types
    (Before even looking towards what they do)

--- regression/python/tests/basic.py
+++ regression/python/tests/basic.py
@@ -7,3 +7,12 @@ class Basic(unittest.TestCase):
             self.assertTrue(True)
         except:
             self.assertTrue(False, "Failed to import esbmc")
+
+    def test_types(self):
+        import esbmc
+        # Ensure that the base list of types are in the type class
+        base_types = ['array', 'bool', 'code', 'cpp_name', 'empty', 'fixedbv',\
+                'pointer', 'signedbv', 'string', 'struct', 'symbol', 'type2t',\
+                'type_ids', 'type_vec', 'union', 'unsignedbv']
+        for x in base_types:
+            self.assertTrue(x in dir(esbmc.type), "Missing type {}".format(x))
