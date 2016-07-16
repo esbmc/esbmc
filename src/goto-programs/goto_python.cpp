1272451243ace69cef70d940f22670b008b320ae
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-16 13:45:13 +0100

    Register goto_functiont.

--- src/goto-programs/goto_python.cpp
+++ src/goto-programs/goto_python.cpp
@@ -19,6 +19,14 @@ build_goto_func_class()
     .def(map_indexing_suite<goto_functionst::function_mapt>());
   class_<goto_functionst>("goto_functionst", no_init)
     .def_readwrite("function_map", &goto_functionst::function_map);
+
+  class_<goto_functiont>("goto_functiont")
+    .def_readwrite("body", &goto_functiont::body)
+    // No old-typet definitions available
+    .def_readwrite("type", &goto_functiont::type)
+    .def_readwrite("body_available", &goto_functiont::body_available)
+    // No easy def for inlined_funcs
+    .def_readwrite("inlined_funcs", &goto_functiont::inlined_funcs);
 }
 
 #endif
