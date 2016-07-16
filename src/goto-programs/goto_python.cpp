217012531738d8b6407534832782f6c73afb671e
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-16 13:52:16 +0100

    Register instruction type enum

--- src/goto-programs/goto_python.cpp
+++ src/goto-programs/goto_python.cpp
@@ -27,6 +27,27 @@ build_goto_func_class()
     .def_readwrite("body_available", &goto_functiont::body_available)
     // No easy def for inlined_funcs
     .def_readwrite("inlined_funcs", &goto_functiont::inlined_funcs);
+
+  enum_<goto_program_instruction_typet>("goto_program_instruction_type")
+    .value("NO_INSTRUCTION_TYPE", goto_program_instruction_typet::NO_INSTRUCTION_TYPE)
+    .value("GOTO", goto_program_instruction_typet::GOTO)
+    .value("ASSUME", goto_program_instruction_typet::ASSUME)
+    .value("ASSERT", goto_program_instruction_typet::ASSERT)
+    .value("OTHER", goto_program_instruction_typet::OTHER)
+    .value("SKIP", goto_program_instruction_typet::SKIP)
+    .value("LOCATION", goto_program_instruction_typet::LOCATION)
+    .value("END_FUNCTION", goto_program_instruction_typet::END_FUNCTION)
+    .value("ATOMIC_BEGIN", goto_program_instruction_typet::ATOMIC_BEGIN)
+    .value("ATOMIC_END", goto_program_instruction_typet::ATOMIC_END)
+    .value("RETURN", goto_program_instruction_typet::RETURN)
+    .value("ASSIGN", goto_program_instruction_typet::ASSIGN)
+    .value("DECL", goto_program_instruction_typet::DECL)
+    .value("DEAD", goto_program_instruction_typet::DEAD)
+    .value("FUNCTION_CALL", goto_program_instruction_typet::FUNCTION_CALL)
+    .value("THROW", goto_program_instruction_typet::THROW)
+    .value("CATCH", goto_program_instruction_typet::CATCH)
+    .value("THROW_DECL", goto_program_instruction_typet::THROW_DECL)
+    .value("THROW_DECL_END", goto_program_instruction_typet::THROW_DECL_END);
 }
 
 #endif
