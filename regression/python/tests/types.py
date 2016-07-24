15ab07bd94e665dd2568422cfacd2a2651aeb52a
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-25 00:54:08 +0100

    Parameterise some tests

--- regression/python/tests/types.py
+++ regression/python/tests/types.py
@@ -1,7 +1,20 @@
 import unittest
 
 class Types(unittest.TestCase):
+    def setUp(self):
+        from esbmc import BigInt
+        self.thirtytwo = BigInt(32)
+
+    def make_u32(self):
+        import esbmc
+        return esbmc.type.unsignedbv.make(32)
+
     def test_empty(self):
         import esbmc
         emptytype = esbmc.type.empty.make()
         self.assertTrue(emptytype != None, "Can't create empty type")
+
+    def test_bv(self):
+        import esbmc
+        unsignedbv = self.make_u32()
+        self.assertTrue(unsignedbv != None, "Can't create bv type")
