cb9012db026e005b725a0c31b4d35b519c12540a
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-25 00:57:36 +0100

    Add additional type tests

--- regression/python/tests/types.py
+++ regression/python/tests/types.py
@@ -18,3 +18,21 @@ class Types(unittest.TestCase):
         import esbmc
         unsignedbv = self.make_u32()
         self.assertTrue(unsignedbv != None, "Can't create bv type")
+
+    def test_bv_data(self):
+        import esbmc
+        unsignedbv = self.make_u32()
+        self.assertTrue(unsignedbv.width == 32, "Can't access bv type field")
+
+    def test_bv_typeid(self):
+        import esbmc
+        unsignedbv = self.make_u32()
+        self.assertTrue(unsignedbv.type_id == esbmc.type.type_ids.unsignedbv,
+                "Can't access bv type_id field")
+
+    def test_bv_pretty(self):
+        import esbmc
+        unsignedbv = self.make_u32()
+        reftext = "unsignedbv\n* width : 32"
+        self.assertTrue(unsignedbv.pretty(0) == reftext,
+                "Can't pretty print types")
