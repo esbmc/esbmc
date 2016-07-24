b05a5cca1aa4bde58d3eea4c313a42e57d36e9d9
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-25 00:59:15 +0100

    Test type equality
    Works. Shocking.

--- regression/python/tests/types.py
+++ regression/python/tests/types.py
@@ -5,9 +5,9 @@ class Types(unittest.TestCase):
         from esbmc import BigInt
         self.thirtytwo = BigInt(32)
 
-    def make_u32(self):
+    def make_unsigned(self, width=32):
         import esbmc
-        return esbmc.type.unsignedbv.make(32)
+        return esbmc.type.unsignedbv.make(width)
 
     def test_empty(self):
         import esbmc
@@ -16,23 +16,30 @@ class Types(unittest.TestCase):
 
     def test_bv(self):
         import esbmc
-        unsignedbv = self.make_u32()
+        unsignedbv = self.make_unsigned()
         self.assertTrue(unsignedbv != None, "Can't create bv type")
 
     def test_bv_data(self):
         import esbmc
-        unsignedbv = self.make_u32()
+        unsignedbv = self.make_unsigned()
         self.assertTrue(unsignedbv.width == 32, "Can't access bv type field")
 
     def test_bv_typeid(self):
         import esbmc
-        unsignedbv = self.make_u32()
+        unsignedbv = self.make_unsigned()
         self.assertTrue(unsignedbv.type_id == esbmc.type.type_ids.unsignedbv,
                 "Can't access bv type_id field")
 
     def test_bv_pretty(self):
         import esbmc
-        unsignedbv = self.make_u32()
+        unsignedbv = self.make_unsigned()
         reftext = "unsignedbv\n* width : 32"
         self.assertTrue(unsignedbv.pretty(0) == reftext,
                 "Can't pretty print types")
+
+    def test_bv_equality(self):
+        import esbmc
+        u32 = self.make_unsigned(32)
+        u16 = self.make_unsigned(16)
+        self.assertTrue(u32 == u32, "Type comparison fails")
+        self.assertTrue(u32 != u16, "Type comparison fails")
