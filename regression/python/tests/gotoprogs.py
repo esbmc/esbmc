2259c1962d7b4782be522eba24fa7d6bc02a7631
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-30 23:16:51 +0100

    Check the usual funcs are in the function map

--- regression/python/tests/gotoprogs.py
+++ regression/python/tests/gotoprogs.py
@@ -10,3 +10,10 @@ class Gotoprogs(unittest.TestCase):
         self.assertTrue(self.ns != None, "No namespace object generated")
         self.assertTrue(self.opts != None, "No options object generated")
         self.assertTrue(self.funcs != None, "No funcs object generated")
+
+    def test_func_list(self):
+        # Should contain the main function and a variety of clib stuff
+        funcnames = [x.key().as_string() for x in self.funcs.function_map]
+        refnames = ['main', 'c::__ESBMC_assume', 'c::assert', 'c::__ESBMC_assert', 'c::main']
+        for name in refnames:
+            self.assertTrue(name in funcnames, "func '{}' should be in function map".format(name))
