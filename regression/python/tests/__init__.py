e9fdb105ec91661907754a62881c992cf56d16c7
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-30 17:07:48 +0100

    Start looking at expr testing

--- regression/python/tests/__init__.py
+++ regression/python/tests/__init__.py
@@ -1,2 +1,3 @@
 from basic import Basic
 from types import Types
+from exprs import Exprs
---
+++ regression/python/tests/exprs.py
@@ -0,0 +1,9 @@
+import unittest
+
+class Exprs(unittest.TestCase):
+    # list of things to test: minmal creation, empty stuff,
+    # typeid, pretty, equality + comparisons, clone, nested foo
+    # downcasting, internal refs, all the kinds, non conversion,
+    # cmps, the right exceptions thrown from failures, vectors
+    # and the likes, composite kinds and their conents
+    pass
