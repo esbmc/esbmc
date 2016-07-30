0dbdb396444a67e01ce37034ec1bcbdd13de683f
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-30 18:10:38 +0100

    Remove some todos

--- regression/python/tests/exprs.py
+++ regression/python/tests/exprs.py
@@ -1,9 +1,6 @@
 import unittest
 
 class Exprs(unittest.TestCase):
-    # list of things to test: minmal creation, empty stuff,
-    # typeid, pretty, equality + comparisons, clone, nested foo
-    # downcasting, internal refs, all the kinds, non conversion,
     # cmps, the right exceptions thrown from failures, vectors
     # and the likes, composite kinds and their conents
 
