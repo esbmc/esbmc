12db7f68039672b5c3dbf4f55c519b69465f2311
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-07-31 19:29:32 +0100

    Begin testing insn basic properties

--- regression/python/tests/gotoinsns.py
+++ regression/python/tests/gotoinsns.py
@@ -6,9 +6,19 @@ class Gotoinsns(unittest.TestCase):
         # cwd = regression/python
         self.ns, self.opts, self.funcs = esbmc.init_esbmc_process(['test_data/00_big_endian_01/main.c', '--big-endian', '--bv'])
         self.main = self.funcs.function_map[esbmc.irep_idt('c::main')].body
+        self.insns = self.main.get_instructions()
 
     def tearDown(self):
         import esbmc
         esbmc.kill_esbmc_process()
         self.ns, self.opts, self.funcs = None, None, None
         self.main = None
+
+    def test_basic(self):
+        import esbmc
+        self.assertTrue(type(self.insns[0]) == esbmc.goto_programs.instructiont, "Insns should have insn type")
+
+    def test_target(self):
+        # At least one insn should have a non-nil target. For our example.
+        targets = [x.target for x in self.insns if x.target != None]
+        self.assertTrue(len(targets) != 0, "At least one insn in main should have branch")
