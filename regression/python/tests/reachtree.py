a07fcaaef6ee8fccb689defc6068ca6246a5c7c5
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-08-04 13:46:37 +0100

    Write a test to manually step an execution state
    This is fairly sketchy and involves frobbing the internals of the
    reachability_tree object a fair bit. But that's entirely the point,
    after all.
    
    Doesn't currently work because all the goto_symext stuff isn't exported

--- regression/python/tests/reachtree.py
+++ regression/python/tests/reachtree.py
@@ -32,3 +32,28 @@ class Reachtree(unittest.TestCase):
         issat = btor.dec_solve()
         # This test case should have a counterexample
         self.assertTrue(issat == esbmc.solve.smt_result.sat, "Full BMC result should be SAT")
+
+    def test_full_internal(self):
+        import esbmc
+        # Run a full model checking process (:O)
+        self.art.setup_for_new_explore()
+
+            # multithreaded crut necessary for flags being set
+        while ((not self.art.get_cur_state().has_cswitch_point_occured() or self.art.get_cur_state().check_if_ileaves_blocked()) and self.art.get_cur_state().can_execution_continue()):
+            self.art.get_cur_state().symex_step(self.art)
+
+        # After running to a blocked point, try to context switch, fail,
+        # which results in flags being set
+        #self.art.create_next_state()
+        self.art.switch_to_next_execution_state()
+        self.assertTrue(self.art.is_has_complete_formula())
+
+        self.art.get_cur_state().finish_formula()
+        self.art.has_complete_formula = False;
+        result = get_cur_state().get_symex_result();
+
+        btor = esbmc.solve.solvers.boolector.make(False, False, self.ns, self.opts)
+        result.target.convert(btor)
+        issat = btor.dec_solve()
+        # This test case should have a counterexample
+        self.assertTrue(issat == esbmc.solve.smt_result.sat, "Full BMC result should be SAT")
