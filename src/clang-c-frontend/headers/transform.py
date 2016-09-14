17c03bc87fdcb93c8d42208b247cceed3b4ce2f0
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-09-14 20:07:47 +0100

    Update transform script
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-c-frontend/headers/transform.py
+++ src/clang-c-frontend/headers/transform.py
@@ -9,9 +9,9 @@ from sys import argv
 script, dire, printopt = argv
 
 onlyfiles = [f for f in os.listdir(dire) if os.path.isfile(os.path.join(dire, f))]
+onlyfiles.sort()
 
 for filename in onlyfiles:
-  name, ext = os.path.splitext(filename)
 
   if(filename == "Makefile.am"):
     continue;
@@ -21,6 +21,8 @@ for filename in onlyfiles:
 
   if(filename == "transform.py"):
     continue;
+
+  name, ext = os.path.splitext(filename)
 
   if(ext != ".h" and ext != ".hs"):
     print "Remove this file, it's not needed: " + filename
