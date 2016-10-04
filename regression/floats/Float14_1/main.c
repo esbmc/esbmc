e339382c4f9ef02260720e0799bb1f7442c4ebe6
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-10-04 12:46:50 +0100

    New test cases without __ESBMC_* calls to check isnan, isfinite, isinf
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ regression/floats/Float14_1/main.c
@@ -0,0 +1,14 @@
+int main()
+{
+  float temp;
+  
+  temp = 1.8e307f + 1.5e50f;	// should produce overflow -> +infinity (according to standard)
+  assert(isinf(temp));
+  
+  float x;
+  
+  x=temp-temp;
+  
+  // should be +inf
+  assert(isinf(temp));
+}
