08ecb11fe4f276ce8326642a48177980f01f7c03
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-10-05 18:03:52 +0100

    Update tests
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/floats/Float14_1/main.c
+++ regression/floats/Float14_1/main.c
@@ -3,12 +3,12 @@ int main()
   float temp;
   
   temp = 1.8e307f + 1.5e50f;	// should produce overflow -> +infinity (according to standard)
-  assert(isinf(temp));
+  assert(isinff(temp));
   
   float x;
   
   x=temp-temp;
   
   // should be +inf
-  assert(isinf(temp));
+  assert(isinff(temp));
 }
--- regression/floats/Float18/main.c
+++ regression/floats/Float18/main.c
@@ -1,4 +1,3 @@
-#define _USE_MATH_DEFINES 
 #include <math.h>
 
 int main()
