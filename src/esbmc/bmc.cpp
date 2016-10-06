e3bd813043419bc62ff6af9470485a35471722a3
Hussama Ismail <hussamaismail@gmail.com>
2016-10-06 10:24:31 +0200

    applying comments on correctness witness prototype.

--- src/esbmc/bmc.cpp
+++ src/esbmc/bmc.cpp
@@ -100,7 +100,7 @@ void bmct::successful_trace(smt_convt &smt_conv,
   switch(ui)
   {
     case ui_message_handlert::GRAPHML:      
-      /* FIXME - implement a correctness witness */
+      /* TODO - implement a correctness witness */
       // status("Building successful trace");
       // build_goto_trace(equation, smt_conv, goto_trace);
       // generate_goto_trace_in_graphml_format(
@@ -644,8 +644,8 @@ bool bmct::run_thread()
 
     if(result->remaining_claims==0)
     {
-      /** TODO - modify to print an empty graphml if the */
-      /* successful_trace(runtime_solver,*equation); */
+      /* TODO - implement a correctness witness */
+      // successful_trace(runtime_solver,*equation);
       report_success();
       return false;
     }
