4bffee68495ac90c12090ce9ac2b648420470400
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-10-14 16:12:43 +0100

    Update model for fmod* and fabs*
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/ansi-c/library/math.c
+++ src/ansi-c/library/math.c
@@ -54,13 +54,128 @@ enum
 #endif
 #endif
 
-double fabs(double x) { return (x < 0) ? -x : x; }
+double fabs(double d)
+{
+  if(d == 0.0)
+    return 0.0;
+  if(__ESBMC_isfinited(d))
+    return INFINITY;
+  if(__ESBMC_isnand(d))
+    return NAN;
+  return __ESBMC_fabs(d);
+}
 
-double fmod(double a, double b) { return a - (b * (int)(a/b)); }
+long double fabsl(long double ld)
+{
+  if(ld == 0.0)
+    return 0.0;
+  if(__ESBMC_isfinitel(ld))
+    return INFINITY;
+  if(__ESBMC_isnanl(ld))
+    return NAN;
+  return __ESBMC_fabsl(ld);
+}
 
-long double fabsl(long double d) { return __ESBMC_fabsl(d); }
+float fabsf(float f)
+{
+  if(f == 0.0)
+    return 0.0;
+  if(__ESBMC_isfinitef(f))
+    return INFINITY;
+  if(__ESBMC_isnanf(f))
+    return NAN;
+  return __ESBMC_fabsf(f);
+}
 
-float fabsf(float f) { return __ESBMC_fabsf(f); }
+double fmod(double x, double y)
+{
+  // If either argument is NaN, NaN is returned
+  if(__ESBMC_isnand(x) || __ESBMC_isnand(y))
+    return NAN;
+
+  // If x is +inf/-inf and y is not NaN, NaN is returned and FE_INVALID is raised
+  if(__ESBMC_isinfd(x))
+    return NAN;
+
+  // If y is +0.0/-0.0 and x is not NaN, NaN is returned and FE_INVALID is raised
+  if(y == 0.0)
+    return NAN;
+
+  // If x is +0.0/-0.0 and y is not zero, +0.0/-0.0 is returned
+  if((x == 0.0) && (y != 0.0))
+  {
+    if(__ESBMC_signd(x))
+      return -0.0;
+    else
+      return +0.0;
+  }
+
+  // If y is +inf/-inf and x is finite, x is returned.
+  if(__ESBMC_isinfd(y) && __ESBMC_isfinited(x))
+    return x;
+
+  return x - (y * (int)(x/y));
+}
+
+float fmodf(float x, float y)
+{
+  // If either argument is NaN, NaN is returned
+  if(__ESBMC_isnanf(x) || __ESBMC_isnanf(y))
+    return NAN;
+
+  // If x is +inf/-inf and y is not NaN, NaN is returned and FE_INVALID is raised
+  if(__ESBMC_isinff(x))
+    return NAN;
+
+  // If y is +0.0/-0.0 and x is not NaN, NaN is returned and FE_INVALID is raised
+  if(y == 0.0)
+    return NAN;
+
+  // If x is +0.0/-0.0 and y is not zero, +0.0/-0.0 is returned
+  if((x == 0.0) && (y != 0.0))
+  {
+    if(__ESBMC_signf(x))
+      return -0.0;
+    else
+      return +0.0;
+  }
+
+  // If y is +inf/-inf and x is finite, x is returned.
+  if(__ESBMC_isinff(y) && __ESBMC_isfinitef(x))
+    return x;
+
+  return x - (y * (int)(x/y));
+}
+
+long double fmodl(long double x, long double y)
+{
+  // If either argument is NaN, NaN is returned
+  if(__ESBMC_isnanld(x) || __ESBMC_isnanld(y))
+    return NAN;
+
+  // If x is +inf/-inf and y is not NaN, NaN is returned and FE_INVALID is raised
+  if(__ESBMC_isinfld(x))
+    return NAN;
+
+  // If y is +0.0/-0.0 and x is not NaN, NaN is returned and FE_INVALID is raised
+  if(y == 0.0)
+    return NAN;
+
+  // If x is +0.0/-0.0 and y is not zero, +0.0/-0.0 is returned
+  if((x == 0.0) && (y != 0.0))
+  {
+    if(__ESBMC_signld(x))
+      return -0.0;
+    else
+      return +0.0;
+  }
+
+  // If y is +inf/-inf and x is finite, x is returned.
+  if(__ESBMC_isinfld(y) && __ESBMC_isfiniteld(x))
+    return x;
+
+  return x - (y * (int)(x/y));
+}
 
 int isfinite(double d) { return __ESBMC_isfinited(d); }
 
