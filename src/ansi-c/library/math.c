45ec32e69455a473ab37fc108cd04e71705186b2
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-10-16 17:34:29 +0100

    Model for copysign*
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/ansi-c/library/math.c
+++ src/ansi-c/library/math.c
@@ -339,6 +339,48 @@ long long llrint(double d)  { return nearbyint(d); }
 
 long long llrintl(long double ld) { return nearbyintl(ld); }
 
+float copysignf(float x, float y)
+{
+  // Easy case, both signs are equal, just return x
+  if(__ESBMC_signf(x) == __ESBMC_signf(y))
+    return x;
+
+  _Bool y_sign = __ESBMC_signf(y);
+
+  if(__ESBMC_isnanf(x))
+    return y_sign ? -NAN : NAN;
+
+  return y_sign ? -__ESBMC_fabsf(x) : __ESBMC_fabsf(x);
+}
+
+double copysign(double x, double y)
+{
+  // Easy case, both signs are equal, just return x
+  if(__ESBMC_signd(x) == __ESBMC_signd(y))
+    return x;
+
+  _Bool y_sign = __ESBMC_signd(y);
+
+  if(__ESBMC_isnand(x))
+    return y_sign ? -NAN : NAN;
+
+  return y_sign ? -__ESBMC_fabsd(x) : __ESBMC_fabsd(x);
+}
+
+long double copysignl(long double x, long double y)
+{
+  // Easy case, both signs are equal, just return x
+  if(__ESBMC_signld(x) == __ESBMC_signld(y))
+    return x;
+
+  _Bool y_sign = __ESBMC_signld(y);
+
+  if(__ESBMC_isnanld(x))
+    return y_sign ? -NAN : NAN;
+
+  return y_sign ? -__ESBMC_fabsld(x) : __ESBMC_fabsld(x);
+}
+
 int isfinite(double d) { return __ESBMC_isfinited(d); }
 
 int __finite(double d) { return __ESBMC_isfinited(d); }
