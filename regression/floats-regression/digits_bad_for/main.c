1df9eed2a63cb875586f5a5d9fe21ff8cf4f619d
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2016-10-19 13:52:44 +0100

    New test cases
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ regression/floats-regression/digits_bad_for/main.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main()
+{
+  double x = 1.0/7.0;
+  long long res = 0;
+
+  for(int i = 1; x != 0; i++)
+  {
+    res += ((int)(x * 10) % 10) * (i * 10);
+    x = (x * 10) - (int) x * 10;
+  }
+
+  assert(res > 56430);
+  return 0;
+}
+
---
+++ regression/floats-regression/digits_bad_while/main.c
@@ -0,0 +1,21 @@
+#include <math.h>
+#include <stdio.h>
+#include <assert.h>
+
+int main()
+{
+  double x = 1.0/7.0;
+  long long res = 0;
+
+  int i = 1;
+  while(x != 0)
+  {
+    res += ((int)(x * 10) % 10) * (i * 10);
+    x = (x * 10) - (int) x * 10;
+    i++;
+  }
+
+  assert(res > 56430);
+  return 0;
+}
+
