ac42c5ef78be14e87b8c90728aba6f0fbd483401
Jeremy Morse <jmorse+git@studentrobotics.org>
2016-12-13 16:13:15 +0000

    Update some changed expr names

--- regression/python/tests/basic.py
+++ regression/python/tests/basic.py
@@ -19,7 +19,7 @@ class Basic(unittest.TestCase):
 
     def test_exprs(self):
         import esbmc
-        base_exprs = ['abs', 'add', 'address_of', 'and', 'ashr', 'bitand',\
+        base_exprs = ['abs', 'add', 'address_of', 'and_', 'ashr', 'bitand',\
                 'bitnand', 'bitnor', 'bitnot', 'bitnxor', 'bitor', 'bitxor',\
                 'byte_extract', 'byte_update', 'code_asm', 'code_assign',\
                 'code_block', 'code_comma_id', 'code_decl', 'code_expression',\
@@ -34,12 +34,12 @@ class Basic(unittest.TestCase):
                 'expr_ids', 'expr_vec', 'greaterthan', 'greaterthanequal',\
                 'if', 'implies', 'index', 'invalid', 'invalid_pointer',\
                 'isinf', 'isnan', 'isnormal', 'lessthan', 'lessthanequal',\
-                'lshr', 'member', 'modulus', 'mul', 'neg', 'not', 'notequal',\
-                'object_descriptor', 'or', 'overflow', 'overflow_cast',\
+                'lshr', 'member', 'modulus', 'mul', 'neg', 'not_', 'notequal',\
+                'object_descriptor', 'or_', 'overflow', 'overflow_cast',\
                 'overflow_neg', 'pointer_object', 'pointer_offset',\
                 'same_object', 'shl', 'sideeffect', 'sideeffect_allockind',\
                 'sub', 'symbol', 'symbol_renaming', 'typecast', 'unknown',\
-                'valid_object', 'with', 'xor']
+                'valid_object', 'with_', 'xor']
         for x in base_exprs:
             self.assertTrue(x in dir(esbmc.expr), "Missing expr {}".format(x))
 
