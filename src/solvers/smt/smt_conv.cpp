a1f0501e5b66bf05bef79876db75b0e781ee5816
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-01-12 19:50:43 +0000

    Set arbitrary value for domain size when creating a store operation in integer mode
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/solvers/smt/smt_conv.cpp
+++ src/solvers/smt/smt_conv.cpp
@@ -2568,9 +2568,8 @@ smt_ast::update(smt_convt *ctx, smt_astt value, unsigned int idx,
   // We're an array; just generate a 'with' operation.
   expr2tc index;
   if (is_nil_expr(idx_expr)) {
-    assert(sort->domain_width != 0 && "Array sort with zero-sized domain "
-           "width");
-    index = constant_int2tc(type2tc(new unsignedbv_type2t(sort->domain_width)),
+    unsigned int dom_width = ctx->int_encoding ? 32 : sort->domain_width;
+    index = constant_int2tc(type2tc(new unsignedbv_type2t(dom_width)),
           BigInt(idx));
   } else {
     index = idx_expr;
