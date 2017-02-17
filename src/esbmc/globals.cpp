2b87c2f6c85aecd567a63a9d0aeb95eb0f203c33
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-02-17 00:01:35 +0000

    Export a version-number constant
    This can be used for programatic determination of what ESBMC version is
    in some arbitary shared object. This is more robust than running strings
    over it from inside some code.

--- src/esbmc/globals.cpp
+++ src/esbmc/globals.cpp
@@ -1,4 +1,5 @@
 #include <langapi/mode.h>
+#include <esbmc/version.h>
 
 const mode_table_et mode_table[] =
 {
@@ -15,3 +16,7 @@ const mode_table_et mode_table[] =
 
 extern "C" uint8_t buildidstring_buf[1];
 uint8_t *esbmc_version_string = buildidstring_buf;
+
+extern "C" {
+uint64_t esbmc_version = ESBMC_VERSION_CONST;
+}
