ca837313ca99c9104096c593af74767d9616995d
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-03-06 17:18:03 +0000

    Update error messages on the new smt fp api
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/solvers/smt/fp_conv.cpp
+++ src/solvers/smt/fp_conv.cpp
@@ -13,16 +13,14 @@ fp_convt::fp_convt(smt_convt *_ctx) : ctx(_ctx)
 
 smt_astt fp_convt::mk_smt_fpbv(const ieee_floatt& thereal)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers\n";
   (void) thereal;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_fpbv_nan(unsigned ew, unsigned sw)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers (NaN)\n";
   (void) ew;
   (void) sw;
   abort();
@@ -30,8 +28,8 @@ smt_astt fp_convt::mk_smt_fpbv_nan(unsigned ew, unsigned sw)
 
 smt_astt fp_convt::mk_smt_fpbv_inf(bool sgn, unsigned ew, unsigned sw)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers "
+            << "(INFINITY)\n";
   (void) sgn;
   (void) ew;
   (void) sw;
@@ -40,56 +38,55 @@ smt_astt fp_convt::mk_smt_fpbv_inf(bool sgn, unsigned ew, unsigned sw)
 
 smt_astt fp_convt::mk_smt_fpbv_rm(ieee_floatt::rounding_modet rm)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers "
+            << "(rounding mode)\n";
   (void) rm;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_typecast_from_fpbv(const typecast2t& cast)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support cast from floating-point "
+            << "numbers\n";
   (void) cast;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_typecast_to_fpbv(const typecast2t& cast)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support cast to floating-point "
+            << "numbers\n";
   (void) cast;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_nearbyint_from_float(const nearbyint2t& expr)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support nearby int from floating-point "
+            << "numbers\n";
   (void) expr;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_fpbv_arith_ops(const expr2tc& expr)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support arithmetic operations with "
+            << "floating-point numbers\n";
   (void) expr;
   abort();
 }
 
 smt_astt fp_convt::mk_smt_fpbv_fma(const expr2tc& expr)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support fused-multiply add "
+            << "floating-point numbers\n";
   (void) expr;
   abort();
 }
 
 smt_sortt fp_convt::mk_fpbv_sort(const unsigned ew, const unsigned sw)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers (type)\n";
   (void) ew;
   (void) sw;
   abort();
@@ -97,8 +94,7 @@ smt_sortt fp_convt::mk_fpbv_sort(const unsigned ew, const unsigned sw)
 
 expr2tc fp_convt::get_fpbv(smt_astt a)
 {
-  std::cerr << __PRETTY_FUNCTION__ <<
-    " not implemented for the chosen solver" << std::endl;
+  std::cerr << "Chosen solver doesn't support floating-point numbers (cex)\n";
   (void) a;
   abort();
 }
