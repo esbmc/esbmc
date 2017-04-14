9b04dd031c2120f5baf7f61e1661815a529c9d74
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-04-14 18:30:17 +0100

    New stub for AST creation
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

---
+++ src/clang-c-frontend/AST/build_ast.cpp
@@ -0,0 +1,9 @@
+/*
+ * build_ast.cpp
+ *
+ *  Created on: Apr 14, 2017
+ *      Author: mramalho
+ */
+
+#include "build_ast.h"
+
---
+++ src/clang-c-frontend/AST/build_ast.h
@@ -0,0 +1,15 @@
+/*
+ * build_ast.h
+ *
+ *  Created on: Apr 14, 2017
+ *      Author: mramalho
+ */
+
+#ifndef CLANG_C_FRONTEND_AST_BUILD_AST_H_
+#define CLANG_C_FRONTEND_AST_BUILD_AST_H_
+
+class build_ast
+{
+};
+
+#endif /* CLANG_C_FRONTEND_AST_BUILD_AST_H_ */
