001e3200ad00dd0ab3f6ca2b0bacd33f61677702
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-04-25 18:07:24 +0100

    Create ASTunit from the compilerInvocation
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-c-frontend/AST/build_ast.cpp
+++ src/clang-c-frontend/AST/build_ast.cpp
@@ -7,10 +7,16 @@
 
 #include "build_ast.h"
 
+#include <clang/Driver/Compilation.h>
+#include <clang/Driver/Driver.h>
+#include <clang/Driver/Options.h>
 #include <clang/Frontend/ASTUnit.h>
 #include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/CompilerInvocation.h>
 #include <clang/Frontend/FrontendActions.h>
+#include <clang/Frontend/TextDiagnosticPrinter.h>
 #include <clang/Tooling/Tooling.h>
+#include <llvm/Option/ArgList.h>
 
 std::unique_ptr<clang::ASTUnit> buildASTs(
   std::string intrinsics,
@@ -30,10 +36,63 @@ std::unique_ptr<clang::ASTUnit> buildASTs(
   llvm::IntrusiveRefCntPtr<clang::FileManager> Files(
     new clang::FileManager(clang::FileSystemOptions(), OverlayFileSystem));
 
-  clang::tooling::ToolInvocation Invocation(
-    compiler_args, new clang::ASTPrintAction, Files.get());
+  llvm::IntrusiveRefCntPtr<clang::DiagnosticOptions> DiagOpts =
+    new clang::DiagnosticOptions();
 
-  Invocation.run();
+  std::unique_ptr<llvm::opt::OptTable> Opts(clang::driver::createDriverOptTable());
 
-  return nullptr;
+  std::vector<const char*> Argv;
+  for (const std::string &Str : compiler_args)
+    Argv.push_back(Str.c_str());
+
+  unsigned MissingArgIndex, MissingArgCount;
+  llvm::opt::InputArgList ParsedArgs = Opts->ParseArgs(
+    llvm::ArrayRef<const char *>(Argv).slice(1),
+    MissingArgIndex,
+    MissingArgCount);
+
+  clang::ParseDiagnosticArgs(*DiagOpts, ParsedArgs);
+
+  clang::TextDiagnosticPrinter *DiagnosticPrinter =
+    new clang::TextDiagnosticPrinter(llvm::errs(), &*DiagOpts);
+
+  clang::DiagnosticsEngine *Diagnostics =
+    new clang::DiagnosticsEngine(
+      llvm::IntrusiveRefCntPtr<clang::DiagnosticIDs>(
+        new clang::DiagnosticIDs()),
+        &*DiagOpts,
+        DiagnosticPrinter,
+        false);
+
+  const std::unique_ptr<clang::driver::Driver> Driver(
+    new clang::driver::Driver(
+      "clang-tool",
+      llvm::sys::getDefaultTargetTriple(),
+      *Diagnostics,
+      std::move(Files->getVirtualFileSystem())));
+  Driver->setTitle("clang_based_tool");
+
+  // Since the input might only be virtual, don't check whether it exists.
+  Driver->setCheckInputsExist(false);
+
+  const std::unique_ptr<clang::driver::Compilation> Compilation(
+    Driver->BuildCompilation(llvm::makeArrayRef(Argv)));
+
+  const clang::driver::JobList &Jobs = Compilation->getJobs();
+  assert(Jobs.size() == 1);
+
+  const llvm::opt::ArgStringList *const CC1Args = &Jobs.begin()->getArguments();
+
+  std::unique_ptr<clang::CompilerInvocation> Invocation(
+    clang::tooling::newInvocation(Diagnostics, *CC1Args));
+
+  std::unique_ptr<clang::ASTUnit> unit(
+    clang::ASTUnit::LoadFromCompilerInvocationAction(
+      std::move(Invocation),
+      std::make_shared<clang::PCHContainerOperations>(),
+      Diagnostics,
+      new clang::SyntaxOnlyAction));
+  assert(unit);
+
+  return std::move(unit);
 }
