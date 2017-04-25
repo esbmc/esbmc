705fe0825520fd43291b5b20da51fae436fcea3b
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-04-25 15:48:40 +0100

    Small example: dump AST
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-c-frontend/AST/build_ast.cpp
+++ src/clang-c-frontend/AST/build_ast.cpp
@@ -8,6 +8,9 @@
 #include "build_ast.h"
 
 #include <clang/Frontend/ASTUnit.h>
+#include <clang/Frontend/CompilerInstance.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
 
 std::unique_ptr<clang::ASTUnit> buildASTs(
   std::string intrinsics,
@@ -15,5 +18,22 @@ std::unique_ptr<clang::ASTUnit> buildASTs(
   std::vector<std::string> clang_headers_name,
   std::vector<std::string> clang_headers_content)
 {
+  // Create virtual file system to add clang's headers
+  llvm::IntrusiveRefCntPtr<clang::vfs::OverlayFileSystem> OverlayFileSystem(
+    new clang::vfs::OverlayFileSystem(clang::vfs::getRealFileSystem()));
+
+  llvm::IntrusiveRefCntPtr<clang::vfs::InMemoryFileSystem> InMemoryFileSystem(
+    new clang::vfs::InMemoryFileSystem);
+
+  OverlayFileSystem->pushOverlay(InMemoryFileSystem);
+
+  llvm::IntrusiveRefCntPtr<clang::FileManager> Files(
+    new clang::FileManager(clang::FileSystemOptions(), OverlayFileSystem));
+
+  clang::tooling::ToolInvocation Invocation(
+    compiler_args, new clang::ASTPrintAction, Files.get());
+
+  Invocation.run();
+
   return nullptr;
 }
