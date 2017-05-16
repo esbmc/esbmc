932d559ca1dc5fa46b9e4f69f3ef766ca86a2981
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-05-16 22:48:17 +0100

    New model for sqrt
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/c2goto/library/libm/sqrt.c
+++ src/c2goto/library/libm/sqrt.c
@@ -8,7 +8,7 @@
 /*Returns the square root of n. Note that the function */
 /*Babylonian method*/
 /*http://www.geeksforgeeks.org/square-root-of-a-perfect-square/*/
-double sqrt(double n)
+double babylonian_sqrt(double n)
 {
   /*We are using n itself as initial approximation
    This can definitely be improved */
@@ -24,3 +24,83 @@ double sqrt(double n)
   return x;
 }
 
+float sqrtf(float n)
+{
+  __ESBMC_HIDE:;
+
+  // If not running in floatbv mode, using our old method
+  if(!__ESBMC_floatbv_mode())
+    return babylonian_sqrt(n);
+
+  // If n is NaN, return NaN
+  if(__ESBMC_isnanf(n))
+    return NAN;
+
+  // If n == +/-0.0, return +/- 0.0
+  if(n == 0.0)
+    return n;
+
+  // if n < 0.0, return NAN
+  if(n < 0.0)
+    return NAN;
+
+  // if +inf, return +inf
+  if(__ESBMC_isinff(n))
+    return INFINITY;
+
+  return __ESBMC_sqrtf(n);
+}
+
+double sqrt(double n)
+{
+  __ESBMC_HIDE:;
+
+  // If not running in floatbv mode, using our old method
+  if(!__ESBMC_floatbv_mode())
+    return babylonian_sqrt(n);
+
+  // If n is NaN, return NaN
+  if(__ESBMC_isnand(n))
+    return NAN;
+
+  // If n == +/-0.0, return +/- 0.0
+  if(n == 0.0)
+    return n;
+
+  // if n < 0.0, return NAN
+  if(n < 0.0)
+    return NAN;
+
+  // if +inf, return +inf
+  if(__ESBMC_isinfd(n))
+    return INFINITY;
+
+  return __ESBMC_sqrtd(n);
+}
+
+long double sqrtl(long double n)
+{
+  __ESBMC_HIDE:;
+
+  // If not running in floatbv mode, using our old method
+  if(!__ESBMC_floatbv_mode())
+    return babylonian_sqrt(n);
+
+  // If n is NaN, return NaN
+  if(__ESBMC_isnanld(n))
+    return NAN;
+
+  // If n == +/-0.0, return +/- 0.0
+  if(n == 0.0)
+    return n;
+
+  // if n < 0.0, return NAN
+  if(n < 0.0)
+    return NAN;
+
+  // if +inf, return +inf
+  if(__ESBMC_isinfld(n))
+    return INFINITY;
+
+  return __ESBMC_sqrtld(n);
+}
