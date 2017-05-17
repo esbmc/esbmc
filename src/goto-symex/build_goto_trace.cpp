4bc860e4897f1240789adf21e5aa19783800b8db
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-05-17 14:30:40 +0100

    Cleanup
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/goto-symex/build_goto_trace.cpp
+++ src/goto-symex/build_goto_trace.cpp
@@ -1,13 +1,3 @@
-/*******************************************************************\
-
-Module: Traces of GOTO Programs
-
-Author: Daniel Kroening
-
-  Date: July 2005
-
-\*******************************************************************/
-
 #include <cassert>
 #include <goto-symex/build_goto_trace.h>
 #include <goto-symex/witnesses.h>
@@ -17,38 +7,32 @@ void build_goto_trace(
   smt_convt &smt_conv,
   goto_tracet &goto_trace)
 {
-  unsigned step_nr=0;
+  unsigned step_nr = 0;
 
-  for(symex_target_equationt::SSA_stepst::const_iterator
-      it=target.SSA_steps.begin();
-      it!=target.SSA_steps.end();
-      it++)
+  for(auto SSA_step : target.SSA_steps)
   {
-
-    const symex_target_equationt::SSA_stept &SSA_step=*it;
     tvt result = smt_conv.l_get(SSA_step.guard_ast);
-
-    if(result != tvt(true))
+    if(!result.is_true())
       continue;
 
     if(SSA_step.assignment_type == symex_target_equationt::HIDDEN
-       && it->is_assignment())
+       && SSA_step.is_assignment())
       continue;
 
     step_nr++;
 
     goto_trace.steps.push_back(goto_trace_stept());
-    goto_trace_stept &goto_trace_step=goto_trace.steps.back();
+    goto_trace_stept &goto_trace_step = goto_trace.steps.back();
 
-    goto_trace_step.thread_nr=SSA_step.source.thread_nr;
-    goto_trace_step.lhs=SSA_step.lhs;
-    goto_trace_step.rhs=SSA_step.rhs;
-    goto_trace_step.pc=SSA_step.source.pc;
-    goto_trace_step.comment=SSA_step.comment;
-    goto_trace_step.original_lhs=SSA_step.original_lhs;
-    goto_trace_step.type=SSA_step.type;
-    goto_trace_step.step_nr=step_nr;
-    goto_trace_step.format_string=SSA_step.format_string;
+    goto_trace_step.thread_nr = SSA_step.source.thread_nr;
+    goto_trace_step.lhs = SSA_step.lhs;
+    goto_trace_step.rhs = SSA_step.rhs;
+    goto_trace_step.pc = SSA_step.source.pc;
+    goto_trace_step.comment = SSA_step.comment;
+    goto_trace_step.original_lhs = SSA_step.original_lhs;
+    goto_trace_step.type = SSA_step.type;
+    goto_trace_step.step_nr = step_nr;
+    goto_trace_step.format_string = SSA_step.format_string;
     goto_trace_step.stack_trace = SSA_step.stack_trace;
 
     // If we're dealing with an array_of, there's no need to fetch the rhs
@@ -57,16 +41,12 @@ void build_goto_trace(
     else if(!is_nil_expr(SSA_step.lhs))
       goto_trace_step.value = smt_conv.get(SSA_step.lhs);
 
-    for(std::list<expr2tc>::const_iterator
-        j=SSA_step.converted_output_args.begin();
-        j!=SSA_step.converted_output_args.end();
-        j++)
+    for(auto it : SSA_step.converted_output_args)
     {
-      const expr2tc &arg = *j;
-      if (is_constant_expr(arg))
-        goto_trace_step.output_args.push_back(arg);
+      if (is_constant_expr(it))
+        goto_trace_step.output_args.push_back(it);
       else
-        goto_trace_step.output_args.push_back(smt_conv.get(arg));
+        goto_trace_step.output_args.push_back(smt_conv.get(it));
     }
 
     if(SSA_step.is_assert() || SSA_step.is_assume())
