d3ef283720abc409d9a1a2b23946d22c0a39358b
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-06-05 18:50:03 +0100

    No need to remove prefix anymore
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/goto-symex/symex_main.cpp
+++ src/goto-symex/symex_main.cpp
@@ -198,8 +198,7 @@ goto_symext::symex_step(reachability_treet & art)
       const irep_idt &id = to_symbol2t(call.function).thename;
       if (has_prefix(id.as_string(), "__ESBMC")) {
         cur_state->source.pc++;
-        std::string name = id.as_string().substr(3);
-        run_intrinsic(call, art, name);
+        run_intrinsic(call, art, id.as_string());
         return;
       }
     }
