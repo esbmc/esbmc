7396dc658527714c4aee7cf552d8cc365f467bda
Jeremy Morse <jmorse+git@studentrobotics.org>
2017-06-22 11:46:36 +0100

    Remove some now defective code
    This was previously dead -- eq was _copied_, sliced, then discarded. Now
    that it's stuffed in a shared_ptr and auto is used though, the true
    formula gets sliced, causing trouble later on.

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -41,10 +41,6 @@ goto_symext::symex_goto(const expr2tc &old_guard)
 
     equality2tc question(gen_true_expr(), new_guard);
     try {
-      symex_slicet slicer;
-      auto eq = boost::dynamic_pointer_cast<symex_target_equationt>(target);
-      slicer.slice_for_symbols(eq, question);
-
       tvt res = rte.get()->ask_solver_question(question);
 
       if (res.is_false())
