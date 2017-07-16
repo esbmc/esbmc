1d8341dab2a26f1537332e1e61a095de778aab04
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-07-16 15:24:04 +0100

    Remove option to compile without clang
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/c2goto/c2goto.cpp
+++ src/c2goto/c2goto.cpp
@@ -66,13 +66,8 @@ int main(int argc, const char **argv)
 
 const mode_table_et mode_table[] =
 {
-#ifndef WITHOUT_CLANG
   LANGAPI_HAVE_MODE_CLANG_C,
-#endif
   LANGAPI_HAVE_MODE_C,
-#ifndef WITHOUT_CLANG
-  LANGAPI_HAVE_MODE_CPP,
-#endif
   LANGAPI_HAVE_MODE_CPP,
   LANGAPI_HAVE_MODE_END
 };
