61031a145e14cb90ba47b10826febfe336dd20fc
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-07-18 19:49:47 +0100

    Add the members from base classes to derived classes
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-cpp-frontend/clang_cpp_convert.cpp
+++ src/clang-cpp-frontend/clang_cpp_convert.cpp
@@ -49,3 +49,43 @@ bool clang_cpp_convertert::get_decl(const clang::Decl &decl, exprt &new_expr)
 
   return false;
 }
+
+bool clang_cpp_convertert::get_struct_union_class_fields(
+  const clang::RecordDecl &recordd,
+  struct_union_typet &type)
+{
+  // If a struct is defined inside a extern C, it will be a RecordDecl
+  const clang::CXXRecordDecl* cxxrd =
+    llvm::dyn_cast<clang::CXXRecordDecl>(&recordd);
+  if(cxxrd != nullptr)
+  {
+    // So this is a CXXRecordDecl, let's check for (virtual) base classes
+    for(const auto &decl : cxxrd->bases())
+    {
+      // The base class is always a CXXRecordDecl
+      const clang::CXXRecordDecl* base =
+        decl.getType().getTypePtr()->getAsCXXRecordDecl();
+      assert(base != nullptr);
+
+      for(const auto &field : base->fields())
+      {
+        // We don't add if private
+        if(field->getAccess() >= clang::AS_private)
+          continue;
+
+        struct_typet::componentt comp;
+        if(get_decl(*field, comp))
+          return true;
+
+        // Don't add fields that have global storage (e.g., static)
+        if(const clang::VarDecl* nd = llvm::dyn_cast<clang::VarDecl>(field))
+          if(nd->hasGlobalStorage())
+            continue;
+
+        type.components().push_back(comp);
+      }
+    }
+  }
+
+  return clang_c_convertert::get_struct_union_class_fields(recordd, type);
+}
