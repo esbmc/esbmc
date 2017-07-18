3c66de49a8431501e49de369927f633439d27042
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-07-18 19:47:00 +0100

    Parse all declaration inside a linkage spec
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-cpp-frontend/clang_cpp_convert.cpp
+++ src/clang-cpp-frontend/clang_cpp_convert.cpp
@@ -13,10 +13,38 @@
 #include <clang/AST/ExprCXX.h>
 #include <clang/AST/StmtCXX.h>
 #include <cpp/cpp_type2name.h>
+#include <util/std_code.h>
+#include <util/std_expr.h>
 
 clang_cpp_convertert::clang_cpp_convertert(
   contextt &_context,
   std::vector<std::unique_ptr<clang::ASTUnit> > &_ASTs)
   : clang_c_convertert(_context, _ASTs)
 {
+}
+
+bool clang_cpp_convertert::get_decl(
+  const clang::Decl& decl,
+  exprt &new_expr)
+{
+  new_expr = code_skipt();
+
+  switch (decl.getKind())
+  {
+    case clang::Decl::LinkageSpec:
+    {
+      const clang::LinkageSpecDecl &lsd =
+        static_cast<const clang::LinkageSpecDecl&>(decl);
+
+      for(auto decl : lsd.decls())
+        if(get_decl(*decl, new_expr))
+          return true;
+      break;
+    }
+
+    default:
+      return clang_c_convertert::get_decl(decl, new_expr);
+  }
+
+  return false;
 }
