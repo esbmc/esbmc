03c06d1b55d2f03d7524e9bdea4c05ccbd7c4a94
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-07-26 13:40:11 +0100

    Support a number of C++ expr
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-cpp-frontend/clang_cpp_convert.h
+++ src/clang-cpp-frontend/clang_cpp_convert.h
@@ -15,24 +15,15 @@ public:
   virtual ~clang_cpp_convertert() = default;
 
 protected:
-  bool get_decl(
-    const clang::Decl &decl,
-    exprt &new_expr) override;
+  bool get_decl(const clang::Decl &decl, exprt &new_expr) override;
 
-  bool get_type(
-    const clang::QualType &type,
-    typet &new_type) override;
+  bool get_type(const clang::QualType &type, typet &new_type) override;
 
-  bool get_type(
-    const clang::Type &the_type,
-    typet &new_type) override;
+  bool get_type(const clang::Type &the_type, typet &new_type) override;
 
-  bool get_function(
-    const clang::FunctionDecl &fd,
-    exprt &new_expr) override;
+  bool get_function(const clang::FunctionDecl &fd, exprt &new_expr) override;
 
-  bool get_struct_union_class(
-    const clang::RecordDecl &rd) override;
+  bool get_struct_union_class(const clang::RecordDecl &rd) override;
 
   bool get_struct_union_class_fields(
     const clang::RecordDecl &recordd,
@@ -42,18 +33,20 @@ protected:
     const clang::RecordDecl &recordd,
     struct_union_typet &type) override;
 
-  template<typename TemplateDecl>
+  template <typename TemplateDecl>
   bool get_template_decl(
     const TemplateDecl *D,
     bool DumpExplicitInst,
     exprt &new_expr);
 
-  template<typename SpecializationDecl>
+  template <typename SpecializationDecl>
   bool get_template_decl_specialization(
     const SpecializationDecl *D,
     bool DumpExplicitInst,
     bool DumpRefOnly,
     exprt &new_expr);
+
+  bool get_expr(const clang::Stmt &stmt, exprt &new_expr) override;
 };
 
 #endif /* CLANG_C_FRONTEND_CLANG_C_CONVERT_H_ */
