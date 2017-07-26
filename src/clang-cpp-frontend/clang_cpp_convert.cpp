67e01fcf8726502807f3b0928b024278d03e46fd
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-07-26 13:25:54 +0100

    Ignore a bunch of declarations that we don't care about
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/clang-cpp-frontend/clang_cpp_convert.cpp
+++ src/clang-cpp-frontend/clang_cpp_convert.cpp
@@ -70,6 +70,16 @@ bool clang_cpp_convertert::get_decl(const clang::Decl &decl, exprt &new_expr)
     break;
   }
 
+  // We can ignore any these declarations
+  case clang::Decl::ClassTemplatePartialSpecialization:
+  case clang::Decl::Using:
+  case clang::Decl::UsingShadow:
+  case clang::Decl::UsingDirective:
+  case clang::Decl::TypeAlias:
+  case clang::Decl::NamespaceAlias:
+  case clang::Decl::AccessSpec:
+    break;
+
   default:
     return clang_c_convertert::get_decl(decl, new_expr);
   }
