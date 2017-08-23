903c279acad15b52cfe17b25d5f6e470ea75050b
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-08-23 15:26:08 +0100

    Rewrite the k-induction code using expr2tc
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/goto-programs/goto_k_induction.h
+++ src/goto-programs/goto_k_induction.h
@@ -12,12 +12,11 @@
 #include <goto-programs/goto_loops.h>
 #include <util/hash_cont.h>
 #include <util/message_stream.h>
-#include <util/std_types.h>
+#include <util/irep2_expr.h>
 
 void goto_k_induction(
   goto_functionst &goto_functions,
   contextt &_context,
-  optionst &options,
   message_handlert &message_handler);
 
 class goto_k_inductiont : public goto_loopst
@@ -28,17 +27,13 @@ public:
     goto_functionst &_goto_functions,
     goto_functiont &_goto_function,
     contextt &_context,
-    optionst &_options,
     message_handlert &_message_handler) :
     goto_loopst(
       _context,
       _function_name,
       _goto_functions,
       _goto_function,
-      _message_handler),
-    state(struct_typet()),
-    constrain_all_states(_options.get_bool_option("constrain-all-states")),
-    options(_options)
+      _message_handler)
   {
     // unwind loops
     if(function_loops.size())
@@ -46,30 +41,25 @@ public:
   }
 
 protected:
-  struct_typet state;
-  bool constrain_all_states;
-  optionst &options;
-
   void goto_k_induction();
-
-  void fill_state(loopst &loop);
 
   void convert_finite_loop(loopst &loop);
 
-  void get_loop_cond(
+  const expr2tc get_loop_cond(
     goto_programt::targett &loop_head,
-    goto_programt::targett &loop_exit,
-    exprt &loop_cond);
+    goto_programt::targett &loop_exit);
 
-  void make_nondet_assign(goto_programt::targett &loop_head);
+  void make_nondet_assign(
+    goto_programt::targett &loop_head,
+    const loopst &loop);
 
   void assume_loop_cond_before_loop(
     goto_programt::targett &loop_head,
-    exprt &loop_cond);
+    expr2tc &loop_cond);
 
   void assume_neg_loop_cond_after_loop(
     goto_programt::targett &loop_exit,
-    exprt &loop_cond);
+    expr2tc &loop_cond);
 
   void duplicate_loop_body(
     goto_programt::targett &loop_head,
@@ -83,13 +73,7 @@ protected:
     goto_programt::targett &loop_head,
     goto_programt::targett &loop_exit);
 
-  void copy(const codet &code,
-    goto_program_instruction_typet type,
-    goto_programt &dest);
-
-  void assume_cond(
-    const exprt &cond,
-    goto_programt &dest);
+  void assume_cond(const expr2tc &cond, goto_programt &dest);
 };
 
 #endif /* GOTO_PROGRAMS_GOTO_K_INDUCTION_H_ */
--- src/goto-programs/loopst.cpp
+++ src/goto-programs/loopst.cpp
@@ -7,44 +7,7 @@
 
 #include <goto-programs/loopst.h>
 
-bool check_var_name(const exprt &expr)
-{
-  std::size_t found = expr.identifier().as_string().find("__ESBMC_");
-  if(found != std::string::npos)
-    return false;
-
-  found = expr.identifier().as_string().find("__CPROVER");
-  if(found != std::string::npos)
-    return false;
-
-  found = expr.identifier().as_string().find("return_value___");
-  if(found != std::string::npos)
-    return false;
-
-  found = expr.identifier().as_string().find("pthread_lib");
-  if(found != std::string::npos)
-    return false;
-
-  // Don't add variables that we created for k-induction
-  found = expr.identifier().as_string().find("$");
-  if(found != std::string::npos)
-    return false;
-
-  if(expr.identifier().as_string() == "__func__"
-     || expr.identifier().as_string() == "__PRETTY_FUNCTION__"
-     || expr.identifier().as_string() == "__LINE__")
-    return false;
-
-  if(expr.location().file().as_string() == "<built-in>"
-     || expr.cmt_location().file().as_string() == "<built-in>"
-     || expr.type().location().file().as_string() == "<built-in>"
-     || expr.type().cmt_location().file().as_string() == "<built-in>")
-    return false;
-
-  return true;
-}
-
-loopst::loop_varst &loopst::get_loop_vars()
+const loopst::loop_varst &loopst::get_loop_vars() const
 {
   return loop_vars;
 }
@@ -74,126 +37,9 @@ void loopst::add_var_to_loop(loopst::loop_varst &_loop_vars)
   loop_vars.insert(_loop_vars.begin(), _loop_vars.end());
 }
 
-void loopst::add_var_to_loop(const exprt &expr)
+void loopst::add_var_to_loop(const expr2tc &expr)
 {
-  loop_vars.insert(
-    std::pair<irep_idt, const exprt>(expr.identifier(), expr));
-}
-
-bool loopst::is_loop_var(exprt& expr)
-{
-  return (loop_vars.find(expr.identifier()) != loop_vars.end());
-}
-
-bool loopst::is_infinite_loop()
-{
-  goto_programt::targett tmp = original_loop_head;
-
-  // First, check if the loop condition is a function
-  // If it is a function, get the guard from the next instruction
-  if(original_loop_head->is_assign())
-  {
-    ++tmp;
-    exprt guard = migrate_expr_back(tmp->guard);
-    return guard.is_true();
-  }
-  else
-  {
-    exprt guard = migrate_expr_back(tmp->guard);
-    assert(!guard.is_nil());
-
-    if(guard.is_true())
-      return true;
-
-    if(guard.is_not())
-      return guard.op0().is_true();
-
-    return false;
-  }
-
-  return false;
-}
-
-bool loopst::is_nondet_loop()
-{
-  goto_programt::targett tmp = original_loop_head;
-
-  // First, check if the loop condition is a function
-  // If it is a function, get the guard from the next instruction
-  if(original_loop_head->is_assign())
-  {
-    ++tmp;
-    exprt guard = migrate_expr_back(tmp->guard);
-    assert(!guard.is_nil());
-    return check_nondet(guard);
-  }
-  else
-  {
-    exprt guard = migrate_expr_back(tmp->guard);
-    assert(!guard.is_nil());
-    if(guard.is_not())
-      return check_nondet(guard.op0());
-    return check_nondet(guard);
-  }
-
-  return false;
-}
-
-bool loopst::check_nondet(exprt &guard)
-{
-  irep_idt exprid = guard.id();
-
-  if(guard.is_false()
-    || guard.is_constant()
-    || guard.is_index())
-  {
-  }
-  else if(exprid == ">"
-    || exprid == ">="
-    || guard.is_typecast())
-  {
-    if (!is_expr_a_constant(guard.op0()))
-      return true;
-  }
-  else if(exprid == "<"
-    || exprid == "<=")
-  {
-    if (!is_expr_a_constant(guard.op1()))
-      return true;
-  }
-  else if (guard.is_and()
-    || guard.is_or()
-    || exprid == "-"
-    || exprid == "+"
-    || exprid == "*"
-    || exprid == "/"
-    || guard.is_notequal())
-  {
-    assert(guard.operands().size()==2);
-
-    check_nondet(guard.op0());
-    check_nondet(guard.op1());
-  }
-  else if (guard.is_dereference())
-  {
-    check_nondet(guard.op0());
-  }
-  else if (guard.is_symbol())
-  {
-    if (!is_expr_a_constant(guard))
-      return true;
-  }
-  else
-  {
-    std::cerr << "**** Warning: this program " << guard.location().get_file()
-              << " contains a '" << guard.id() << "' unsupported operator at line "
-              << guard.location().get_line()
-              << ", so we are not applying the inductive step to this program!"
-              << std::endl;
-    abort();
-  }
-
-  return false;
+  loop_vars.insert(expr);
 }
 
 void loopst::dump()
@@ -218,27 +64,11 @@ void loopst::dump()
 
 void loopst::dump_loop_vars()
 {
-  std::cout << "Loop variables:" << std::endl;
+  std::cout << "Loop variables:\n";
 
-  u_int i = 0;
-  for (std::pair<irep_idt, const exprt> expr : loop_vars)
-    std::cout << ++i << ". \t" << "identifier: " << expr.first << std::endl
-    << " " << expr.second << std::endl << std::endl;
-  std::cout << std::endl;
-}
+  unsigned int i = 0;
+  for (auto var : loop_vars)
+    std::cout << ++i << ". \t" << to_symbol2t(var).thename << '\n';
 
-bool loopst::is_expr_a_constant(exprt& expr)
-{
-  // TODO: handle pointers
-  if (expr.is_constant() || expr.type().id() == "pointer")
-    return true;
-
-  if(expr.is_symbol())
-  {
-    exprt value = namespacet(context).lookup(expr.identifier()).value;
-    if (value.is_constant())
-      return true;
-  }
-
-  return false;
+  std::cout << '\n';
 }
