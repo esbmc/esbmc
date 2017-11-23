b73d79557e597fe52c328c0627dbf0010c5b2c56
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2017-11-23 15:57:40 +0000

    Model for some missing functions in the SV-COMP
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/c2goto/library/stdlib.c
+++ src/c2goto/library/stdlib.c
@@ -69,3 +69,77 @@ char *getenv(const char *name)
   buffer[buf_size-1]=0;
   return buffer;
 }
+
+typedef unsigned int gfp_t;
+
+void *__kmalloc(size_t size, gfp_t flags)
+{
+  (void) flags;
+  return malloc(size);
+}
+
+void *kmalloc(size_t size, gfp_t flags)
+{
+  (void) flags;
+  return malloc(size);
+}
+
+void *kzalloc(size_t size, gfp_t flags)
+{
+  (void) flags;
+  return malloc(size);
+}
+
+void *ldv_malloc(size_t size)
+{
+  return malloc(size);
+}
+
+void *ldv_zalloc(size_t size)
+{
+  return malloc(size);
+}
+
+void *kmalloc_array(size_t n, size_t size, gfp_t flags)
+{
+  return __kmalloc(n * size, flags);
+}
+
+void *kcalloc(size_t n, size_t size, gfp_t flags)
+{
+  (void) flags;
+  return calloc(n, size);
+}
+
+void kfree(void * objp)
+{
+  free(objp);
+}
+
+size_t
+strlcat(char *dst, const char *src, size_t siz)
+{
+  char *d = dst;
+  const char *s = src;
+  size_t n = siz;
+  size_t dlen;
+
+  /* Find the end of dst and adjust bytes left but don't go past end */
+  while (n-- != 0 && *d != '\0')
+    d++;
+  dlen = d - dst;
+  n = siz - dlen;
+
+  if (n == 0)
+    return(dlen + strlen(s));
+  while (*s != '\0') {
+    if (n != 1) {
+      *d++ = *s;
+      n--;
+    }
+    s++;
+  }
+  *d = '\0';
+
+  return(dlen + (s - src)); /* count does not include NUL */
+}
