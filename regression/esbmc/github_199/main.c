93d95c72fcf095e224f6fcdc1d31a7bcf02009fc
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2018-02-15 17:55:03 +0000

    Fix assert
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- regression/esbmc/github_199/main.c
+++ regression/esbmc/github_199/main.c
@@ -5,8 +5,8 @@ int main() {
 
   for (i=0; i<nlen; i++) {
     for (j=0; j<8; j++) {
-      __VERIFIER_assert(0 <= nlen-1-i);
-      __VERIFIER_assert(nlen-1-i < nlen);
+      assert(0 <= nlen-1-i);
+      assert(nlen-1-i < nlen);
     }
   }
   return 0;
