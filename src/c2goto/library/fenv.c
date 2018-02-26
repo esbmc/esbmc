c954f60a1b1600396bdc44bbeb8cf05c22f092ff
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2018-02-26 12:55:57 +0000

    Remove some typedefs
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/c2goto/library/fenv.c
+++ src/c2goto/library/fenv.c
@@ -7,18 +7,14 @@ inline int fegetround(void)
 __ESBMC_HIDE:;
   switch(__ESBMC_rounding_mode)
   {
-#ifdef FE_DOWNWARD
-  case 1:
-    return FE_DOWNWARD;
-#endif
   case 0:
     return FE_TONEAREST;
-  case 3:
-    return FE_TOWARDZERO;
-#ifdef FE_UPWARD
+  case 1:
+    return FE_DOWNWARD;
   case 2:
     return FE_UPWARD;
-#endif
+  case 3:
+    return FE_TOWARDZERO;
   default:
     break;
   }
@@ -30,22 +26,18 @@ inline int fesetround(int rounding_mode)
 __ESBMC_HIDE:;
   switch(rounding_mode)
   {
-#ifdef FE_DOWNWARD
+  case FE_TONEAREST:
+    __ESBMC_rounding_mode = 0;
+    break;
   case FE_DOWNWARD:
     __ESBMC_rounding_mode = 1;
     break;
-#endif
-  case FE_TONEAREST:
-    __ESBMC_rounding_mode = 0;
+  case FE_UPWARD:
+    __ESBMC_rounding_mode = 2;
     break;
   case FE_TOWARDZERO:
     __ESBMC_rounding_mode = 3;
     break;
-#ifdef FE_UPWARD
-  case FE_UPWARD:
-    __ESBMC_rounding_mode = 2;
-    break;
-#endif
   default:
     break;
   }
