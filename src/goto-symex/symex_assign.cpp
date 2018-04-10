6ce369bf6a0902d5f17c585d364b01d211ea4c8e
Mikhail Ramalho <mikhail.ramalho@gmail.com>
2018-04-10 16:58:13 +0100

    Checking parallel k-induction is not necessary in symex
    Signed-off-by: Mikhail Ramalho <mikhail.ramalho@gmail.com>

--- src/goto-symex/symex_assign.cpp
+++ src/goto-symex/symex_assign.cpp
@@ -47,9 +47,7 @@ goto_symext::goto_symext(
     no_simplify(options.get_bool_option("no-simplify")),
     no_unwinding_assertions(options.get_bool_option("no-unwinding-assertions")),
     partial_loops(options.get_bool_option("partial-loops")),
-    k_induction(
-      options.get_bool_option("k-induction") ||
-      options.get_bool_option("k-induction-parallel")),
+    k_induction(options.get_bool_option("k-induction")),
     base_case(options.get_bool_option("base-case")),
     forward_condition(options.get_bool_option("forward-condition")),
     inductive_step(options.get_bool_option("inductive-step"))
