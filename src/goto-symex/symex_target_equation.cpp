84ac2042b6eacb95880dc75fbf44ccbb222063c3
Mikhail Gadelha <mikhail.gadelha@sidia.com>
2019-10-29 17:14:29 -0400

    Fix ssa step dump option
    Signed-off-by: Mikhail Gadelha <mikhail.gadelha@sidia.com>

--- src/goto-symex/symex_target_equation.cpp
+++ src/goto-symex/symex_target_equation.cpp
@@ -294,7 +294,7 @@ void symex_target_equationt::SSA_stept::output(
   if(is_assert())
     out << comment << std::endl;
 
-  if(config.options.get_bool_option("show-guards"))
+  if(config.options.get_bool_option("ssa-guards"))
     out << "Guard: " << from_expr(ns, "", migrate_expr_back(guard))
         << std::endl;
 }
