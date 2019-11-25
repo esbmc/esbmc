e5890cfa042c1bb1f697d27fa97e3256e1527246
feliperodri <rms.felipe@gmail.com>
2019-11-24 23:15:31 -0400

    Add new regression tests for popcount
    Signed-off-by: feliperodri <rms.felipe@gmail.com>

--- regression/esbmc/gcc_popcount_01/main.c
+++ regression/esbmc/gcc_popcount_01/main.c
@@ -2,9 +2,13 @@
 
 #ifndef __GNUC__
 int __builtin_popcount(unsigned int);
+int __builtin_popcountl(unsigned long);
+int __builtin_popcountll(unsigned long long);
 #endif
 
 unsigned int __VERIFIER_nondet_unsigned();
+unsigned long __VERIFIER_nondet_unsigned_long();
+unsigned long long __VERIFIER_nondet_unsigned_long_long();
 
 // Hacker's Delight
 // http://www.hackersdelight.org/permissions.htm
@@ -27,6 +31,9 @@ int main()
 {
   assert(pop4(42) == 3);
   assert(__builtin_popcount(42) == 3);
+  assert(__builtin_popcountl(42) == 3);
+  assert(__builtin_popcountll(42) == 3);
+
   unsigned int u = __VERIFIER_nondet_unsigned();
   assert(pop4(u) == __builtin_popcount(u));
 
