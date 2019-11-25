02e516d8cd62a26078c2bfda6e97866487d8ec48
Mikhail Gadelha <mikhail.ramalho@gmail.com>
2019-11-25 19:45:01 -0400

    Remove some function models that were causing problems
    Signed-off-by: Mikhail Gadelha <mikhail.ramalho@gmail.com>

--- src/c2goto/library/inet.c
+++ src/c2goto/library/inet.c
@@ -1,33 +1,9 @@
-#include <arpa/inet.h>
 #include <stdint.h>
 
 #undef htonl
 #undef htons
 #undef ntohl
 #undef ntohs
-
-in_addr_t __VERIFIER_nondet_in_addr_t();
-in_addr_t inet_addr(const char *cp)
-{
-__ESBMC_HIDE:;
-  (void)*cp;
-  return __VERIFIER_nondet_in_addr_t();
-}
-
-int inet_aton(const char *cp, struct in_addr *pin)
-{
-__ESBMC_HIDE:;
-  (void)*cp;
-  (void)*pin;
-  return __VERIFIER_nondet_int();
-}
-
-in_addr_t inet_network(const char *cp)
-{
-__CPROVER_HIDE:;
-  (void)*cp;
-  return __VERIFIER_nondet_in_addr_t();
-}
 
 uint32_t htonl(uint32_t hostlong)
 {
