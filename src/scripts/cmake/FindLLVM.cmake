a37e656b3d32db1c1f943b72cdab8493b0a7526c
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2019-11-30 06:50:47 -0400

    fixed wrong variable name
    Signed-off-by: Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>

--- src/scripts/cmake/FindLLVM.cmake
+++ src/scripts/cmake/FindLLVM.cmake
@@ -1,6 +1,6 @@
 # Module to find LLVM and checks it's version
 
-if(NOT (${LLVM_DIR} STREQUAL "LLVM_DIR-NOTFOUND"))
+if(NOT (("${LLVM_DIR}" STREQUAL "LLVM_DIR-NOTFOUND") OR ("${LLVM_DIR}" STREQUAL "")))
   set(Clang_DIR ${LLVM_DIR})  
   find_package(LLVM REQUIRED CONFIG
     PATHS ${LLVM_DIR}
@@ -19,6 +19,6 @@ endif()
 if (${LLVM_VERSION_MAJOR} GREATER_EQUAL 7)
   message(STATUS "LLVM version: ${LLVM_VERSION}")
 else()
-  message(SEND_ERROR "Could not find LLVM/Clang 7.0 at all: please specify with -DLLVM_DIR")
+  message(FALTA_ERROR "Could not find LLVM/Clang 7.0 at all: please specify with -DLLVM_DIR")
 endif()
 # BUG: For some reason, ESBMC is not linking with Systems LLVM
