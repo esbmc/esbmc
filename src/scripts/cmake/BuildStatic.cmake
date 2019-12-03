62aa77aadfe44f056aaeb385bd7f1446f9e68a1e
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2019-12-03 15:17:32 -0400

    removed reserverd flags from BuildStatic.cmake

--- src/scripts/cmake/BuildStatic.cmake
+++ src/scripts/cmake/BuildStatic.cmake
@@ -1,8 +1,8 @@
 # Module to configure static build
 
 message(STATUS "ESBMC will be built in static mode")
-set(CMAKE_EXE_LINKER_FLAGS " -static -static-libgcc -static-libstdc++ ")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -lstdc++")
+set(CMAKE_EXE_LINKER_FLAGS " -static")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static")
 set(CMAKE_CC_FLAGS "${CMAKE_CC_FLAGS} -static")
 set(Boost_USE_STATIC_LIBS        ON)
 set(Boost_USE_STATIC_RUNTIME     ON)
