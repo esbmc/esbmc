c6b60d0b014d059c71d2216ac0c0656980a43c0a
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2019-12-26 07:04:16 -0400

    added apple configuration module

---
+++ src/scripts/cmake/AppleConfiguration.cmake
@@ -0,0 +1,15 @@
+# Module to setup Apple specific flags
+
+if (APPLE)
+    message(STATUS "Detected APPLE")
+    # Suppress superfluous warnings about "*.a" having no symbols on macOS X
+	set(CMAKE_C_ARCHIVE_CREATE   "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
+	set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
+	set(CMAKE_C_ARCHIVE_FINISH   "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
+    set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
+
+    # MacOS Z3 does not need libgomp
+    set(LIBGOMP_LIB "")
+else()
+    set(LIBGOMP_LIB "-lgomp")
+endif()
