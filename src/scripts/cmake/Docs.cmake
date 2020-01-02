ef385b6638878a8cbc675123e0f9c6280d88e66c
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2020-01-02 14:06:13 -0400

    when enabled doxygen will be built in all targets
    Signed-off-by: Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>

--- src/scripts/cmake/Docs.cmake
+++ src/scripts/cmake/Docs.cmake
@@ -5,6 +5,7 @@ if(BUILD_DOC)
 
     doxygen_add_docs(docs
             ${PROJECT_SOURCE_DIR}/src
+            ALL
             COMMENT "Generating API documentation with Doxygen")
 
     set(DOXYGEN_GENERATE_HTML NO)
@@ -12,8 +13,7 @@ if(BUILD_DOC)
 
     doxygen_add_docs(man
             ${PROJECT_SOURCE_DIR}/src
+            ALL
             COMMENT "Generating API documentation with Doxygen")
 
-    install(DIRECTORY ${PROJECT_BINARY_DIR}/src/man DESTINATION share)
-    install(DIRECTORY ${PROJECT_BINARY_DIR}/src/html DESTINATION share)
 endif()
