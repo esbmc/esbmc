ef8d36a820da278219b3eb01f5962fab1a05700a
Mikhail Gadelha <mikhail.gadelha@sidia.com>
2020-01-08 13:40:47 -0400

    Convert Boolector backend to use new_ast from smt_convt
    Also, fix several places where smt_ast* was used instead of smt_astt
    
    Signed-off-by: Mikhail Gadelha <mikhail.gadelha@sidia.com>

--- src/solvers/boolector/boolector_conv.h
+++ src/solvers/boolector/boolector_conv.h
@@ -29,7 +29,7 @@ public:
   resultt dec_solve() override;
   const std::string solver_text() override;
 
-  void assert_ast(const smt_ast *a) override;
+  void assert_ast(smt_astt a) override;
 
   smt_astt mk_bvadd(smt_astt a, smt_astt b) override;
   smt_astt mk_bvsub(smt_astt a, smt_astt b) override;
@@ -74,42 +74,34 @@ public:
   smt_sortt mk_bvfp_sort(std::size_t width, std::size_t swidth) override;
   smt_sortt mk_bvfp_rm_sort() override;
 
-  smt_ast *mk_smt_int(const mp_integer &theint, bool sign) override;
-  smt_ast *mk_smt_real(const std::string &str) override;
+  smt_astt mk_smt_int(const mp_integer &theint, bool sign) override;
+  smt_astt mk_smt_real(const std::string &str) override;
   smt_astt mk_smt_bv(const mp_integer &theint, smt_sortt s) override;
-  smt_ast *mk_smt_bool(bool val) override;
-  smt_ast *mk_smt_symbol(const std::string &name, const smt_sort *s) override;
-  smt_ast *mk_array_symbol(
+  smt_astt mk_smt_bool(bool val) override;
+  smt_astt mk_smt_symbol(const std::string &name, const smt_sort *s) override;
+  smt_astt mk_array_symbol(
     const std::string &name,
     const smt_sort *s,
     smt_sortt array_subtype) override;
-  smt_astt
-  mk_extract(const smt_ast *a, unsigned int high, unsigned int low) override;
+  smt_astt mk_extract(smt_astt a, unsigned int high, unsigned int low) override;
   smt_astt mk_sign_ext(smt_astt a, unsigned int topwidth) override;
   smt_astt mk_zero_ext(smt_astt a, unsigned int topwidth) override;
   smt_astt mk_concat(smt_astt a, smt_astt b) override;
   smt_astt mk_ite(smt_astt cond, smt_astt t, smt_astt f) override;
 
-  const smt_ast *
+  smt_astt
   convert_array_of(smt_astt init_val, unsigned long domain_width) override;
 
-  bool get_bool(const smt_ast *a) override;
+  bool get_bool(smt_astt a) override;
   BigInt get_bv(smt_astt a) override;
-  expr2tc get_array_elem(
-    const smt_ast *array,
-    uint64_t index,
-    const type2tc &subtype) override;
+  expr2tc get_array_elem(smt_astt array, uint64_t index, const type2tc &subtype)
+    override;
 
-  const smt_ast *overflow_arith(const expr2tc &expr) override;
-
-  inline btor_smt_ast *new_ast(BoolectorNode *_e, smt_sortt _s)
-  {
-    return new btor_smt_ast(this, _s, _e);
-  }
+  smt_astt overflow_arith(const expr2tc &expr) override;
 
   typedef BoolectorNode *(
     *shift_func_ptr)(Btor *, BoolectorNode *, BoolectorNode *);
-  smt_ast *fix_up_shift(
+  smt_astt fix_up_shift(
     shift_func_ptr fptr,
     smt_astt op0,
     smt_astt op1,
@@ -121,7 +113,7 @@ public:
   // Members
   Btor *btor;
 
-  typedef hash_map_cont<std::string, smt_ast *> symtable_type;
+  typedef hash_map_cont<std::string, smt_astt> symtable_type;
   symtable_type symtable;
 };
 
