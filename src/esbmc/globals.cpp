14a4178f5349115a50a411ebe3f5df4a6f79e672
Mikhail Gadelha <mikhail.gadelha@sidia.com>
2020-01-14 13:54:36 -0400

    Get rid of the version header. CMake generates that for us now
    Signed-off-by: Mikhail Gadelha <mikhail.gadelha@sidia.com>

--- src/esbmc/globals.cpp
+++ src/esbmc/globals.cpp
@@ -1,4 +1,3 @@
-#include <esbmc/version.h>
 #include <langapi/mode.h>
 
 const mode_table_et mode_table[] = {LANGAPI_HAVE_MODE_CLANG_C,
@@ -9,8 +8,3 @@ const mode_table_et mode_table[] = {LANGAPI_HAVE_MODE_CLANG_C,
 
 extern "C" uint8_t buildidstring_buf[1];
 uint8_t *esbmc_version_string = buildidstring_buf;
-
-extern "C"
-{
-  uint64_t esbmc_version = ESBMC_VERSION_CONST;
-}
