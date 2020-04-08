03d1b7eaf4548508fc64f70df339dbccd162671f
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-04-08 20:35:16 +0100

    added test case to check pthread_setspecific
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/01_pthread50/main.c
@@ -0,0 +1,23 @@
+#include <errno.h>
+#include <pthread.h>
+#include <assert.h>
+
+
+int main() {
+  pthread_key_t key;
+  int r = pthread_key_create( &key, NULL );
+  assert( r == 0 || r == EAGAIN || r == ENOMEM);
+  pthread_t tid;
+
+  long val = (long)pthread_getspecific( key );
+  assert( val == 0 );
+
+  val = (long)pthread_getspecific( key );
+  assert( val == 0);
+
+  r = pthread_setspecific( key, nondet_bool() ? (void *)16 : NULL );
+  assert( r == 0 || r == EINVAL || r == ENOMEM);
+
+  return 0;
+}
+
