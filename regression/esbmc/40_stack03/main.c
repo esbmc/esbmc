92aaa3dcee0aa3fef62c8a8ad14b1184e12430b6
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-05-23 19:32:52 +0100

    added test case with pointer to check the stack size
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/40_stack03/main.c
@@ -0,0 +1,16 @@
+#define STACK_LIM 160
+int main() {
+  int y = 5;  // 4 * 8 = 32
+  int *yPtr;  // 8 * 8 = 64
+  // Expected total here: 32+32= 96.
+ 
+  yPtr = &y;
+
+  // This assert is throwing error because of the latter
+  // pointer variable xPtr
+  __ESBMC_assert(__ESBMC_stack_size() < STACK_LIM, "ERROR");
+ 
+  int *xPtr; // 8 * 8 = 64
+  // The total now is 40 + 32 = 160.
+  return 0;
+}
