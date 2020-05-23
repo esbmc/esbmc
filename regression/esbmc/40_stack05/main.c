3680cc94aa173a93d46e6917764d4db8fdc1c3f6
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-05-23 19:44:44 +0100

    added test case using pointer for 32-bit arch
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/40_stack05/main.c
@@ -0,0 +1,16 @@
+#define STACK_LIM 96
+int main() {
+  int y = 5;  // 4 * 8 = 32
+  int *yPtr;  // 4 * 8 = 32
+  // Expected total here: 32+32= 64.
+ 
+  yPtr = &y;
+
+  // This assert is throwing error because of the latter
+  // pointer variable xPtr
+  __ESBMC_assert(__ESBMC_stack_size() < STACK_LIM, "ERROR");
+ 
+  int *xPtr; // 4 * 8 = 32
+  // The total now is 64 + 32 = 96.
+  return 0;
+}
