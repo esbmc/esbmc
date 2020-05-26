dd76c8c448cbb284d93496a927516918913885aa
Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>
2020-05-26 12:04:11 -0400

    added stack limit test cases
    Signed-off-by: Rafael Sá Menezes <rafael.sa@icomp.ufam.edu.br>

---
+++ regression/esbmc/40_stack_64_inner_scope_true/main.c
@@ -0,0 +1,8 @@
+int main() {
+  int a; // 8
+  {
+    int a; // 8
+    // Total: 16
+  }
+  return 0;
+}
---
+++ regression/esbmc/40_stack_64_while_true/main.c
@@ -0,0 +1,9 @@
+int main() {
+  int a; // 8
+  while(a > 0) {
+    int b; //8
+    a--;
+  }
+  // Total: 16
+  return 0;
+}
