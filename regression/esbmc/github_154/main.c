c77af08f1e45f29c9f30aa07561735fc93da3055
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-06-10 15:30:31 +0100

    added explanation about the violation
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/github_154/main.c
+++ regression/esbmc/github_154/main.c
@@ -1,27 +1,33 @@
 #include <stdlib.h>
 #include <assert.h>
 
-typedef struct pthread_id_value {
+typedef struct pthread_id_value
+{
   int id;
   void *value;
 } pthread_id_valuet;
 
-pthread_id_valuet* __ESBMC_thread_keys[];
+/* This is an array with one element. */
+pthread_id_valuet *__ESBMC_thread_keys[];
 
 int main()
 {
-  pthread_id_valuet* id_val = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
-  __ESBMC_thread_keys[0] = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
-  
+  pthread_id_valuet *id_val =
+    (pthread_id_valuet *)malloc(sizeof(pthread_id_valuet));
+  __ESBMC_thread_keys[0] =
+    (pthread_id_valuet *)malloc(sizeof(pthread_id_valuet));
+
   id_val->id = 10;
   id_val->value = (void *)20;
   __ESBMC_thread_keys[0][0] = *id_val;
 
-  __ESBMC_thread_keys[1] = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
+  /* array bounds violated: array `__ESBMC_thread_keys' upper bound */
+  __ESBMC_thread_keys[1] =
+    (pthread_id_valuet *)malloc(sizeof(pthread_id_valuet));
   id_val->id = 40;
   id_val->value = (void *)50;
   __ESBMC_thread_keys[1][0] = *id_val;
-  
+
   pthread_id_valuet *tmp = &__ESBMC_thread_keys[0][0];
   assert(tmp->id == 10);
   assert(tmp->value == (void *)20);
