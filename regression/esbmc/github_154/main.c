275eec3242eb6539bafce64490c3e562f55e0cfb
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-06-10 13:24:51 +0100

    added test case to check global array of pointers
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/github_154/main.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <assert.h>
+
+typedef struct pthread_id_value {
+  int id;
+  void *value;
+} pthread_id_valuet;
+
+pthread_id_valuet* __ESBMC_thread_keys[];
+
+int main()
+{
+  pthread_id_valuet* id_val = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
+  __ESBMC_thread_keys[0] = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
+  
+  id_val->id = 10;
+  id_val->value = (void *)20;
+  __ESBMC_thread_keys[0][0] = *id_val;
+
+  __ESBMC_thread_keys[1] = (pthread_id_valuet *) malloc(sizeof(pthread_id_valuet));
+  id_val->id = 40;
+  id_val->value = (void *)50;
+  __ESBMC_thread_keys[1][0] = *id_val;
+  
+  pthread_id_valuet *tmp = &__ESBMC_thread_keys[0][0];
+  assert(tmp->id == 10);
+  assert(tmp->value == (void *)20);
+
+  tmp = &__ESBMC_thread_keys[1][0];
+  assert(tmp->id == 40);
+  assert(tmp->value == (void *)50);
+
+  return 0;
+}
