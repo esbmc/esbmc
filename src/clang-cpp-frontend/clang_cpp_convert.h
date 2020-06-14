db539f0d994923679a332d3cbee5cd7681a01f9e
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-06-14 17:26:10 -0400

    Basic classes for the clang cpp frontend
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

---
+++ src/clang-cpp-frontend/clang_cpp_convert.h
@@ -0,0 +1,18 @@
+#ifndef CLANG_CPP_FRONTEND_CLANG_CPP_CONVERT_H_
+#define CLANG_CPP_FRONTEND_CLANG_CPP_CONVERT_H_
+
+#define __STDC_LIMIT_MACROS
+#define __STDC_FORMAT_MACROS
+
+#include <clang-c-frontend/clang_c_convert.h>
+
+class clang_cpp_convertert : public clang_c_convertert
+{
+public:
+  clang_cpp_convertert(
+    contextt &_context,
+    std::vector<std::unique_ptr<clang::ASTUnit>> &_ASTs);
+  virtual ~clang_cpp_convertert() = default;
+};
+
+#endif /* CLANG_C_FRONTEND_CLANG_C_CONVERT_H_ */
