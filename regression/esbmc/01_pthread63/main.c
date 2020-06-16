27a3667d036cc9171564ee90412ce6d487907053
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-06-16 16:58:31 +0100

    applied code style
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/01_pthread63/main.c
+++ regression/esbmc/01_pthread63/main.c
@@ -12,27 +12,19 @@ void *t1(void *arg)
 void *t2(void *arg)
 {
   j = 1;
-  a[j]=3;
+  a[j] = 3;
 }
-
-//void *t3(void *arg)
-//{
-//  k = 2;
-//  a[k]=5;
-//}
 
 int main()
 {
-  pthread_t id1, id2; //, id3;
+  pthread_t id1, id2;
 
   pthread_create(&id1, NULL, t1, NULL);
   pthread_create(&id2, NULL, t2, NULL);
- // pthread_create(&id3, NULL, t3, NULL);
 
   pthread_join(id1, NULL);
   pthread_join(id2, NULL);
-  //pthread_join(id3, NULL);
 
-  assert(a[i]==2 && a[j]==3 /*&& a[k]==5*/);
-
+  assert(a[i] == 2 && a[j] == 3);
+  return 0;
 }
