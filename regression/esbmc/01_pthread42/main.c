cf0684241c92a712c3c9c645250d7fe8162d5731
Lucas Cordeiro <lucasccordeiro@gmail.com>
2020-06-25 21:08:42 +0100

    passed the thread idenifier address
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/01_pthread42/main.c
+++ regression/esbmc/01_pthread42/main.c
@@ -3,36 +3,38 @@
 
 int x1;
 
-void* t1(void* arg)
+void *t1(void *arg)
 {
-  x1++; 
+  x1++;
 }
 
-void* t2(void* arg)
+void *t2(void *arg)
 {
-  x1=x1+5;
+  x1 = x1 + 5;
 }
 
-void* t3(void* arg)
+void *t3(void *arg)
 {
-  if (x1==1)
+  if(x1 == 1)
     x1--;
 
-  if (x1==5)
-    x1=2;
+  if(x1 == 5)
+    x1 = 2;
 }
 
-void* t4(void* arg)
+void *t4(void *arg)
 {
-  assert(x1==0 || x1==5 || x1==4 || x1==2 || x1==6 || x1==1);
+  assert(x1 == 0 || x1 == 5 || x1 == 4 || x1 == 2 || x1 == 6 || x1 == 1);
 }
 
-void main()
+int main()
 {
   pthread_t id1, id2, id3, id4;
 
-  pthread_create(id1, NULL, t1, NULL);
-  pthread_create(id2, NULL, t2, NULL);
-  pthread_create(id3, NULL, t3, NULL);
-  pthread_create(id4, NULL, t4, NULL);
+  pthread_create(&id1, NULL, t1, NULL);
+  pthread_create(&id2, NULL, t2, NULL);
+  pthread_create(&id3, NULL, t3, NULL);
+  pthread_create(&id4, NULL, t4, NULL);
+
+  return 0;
 }
