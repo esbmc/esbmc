2dfaec830c3094f4882d68e9a2239048d1a3750b
Vanderson Rocha <vanderson.rocha@gmail.com>
2020-07-17 11:08:59 -0400

    Added initial regression test cases

---
+++ regression/string-solver/strcmp01/main.c
@@ -0,0 +1,10 @@
+int main() {
+    int test;
+    char strA[7];
+    char strB[9];
+    strcpy(strA, "01234");
+    strcpy(strB, "0123");
+    test = strcmp(strA, strB);
+    assert(test != 0);
+    return 0;
+}
---
+++ regression/string-solver/strcpy01/main.c
@@ -0,0 +1,6 @@
+int main() {
+	char strA[7];
+	strcpy(strA, "0123");
+ 	assert(str[3] == '3');
+	return 0;
+}
