0ddcd5b48b88039e632df1c924824a14b0f17e0e
Erickson Alves <erickson.higor@gmail.com>
2020-08-26 10:22:44 -0400

    Disable k-induction parallel
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -433,11 +433,11 @@ int esbmc_parseoptionst::doit()
   if(cmdline.isset("k-induction"))
     return doit_k_induction();
 
-#if 0
+#if 1
   if(cmdline.isset("k-induction-parallel"))
     return doit_k_induction_parallel();
 #endif
-  return doit_k_induction_parallel();
+  // return doit_k_induction_parallel();
 
   optionst opts;
   get_command_line_options(opts);
