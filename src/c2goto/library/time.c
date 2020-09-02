4625850cf469d257efdff694fe93c85de7d33176
Erickson Alves <erickson.higor@gmail.com>
2020-09-02 19:29:42 -0400

    Update time function model
    Signed-off-by: Erickson Alves <erickson.higor@gmail.com>

--- src/c2goto/library/time.c
+++ src/c2goto/library/time.c
@@ -6,9 +6,11 @@
 #include <time.h>
 #undef time
 
+time_t __VERIFIER_nondet_time_t();
+
 time_t time(time_t *tloc)
 {
-  time_t res;
+  time_t res = __VERIFIER_nondet_time_t();
   if(!tloc)
     *tloc = res;
   return res;
