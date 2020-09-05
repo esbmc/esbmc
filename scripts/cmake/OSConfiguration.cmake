878dd5fba04a3b139dadb2073ffde9d2c3564603
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-09-05 15:44:21 -0400

    Separeted FLEX targets

--- scripts/cmake/OSConfiguration.cmake
+++ scripts/cmake/OSConfiguration.cmake
@@ -11,8 +11,10 @@ be for finding executables or custom links
 The global variables that all modules NEED to define are:
 
 - LIBGOMP_LIB: Flags to link libgomp (used by Z3);
-- OS_FLEX_FLAGS: Flags used for Flex targets. This may add defines exclusive to
-    the OS;
+- OS_FLEX_ANSI_FLAGS: Flags used for Flex targets. This may add defines exclusive to
+    the OS (for ansi-c);
+- OS_FLEX_SMTLIB_FLAGS: Flags used for Flex targets. This may add defines exclusive to
+    the OS (for smtlib);
 - OS_X86_INCLUDE_FOLDER: Path to the default multiarch headers of the system,
     when generating goto models some of those are in 32bits. This is were they
     will find the headers by adding -I <folder>;
@@ -35,6 +37,7 @@ include(WindowsConfiguration)
 
 # Assert that the variables were assigned at all
 assert_variable_is_defined(LIBGOMP_LIB)
-assert_variable_is_defined(OS_FLEX_FLAGS)
+assert_variable_is_defined(OS_FLEX_ANSI_FLAGS)
+assert_variable_is_defined(OS_FLEX_SMTLIB_FLAGS)
 assert_variable_is_defined(OS_X86_INCLUDE_FOLDER)
 assert_variable_is_defined(OS_C2GOTO_FLAGS)
--- scripts/cmake/WindowsConfiguration.cmake
+++ scripts/cmake/WindowsConfiguration.cmake
@@ -4,19 +4,21 @@ if (WIN32)
   # Prebuilt LLVM for Windows doesn't come with CMake files
   message(STATUS "Detected MS Windows")
   #add_definitions(/bigobj)
-  # There are a LOT of warnings from clang headers
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-everything")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-everything")
   
   set(LIBGOMP_LIB "-lgomp -ldl")
-  set(OS_FLEX_FLAGS "--wincompat")
+  set(OS_FLEX_ANSI_FLAGS "--nounistd")
+  set(OS_FLEX_SMTLIB_FLAGS "--wincompat")
   set(OS_X86_INCLUDE_FOLDER "C:/")
   set(OS_C2GOTO_FLAGS "-D_MSVC")
 endif()
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-
+  # There are a LOT of warnings from clang headers
+  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-everything")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-everything")
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+  # This is needed for unordered_set
+  #add_definitions(/std:c++latest)
 else()
 message(AUTHOR_WARNING "${CMAKE_CXX_COMPILER_ID} is not tested in Windows. You may run into issues.")	
 endif()
