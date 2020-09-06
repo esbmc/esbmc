454ed83d69e48832b0bbcc33c3ad278d8b2972b9
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-09-05 20:28:26 -0400

    Changed compiler flags to be inside if(WIN)

--- scripts/cmake/WindowsConfiguration.cmake
+++ scripts/cmake/WindowsConfiguration.cmake
@@ -11,16 +11,17 @@ if (WIN32)
   set(OS_FLEX_SMTLIB_FLAGS "--wincompat")
   set(OS_X86_INCLUDE_FOLDER "C:/")
   set(OS_C2GOTO_FLAGS "-D_MSVC")
-endif()
 
-if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
-  # There are a LOT of warnings from clang headers
-  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-everything")
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-everything")
-  set(OS_Z3_LIBS "stdc++")
   
-elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
-  set(OS_Z3_LIBS "")
-else()
-  message(AUTHOR_WARNING "${CMAKE_CXX_COMPILER_ID} is not tested in Windows. You may run into issues.")	
+	if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+	  # There are a LOT of warnings from clang headers
+	  set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -Wno-everything")
+	  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-everything")
+	  set(OS_Z3_LIBS "stdc++")
+  
+	elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+	  set(OS_Z3_LIBS "")
+	else()
+	  message(AUTHOR_WARNING "${CMAKE_CXX_COMPILER_ID} is not tested in Windows. You may run into issues.")	
+	endif()
 endif()
