c936d07cf1e90f2a2ca83ded83070ef0741e8732
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-09-07 10:38:20 -0400

    Added if APPLE and Linux

--- scripts/cmake/BuildStatic.cmake
+++ scripts/cmake/BuildStatic.cmake
@@ -1,10 +1,18 @@
 # Module to configure static build
 
 message(STATUS "ESBMC will be built in static mode")
-if(NOT APPLE)
+if(UNIX AND NOT APPLE)
     set(CMAKE_EXE_LINKER_FLAGS " -static")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -static -fPIC")
     set(CMAKE_CC_FLAGS "${CMAKE_CC_FLAGS} -static -fPIC")
 endif()
+
+if(UNIX)
+    set(CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
+endif()
+
+if(CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
+	  set(MSVC_RUNTIME_LIBRARY MultiThreaded)
+endif()
 set(Boost_USE_STATIC_LIBS        ON)
-set(CMAKE_FIND_LIBRARY_SUFFIXES .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
+
