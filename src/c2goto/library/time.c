8ce5c9e079d5cafdfe0fb961cfd0544684f2201b
Thales Silva <thales.tas@gmail.com>
2020-09-09 12:21:13 -0400

    Added __ESBMC_HIDE and removed unecessary NULL

--- src/c2goto/library/time.c
+++ src/c2goto/library/time.c
@@ -10,8 +10,9 @@ time_t __VERIFIER_nondet_time_t();
 
 time_t time(time_t *tloc)
 {
+__ESBMC_HIDE:;
   time_t res = __VERIFIER_nondet_time_t();
-  if(tloc != NULL)
+  if(tloc)
     *tloc = res;
   return res;
 }
