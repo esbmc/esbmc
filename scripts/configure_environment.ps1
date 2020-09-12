2b7760a6817634096f73c9c23ffaa7eda8eebb4d
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-09-11 20:56:03 -0400

    Update configure_environment.ps1

--- scripts/configure_environment.ps1
+++ scripts/configure_environment.ps1
@@ -5,5 +5,5 @@ $URL = "https://www.dropbox.com/s/0b6tw7uo5dx5p5h/llvm9d-win.zip?dl=1"
 $LLVM_ZIP = "clang.zip"
 $LLVM_DEST = "C:\deps\clang"
 
-# Invoke-WebRequest $URL -OutFile $LLVM_ZIP
+Invoke-WebRequest $URL -OutFile $LLVM_ZIP
 Expand-Archive -LiteralPath $LLVM_ZIP -DestinationPath $LLVM_DEST
