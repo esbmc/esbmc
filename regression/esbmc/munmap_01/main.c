8d56f66e02914b26e580a370038479ae98818e3d
Thales Silva <thales.tas@gmail.com>
2020-09-26 18:55:34 -0400

    Added mmap and munmap regression tests

---
+++ regression/esbmc/munmap_01/main.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/mman.h>
+
+int main(int argc, char *argv[])
+{
+	int *p;
+	unsigned int n  = nondet_uint();
+	
+	__ESBMC_assume(n > 1);
+	__ESBMC_assume(n < 10000000);
+
+	p = mmap(NULL, n*sizeof(int), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+	__ESBMC_assume(p);	
+
+	munmap(p, n*sizeof(int));
+
+	return 0;
+}
