6c7bc85e9593567d33b1c223592ad79e6e9d2072
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-10-01 07:17:10 -0400

    msvc fixes

--- src/esbmc/globals.cpp
+++ src/esbmc/globals.cpp
@@ -10,4 +10,4 @@ const mode_table_et mode_table[] = {
   LANGAPI_HAVE_MODE_END};
 
 extern "C" uint8_t buildidstring_buf[1];
-uint8_t *esbmc_version_string = buildidstring_buf;
+extern "C" uint8_t *esbmc_version_string = buildidstring_buf;
