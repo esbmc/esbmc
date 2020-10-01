b1b24088f44777c2aac812745e9213d377f2f91a
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-10-01 08:45:10 -0400

    Initial windows CI

---
+++ scripts/configure_environment.ps1
@@ -0,0 +1,8 @@
+# Download and extract prebuilt LLVM+Clang for Windows
+
+$URL = "https://www.dropbox.com/s/0b6tw7uo5dx5p5h/llvm9d-win.zip?dl=1"
+$LLVM_ZIP = "clang.zip"
+$LLVM_DEST = "C:\deps\clang"
+
+Invoke-WebRequest $URL -OutFile $LLVM_ZIP
+Expand-Archive -LiteralPath $LLVM_ZIP -DestinationPath $LLVM_DEST
