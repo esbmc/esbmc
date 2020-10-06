dc6a43c1a7bb3ce3e506faad0394ed4ea32b381a
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-10-06 18:14:35 -0400

    Removed unused warning

--- scripts/cmake/Irep2Optimization.cmake
+++ scripts/cmake/Irep2Optimization.cmake
@@ -6,7 +6,6 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
 	return()
 endif()
 
-message(WARNING ${CMAKE_CXX_COMPILER_ID})
 target_precompile_headers(util_esbmc PRIVATE ${CMAKE_SOURCE_DIR}/src/util/irep2.h ${CMAKE_SOURCE_DIR}/src/util/irep2_expr.h)
 target_precompile_headers(clangcfrontend_stuff REUSE_FROM util_esbmc)
 target_precompile_headers(clangcppfrontend REUSE_FROM util_esbmc)
