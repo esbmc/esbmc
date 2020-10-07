f8b1fa91ca27610dc63ba49a02996484c934370f
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2020-10-07 09:59:22 -0400

    Test cases
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

---
+++ regression/esbmc/github_287_1/main.c
@@ -0,0 +1,7 @@
+float a[][6];
+int b;
+float c;
+main() {
+  c = a[b][5];
+  assert(c);
+}
---
+++ regression/esbmc/github_287_2/main.c
@@ -0,0 +1,7 @@
+a[][3];
+b;
+main() {
+  int c, d = a[c][2];
+  __VERIFIER_assume(d >= 0);
+  assert(b);
+}
