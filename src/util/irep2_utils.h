63e378347f30c58a2859bbb534166bf55558a3f3
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2020-10-24 13:20:12 -0400

    Added a function to check if byte_extract is constant

--- src/util/irep2_utils.h
+++ src/util/irep2_utils.h
@@ -83,9 +83,18 @@ inline bool is_constant_expr(const expr2tc &t)
          t->expr_id == expr2t::constant_struct_id ||
          t->expr_id == expr2t::constant_union_id ||
          t->expr_id == expr2t::constant_array_id ||
-         t->expr_id == expr2t::constant_array_of_id ||
-         t->expr_id == expr2t::byte_extract_id;
+    t->expr_id == expr2t::constant_array_of_id;
 }
+
+inline bool is_byte_extract_constant(const expr2tc &t)
+{
+  if(t->expr_id != expr2t::byte_extract_id)
+    return false;
+
+  std::shared_ptr<byte_extract_data> data = std::dynamic_pointer_cast<byte_extract_data>(t);
+  return is_constant_expr(data->source_value) && is_constant_expr(data->source_offset);
+}
+
 
 inline bool is_structure_type(const type2tc &t)
 {
