fc8557ea36ea1a1eddd04c070d2b92db470143fa
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-12-14 17:23:49 -0400

    marked __builtin_unreachable as an reachable error

--- src/goto-programs/builtin_functions.cpp
+++ src/goto-programs/builtin_functions.cpp
@@ -589,7 +589,7 @@ void goto_convertt::do_function_call_symbol(
       throw id2string(base_name) + " expected not to have LHS";
     }
   }
-  else if(base_name == "__VERIFIER_error" || base_name == "reach_error")
+  else if(base_name == "__VERIFIER_error" || base_name == "reach_error" || base_name == "__builtin_unreachable")
   {
     if(!arguments.empty())
     {
