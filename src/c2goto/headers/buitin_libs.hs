5a454cdbeb9e580d2bceeb91a7500c06177d7c15
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2020-12-14 17:24:51 -0400

    added builtin_libs models and testcases

---
+++ src/c2goto/headers/buitin_libs.hs
@@ -0,0 +1,18 @@
+/*******************************************************************\
+
+Module: Builtin Models
+
+Author: Rafael Menezes
+
+Description: Models for custom functions that are easier to implement
+ and test here
+
+\*******************************************************************/
+
+
+#ifndef ESBMC_BUILTIN_LIBS_HS_H
+#define ESBMC_BUILTIN_LIBS_HS_H
+
+int __builtin_esbmc_sync_fetch_and_add(int *ptr, int value);
+
+#endif //ESBMC_BUILTIN_LIBS_HS_H
---
+++ unit/c2goto/library/builtin_libs.fuzz.cpp
@@ -0,0 +1,38 @@
+/*******************************************************************
+ Module: Builtin Libs Model unit test
+
+ Author: Rafael Sá Menezes
+
+ Date: December 2019
+
+ Fuzz Plan:
+   - Functions
+ \*******************************************************************/
+
+#include <stdlib.h>
+#include <cassert>
+
+
+void __ESBMC_atomic_begin() {}
+void __ESBMC_atomic_end() {}
+extern int __builtin_esbmc_sync_fetch_and_add(int*, int);
+
+bool is_valid_input(const int *Data, size_t Size) {
+  return Size >= 2 && Size < 3;
+}
+
+void test_sync_fetch_add(int initial, int value) {
+  int actual = initial;
+  int num = value;
+  int expected = initial + value;
+  int fetch = __builtin_esbmc_sync_fetch_and_add(&actual, num);
+  assert(expected == actual);
+  assert(fetch == initial);
+}
+
+
+extern "C" int LLVMFuzzerTestOneInput(const int *Data, size_t Size) {
+  if(!is_valid_input(Data, Size)) return 0;
+  test_sync_fetch_add(Data[0], Data[1]);
+  return 0;
+}
