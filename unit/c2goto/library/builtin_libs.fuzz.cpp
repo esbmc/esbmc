b360677e9257ca549432a9d67f426b9b763aa2b5
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-05 12:58:05 -0400

    model testcases are using the implementation directly

--- unit/c2goto/library/builtin_libs.fuzz.cpp
+++ unit/c2goto/library/builtin_libs.fuzz.cpp
@@ -18,7 +18,7 @@ void __ESBMC_atomic_begin()
 void __ESBMC_atomic_end()
 {
 }
-extern int __ESBMC_sync_fetch_and_add(int *, int);
+#include <library/builtin_libs.c>
 
 bool is_valid_input(const int *Data, size_t Size)
 {
@@ -30,7 +30,7 @@ void test_sync_fetch_add(int initial, int value)
   int actual = initial;
   int num = value;
   int expected = initial + value;
-  int fetch = __builtin_esbmc_sync_fetch_and_add(&actual, num);
+  int fetch = __ESBMC_sync_fetch_and_add(&actual, num);
   assert(expected == actual);
   assert(fetch == initial);
 }
