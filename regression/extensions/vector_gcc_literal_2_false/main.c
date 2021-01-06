c65f7064707f6c27ec3756f06e65742b5e2a5fc4
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-06 14:36:49 -0400

    added nondet testcase

--- regression/extensions/vector_gcc_literal_2_false/main.c
+++ regression/extensions/vector_gcc_literal_2_false/main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 typedef int v4si __attribute__((__vector_size__(16)));
-v4si vsi = (v4si){1, 2, 3, 4};
 
 
 // Should Initialize Correctly
 int main() {
-  vsi++;
-   for(int i = 0; i < 4; i++)
-     __ESBMC_assert(vsi[i] == i, "The vector should be initialized correctly");
-   return 0; 
+  int a = __VERIFIER_nondet_int();
+  v4si vsi = (v4si){1, 2, 3, a};
+  for(int i = 0; i < 4; i++)
+    __ESBMC_assert(vsi[i] == i+1, "The vector should be initialized correctly");
+  return 0;
 }
--- regression/extensions/vector_gcc_literal_2_true/main.c
+++ regression/extensions/vector_gcc_literal_2_true/main.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 typedef int v4si __attribute__((__vector_size__(16)));
-v4si vsi = (v4si){1};
+
 
 // Should Initialize Correctly
 int main() {
+  int a = __VERIFIER_nondet_int();
+  __VERIFIER_assume(a == 4);
+  v4si vsi = (v4si){1, 2, 3, a};
   for(int i = 0; i < 4; i++)
-    __ESBMC_assert(vsi[i] == 1, "The vector should be initialized correctly");
+    __ESBMC_assert(vsi[i] == i+1, "The vector should be initialized correctly");
   return 0;
 }
