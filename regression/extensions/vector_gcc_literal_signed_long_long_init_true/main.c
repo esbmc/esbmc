e46fc70906bb5fc171b82587ebf48770ff70513f
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-08 10:29:30 -0400

    added testcases for signed and unsigned integer types

--- regression/extensions/vector_gcc_literal_signed_long_long_init_true/main.c
+++ regression/extensions/vector_gcc_literal_signed_long_long_init_true/main.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 
-typedef int v4si __attribute__((__vector_size__(16)));
+
+#define test_type long long
+#define test_length sizeof(test_type)*4
+
+typedef test_type v4si __attribute__((__vector_size__(test_length)));
 v4si vsi = (v4si){1, 2, 3, 4};
 
 // Should Initialize Correctly
