ce6e40c3bc087d7397e007e6a35c1103526908fe
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-08 10:29:30 -0400

    added testcases for signed and unsigned integer types

---
+++ regression/extensions/vector_gcc_literal_signed_short_init_true/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+#define test_type short
+#define test_length sizeof(test_type)*4
+
+typedef test_type v4si __attribute__((__vector_size__(test_length)));
+v4si vsi = (v4si){1, 2, 3, 4};
+
+// Should Initialize Correctly
+int main() {
+   for(int i = 0; i < 4; i++)
+      __ESBMC_assert(vsi[i] == i+1, "The vector should be initialized correctly");
+   return 0; 
+}
