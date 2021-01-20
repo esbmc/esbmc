c9ba09937df08ffc9f5d14dc4445eb888d056756
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-20 17:02:23 -0300

    added to union typecast

--- src/clang-c-frontend/typecast.cpp
+++ src/clang-c-frontend/typecast.cpp
@@ -25,3 +25,30 @@ void gen_typecast_arithmetic(const namespacet &ns, exprt &expr)
   c_typecastt c_typecast(ns);
   c_typecast.implicit_typecast_arithmetic(expr);
 }
+
+void gen_typecast_to_union(exprt &e, const typet &t)
+{
+  // If RHS is already of same union type, don't do anything
+  if(e.type() == t.type())
+    return;
+
+  union_exprt new_result(t);
+  for(auto component : to_union_type(t).components())
+  {
+    // Search for the component with the same type
+    if(component.type() == e.type())
+    {
+      // Found it. Set the operator and component
+      new_result.set_component_name(component.name());
+      new_result.copy_to_operands(e);
+      e.swap(new_result);
+      return;
+    }
+  }
+
+  /* We should never reach here since clang frontend already checks for this
+   * however... we should prevent any funny things to happen */
+  std::cerr << "Couldn't map type " << e.type().pretty_name() << " into the union" << std::endl;
+  t.dump();
+  abort();
+}
--- src/clang-c-frontend/typecast.h
+++ src/clang-c-frontend/typecast.h
@@ -13,4 +13,15 @@ gen_typecast_arithmetic(const namespacet &ns, exprt &expr1, exprt &expr2);
 
 extern void gen_typecast_arithmetic(const namespacet &ns, exprt &expr);
 
+/**
+ * @brief Perform the typecast by creating a tmp variable on RHS
+ *
+ * The idea is to look for all components of the union and match
+ * the type. If not found, throws an error
+ *
+ * @param dest RHS dest
+ * @param type Union type
+ */
+extern void gen_typecast_to_union(exprt &dest, const typet &type);
+
 #endif /* TYPECAST_H_ */
