2f272adc0c7524086591b5a361edb4b2f74cc86c
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-20 22:44:31 -0300

    added clang-c-frontend unit tests

---
+++ unit/clang-c-frontend/typecast.fuzz.cpp
@@ -0,0 +1,102 @@
+/*******************************************************************
+ Module: Typecast fuzz tests
+
+ Author: Rafael Sá Menezes
+
+ Date: January 2021
+
+ Fuzz Plan:
+   - ToUnion (builtins)
+ \*******************************************************************/
+
+// ToUnion
+
+/**
+ * The idea is to initialize an union_typet with N types
+ * identifying them by an integer. After that randomly select
+ * two indexes, to confirm the types and to confirm the exception
+ *
+ */
+
+#include <clang-c-frontend/typecast.h>
+#include <util/type.h>
+#include <util/expr_util.h>
+#include "util.h"
+#include <sstream>
+
+void test_to_union(const int *Data, size_t Size)
+{
+  union_typet t;
+
+  // Get the index where the expected value will be
+  int rand_index = Data[Size - 1] % 40;
+
+  bool added_check = false;
+  Builtin_Type bt_check = (Builtin_Type)(Data[0]);
+  auto component_check = gen_component("special", bt_check);
+
+  Builtin_Type bt_error = (Builtin_Type)(Data[1]);
+
+  // Don't need to worry with upper bound (libFuzzer)
+  for(size_t i = 2; i < Size - 1; i++)
+  {
+    if(Data[i] == Data[1])
+      continue;
+    if(!added_check && (Data[i] == Data[0] || i > (size_t)rand_index))
+    {
+      t.components().push_back(component_check);
+      added_check = true;
+      continue;
+    }
+    std::ostringstream name;
+    name << "var_" << i;
+    auto component = gen_component(name.str().c_str(), Builtin_Type::Int);
+    t.components().push_back(component);
+  }
+
+  // It could happen that Data[1] until Data[-1]
+  // are the same, so it would never add the check
+  if(!added_check)
+    return;
+
+  typet builtin;
+  gen_builtin_type(builtin, bt_check);
+  exprt e = gen_zero(builtin);
+
+  gen_typecast_to_union(e, t);
+
+  assert(to_union_expr(e).get_component_name() == component_check.name());
+  assert(to_union_expr(e).op0().type() == component_check.type());
+
+  try
+  {
+    typet error_builtin;
+    gen_builtin_type(error_builtin, bt_error);
+    exprt error = gen_zero(error_builtin);
+    //Trigger exception
+    gen_typecast_to_union(error, t);
+    // This shouldn't be reached
+    assert(0);
+  }
+  catch(const std::domain_error &)
+  {
+    // OK
+    return;
+  }
+}
+
+extern "C" int LLVMFuzzerTestOneInput(const int *Data, size_t Size)
+{
+  if(Size < 5 && (Data[0] == Data[1]))
+    return 0;
+  for(size_t i = 0; i < Size; i++)
+  {
+    if(Data[i] <= (int)Builtin_Type::Void || Data[i] >= (int)Builtin_Type::Last)
+    {
+      return 0;
+    }
+  }
+
+  test_to_union(Data, Size);
+  return 0;
+}
---
+++ unit/clang-c-frontend/typecast.test.cpp
@@ -0,0 +1,110 @@
+/*******************************************************************
+ Module: Typecast unit test
+
+ Author: Rafael Sá Menezes
+
+ Date: January 2021
+
+ Test Plan:
+   - ToUnion (builtins, exceptions)
+ \*******************************************************************/
+
+#define BOOST_TEST_MODULE "Typecast Test"
+#include <clang-c-frontend/typecast.h>
+#include <util/type.h>
+#include <util/expr_util.h>
+#include "util.h"
+#include <boost/test/included/unit_test.hpp>
+namespace utf = boost::unit_test;
+
+// ******************** TESTS ********************
+
+BOOST_AUTO_TEST_SUITE(ToUnion)
+BOOST_AUTO_TEST_CASE(basic_union_1)
+{
+  auto component = gen_component("var_1", Builtin_Type::Int);
+
+  union_typet t;
+  t.components().push_back(component);
+
+  typet builtin;
+  gen_builtin_type(builtin, Builtin_Type::Int);
+  exprt e = gen_zero(builtin);
+
+  gen_typecast_to_union(e, t);
+
+  BOOST_TEST(to_union_expr(e).get_component_name() == "var_1");
+  BOOST_TEST(to_union_expr(e).op0().type() == component.type());
+}
+
+BOOST_AUTO_TEST_CASE(basic_union_1_error)
+{
+  auto component = gen_component("var_1", Builtin_Type::Int);
+
+  union_typet t;
+  t.components().push_back(component);
+
+  typet builtin;
+  gen_builtin_type(builtin, Builtin_Type::UInt);
+  exprt e = gen_zero(builtin);
+
+  BOOST_CHECK_THROW(gen_typecast_to_union(e, t), std::domain_error);
+}
+
+BOOST_AUTO_TEST_CASE(basic_union_2)
+{
+  auto component = gen_component("var_1", Builtin_Type::Int);
+  auto component2 = gen_component("var_2", Builtin_Type::UInt);
+
+  union_typet t;
+  t.components().push_back(component);
+  t.components().push_back(component2);
+
+  typet builtin;
+  gen_builtin_type(builtin, Builtin_Type::UInt);
+  exprt e = gen_zero(builtin);
+
+  BOOST_CHECK_NO_THROW(gen_typecast_to_union(e, t));
+  BOOST_TEST(to_union_expr(e).get_component_name() == "var_2");
+  BOOST_TEST(to_union_expr(e).op0().type() == component2.type());
+}
+
+BOOST_AUTO_TEST_CASE(basic_union_3)
+{
+  auto component = gen_component("var_1", Builtin_Type::Double);
+  auto component2 = gen_component("var_2", Builtin_Type::UInt);
+
+  union_typet t;
+  t.components().push_back(component);
+  t.components().push_back(component2);
+
+  typet builtin;
+  gen_builtin_type(builtin, Builtin_Type::UInt);
+  exprt e = gen_zero(builtin);
+
+  BOOST_CHECK_NO_THROW(gen_typecast_to_union(e, t));
+  BOOST_TEST(to_union_expr(e).get_component_name() == "var_2");
+  BOOST_TEST(to_union_expr(e).op0().type() == component2.type());
+}
+
+BOOST_AUTO_TEST_CASE(basic_union_4)
+{
+  auto component = gen_component("var_1", Builtin_Type::Double);
+  auto component2 = gen_component("var_2", Builtin_Type::UInt);
+  auto component3 = gen_component("var_3", Builtin_Type::UInt);
+
+  union_typet t;
+  t.components().push_back(component);
+  t.components().push_back(component2);
+  t.components().push_back(component3);
+
+  typet builtin;
+  gen_builtin_type(builtin, Builtin_Type::UInt);
+  exprt e = gen_zero(builtin);
+
+  BOOST_CHECK_NO_THROW(gen_typecast_to_union(e, t));
+  BOOST_TEST(to_union_expr(e).get_component_name() == "var_2");
+  BOOST_TEST(to_union_expr(e).op0().type() == component2.type());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
