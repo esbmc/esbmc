0e2bf4adfd1dc1940af3dc7b8aba9db6aca013fa
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-01-25 21:23:11 -0300

    code-style fix

--- src/util/type_byte_size.cpp
+++ src/util/type_byte_size.cpp
@@ -58,7 +58,8 @@ BigInt member_offset(const type2tc &type, const irep_idt &member)
   unsigned idx = 0;
 
   // empty union generate an array
-  if(!is_struct_type(type)) return result;
+  if(!is_struct_type(type))
+    return result;
   const struct_type2t &thetype = to_struct_type(type);
 
   for(auto const &it : thetype.members)
