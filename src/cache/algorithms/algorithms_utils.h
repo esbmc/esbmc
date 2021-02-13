f82b6e540f2649bf16442f727c911b2a3aa0b52c
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-02-13 10:51:53 -0300

    added cache

---
+++ src/cache/algorithms/algorithms_utils.h
@@ -0,0 +1,52 @@
+/*******************************************************************\
+ Module: Algorithm Utilities
+ Author: Rafael Sá Menezes
+ Description:
+  This file contains helper functions to manipulate SSA's and
+  expressions that are common between all algorithms.
+  - Only static methods are available
+\*******************************************************************/
+
+#ifndef ESBMC_ALGORITHMS_UTILS_H
+#define ESBMC_ALGORITHMS_UTILS_H
+
+#include <util/irep2.h>
+#include <util/irep2_expr.h>
+
+// CLASSES
+
+class ssa_algorithm_util
+{
+private:
+  ssa_algorithm_util() = default;
+};
+
+class expr_algorithm_util
+{
+public:
+  static std::string get_symbol_name(expr2tc &expr) noexcept
+  {
+    // This is meant to be used only in symbols
+    assert(expr->expr_id != expr2t::expr_ids::symbol_id);
+    std::shared_ptr<symbol_data> symbol;
+    symbol = std::dynamic_pointer_cast<symbol_data>(expr);
+    return symbol->get_symbol_name();
+  }
+
+  static bool is_guard(const expr2tc &expr, std::string &name)
+  {
+    if(expr->expr_id != expr2t::expr_ids::symbol_id)
+      return false;
+
+    std::shared_ptr<symbol_data> symbol;
+    symbol = std::dynamic_pointer_cast<symbol_data>(expr);
+    std::string symbol_name = symbol->get_symbol_name();
+    name = symbol_name;
+    return symbol_name.find("goto_symex::guard") != std::string::npos;
+  }
+
+private:
+  expr_algorithm_util() = default;
+};
+
+#endif //ESBMC_ALGORITHMS_UTILS_H
---
+++ src/cache/containers/lru_cache.h
@@ -0,0 +1,91 @@
+/*******************************************************************\
+ Module: LRU Cache
+
+ Author: Rafael Sá Menezes
+ Date: March 2020
+ This cache is used to keep track of which data was accessed recently and keep
+ removing old entries.
+\*******************************************************************/
+
+#ifndef LRU_CACHE
+#define LRU_CACHE
+
+#include <list>
+#include <algorithm>
+#include <unordered_map>
+#include <stdexcept>
+
+template <typename K>
+class lru_cache
+{
+protected:
+  // std::list is a double ended queue
+  std::list<K> cache;
+  size_t max_capacity = 0;
+
+public:
+  /**
+   * Constructs LRU Cache
+   * @param length max size of cache
+   */
+  explicit lru_cache(size_t length) : max_capacity(length)
+  {
+  }
+  virtual ~lru_cache()
+  {
+    cache.clear();
+  }
+
+  /**
+   * Resets the cache
+   */
+  void clear()
+  {
+    cache.clear();
+  }
+  /**
+   * Insert element into cache
+   * @param key key of the element
+   */
+  void insert(const K &key)
+  {
+    if(exists(key))
+    {
+      return;
+    }
+    while(cache.size() >= max_capacity)
+    {
+      cache.pop_back();
+    }
+    cache.emplace_front(key);
+  }
+
+  /**
+   * Checks whether the cache contains the element
+   * @param key index of the element
+   * @return boolean representing if the element is on the cache
+   */
+  bool exists(const K &key) const
+  {
+    return std::find(cache.begin(), cache.end(), key) != cache.end();
+  }
+
+  /**
+   * Get current size of the cache
+   * @return
+   */
+  size_t size()
+  {
+    return cache.size();
+  }
+
+  /**
+   * Get max capacity of the cache
+   * @return
+   */
+  constexpr size_t max_size()
+  {
+    return max_capacity;
+  }
+};
+#endif
