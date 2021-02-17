f625a2558f937ab5e5d345284cffd88cfd29c29b
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-02-17 13:45:49 +0000

    added regression tests for multi-dimensional array
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/cbmc/01_cbmc_Multi_Dimensional_Array2/main.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+
+unsigned int a[10][4];
+unsigned int *p;
+
+int main()
+{
+  assert(sizeof(a[0]) == sizeof(int) * 4);
+  p = a[9];
+  *p = 1;
+  assert(a[9][0] == 2);
+  p++;
+  *p = 2;
+  assert(a[9][1] == 2);
+}
---
+++ regression/esbmc/multi_dimensional_array_2/main.c
@@ -0,0 +1,15 @@
+int main()
+{
+  unsigned N, M, O;
+  __ESBMC_assume(N > 30);
+  __ESBMC_assume(M > 30);
+  __ESBMC_assume(O > 10);
+  int arr[N][M][O];
+  arr[15][25][2] = arr[10][15][2];
+  while(1)
+  {
+    __ESBMC_assert(arr[15][25][2] == arr[10][15][2], "This should be constant");
+  }
+
+  return 0;
+}
