0fd81d4aef2d8f87aa78e3d983a392b48dcfcbe6
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-02-24 11:44:22 -0400

    Move bitcast handling to a separate file in the backend
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- src/solvers/smt/smt_conv.cpp
+++ src/solvers/smt/smt_conv.cpp
@@ -1071,30 +1071,7 @@ smt_astt smt_convt::convert_ast(const expr2tc &expr)
   }
   case expr2t::bitcast_id:
   {
-    const bitcast2t &cast = to_bitcast2t(expr);
-    assert(is_scalar_type(cast.type) && is_scalar_type(cast.from));
-
-    // As it stands, the only circumstance where bitcast can make a difference
-    // is where we're casting to or from a float, where casting by value means
-    // something different. Filter that case out, pass everything else to normal
-    // cast.
-    if(is_floatbv_type(cast.type) && is_bv_type(cast.from))
-    {
-      a = fp_api->mk_from_bv_to_fp(args[0], convert_sort(cast.type));
-    }
-    else if(is_floatbv_type(cast.from) && is_bv_type(cast.type))
-    {
-      unsigned int sz = expr->type->get_width() - cast.from->type->get_width();
-      a = is_signedbv_type(expr->type)
-            ? mk_sign_ext(fp_api->mk_from_fp_to_bv(args[0]), sz)
-            : mk_zero_ext(fp_api->mk_from_fp_to_bv(args[0]), sz);
-    }
-    else
-    {
-      // Cast by value is fine
-      typecast2tc tcast(cast.type, cast.from);
-      a = convert_ast(tcast);
-    }
+    a = convert_bitcast(expr);
     break;
   }
   case expr2t::extract_id:
