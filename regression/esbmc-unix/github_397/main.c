c9674adff091be94e6bb574d8bf4e9169ba71924
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-02-28 21:37:46 +0000

    added tests to check nondeterministic thread creation

---
+++ regression/esbmc-unix/github_397/main.c
@@ -0,0 +1,18 @@
+#include <pthread.h>
+pthread_t c;
+int d;
+void *e(void *)
+{
+  d = 6;
+}
+int main()
+{
+  if(nondet_bool())
+  {
+    pthread_create(&c, 0, e, 0);
+    return 0;
+  }
+  d = 3;
+  assert(d == 3);
+  return 0;
+}
---
+++ regression/esbmc-unix/github_397_1/main.c
@@ -0,0 +1,21 @@
+#include <pthread.h>
+pthread_t c;
+int d;
+void *e(void *)
+{
+  d = 6;
+}
+int main()
+{
+  if(nondet_bool())
+  {
+    pthread_create(&c, 0, e, 0);
+    return 0;
+  }
+  else
+  {
+    d = 3;
+    assert(d == 3);
+  }
+  return 0;
+}
