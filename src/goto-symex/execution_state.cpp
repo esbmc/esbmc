b71ceeacf06f482526cc8e9af192562ad9c89f67
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-03-01 12:00:06 +0000

    Do not add ESBMC_rounding_mode to global variables
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/goto-symex/execution_state.cpp
+++ src/goto-symex/execution_state.cpp
@@ -804,7 +804,7 @@ void execution_statet::get_expr_globals(
 
     if(
       name == "c:@__ESBMC_alloc" || name == "c:@__ESBMC_alloc_size" ||
-      name == "c:@__ESBMC_is_dynamic")
+      name == "c:@__ESBMC_is_dynamic" || name == "c:@__ESBMC_rounding_mode")
     {
       return;
     }
