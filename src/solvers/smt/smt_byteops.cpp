1b9d76de1f4175e0fd2f435961779200eb03985c
Mikhail R. Gadelha <mikhail.ramalho@gmail.com>
2021-03-06 18:52:17 -0400

    Code style
    Signed-off-by: Mikhail R. Gadelha <mikhail.ramalho@gmail.com>

--- src/solvers/smt/smt_byteops.cpp
+++ src/solvers/smt/smt_byteops.cpp
@@ -3,6 +3,14 @@
 
 smt_astt smt_convt::convert_byte_extract(const expr2tc &expr)
 {
+  if(int_encoding)
+  {
+    std::cerr << "Refusing to byte extract in integer mode; re-run in "
+                 "bitvector mode"
+              << std::endl;
+    abort();
+  }
+
   const byte_extract2t &data = to_byte_extract2t(expr);
   expr2tc source = data.source_value;
   unsigned int src_width = source->type->get_width();
@@ -59,28 +67,25 @@ smt_astt smt_convt::convert_byte_extract(const expr2tc &expr)
 
   smt_astt source_ast = convert_ast(source);
 
-  if(int_encoding)
+  unsigned int sort_sz = data.source_value->type->get_width();
+  if(sort_sz <= upper)
   {
-    std::cerr << "Refusing to byte extract in integer mode; re-run in "
-                 "bitvector mode"
-              << std::endl;
-    abort();
+    smt_sortt s = mk_int_bv_sort(8);
+    return mk_smt_symbol("out_of_bounds_byte_extract", s);
   }
-  else
-  {
-    unsigned int sort_sz = data.source_value->type->get_width();
-    if(sort_sz <= upper)
-    {
-      smt_sortt s = mk_int_bv_sort(8);
-      return mk_smt_symbol("out_of_bounds_byte_extract", s);
-    }
 
-    return mk_extract(source_ast, upper, lower);
-  }
+  return mk_extract(source_ast, upper, lower);
 }
 
 smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
 {
+  if(int_encoding)
+  {
+    std::cerr << "Can't byte update in integer mode; rerun in bitvector mode"
+              << std::endl;
+    abort();
+  }
+
   const byte_update2t &data = to_byte_update2t(expr);
   assert(data.type == data.source_value->type);
 
@@ -148,14 +153,12 @@ smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
     is_number_type(data.source_value->type) &&
     "Byte update of unsupported data type");
 
-  smt_astt value, src_value;
-  unsigned int width_op0, src_offset;
+  smt_astt value = convert_ast(data.update_value);
+  smt_astt src_value = convert_ast(data.source_value);
 
-  value = convert_ast(data.update_value);
-  src_value = convert_ast(data.source_value);
-
-  width_op0 = data.source_value->type->get_width();
-  src_offset = to_constant_int2t(data.source_offset).value.to_uint64();
+  unsigned int width_op0 = data.source_value->type->get_width();
+  unsigned int src_offset =
+    to_constant_int2t(data.source_offset).value.to_uint64();
 
   // Flip location if we're in big-endian mode
   if(data.big_endian)
@@ -163,13 +166,6 @@ smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
     unsigned int data_size =
       type_byte_size(data.source_value->type).to_uint64() - 1;
     src_offset = data_size - src_offset;
-  }
-
-  if(int_encoding)
-  {
-    std::cerr << "Can't byte update in integer mode; rerun in bitvector mode"
-              << std::endl;
-    abort();
   }
 
 // Assertion some of our assumptions, which broadly mean that we'll only work
@@ -187,14 +183,13 @@ smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
   if(src_offset >= (width_op0 / 8))
     return convert_ast(data.source_value);
 
-  smt_astt top, middle, bottom;
-
   // Build in three parts: the most significant bits, any in the middle, and
   // the bottom, of the reconstructed / merged output. There might not be a
   // middle if the update byte is at the top or the bottom.
   unsigned int top_of_update = (8 * src_offset) + 8;
   unsigned int bottom_of_update = (8 * src_offset);
 
+  smt_astt top;
   if(top_of_update == width_op0)
   {
     top = value;
@@ -202,6 +197,7 @@ smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
   else
     top = mk_extract(src_value, width_op0 - 1, top_of_update);
 
+  smt_astt middle;
   if(top == value)
   {
     middle = nullptr;
@@ -211,6 +207,7 @@ smt_astt smt_convt::convert_byte_update(const expr2tc &expr)
     middle = value;
   }
 
+  smt_astt bottom;
   if(src_offset == 0)
   {
     middle = nullptr;
