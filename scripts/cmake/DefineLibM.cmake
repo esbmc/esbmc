4d97178de4e269e7b6d75030c2aec24bc3d787a1
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-03-07 18:33:17 -0300

    Removed libm script

--- scripts/cmake/DefineLibM.cmake
+++
@@ -1,9 +0,0 @@
-# Sets LIBM
-
-if(ENABLE_LIBM)
-  set(LIBM_PATTERN "${PROJECT_SOURCE_DIR}/src/c2goto/library/libm/*.c")
-else()
-  set(LIBM_PATTERN "")
-endif()
-
-message(STATUS "LIBM: ${LIBM_PATTERN}")
