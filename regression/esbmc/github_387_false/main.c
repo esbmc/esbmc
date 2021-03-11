cbe3d5bb8f6f0f8cd062ade7ef89cb3136902713
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-03-11 16:48:30 -0300

    added testcases for stack overflow in z3

---
+++ regression/esbmc/github_387_false/main.c
@@ -0,0 +1,8 @@
+typedef struct {
+  int a
+} b;
+b *c;
+main() {
+  c = malloc(sizeof(b));
+  c->a || d();
+}
---
+++ regression/esbmc/github_387_true/main.c
@@ -0,0 +1,9 @@
+typedef struct {
+  int a
+} b;
+b *c;
+main() {
+  c = malloc(sizeof(b));
+  c->a || d();
+  free(c);
+}
