5e57320942198fe9b665f91d422cc0493ef9b149
Kunjian Song <kunjian90@gmail.com>
2021-04-06 17:55:51 +0100

    Added code placeholders for Solidity's ast_convert, parse_treet, declarationt, symbolt, exprt and irept

---
+++ src/util_solidity/solidity_irep.cpp
@@ -0,0 +1,12 @@
+/*******************************************************************\
+
+Module: Internal Representation for Solidity
+
+Author: Kunjian Song
+
+\*******************************************************************/
+
+#include <cassert>
+#include <cstdlib>
+//#include <util/i2string.h>
+#include <util_solidity/solidity_irep.h>
---
+++ src/util_solidity/solidity_symbol.cpp
@@ -0,0 +1,20 @@
+/*******************************************************************\
+
+Module:
+
+Author: Kunjian Song
+
+\*******************************************************************/
+
+//#include <util/location.h>
+#include <util_solidity/solidity_symbol.h>
+
+solidity_symbolt::solidity_symbolt()
+{
+  clear();
+}
+
+void symbolt::clear()
+{
+    assert(!"hit");
+}
