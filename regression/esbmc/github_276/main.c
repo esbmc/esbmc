bde58ec409872ebf70c130b4a508576108b2dc69
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-04-29 14:17:51 +0100

    Added test case for arithmetic overflow
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc/github_276/main.c
@@ -0,0 +1,13 @@
+long nondet_long(void);
+ 
+int main(void) {
+  long l = 0;
+  int c = 0;
+  long t = nondet_long();
+  long s = nondet_long();
+  long *b = &s;
+ 
+  l = (t + *b) & (0xffffffffL); 
+  c += (l < t);
+}
+
