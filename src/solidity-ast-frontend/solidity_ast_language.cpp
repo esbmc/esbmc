a2df0834389dd4e407bb5fb6602e9a407d20702d
kunjsong01 <kunjian90@gmail.com>
2021-06-03 20:06:34 +0100

    added internal intrinsics and started to convert and add symbols to the context

--- src/solidity-ast-frontend/solidity_ast_language.cpp
+++ src/solidity-ast-frontend/solidity_ast_language.cpp
@@ -21,64 +21,55 @@ bool solidity_ast_languaget::parse(
   message_handlert &message_handler)
 {
     /* store AST text and json contents in stream objects to facilitate parsing and node visiting */
+
+#if 0
+    // For plain-text AST processing...
     printf("plaintext ast path: %s\n", plaintext_ast_path.c_str());
     assert(plaintext_ast_path != "");
-
-    std::ifstream ast_text_file_stream(plaintext_ast_path), ast_json_file_stream(path);
+    std::ifstream ast_text_file_stream(plaintext_ast_path);
     std::stringstream ast_text_stream;
-    std::string new_line, sol_name, ast_json_content;
-
     printf("\n### ast_text_file stream processing:... \n");
     while (getline(ast_text_file_stream, new_line)) {
         printf("new_line: %s\n", new_line.c_str());
         ast_text_stream << new_line << "\n"; // store AST text in stream object
     }
 
-    while (getline(ast_json_file_stream, new_line)) {
-        if (new_line.find(".sol =======") != std::string::npos) {
-            printf("found .sol ====== , breaking ...\n");
-            sol_name = Sif::Utils::substr_by_edge(new_line, "======= ", " =======");
-            break;
-        }
+#endif
+
+  // Process AST json file
+  std::ifstream ast_json_file_stream(path);
+  std::string new_line, sol_name, ast_json_content;
+
+  printf("\n### ast_json_file_stream processing:... \n");
+  while (getline(ast_json_file_stream, new_line)) {
+    if (new_line.find(".sol =======") != std::string::npos) {
+      printf("found .sol ====== , breaking ...\n");
+      sol_name = Sif::Utils::substr_by_edge(new_line, "======= ", " =======");
+      break;
     }
-
-    printf("\n### ast_json_file_stream processing:... \n");
-    while (getline(ast_json_file_stream, new_line)) { // file pointer continues from "=== *.sol ==="
-        //printf("new_line: %s\n", new_line.c_str());
-        if (new_line.find(".sol =======") == std::string::npos) {
-            ast_json_content = ast_json_content + new_line + "\n";
-        }
-        else {
-            assert(!"Unsupported feature: found multiple contracts defined in a single .sol file");
-        }
+  }
+  while (getline(ast_json_file_stream, new_line)) { // file pointer continues from "=== *.sol ==="
+    //printf("new_line: %s\n", new_line.c_str());
+    if (new_line.find(".sol =======") == std::string::npos)
+    {
+      printf("  new_line: ");
+      printf("%s\n", new_line.c_str());
+      ast_json_content = ast_json_content + new_line + "\n";
     }
-    printf("\n ### ast_json_content: ###");
-    printf("%s", ast_json_content.c_str());
-    printf("\n");
-
-    std::string visitor_arg = "";
-    if (ast_json_content != "") {
-        ast_json = nlohmann::json::parse(ast_json_content); // parse explicitly
-        //std::cout << "This is ast_json: " << ast_json.at("absolutePath") << std::endl;
-        Sif::ASTAnalyser ast_analyser(ast_text_stream, ast_json, true, sol_name, visitor_arg);
-
-        Sif::RootNodePtr root_node = ast_analyser.analyse();
-        Sif::Indentation indentation;
-        std::string new_source = root_node->source_code(indentation);
-        //std::cout << sol_name << " " << ast_json.at("absolutePath") << std::endl;
-        std::string output_file_name = "generated_contract.sol";
-        if (output_file_name != "") {
-            std::ofstream output_file_stream(output_file_name);
-            output_file_stream << new_source;
-            output_file_stream.close();
-        } else {
-            assert(!"should not be here");
-        }
+    else
+    {
+      assert(!"Unsupported feature: found multiple contracts defined in a single .sol file");
     }
+  }
+  ast_json = nlohmann::json::parse(ast_json_content); // parse explicitly
 
-    ast_json_file_stream.close();
+  // add internal additions
+  nlohmann::json intrinsic_json = nlohmann::json::parse(internal_additions()); // parse explicitly
+  ast_json.update(intrinsic_json);
 
-    return false;
+  print_json(ast_json);
+
+  return false;
 }
 
 bool solidity_ast_languaget::typecheck(
@@ -126,4 +117,34 @@ bool solidity_ast_languaget::from_type(
 {
   assert(!"come back and continue - solidity_ast_languaget::from_type");
   return false;
+}
+
+void solidity_ast_languaget::print_json(const nlohmann::json &json_in)
+{
+  printf("\n### json_content: ###\n");
+  std::cout << std::setw(2) << json_in << '\n'; // '2' means 2x indentations
+  printf("\n");
+}
+
+std::string solidity_ast_languaget::internal_additions()
+{
+  std::string intrinsics =
+    R"(
+        {
+         "Image": {
+             "Width":  800,
+             "Height": 600,
+             "Title":  "View from 15th Floor",
+             "Thumbnail": {
+                 "Url":    "http://www.example.com/image/481989943",
+                 "Height": 125,
+                 "Width":  100
+             },
+             "Animated" : false,
+             "IDs": [116, 943, 234, 38793]
+         }
+     }
+    )";
+
+    return intrinsics;
 }
