4b475048a45135f976ff5eb017f7f8c5b243e249
kunjsong01 <kunjian90@gmail.com>
2021-06-05 23:19:55 +0100

    added json tracker configure file

---
+++ src/solidity-ast-frontend/solidity_decl_tracker.cpp
@@ -0,0 +1,9 @@
+#include <solidity-ast-frontend/solidity_decl_tracker.h>
+
+namespace ConfigureTracker
+{
+  void configre_decl_function_tracker(const nlohmann::json& decl, DeclTrackerPtr& json_tracker)
+  {
+    assert(!"configuring ...");
+  }
+};
