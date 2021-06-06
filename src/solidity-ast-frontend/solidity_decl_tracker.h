42cf866c80ca801c5b1fb48d927040a3687b985c
kunjsong01 <kunjian90@gmail.com>
2021-06-06 15:49:02 +0100

    added get_type for Builtin type

--- src/solidity-ast-frontend/solidity_decl_tracker.h
+++ src/solidity-ast-frontend/solidity_decl_tracker.h
@@ -26,9 +26,13 @@ public:
   //    Let's not do it for the time being. Leave it for future improvement
   decl_function_tracker& operator=(decl_function_tracker &&rhs) { assert(!"move assignment is not allowed at the moment"); }
 
-  void clear_all()
+  void clear_all() // reset to default values
   {
     isImplicit = false;
+    isDefined = false;
+    isThisDeclarationADefinition = false;
+    type_class = SolidityTypes::TypeError;
+    decl_class = SolidityTypes::DeclError;
   }
 
   // for debug print
@@ -39,16 +43,28 @@ public:
 
   // getters
   bool get_isImplicit() { return isImplicit; }
+  bool get_isDefined() { return isDefined; }
+  bool get_isThisDeclarationADefinition() { return isThisDeclarationADefinition; }
+  SolidityTypes::typeClass getTypeClass() { return type_class; }
+  SolidityTypes::declClass getDeclClass() { return decl_class; }
 
 private:
   // TODO: nlohmann_json has explicit copy constructor defined. probably better to just make a copy instead of using a ref member.
   //       makes it easier to set copy assignment operator and move assignment operator for the future.
   nlohmann::json& decl_func;
   bool isImplicit;
+  bool isDefined;
+  bool isThisDeclarationADefinition;
+  SolidityTypes::typeClass type_class;
+  SolidityTypes::declClass decl_class;
 
   // private setters : set the member values based on the corresponding json value. Used by config() only.
   // Setting them outside this class is NOT allowed.
   void set_isImplicit();
+  void set_isDefined();
+  void set_isThisDeclarationADefinition();
+  void set_type_class();
+  void set_decl_class();
 };
 
 #endif // END of SOLIDITY_AST_FRONTEND_SOLIDITY_DECL_TRACKER_H_
--- src/solidity-ast-frontend/solidity_type.cpp
+++ src/solidity-ast-frontend/solidity_type.cpp
@@ -16,7 +16,48 @@ namespace SolidityTypes
     {
       assert(!"Unsupported declClass");
     }
-
     return DeclError;
+  }
+
+  const char* declClass_to_str(declClass the_decl)
+  {
+    switch(the_decl)
+    {
+      ENUM_TO_STR(DeclLabel)
+      ENUM_TO_STR(DeclFunction)
+      ENUM_TO_STR(DeclError)
+      default:
+      {
+        assert(!"Unknown declClass");
+        return "UNKNOWN";
+      }
+    }
+  }
+
+  typeClass get_type_class(const std::string& kind)
+  {
+    if (kind == "TypeBuiltin")
+    {
+      return TypeBuiltin;
+    }
+    else
+    {
+      assert(!"Unsupported declClass");
+    }
+    return TypeError;
+  }
+
+  const char* typeClass_to_str(typeClass the_type)
+  {
+    switch(the_type)
+    {
+      ENUM_TO_STR(TypeBuiltin)
+      ENUM_TO_STR(TypeError)
+      default:
+      {
+        assert(!"Unknown typeClass");
+        return "UNKNOWN";
+      }
+    }
   }
 };
