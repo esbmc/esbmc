36a991b6a9abe4163d497ae7aa0140d6bb336cf4
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-06-18 15:36:41 -0300

    removed ui_message_handler dependencies

--- src/util/ui_message.h
+++ src/util/ui_message.h
@@ -9,19 +9,16 @@ Author: Daniel Kroening, kroening@kroening.com
 #ifndef CPROVER_UI_LANGUAGE_H
 #define CPROVER_UI_LANGUAGE_H
 
-#include <iostream>
 #include <util/message.h>
 
 class ui_message_handlert : public message_handlert
 {
 public:
-
   ui_message_handlert() = default;
 
-protected:
-  
+protected:  
   // overloading
-  void print(unsigned level, const std::string &message) override;
+  virtual void print(unsigned level, const std::string &message);
 
   // overloading
   void print(
