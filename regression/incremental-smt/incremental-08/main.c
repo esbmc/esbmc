fb658e1f0d5e1bbd551d158f794cdb201565048a
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-07-15 11:23:35 +0100

    added test cases for incremental solving

--- regression/incremental-smt/incremental-08/main.c
+++ regression/incremental-smt/incremental-08/main.c
@@ -1,10 +1,10 @@
 int main()
 {
   unsigned int x, y = 0;
-  for(x = 0; x < 1000; x++)
+  for(x = 0; x < 100; x++)
   {
     y = y + x;
-    if(y > 10)
+    if(y > 10000)
       assert(0);
   }
   return 0;
---
+++ regression/incremental-smt/incremental-09/main.c
@@ -0,0 +1,17 @@
+#include <assert.h>
+int nondet_int();
+int main() {
+  int x = nondet_int(); // input
+  int y = nondet_int(); // input
+  int z = nondet_int(); // input
+  int* r1 = &x;
+  int* r2 = &y;
+  while( z>0 ) {
+    int* tmp = r1;
+    r1 = r2;
+    r2 = tmp;
+    z = z-1;
+    assert(*r1 == x || *r1 == y); 
+  }
+  return 0;
+}
