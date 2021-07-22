8a21ec1072adc74e95f14dc3ebaf05753695b0af
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-07-21 21:44:01 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@ sudo apt-get update && sudo apt-get install -y gperf libgmp-dev cmake bison flex
 git clone --depth=1 --branch=3.2.1 https://github.com/boolector/boolector && cd boolector && ./contrib/setup-lingeling.sh && ./contrib/setup-btor2tools.sh && ./configure.sh --prefix $PWD/../boolector-release && cd build && make -j4 && make install
 cd ../../
 wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz
-tar xf clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz && && mv clang+llvm-11.0.0-aarch64-linux-gnu clang
+tar xf clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz && mv clang+llvm-11.0.0-aarch64-linux-gnu clang
 mkdir build && cd build
 cmake .. -GNinja -DClang_DIR=$PWD/../clang -DLLVM_DIR=$PWD/../clang -DBUILD_STATIC=On -DBoolector_DIR=$PWD/../boolector-release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../release -DCMAKE_BUILD_TYPE=Debug
 cmake --build . && ninja install
