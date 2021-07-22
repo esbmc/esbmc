1b6926f92ec5fa9d6cb6226afc84e5b7fa5a818e
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2021-07-22 19:54:33 -0300

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,4 +5,4 @@ wget https://github.com/llvm/llvm-project/releases/download/llvmorg-11.0.0/clang
 tar xf clang+llvm-11.0.0-aarch64-linux-gnu.tar.xz && mv clang+llvm-11.0.0-aarch64-linux-gnu clang
 mkdir build && cd build
 cmake .. -DClang_DIR=$PWD/../clang -DLLVM_DIR=$PWD/../clang -DBUILD_STATIC=On -DBoolector_DIR=$PWD/../boolector-release -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../release -DCMAKE_BUILD_TYPE=Debug
-make -j2 .
+make -j2 . && make install
