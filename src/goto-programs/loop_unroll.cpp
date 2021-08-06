59dae56da2ed67b7bd642709b0acf2b63aad143a
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2021-08-06 10:15:07 -0300

    added cast to comparation

--- src/goto-programs/loop_unroll.cpp
+++ src/goto-programs/loop_unroll.cpp
@@ -158,7 +158,7 @@ int bounded_loop_unroller::get_loop_bounds(loopst &loop)
     }
 
   int bound = k - k0;
-  if(bound <= 0 || bound > unroll_limit)
+  if(bound <= 0 || (size_t)bound > unroll_limit)
     return 0;
 
   number_of_bounded_loops++;
