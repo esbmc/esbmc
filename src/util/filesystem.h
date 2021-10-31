452c33bda5502d9ff34c874ff032efc761db7ad6
Franz Brauße <franz.brausse@manchester.ac.uk>
2021-10-31 12:43:32 +0100

    [filesystem] replace tmp_dir by proper abstraction tmp_path + add tmp_file
    The race condition inherent to all methods of creating temporary files (or
    directories) that separate path construction from obtaining the corresponding
    resource can lead to abort() in the existing implementation
    file_operations::get_unique_path(), even though it contains a loop exactly
    to prevent that.
    
    On POSIX there is mk{s,d}temp(3p) to get around this loop, however, for temp
    directories on Windows it does not seem that this functionality can easily be
    replicated.
    
    We avoid the abort() case by moving the creation of file/dir into the loop,
    which now only fails (with a boost::filesystem::filesystem_error being thrown),
    when all names of the given pattern are already used. In case the above race
    condition is triggered, the new implementation can still loop unboundedly.
    
    Additionally, two new classes, tmp_path and the derived tmp_file, are
    introduced which take care of removing the respective path from the file
    system upon destruction, and, in case of the latter, also closes the
    associated FILE. The purpose of this RAII style API is to encapsulate the
    associated resource(s), freeing users of the finalization tasks.

--- src/util/filesystem.h
+++ src/util/filesystem.h
@@ -8,46 +8,86 @@ Author: Rafael Menezes, rafael.sa.menezes@outlook.com
 
 #pragma once
 
+#include <cstdio> /* FILE */
 #include <string>
+
 /**
  * @brief this file will contains helper functions for manipulating
-          files
+ *        files
  */
 
-namespace file_operations {
-
+namespace file_operations
+{
 /**
- * @brief Represents a temporary directory, which is removed by the destructor.
+ * @brief Represents a temporary path, which is (optionally) removed by the
+ *        destructor.
+ *
+ * On destruction, optionally (default: yes), the path removed along with all
+ * contained paths if it points to a directory. The default ctor is provided
+ * only to ease array allocation; it does not construct valid temporary paths.
+ * As an instance of this class represents a bound resource, it cannot be
+ * copied, only moved.
  */
-class tmp_dir
+class tmp_path
 {
   std::string _path;
-  bool _keep;
+
+protected:
+  bool _keep = true;
 
 public:
-  tmp_dir(std::string path, bool keep = false);
-  ~tmp_dir();
+  tmp_path() = default;
+  tmp_path(std::string path, bool keep = false);
+  tmp_path(const tmp_path &) = delete;
+  tmp_path(tmp_path &&o);
 
-  tmp_dir(const tmp_dir &) = delete;
+  ~tmp_path();
 
-  tmp_dir & operator=(const tmp_dir &) = delete;
+  tmp_path &operator=(tmp_path o);
 
-  const std::string &path() const noexcept
+  friend void swap(tmp_path &a, tmp_path &b)
   {
-    return _path;
+    using std::swap;
+    swap(a._path, b._path);
+    swap(a._keep, b._keep);
   }
 
-  tmp_dir &keep(bool yes) &noexcept
+  const std::string &path() const noexcept;
+
+  tmp_path &keep(bool yes) &noexcept;
+  tmp_path &&keep(bool yes) &&noexcept;
+};
+
+/**
+ * @brief Temporary path to an open file with an associated `FILE` handle.
+ *
+ * On destruction, optionally (default: yes), the file is closed and the path
+ * removed. The default ctor is provided only to ease array allocation; it
+ * does not construct valid temporary files. As an instance of this class
+ * represents a bound resource, it cannot be copied, only moved.
+ */
+class tmp_file : public tmp_path
+{
+  FILE *_file;
+
+public:
+  tmp_file() = default;
+  tmp_file(FILE *f, tmp_path path);
+  tmp_file(const tmp_file &) = delete;
+  tmp_file(tmp_file &&o) = default;
+
+  ~tmp_file();
+
+  tmp_file &operator=(tmp_file o);
+
+  friend void swap(tmp_file &a, tmp_file &b)
   {
-    _keep = yes;
-    return *this;
+    using std::swap;
+    swap(static_cast<tmp_path &>(a), static_cast<tmp_path &>(b));
+    swap(a._file, b._file);
   }
 
-  tmp_dir &&keep(bool yes) &&noexcept
-  {
-    _keep = yes;
-    return std::move(*this);
-  }
+  FILE *file() noexcept;
 };
 
 /**
@@ -63,4 +103,10 @@ public:
  */
 const std::string get_unique_tmp_path(const std::string &format);
 
-}
+tmp_file create_tmp_file(
+  const std::string &format = "esbmc.%%%%-%%%%-%%%%",
+  const char *mode = "w+");
+
+tmp_path create_tmp_dir(const std::string &format = "esbmc.%%%%-%%%%-%%%%");
+
+} // namespace file_operations
