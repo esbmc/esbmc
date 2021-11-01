6e712c5f4eb32477c677ae2f38650586db7e6830
Fedor Shmarov <shmarovfedor@mail.ru>
2021-11-01 17:13:06 +0000

    [options] removed use-bit-precision option

--- src/esbmc/options.cpp
+++ src/esbmc/options.cpp
@@ -75,7 +75,6 @@ const struct group_opt_templ all_cmd_options[] = {
     {"16", NULL, "set width of machine word (default is 64)"},
     {"32", NULL, "set width of machine word (default is 64)"},
     {"64", NULL, "set width of machine word (default is 64)"},
-    {"use-bit-precision", NULL, "use bit precision (experimental)"},
     {"version", NULL, "show current ESBMC version and exit"},
     {"witness-output",
      boost::program_options::value<std::string>(),
