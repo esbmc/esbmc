52118f0a6ca51a2abca632fbdced7662819e25f4
Fedor Shmarov <shmarovfedor@mail.ru>
2021-11-04 18:52:59 +0000

    [std_types] added is_unnamed_bitfield property to componentt class

--- src/util/std_types.h
+++ src/util/std_types.h
@@ -175,6 +175,16 @@ public:
     {
       return set("is_padding", is_padding);
     }
+
+    bool get_is_unnamed_bitfield() const
+    {
+      return get_bool("is_unnamed_bitfield");
+    }
+
+    void set_is_unnamed_bitfield(bool is_unnamed_bitfield)
+    {
+      return set("is_unnamed_bitfield", is_unnamed_bitfield);
+    }
   };
 
   typedef std::vector<componentt> componentst;
