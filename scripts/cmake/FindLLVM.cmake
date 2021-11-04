c1c71257f47f9c34bb056f9dc76a6ac2d884b38b
Franz Brauße <franz.brausse@manchester.ac.uk>
2021-11-04 11:27:32 +0100

    [cmake LLVM] added CMake options to control bundling of clang headers
    Added the new CMake variable CLANG_HEADERS_BUNDLED, which can take
    (detect, esbmc, On, Off) defaulting to 'detect' and controls whether
    bundling of headers should be enabled:
    - detect: based on BUILD_STATIC
    - esbmc: enabled and use headers from the source distribution
    - On: enabled and use the headers in clang's resource-dir (see below),
          unless overriden by the new CMake variable OVERRIDE_CLANG_HEADER_DIR
    - Off: disabled, hard-code path to clang's resource-dir and pass it via -I
           directly to libclang-cpp
    
    The default 'detect' is different from previous behaviour ('esbmc') in that
    non-static builds against the system's clang now default to using the system
    clang's headers.
    
    Clang's resource-dir is determined by compiling scripts/cmake/try_clang.cc
    against the headers and libraries found in Clang_DIR (or LLVM_DIR, if unset),
    extracting the value of the C-macro CLANG_RESOURCE_DIR used by the clang
    tooling to derive it's own resource-dir:
    
      <https://clang.llvm.org/doxygen/Tooling_8cpp_source.html#l00462>
    
    Additionally, when BUILD_STATIC and the final value of CLANG_HEADERS_BUNDLED
    do not match, warnings about possible inconsistencies on updates are printed
    by CMake. These are necessary in order to inform users about the dependency
    between the headers and libclang.
    
    See scripts/cmake/FindLLVM.cmake for details.
    
    TODO: One of the cases introduced by this change is not implemented:
    CLANG_HEADERS_BUNDLED=On and OVERRIDE_CLANG_HEADER_DIR is set. In this case,
    ideally, we should bundle the headers in the given directory and re-generate
    src/clang-c-frontend/clang_headers.cpp, but the infrastructure is not set up,
    yet.

--- scripts/cmake/FindLLVM.cmake
+++ scripts/cmake/FindLLVM.cmake
@@ -23,39 +23,81 @@ else()
 endif()
 # BUG: For some reason, ESBMC is not linking with Systems LLVM [fb: is this still the case?]
 
+message(STATUS "Clang found in: ${CLANG_INSTALL_PREFIX}")
 if (CLANG_LINK_CLANG_DYLIB AND NOT BUILD_STATIC)
   set(ESBMC_CLANG_LIBS clang-cpp LLVM)
+  set(CLANG_HEADERS_SHOULD_BUNDLE FALSE)
   message(STATUS "Linking libclang: shared")
+else()
+  set(ESBMC_CLANG_LIBS clangTooling clangAST clangIndex)
+  set(CLANG_HEADERS_SHOULD_BUNDLE TRUE)
+  message(STATUS "Linking libclang: static")
+endif()
 
-  # LLVM upstream decided not to export this variable for the installed tree,
-  # since "running 'clang -print-resource-dir'" is good enough:
-  # <https://reviews.llvm.org/D49486>
-  # Here, we are not running clang, since we only require access to the resources
-  # used by the library and do not necessarily want to depend on the clang
-  # executable
+# LLVM upstream decided not to export the variable CLANG_RESOURCE_DIR for the
+# installed tree, since "running 'clang -print-resource-dir' is good enough":
+# <https://reviews.llvm.org/D49486>
+# Here, we are not running clang, since we only require access to the
+# resources used by the library and do not necessarily want to depend on the
+# clang executable, which in, e.g., Ubuntu, lives in a package separate from
+# libclang-cpp.so.
 
-  try_run(TRY_CLANG_RUNS TRY_CLANG_COMPILES ${CMAKE_CURRENT_BINARY_DIR}
-          ${PROJECT_SOURCE_DIR}/scripts/cmake/try_clang.cc
-          CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CLANG_INCLUDE_DIRS}
-          RUN_OUTPUT_VARIABLE CLANG_RESOURCE_DIR)
+try_run(TRY_CLANG_RUNS TRY_CLANG_COMPILES ${CMAKE_CURRENT_BINARY_DIR}
+        ${PROJECT_SOURCE_DIR}/scripts/cmake/try_clang.cc
+        CMAKE_FLAGS -DINCLUDE_DIRECTORIES=${CLANG_INCLUDE_DIRS}
+        LINK_LIBRARIES ${ESBMC_CLANG_LIBS}
+        COMPILE_OUTPUT_VARIABLE TRY_CLANG_COMPILE_OUTPUT
+        RUN_OUTPUT_VARIABLE CLANG_RESOURCE_DIR)
 
-  if (TRY_CLANG_COMPILES AND (TRY_CLANG_RUNS EQUAL 0))
-    string(STRIP "${CLANG_RESOURCE_DIR}" CLANG_RESOURCE_DIR)
-    set(CLANG_RESOURCE_DIR "${CLANG_INCLUDE_DIRS}/${CLANG_RESOURCE_DIR}")
-    message(STATUS "Clang resource dir: ${CLANG_RESOURCE_DIR}")
-    file(REAL_PATH "${CLANG_RESOURCE_DIR}/include" ESBMC_CLANG_HEADER_DIR)
-    if (IS_DIRECTORY "${ESBMC_CLANG_HEADER_DIR}")
-      message("Using system clang's header files in: ${ESBMC_CLANG_HEADER_DIR}")
-    else()
-      unset(ESBMC_CLANG_HEADER_DIR)
-    endif()
+if (NOT TRY_CLANG_COMPILES)
+  message(FATAL_ERROR "Cannot compile against Clang: ${TRY_CLANG_COMPILE_OUTPUT}")
+endif()
+
+if (TRY_CLANG_RUNS EQUAL 0)
+  string(STRIP "${CLANG_RESOURCE_DIR}" CLANG_RESOURCE_DIR)
+  # see clang-toolings's injectResourceDir():
+  # <https://clang.llvm.org/doxygen/Tooling_8cpp_source.html#l00462>
+  set(CLANG_RESOURCE_DIR "${CLANG_INSTALL_PREFIX}/bin/${CLANG_RESOURCE_DIR}")
+  message(STATUS "Clang resource dir: ${CLANG_RESOURCE_DIR}")
+  set(ESBMC_CLANG_HEADER_DIR "${CLANG_RESOURCE_DIR}/include")
+  if (NOT IS_DIRECTORY "${ESBMC_CLANG_HEADER_DIR}")
+    message(STATUS "Failed to determine path to Clang headers: not a directory: ${ESBMC_CLANG_HEADER_DIR}")
+    unset(ESBMC_CLANG_HEADER_DIR)
   endif()
+endif()
 
-  if (NOT ESBMC_CLANG_HEADER_DIR)
-    message("Not using system clang's header files")
+if (NOT ${OVERRIDE_CLANG_HEADER_DIR} STREQUAL "")
+  set(ESBMC_CLANG_HEADER_DIR ${OVERRIDE_CLANG_HEADER_DIR})
+endif()
+
+if (${CLANG_HEADERS_BUNDLED} STREQUAL "detect")
+  set(CLANG_HEADERS_BUNDLED ${CLANG_HEADERS_SHOULD_BUNDLE})
+elseif(${CLANG_HEADERS_BUNDLED} STREQUAL "esbmc")
+  unset(ESBMC_CLANG_HEADER_DIR)
+endif()
+
+if (CLANG_HEADERS_BUNDLED AND NOT CLANG_HEADERS_SHOULD_BUNDLE)
+  message(WARNING "Bundling headers can lead to inconsistencies when libclang is updated independently from ESBMC")
+elseif(NOT CLANG_HEADERS_BUNDLED AND CLANG_HEADERS_SHOULD_BUNDLE)
+  message(WARNING "Not bundling headers can lead to inconsistencies when they are updated independently from ESBMC")
+endif()
+
+if (CLANG_HEADERS_BUNDLED)
+
+  if (ESBMC_CLANG_HEADER_DIR)
+    message(STATUS "Using bundled header files from: ${ESBMC_CLANG_HEADER_DIR}")
+    message(FATAL_ERROR "Bundling external headers is not implemented")
+    # TODO: re-generate src/clang-c-frontend/clang_headers.cpp and unset ESBMC_CLANG_HEADER_DIR
+  else()
+    message(STATUS "Using bundled header files from ESBMC")
   endif()
 
 else()
-  set(ESBMC_CLANG_LIBS clangTooling clangAST clangIndex)
-  message(STATUS "Linking libclang: static")
+
+  if (ESBMC_CLANG_HEADER_DIR)
+    message(STATUS "Hard-coding path to clang's header files: ${ESBMC_CLANG_HEADER_DIR}")
+  else()
+    message(FATAL_ERROR "Cannot find path to Clang headers, please specify it using -DOVERRIDE_CLANG_HEADER_DIR=<path> or enable the bundled headers by -DCLANG_HEADERS_BUNDLED=esbmc")
+  endif()
+
 endif()
