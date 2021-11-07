848e11d4103ccfb7b55f61d5367db2f2caeee885
Franz Brauße <franz.brausse@manchester.ac.uk>
2021-11-07 14:10:28 +0100

    [regression] add orig. and simplified test cases for #447

---
+++ regression/esbmc/github_447/main.c
@@ -0,0 +1,23 @@
+#define UINT8  unsigned __int8
+unsigned arr[100];
+int tt;
+
+int foo() {
+	int x;
+    tt = nondet_int();
+	tt += 1;
+	int y = 25;
+	int res = tt + y;
+
+	return res;
+}
+
+int loop_init() {
+	//__ESBMC_init_var(&arr);
+	int a = foo();
+	while (a > 1)
+		a = a / 2;
+	__ESBMC_assert(a == 2, "Failed as expected");
+
+	return 0;
+}
