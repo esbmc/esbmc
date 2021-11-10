5c3aad05032df66cd0cbfacc8c7266f66a156bfe
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2021-11-10 14:41:26 -0300

    [jimple-frontend] added symbol expr

--- src/jimple-frontend/AST/jimple_expr.cpp
+++ src/jimple-frontend/AST/jimple_expr.cpp
@@ -16,3 +16,45 @@ exprt jimple_constant::to_exprt(
   return constant_exprt(
     integer2binary(as_number, 10), integer2string(as_number), int_type());
 };
+
+void jimple_symbol::from_json(const json &j)
+{
+  j.at("value").get_to(var_name);
+}
+
+exprt jimple_symbol::to_exprt(
+    contextt &ctx,
+    const std::string &class_name,
+    const std::string &function_name) const
+{
+  std::ostringstream oss;
+    oss << class_name << ":" << function_name << "@" << var_name;
+
+    symbolt &test = *ctx.find_symbol(oss.str());
+    return symbol_expr(test);
+};
+
+void jimple_add::from_json(const json &j)
+{
+  j.at("value").get_to(value);
+  j.at("var_name").get_to(var_name);
+}
+
+exprt jimple_add::to_exprt(
+    contextt &ctx,
+    const std::string &class_name,
+    const std::string &function_name) const
+{
+  std::ostringstream oss;
+    oss << class_name << ":" << function_name << "@" << var_name;
+
+    symbolt &test = *ctx.find_symbol(oss.str());
+    auto lhs = symbol_expr(test);
+
+    auto as_number = std::stoi(value);
+    auto rhs = constant_exprt(
+    integer2binary(as_number, 10), integer2string(as_number), int_type());
+
+
+    return gen_binary("add", int_type(), lhs, rhs);
+};
