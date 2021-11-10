fb77ce43bcc0c381e621b55e588dbf54133a229e
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2021-11-10 14:41:26 -0300

    [jimple-frontend] added symbol expr

--- src/jimple-frontend/AST/jimple_expr.h
+++ src/jimple-frontend/AST/jimple_expr.h
@@ -32,22 +32,41 @@ protected:
   std::string value;
 };
 
-/*
 class jimple_symbol : public jimple_expr
 {
 public:
   virtual void from_json(const json &j) override;
   virtual std::string to_string() const override
   {
-    return value;
+    return var_name;
   }
-  virtual exprt to_exprt() const override;
+  virtual exprt to_exprt(
+    contextt &ctx,
+    const std::string &class_name,
+    const std::string &function_name) const override;
 
 protected:
   std::string var_name;
 };
 
-*/
+class jimple_add : public jimple_expr
+{
+public:
+  virtual void from_json(const json &j) override;
+  virtual std::string to_string() const override
+  {
+    return var_name;
+  }
+  virtual exprt to_exprt(
+    contextt &ctx,
+    const std::string &class_name,
+    const std::string &function_name) const override;
+
+protected:
+  std::string var_name;
+  std::string value;
+};
+
 /*
 class jimple_new_expr : public jimple_expr {
     protected:
