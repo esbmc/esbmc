b5643b58bf529d830a5ab97977a3445987df537f
Lucas Cordeiro <lucasccordeiro@gmail.com>
2021-12-21 15:17:07 +0000

    [c2goto] Added models for missing io functions
    Added operational models for the missing gets and fgets
    IO functions from the stdio library.

--- src/c2goto/library/io.c
+++ src/c2goto/library/io.c
@@ -37,6 +37,42 @@ __ESBMC_HIDE:;
   return ret;
 }
 
+// Reads characters from the standard input (stdin)
+// and stores them as a C string into str until a newline character
+// or the end-of-file is reached.
+// Source: https://www.cplusplus.com/reference/cstdio/gets/
+char *gets(char *str)
+{
+__ESBMC_HIDE:;
+  size_t i = 0;
+
+  // produce a nondet number for the number of
+  // characters to read from the standard input
+  // which can lead to buffer overflows
+  size_t size = nondet_uint();
+
+  // add non-deterministic characters to str
+  for(i = 0; i < size; i++)
+  {
+    // produce the non-deterministic character
+    char character = nondet_char();
+    // if the newline character is found, it is not copied into str
+    if(character == '\n')
+    {
+      // append a terminating '\0' to the current position and return
+      str[i] = '\0';
+      return str;
+    }
+    // this should store the character
+    str[i] = character;
+  }
+
+  // append a terminating '\0' to the last position and return
+  str[size - 1] = '\0';
+
+  return str;
+}
+
 FILE *fopen(const char *filename, const char *m)
 {
 __ESBMC_HIDE:;
@@ -58,11 +94,68 @@ __ESBMC_HIDE:;
   return f;
 }
 
+// fgets reads a line from the specified stream and stores it
+// into the string pointed to by str. It stops when either (n-1)
+// characters are read, the newline character is read, or the
+// end-of-file is reached, whichever comes first.
+// source: https://www.cplusplus.com/reference/cstdio/fgets/
 char *fgets(char *str, int size, FILE *stream)
 {
 __ESBMC_HIDE:;
-  _Bool error;
-  return error ? 0 : str;
+  // this is used to check for early loop termination
+  _Bool early_termination = 0;
+  // check for pre-conditions
+  __ESBMC_assert(
+    stream == stdin || stream != NULL,
+    "the pointer to a file object must be a valid argument");
+
+  //do nothing, report error
+  if(size < 2)
+    return NULL;
+
+  int i = 0;
+  // add non-deterministic characters to str
+  for(i = 0; i <= (size - 1); i++)
+  {
+    // produce non-deterministic character
+    int character = getc(stream);
+    // stop if we get a new line character or an EOF
+    if(character == '\n')
+    {
+      // A newline character makes fgets stop reading,
+      // but it is considered a valid character by the function
+      // and included in the string copied to str
+      str[i] = '\n';
+      early_termination = 1;
+      break;
+    }
+    else if(character == EOF)
+    {
+      // we end the loop based on non-determinism
+      early_termination = 1;
+      break;
+    }
+    // this should store the character
+    str[i] = character;
+  }
+
+  // has the loop terminated before filling in all positions?
+  // note that the loop can stop at any iteration
+  if(early_termination)
+  {
+    // didn't we reach the total buffer size?
+    if(i < (size - 1))
+    {
+      // append a terminating '\0' to the next position and return
+      str[++i] = '\0';
+      return str;
+    }
+  }
+
+  // append a terminating '\0' to the last position and return
+  str[size - 1] = '\0';
+
+  return str;
 }
 
 size_t fread(void *ptr, size_t size, size_t nitems, FILE *stream)
