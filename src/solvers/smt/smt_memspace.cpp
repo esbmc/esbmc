d4a32a829a21ef1620b1a6ce4b20861a44b169ec
Franz Brauße <franz.brausse@manchester.ac.uk>
2021-12-29 18:08:25 +0100

    [cheri] WIP

--- src/solvers/smt/smt_memspace.cpp
+++ src/solvers/smt/smt_memspace.cpp
@@ -335,8 +335,8 @@ smt_astt smt_convt::convert_identifier_pointer(
 smt_astt smt_convt::init_pointer_obj(unsigned int obj_num, const expr2tc &size)
 {
   std::vector<expr2tc> membs;
-  membs.push_back(constant_int2tc(machine_ptr, BigInt(obj_num)));
-  membs.push_back(constant_int2tc(machine_ptr, BigInt(0)));
+  membs.push_back(constant_int2tc(pointer_struct->members[0], BigInt(obj_num)));
+  membs.push_back(constant_int2tc(pointer_struct->members[1], BigInt(0)));
   constant_struct2tc ptr_val_s(pointer_struct, membs);
   smt_astt ptr_val = tuple_api->tuple_create(ptr_val_s);
 
@@ -397,7 +397,7 @@ smt_astt smt_convt::init_pointer_obj(unsigned int obj_num, const expr2tc &size)
   type2tc arrtype(new array_type2t(get_bool_type(), expr2tc(), true));
   symbol2tc allocarr(arrtype, dyn_info_arr_name);
   constant_int2tc objid(
-    get_uint_type(config.ansi_c.int_width), BigInt(obj_num));
+    get_uint_type(sizeof(obj_num) * CHAR_BIT), BigInt(obj_num));
   index2tc idx(get_bool_type(), allocarr, objid);
   equality2tc dyn_eq(idx, gen_false_expr());
   assert_expr(dyn_eq);
@@ -573,9 +573,15 @@ void smt_convt::init_addr_space_array()
   bump_addrspace_array(pointer_logic.back().get_invalid_object(), addr1_tuple);
 
   constant_struct2tc null_ptr_tuple(
-    pointer_struct, std::vector<expr2tc>{zero_ptr_int, zero_ptr_int});
+    pointer_struct,
+    std::vector<expr2tc>{
+      constant_int2tc(pointer_struct->members[0], 0),
+      constant_int2tc(pointer_struct->members[1], 0)});
   constant_struct2tc invalid_ptr_tuple(
-    pointer_struct, std::vector<expr2tc>{one_ptr_int, zero_ptr_int});
+    pointer_struct,
+    std::vector<expr2tc>{
+      constant_int2tc(pointer_struct->members[0], 1),
+      constant_int2tc(pointer_struct->members[1], 0)});
 
   null_ptr_ast = convert_ast(null_ptr_tuple);
   invalid_ptr_ast = convert_ast(invalid_ptr_tuple);
