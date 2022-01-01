d512658dce3a0108c86ffd2ba3a5a4b63ed20831
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-01-01 15:45:18 +0100

    [goto-programs] remove unused version of read_goto_binary()

--- src/goto-programs/read_goto_binary.cpp
+++ src/goto-programs/read_goto_binary.cpp
@@ -10,17 +10,6 @@ Author:
 #include <goto-programs/read_goto_binary.h>
 #include <fstream>
 
-void read_goto_binary(
-  std::istream &in,
-  contextt &context,
-  goto_functionst &dest,
-  const messaget &message_handler)
-{
-  bool fail [[gnu::unused]] =
-    read_bin_goto_object(in, "", context, dest, message_handler);
-  assert(!fail);
-}
-
 bool read_goto_binary(
   const std::string &path,
   contextt &context,
