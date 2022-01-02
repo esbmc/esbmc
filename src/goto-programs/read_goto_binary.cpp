87f617505a4efba6a9d3aaf64dd5dfc54fce708f
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-01-02 12:58:22 +0100

    [goto-programs c2goto] add_cprover_library(): avoid round-trip via tmp-file

--- src/goto-programs/read_goto_binary.cpp
+++ src/goto-programs/read_goto_binary.cpp
@@ -9,6 +9,20 @@ Author:
 #include <goto-programs/read_bin_goto_object.h>
 #include <goto-programs/read_goto_binary.h>
 #include <fstream>
+#include <boost/iostreams/device/array.hpp>
+#include <boost/iostreams/stream.hpp>
+
+bool read_goto_binary_array(
+  const void *data,
+  size_t size,
+  contextt &context,
+  goto_functionst &dest,
+  const messaget &msg)
+{
+  using namespace boost::iostreams;
+  stream<array_source> src(static_cast<const char *>(data), size);
+  return read_bin_goto_object(src, "", context, dest, msg);
+}
 
 bool read_goto_binary(
   const std::string &path,
