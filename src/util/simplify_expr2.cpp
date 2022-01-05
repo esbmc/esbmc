a5b9e61e433e1c261f5cdcd1c290bd59c9d8506f
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-01-05 13:16:04 +0100

    [util/simplify_expr2] simplify do_bit_munge_operation()

--- src/util/simplify_expr2.cpp
+++ src/util/simplify_expr2.cpp
@@ -1252,42 +1252,18 @@ static expr2tc do_bit_munge_operation(
   int64_t val1 = int1.value.to_int64();
   int64_t val2 = int2.value.to_int64();
 
-  if(int1.value.is_negative())
+  uint64_t r = opfunc(val1, val2);
+
+  if(type->get_width() < 64)
   {
-    if(val1 & 0x8000000000000000ULL)
-    {
-      // Too large to fit, negative, in an int64_t.
-      return expr2tc();
-    }
-
-    val1 = -val1;
+    // truncate the result to the type's width
+    uint64_t trunc_mask = ~(uint64_t)0 << type->get_width();
+    r &= ~trunc_mask;
+    // if the type is signed and r's sign-bit is set, sign-extend it
+    if(is_signedbv_type(type) && r >> (type->get_width() - 1))
+      r |= trunc_mask;
   }
-
-  if(int2.value.is_negative())
-  {
-    if(val2 & 0x8000000000000000ULL)
-    {
-      // Too large to fit, negative, in an int64_t.
-      return expr2tc();
-    }
-
-    val2 = -val2;
-  }
-
-  val1 = opfunc(val1, val2);
-
-  // This has potentially become negative. Check the top bit.
-  if(val1 & (1 << (type->get_width() - 1)) && is_signedbv_type(type))
-  {
-    // Sign extend.
-    val1 |= ULLONG_MAX << (type->get_width());
-  }
-
-  // We should perform a mask to truncate the numbers
-  BigInt r;
-  r.setPower2(type->get_width());
-  r -= 1;
-  return constant_int2tc(type, BigInt(val1 & r.to_int64()));
+  return constant_int2tc(type, BigInt((int64_t)r));
 }
 
 expr2tc bitand2t::do_simplify() const
@@ -1356,7 +1332,7 @@ expr2tc bitnot2t::do_simplify() const
 expr2tc shl2t::do_simplify() const
 {
   std::function<int64_t(int64_t, int64_t)> op = [](int64_t op1, int64_t op2) {
-    return (op1 << op2);
+    return ((uint64_t)op1 << op2);
   };
 
   return do_bit_munge_operation<shl2t>(op, type, side_1, side_2);
