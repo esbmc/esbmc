1fe1c24aa87bb5b40607c6cb4a37146a104ed5ba
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-01-08 11:45:16 +0100

    [flail.py] support multiple inputs + [cmake] de-duplicate mangle_hs -> mangle

--- scripts/cmake/Utils.cmake
+++ scripts/cmake/Utils.cmake
@@ -31,3 +31,76 @@ macro(SUBDIRLIST result curdir)
   endforeach()
   set(${result} ${dirlist})
 endmacro()
+
+function(mangle output_c dir)
+  # optional flags
+  set(kw0 SINGLE           # accumulate all outputs in the single .c file ${output_c}
+     )
+  # optional 1-parameter keywords to this function
+  set(kw1 MACRO            # sets flail.py --macro parameter to the given argument
+          ACC_HEADERS_INTO # generates a header that #include-s all generated headers
+          WILDCARD         # globs ${dir}/${MANGLE_WILDCARD} instead of using the unparsed arguments as filenames under ${dir}
+          PREFIX           # sets flail.py --prefix parameter to the given argument
+     )
+  cmake_parse_arguments(MANGLE "${kw0}" "${kw1}" "" ${ARGN})
+  if (MANGLE_WILDCARD)
+    set(single_file_desc "${MANGLE_WILDCARD} in ${dir}/")
+    file(GLOB inputs CONFIGURE_DEPENDS ${dir}/${MANGLE_WILDCARD} ${MANGLE_UNPARSED_ARGUMENTS})
+  else()
+    set(inputs ${MANGLE_UNPARSED_ARGUMENTS})
+    list(JOIN ", " inputs single_file_desc)
+    set(single_file_desc "${inputs} in ${dir}")
+    list(TRANSFORM inputs PREPEND ${dir}/)
+  endif()
+  set(cmd0 ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/scripts/flail.py)
+  if (MANGLE_MACRO)
+    list(APPEND cmd0 --macro ${MANGLE_MACRO})
+  endif()
+  if (MANGLE_PREFIX)
+    list(APPEND cmd0 --prefix ${MANGLE_PREFIX})
+  endif()
+  if (MANGLE_SINGLE)
+    set(outputs ${output_c})
+    set(dep ${CMAKE_SOURCE_DIR}/scripts/flail.py)
+    set(cmd ${cmd0} -o ${output_c})
+    if (MANGLE_ACC_HEADERS_INTO)
+      list(APPEND outputs ${MANGLE_ACC_HEADERS_INTO})
+      list(APPEND cmd --header ${MANGLE_ACC_HEADERS_INTO})
+    endif()
+    add_custom_command(OUTPUT ${outputs}
+                       COMMAND ${cmd} ${inputs}
+                       DEPENDS ${dep} ${inputs}
+                       WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+                       COMMENT "Converting ${single_file_desc} to data"
+                       VERBATIM)
+  else()
+    set(result_c "")
+    set(includes "")
+    foreach(in_f ${inputs})
+      file(RELATIVE_PATH out ${dir} ${in_f})
+      string(PREPEND out "${CMAKE_CURRENT_BINARY_DIR}/")
+      set(outputs ${out}.c)
+      set(cmd ${cmd0} -o ${out}.c)
+      set(dep ${in_f} ${CMAKE_SOURCE_DIR}/scripts/flail.py)
+      if (MANGLE_ACC_HEADERS_INTO)
+        list(APPEND outputs ${out}.h)
+        list(APPEND cmd --header ${out}.h)
+        list(APPEND dep ${MANGLE_ACC_HEADERS_INTO})
+        list(APPEND includes "#include \"${out}.h\"")
+      endif()
+      add_custom_command(OUTPUT ${outputs}
+        COMMAND ${cmd} ${in_f}
+        DEPENDS ${dep} ${in_f}
+        WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+        COMMENT "Converting ${in_f} to data"
+        VERBATIM
+        )
+      list(APPEND result_c ${out}.c)
+    endforeach()
+    set(${output_c} "${result_c}" PARENT_SCOPE)
+    if (MANGLE_ACC_HEADERS_INTO)
+      list(JOIN includes "\n" includes)
+      file(WRITE ${MANGLE_ACC_HEADERS_INTO} "${includes}")
+    endif()
+  endif()
+endfunction()
