3ed74060900f8beba825de19073001ad84f64801
Lucas Cordeiro <lucasccordeiro@gmail.com>
2022-01-09 07:01:45 +0000

    [Regression] Indicated as known bug
    After fixing the dereference function call issue, ESBMC
    started verifying this code. However, there is another bug
    here possibly due to the generation of the guards. CBMC
    fails with a message like `pointer handling is unsound in
    concurrency`.

--- regression/esbmc-unix2/11_lu-fig2.fixed/lu-fig2.fixed.c
+++ regression/esbmc-unix2/11_lu-fig2.fixed/lu-fig2.fixed.c
@@ -10,11 +10,11 @@ int thr1() { //nsThread::Init (mozilla/xpcom/threads/nsThread.cpp 1.31)
   int PR_CreateThread__RES = 1;
   acquire(mStartLock);
   start_main=1;
-  { __ESBMC_atomic_begin();
+  { __VERIFIER_atomic_begin();
       if( __COUNT__ == 0 ) { // atomic check(0);
 	mThread = PR_CreateThread__RES; 
 	__COUNT__ = __COUNT__ + 1; 
-    __ESBMC_atomic_end();
+    __VERIFIER_atomic_end();
       } else { assert(0); } 
   }
   release(mStartLock);
@@ -29,12 +29,12 @@ void thr2() { //nsThread::Main (mozilla/xpcom/threads/nsThread.cpp 1.31)
   while (start_main==0);
   acquire(mStartLock);
   release(mStartLock);
-  { __ESBMC_atomic_begin();
+  { __VERIFIER_atomic_begin();
       if( __COUNT__ == 1 ) { // atomic check(1);
 	    int rv = self; // self->RegisterThreadSelf();
 	    __COUNT__ = __COUNT__ + 1;
       } else { assert(0); } 
-    __ESBMC_atomic_end();
+    __VERIFIER_atomic_end();
   }
 }
 
