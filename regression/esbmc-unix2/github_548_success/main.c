3f79744f1e813ff9a54cdc063a59ad7c5926626f
Lucas Cordeiro <lucasccordeiro@gmail.com>
2022-01-18 08:53:30 +0000

    [Regression] Added tests for mutex initialization

---
+++ regression/esbmc-unix2/github_548_success/main.c
@@ -0,0 +1,33 @@
+#include <pthread.h>
+#include <assert.h>
+
+int glob1 = 0;
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+void *t_fun(void *arg)
+{
+  pthread_mutex_lock(&mutex2);
+  glob1 = 5;
+  pthread_mutex_unlock(&mutex2);
+  pthread_mutex_lock(&mutex2);
+  glob1 = 10;
+  pthread_mutex_unlock(&mutex2);
+  return NULL;
+}
+
+void *foo(void *arg)
+{
+  pthread_mutex_lock(&mutex2);
+  assert(glob1 == 0 || glob1 == 5 || glob1 == 10);
+  pthread_mutex_unlock(&mutex2);
+  return NULL;
+}
+
+int main(void)
+{
+  pthread_t id;
+  pthread_create(&id, NULL, t_fun, NULL);
+  foo(NULL);
+  pthread_join(id, NULL);
+  return 0;
+}
