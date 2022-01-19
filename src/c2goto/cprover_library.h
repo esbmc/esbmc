8c8f4cda061ec10f46967889bce9739f6ff46537
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-01-19 10:16:43 +0100

    [c2goto] add access to header-dir of internal libc

--- src/c2goto/cprover_library.h
+++ src/c2goto/cprover_library.h
@@ -14,6 +14,11 @@ Author: Daniel Kroening, kroening@kroening.com
 
 class languaget;
 
+/* Returns the path the headers of the internal libc have been extracted to
+ * or NULL if no library is configured (either via config.ansi_c.lib or during
+ * build time). */
+const std::string *internal_libc_header_dir();
+
 void add_cprover_library(
   contextt &context,
   const messaget &message_handler,
