d382bba7b4af258b72147d2f2fbeaa1bbbc0eb25
Lucas Cordeiro <lucasccordeiro@gmail.com>
2022-01-19 16:35:59 +0000

    [regression] Enabled test case
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc-unix2/11_time_var_mutex/time_var_mutex.c
+++ regression/esbmc-unix2/11_time_var_mutex/time_var_mutex.c
@@ -6,27 +6,44 @@ int inode;
 int m_inode=0; // protects the inode
 int m_busy=0; // protects the busy flag
 
+void assume_abort_if_not(int cond) {
+   if(!cond) {abort();}
+ }
+
+ void acquire(int *lock) {
+     __VERIFIER_atomic_begin();
+     assume_abort_if_not(*lock == 0);
+     *lock = 1;
+     __VERIFIER_atomic_end();
+ }
+ void release(int *lock) {
+     __VERIFIER_atomic_begin();
+     assume_abort_if_not(*lock == 1);
+     *lock = 0;
+     __VERIFIER_atomic_end();
+ }
+
 thr1(){
   glb_init(inode == busy);
-  acquire(m_inode);
+  acquire(&m_inode);
   if(inode == 0){
-    acquire(m_busy);
+    acquire(&m_busy);
     busy = 1;
-    release(m_busy);
+    release(&m_busy);
     inode = 1;
   }
   block = 1;
   assert(block == 1);
-  release(m_inode);
+  release(&m_inode);
 }
 
 thr2(){
-  acquire(m_busy);
+  acquire(&m_busy);
   if(busy == 0){
     block = 0;
     assert(block == 0);
   }
-  release(m_busy);
+  release(&m_busy);
 }
  
 int main() {
