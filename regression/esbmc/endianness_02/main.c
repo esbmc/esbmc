2d2c67d30f35dfa3c8ad39f5b929ba5e4fcf9efa
Fedor Shmarov <shmarovfedor@mail.ru>
2022-02-04 15:53:37 +0000

    [regression] added testcases for endianness

---
+++ regression/esbmc/endianness_02/main.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include <assert.h>
+
+int main()
+{
+	char char_array[4] = { 0x1, 0x2, 0x3, 0x4 };
+	short short_array[2];
+	memcpy(short_array, char_array, 4);
+	short s0 = short_array[0];
+	short s1 = short_array[1];
+  /* big-endian */
+	assert(s0 == 0x0102);
+	assert(s1 == 0x0304);
+}
+
