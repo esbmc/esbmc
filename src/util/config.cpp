b56f60d1f496c41035a2bc0b56e78eafe1300efe
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-02-09 12:38:57 +0100

    [config clang-c-frontend] cheri: enable purecap arch/flags only for --cheri purecap

--- src/util/config.cpp
+++ src/util/config.cpp
@@ -202,10 +202,13 @@ bool configt::set(const cmdlinet &cmdline, const messaget &msg)
     arch = "mips64el"; /* CHERI-TODO: either big-endian MIPS or maybe RISC-V */
 #endif
     os = "freebsd";
-    if(!flavor.empty())
-      msg.warning(
-        "overriding flavor '" + flavor + "' by 'purecap' due to --cheri");
-    flavor = "purecap";
+    if(ansi_c.cheri == ansi_ct::CHERI_PURECAP)
+    {
+      if(flavor != "purecap")
+        msg.warning(
+          "overriding flavor '" + flavor + "' by 'purecap' due to --cheri");
+      flavor = "purecap";
+    }
     req_target++;
   }
 
