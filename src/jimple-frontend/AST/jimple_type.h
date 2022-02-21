748519446ea2c144678e6c40fd524f19af23257c
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2022-02-21 09:21:50 +0000

    [jimple] added more basic types

--- src/jimple-frontend/AST/jimple_type.h
+++ src/jimple-frontend/AST/jimple_type.h
@@ -53,12 +53,20 @@ private:
   BASE_TYPES bt;
   std::map<std::string, BASE_TYPES> from_map = {
     {"int", BASE_TYPES::INT},
+    {"byte", BASE_TYPES::INT},
+    {"char", BASE_TYPES::INT},
+    {"short", BASE_TYPES::INT},
     {"boolean", BASE_TYPES::INT},
+    {"long", BASE_TYPES::INT},
+    {"float", BASE_TYPES::INT},
+    {"double", BASE_TYPES::INT},
     {"void", BASE_TYPES::_VOID},
     {"Main", BASE_TYPES::INT}, // TODO: handle this properly
-
+    {"java.util.Random", BASE_TYPES::INT},         // We dont really care about the initialization of this mode
     {"java.lang.String", BASE_TYPES::INT},         // TODO: handle this properly
     {"java.lang.AssertionError", BASE_TYPES::INT}, // TODO: handle this properly
+    {"java.lang.Runtime", BASE_TYPES::INT}, // TODO: handle this properly
+    {"java.lang.Class", BASE_TYPES::INT}, // TODO: handle this properly
     {"__other", BASE_TYPES::OTHER}};
 };
 
