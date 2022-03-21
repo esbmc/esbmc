36a6e57d8af3adfbeb1dae1a4edef42c3ab4ea3f
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2022-03-21 14:09:52 +0000

    [options] added an option to not crash on non supported models

--- src/solvers/smt/smt_conv.cpp
+++ src/solvers/smt/smt_conv.cpp
@@ -2283,9 +2283,19 @@ expr2tc smt_convt::get_by_ast(const type2tc &type, smt_astt a)
   }
 
   default:
-    msg.error(fmt::format(
-      "Unimplemented type'd expression ({}) in smt get", type->type_id));
-    abort();
+    if(options.get_bool_option("non-supported-models-as-zero"))
+    {
+      msg.error(fmt::format(
+        "Unimplemented type'd expression ({}) in smt get", type->type_id));
+      abort();
+    }
+    else
+    {
+      msg.warning(fmt::format(
+        "Unimplemented type'd expression ({}) in smt get. Returning zero!",
+        type->type_id));
+      return gen_zero(type);
+    }
   }
 }
 
@@ -2309,9 +2319,20 @@ expr2tc smt_convt::get_by_type(const expr2tc &expr)
     return tuple_api->tuple_get(expr);
 
   default:
-    msg.error(fmt::format(
-      "Unimplemented type'd expression ({}) in smt get", expr->type->type_id));
-    abort();
+    if(options.get_bool_option("non-supported-models-as-zero"))
+    {
+      msg.error(fmt::format(
+        "Unimplemented type'd expression ({}) in smt get",
+        expr->type->type_id));
+      abort();
+    }
+    else
+    {
+      msg.warning(fmt::format(
+        "Unimplemented type'd expression ({}) in smt get. Returning zero!",
+        expr->type->type_id));
+      return gen_zero(expr->type);
+    }
   }
 }
 
