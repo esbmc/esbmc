6cf9bab977deaaaa8e4dc3c9fe7e7dfe7fce2ca6
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2022-03-30 15:48:25 +0000

    [goto-contractor] simplified generation library

--- src/util/goto_expr_factory.h
+++ src/util/goto_expr_factory.h
@@ -1,98 +1,38 @@
 /*******************************************************************\
-Module: Cache unit test utilities
+Module: Helper functions to generate GOTO expressions
 Author: Rafael Sá Menezes
-Date: March 2020
+Date: March 2021
 \*******************************************************************/
-#ifndef ESBMC_GOTO_EXPR_FACTORY_H
-#define ESBMC_GOTO_EXPR_FACTORY_H
+#pragma once
 
-//#include <cache/ssa_step_algorithm.h>
 #include "goto-symex/symex_target_equation.h"
-// TODO: Test all functions from this file
 
-bool is_symbols_equal(expr2tc &v1, expr2tc &v2);
+/**
+ * @brief Create a constant_int expression
+ *
+ * @param value: numeric value of the constant
+ * @param type: type of the expression
+ * @return int negative means error, positive is the quantity to unroll
+ */
+constant_int2tc create_value_expr(int value, type2tc type);
 
-bool is_unsigned_equal(expr2tc &v1, expr2tc &v2);
-
-symbol2tc create_unsigned_32_symbol_expr(std::string name);
-
-constant_int2tc create_unsigned_32_value_expr(unsigned value);
-
-constant_int2tc create_signed_32_value_expr(int value);
-
-add2tc create_unsigned_32_add_expr(expr2tc &side1, expr2tc &side2);
-
-neg2tc create_unsigned_32_neg_expr(expr2tc &value);
-
-not2tc create_not_expr(expr2tc &value);
-
-add2tc create_signed_32_add_expr(expr2tc &side1, expr2tc &side2);
-
-mul2tc create_unsigned_32_mul_expr(expr2tc &side1, expr2tc &side2);
-
-mul2tc create_signed_32_mul_expr(expr2tc &side1, expr2tc &side2);
-
-lessthan2tc create_lesser_relation(expr2tc &lhs, expr2tc &rhs);
-
+/**
+ * @brief Create a lessthanequal expression of the form LHS <= RHS
+ * The comparation will use LHS for typecast
+ *
+ * @param lhs: left operator
+ * @param rhs: right operator
+ * @return int negative means error, positive is the quantity to unroll
+ */
 lessthanequal2tc create_lessthanequal_relation(expr2tc &lhs, expr2tc &rhs);
 
+/**
+ * @brief Create a lessthanequal expression of the form LHS <= RHS
+ * The comparation will use LHS for typecast
+ *
+ * @param lhs: left operator
+ * @param rhs: right operator
+ * @return int negative means error, positive is the quantity to unroll
+ */
 greaterthanequal2tc
 create_greaterthanequal_relation(expr2tc &lhs, expr2tc &rhs);
-
-greaterthan2tc create_greater_relation(expr2tc &lhs, expr2tc &rhs);
-
-equality2tc create_equality_relation(expr2tc &lhs, expr2tc &rhs);
-
-void create_assignment(
- symex_target_equationt::SSA_stepst &output,
- expr2tc &rhs);
-
-void create_assumption(
- symex_target_equationt::SSA_stepst &output,
- expr2tc &cond);
-
-void create_assert(symex_target_equationt::SSA_stepst &output, expr2tc &cond);
-
-// Pre-Built expressions
-// These expressions will be used thorough the
-// test cases as simple validations.
-// The fuzzer should use the above method as a way to construct
-// and validate expressions
-
-void init_test_values();
-
-// ((y + x) + 7) == 9
-expr2tc equality_1();
-// ((x + y) + 7) == 9
-expr2tc equality_1_ordered();
-// ((x + y) + -2) == 0
-expr2tc equality_1_green_normal();
-
-void is_equality_1_equivalent(expr2tc &actual, expr2tc &expected);
-
-// (1 + x) == 0
-expr2tc equality_2();
-
-// (x + 1) == 0
-expr2tc equality_2_ordered();
-
-// (x + 1) == 0
-expr2tc equality_2_green_normal();
-
-// (y + 4) == 8
-expr2tc equality_3();
-
-// (y + 4) == 8
-expr2tc equality_3_ordered();
-
-// (y - 4) == 0
-expr2tc equality_3_green_normal();
-// (x + 0) == 0
-expr2tc equality_4();
-
-// (x + 0) == 0
-expr2tc equality_4_ordered();
-// (x + 0) == 0
-expr2tc equality_4_green_normal();
-
-#endif //ESBMC_GOTO_EXPR_FACTORY_H
