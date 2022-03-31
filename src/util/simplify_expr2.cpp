b9bf34d27b32935e7d516a708f85c34726e7cfb4
Uchoa <maktheus@gmail.com>
2022-03-31 15:55:12 -0400

    lambda update

--- src/util/simplify_expr2.cpp
+++ src/util/simplify_expr2.cpp
@@ -414,38 +414,59 @@ static type2tc common_arith_op2_type(expr2tc &e, expr2tc &f)
 
 expr2tc add2t::do_simplify() const
 {
+  // X + X --> X << 1
+  if(is_symbol2t(side_1) && is_symbol2t(side_2))
+  {
+    dump();
+    auto name_1 = to_symbol2t(side_1).get_symbol_name();
+    auto name_2 = to_symbol2t(side_2).get_symbol_name();
+
+    if(name_1 == name_2)
+    {
+      return shl2tc(type, side_1, from_integer(1, type));
+    }
+  }
+
   // (A + 1) + ~B --> A - B
   if(is_add2t(side_1) && is_bitnot2t(side_2))
   {
     auto sidecheck = to_add2t(side_1).side_2;
     auto sidecheck2 = to_add2t(side_1).side_1;
-    if(to_constant_int(sidecheck).value == 1)
+    if(is_constant_int2t(sidecheck) == 1)
     {
       auto B = to_bitnot2t(side_2).value;
-      auto new_operand = sub2tc(side_1->type, sidecheck2, B);      
+      auto new_operand = sub2tc(side_1->type, sidecheck2, B);
       return new_operand;
     }
   }
+
+  auto side_check = [](expr2tc &side_1, expr2tc &side_2)
+  {
+    if(is_bitnot2t(side_1) && is_symbol2t(side_2))
+    {
+      auto B = to_bitnot2t(side_1).value;
+      auto new_operand = sub2tc(side_1->type, side_2, B);
+      return new_operand;
+    }
+  };
+
+
   // (~B + A) + 1 --> A - B
   //&&
   // (A + ~B) + 1 --> A - B
-  if(is_add2t(side_1) && to_constant_int(side_2).value == 1)
+  
+  if(is_add2t(side_1) && is_constant_int2t(side_2) == 1)
   {
     auto sidecheck_1 = to_add2t(side_1).side_1;
     auto sidecheck_2 = to_add2t(side_1).side_2;
 
-    if(is_bitnot2t(sidecheck_1) && is_symbol2t(sidecheck_2))
+    if(side_check(sidecheck_1, sidecheck_2))
     {
-      auto B = to_bitnot2t(sidecheck_1).value;
-      auto new_operand = sub2tc(side_1->type, sidecheck_2, B);
-      return new_operand;
+      return side_check(sidecheck_1, sidecheck_2);
     }
-
-    if(is_symbol2t(sidecheck_1) && is_bitnot2t(sidecheck_2))
+    if(side_check(sidecheck_2, sidecheck_1))
     {
-      auto B = to_bitnot2t(sidecheck_2).value;
-      auto new_operand_2 = sub2tc(side_1->type, sidecheck_1, B);
-      return new_operand_2;
+      return side_check(sidecheck_2, sidecheck_1);
     }
   }
 
@@ -454,28 +475,110 @@ expr2tc add2t::do_simplify() const
   {
     auto sidecheck_1 = to_add2t(side_2).side_1;
     auto sidecheck_2 = to_add2t(side_2).side_2;
-    if(is_symbol2t(sidecheck_1) && to_constant_int(sidecheck_2).value == 1)
+    if(is_symbol2t(sidecheck_1) && is_constant_int2t(sidecheck_2) == 1)
     {
       auto B = to_bitnot2t(side_1).value;
       auto new_operand_2 = sub2tc(side_1->type, sidecheck_1, B);
+
       return new_operand_2;
     }
   }
 
-  expr2tc res = simplify_arith_2ops<Addtor, add2t>(type, side_1, side_2);
-  if(!is_nil_expr(res))
-    return res;
+  // X + (Y - X) -> Y
 
-  // Attempt associative simplification
-  std::function<expr2tc(const expr2tc &arg1, const expr2tc &arg2)> add_wrapper =
-    [this](const expr2tc &arg1, const expr2tc &arg2) -> expr2tc
+  if(is_symbol2t(side_1) && is_sub2t(side_2))
   {
-    expr2tc a = arg1, b = arg2;
-    type2tc t = common_arith_op2_type(a, b);
-    return add2tc(t, a, b);
-  };
+    auto sidecheck_1 = to_sub2t(side_2).side_1;
+    auto sidecheck_2 = to_sub2t(side_2).side_2;
+    dump();
 
-  return attempt_associative_simplify(*this, add_wrapper);
+    if(is_symbol2t(sidecheck_1) && is_symbol2t(sidecheck_2))
+    {
+      auto name_1 = to_symbol2t(sidecheck_2).get_symbol_name();
+      auto name_2 = to_symbol2t(side_1).get_symbol_name();
+
+      if(name_1 == name_2)
+      {
+        return sidecheck_1;
+      }
+    }
+  }
+
+  // (Y - X) + X -> Y
+
+  if(is_sub2t(side_1) && is_symbol2t(side_2))
+  {
+    auto sidecheck_1 = to_sub2t(side_1).side_1;
+    auto sidecheck_2 = to_sub2t(side_1).side_2;
+
+    if(is_symbol2t(sidecheck_1) && is_symbol2t(sidecheck_2))
+    {
+      auto name_1 = to_symbol2t(sidecheck_2).get_symbol_name();
+      auto name_2 = to_symbol2t(side_2).get_symbol_name();
+
+      if(name_1 == name_2)
+      {
+        return sidecheck_1;
+      }
+    }
+  }
+
+  // X + ~X -> -1
+
+  if(is_symbol2t(side_1) && is_bitnot2t(side_2))
+  {
+    auto name_1 = to_symbol2t(side_1).get_symbol_name();
+    auto bitnot_value = to_bitnot2t(side_2).value;
+    auto name_2 = to_symbol2t(bitnot_value).get_symbol_name();
+
+    if(name_1 == name_2)
+    {
+      return constant_int2tc(side_1->type, -1);
+    }
+  }
+
+  // ~X + X -> -1
+
+  if(is_bitnot2t(side_1) && is_symbol2t(side_2))
+  {
+    auto name_1 = to_symbol2t(side_2).get_symbol_name();
+    auto bitnot_value = to_bitnot2t(side_1).value;
+    auto name_2 = to_symbol2t(bitnot_value).get_symbol_name();
+
+    if(name_1 == name_2)
+    {
+      return constant_int2tc(side_2->type, -1);
+    }
+  }
+}
+
+// ~B + (A + 1) --> A - B
+if(is_bitnot2t(side_1) && is_add2t(side_2))
+{
+  auto sidecheck_1 = to_add2t(side_2).side_1;
+  auto sidecheck_2 = to_add2t(side_2).side_2;
+  if(is_symbol2t(sidecheck_1) && to_constant_int(sidecheck_2).value == 1)
+  {
+    auto B = to_bitnot2t(side_1).value;
+    auto new_operand_2 = sub2tc(side_1->type, sidecheck_1, B);
+    return new_operand_2;
+  }
+}
+
+expr2tc res = simplify_arith_2ops<Addtor, add2t>(type, side_1, side_2);
+if(!is_nil_expr(res))
+  return res;
+
+// Attempt associative simplification
+std::function<expr2tc(const expr2tc &arg1, const expr2tc &arg2)> add_wrapper =
+  [this](const expr2tc &arg1, const expr2tc &arg2) -> expr2tc
+{
+  expr2tc a = arg1, b = arg2;
+  type2tc t = common_arith_op2_type(a, b);
+  return add2tc(t, a, b);
+};
+
+return attempt_associative_simplify(*this, add_wrapper);
 }
 
 template <class constant_type>
