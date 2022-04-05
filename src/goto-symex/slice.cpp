ad7bbddccb5d4377d240a43e293d9d0a9fac28b9
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2022-04-05 19:49:22 +0100

    [slicer] improved documentation

--- src/goto-symex/slice.cpp
+++ src/goto-symex/slice.cpp
@@ -2,25 +2,111 @@
 
 namespace
 {
+/**
+ * @brief Class for the symex-slicer, this slicer is to be executed
+ * on SSA formula in order to remove every symbol that does not depends
+ * on it
+ *
+ * It works by constructing a symbol dependency list by transversing
+ * the SSA formula in reverse order. If any assume, assignment, or renumber
+ * step does not belong into this dependency, then it will be ignored.
+ */
 class symex_slicet
 {
 public:
   explicit symex_slicet(bool assume);
+
+  /**
+   * Iterate over all steps of the \eq in REVERSE order,
+   * getting symbol dependencies. If an
+   * assignment, renumber or assume does not contain one
+   * of the dependency symbols, then it will be ignored.
+   *
+   * @param eq symex formula to be sliced
+   */
   void slice(std::shared_ptr<symex_target_equationt> &eq);
 
-  typedef std::unordered_set<std::string> symbol_sett;
-  symbol_sett depends;
-  BigInt ignored;
+  /**
+   * Holds the symbols the current equation depends on.
+   */
+  std::unordered_set<std::string> depends;
+
+  BigInt ignored; /// tracks how many steps were sliced
 
 protected:
   bool slice_assumes;
 
+  /**
+   * Recursively explores the operands of an expression \expr
+   * If a symbol is found, then it is added into the #depends
+   * member if `Add` is true, otherwise returns true.
+   *
+   * @param expr expression to extract every symbol
+   * @return true if at least one symbol was found
+   */
   template <bool Add>
   bool get_symbols(const expr2tc &expr);
 
+  /**
+   * Helper function, it is used to select specialization will
+   * be used, i.e. assume, assignment or renumber
+   *
+   * Note 1: ASSERTS are not sliced, only their symbols are added
+   * into the #depends
+   *
+   * Note 2: Similar to ASSERTS, if 'slice-assumes' option is
+   * is not enabled. Then only its symbols are added into the
+   * #depends
+   *
+   * TODO: All slice specialization can be converted into a lambda
+   *
+   * @param SSA_step any kind of SSA expression
+   */
   void slice(symex_target_equationt::SSA_stept &SSA_step);
+
+  /**
+   * Remove unneeded assumes from the formula
+   *
+   * Check if the Assume cond symbol is in the #depends, if
+   * it is not then mark the \SSA_Step as ignored.
+   *
+   * If the assume cond is in the #depends, then add its guards
+   * and cond into the #depends
+   *
+   * Note 1: All the conditions operands are going to be added
+   * into the #depends. This makes that the condition itself as
+   * a "reverse taint"
+   *
+   * TODO: What happens if the ASSUME would result in false?
+   *
+   * @param SSA_step an assume step
+   */
   void slice_assume(symex_target_equationt::SSA_stept &SSA_step);
+
+  /**
+   * Remove unneeded assignments from the formula
+   *
+   * Check if the LHS symbol is in the #depends, if
+   * it is not then mark the \SSA_Step as ignored.
+   *
+   * If the assume cond is in the #depends, then add its guards
+   * and cond into the #depends
+   *
+   * @param SSA_step an assignment step
+   */
   void slice_assignment(symex_target_equationt::SSA_stept &SSA_step);
+
+  /**
+   * Remove unneeded renumbers from the formula
+   *
+   * Check if the LHS symbol is in the #depends, if
+   * it is not then mark the \SSA_Step as ignored.
+   *
+   * If the assume cond is in the #depends, then add its guards
+   * and cond into the #depends
+   *
+   * @param SSA_step an renumber step
+   */
   void slice_renumber(symex_target_equationt::SSA_stept &SSA_step);
 };
 
@@ -40,6 +126,7 @@ template <bool Add>
 bool symex_slicet::get_symbols(const expr2tc &expr)
 {
   bool res = false;
+  // Recursively look if any of the operands has a inner symbol
   expr->foreach_operand([this, &res](const expr2tc &e) {
     if(!is_nil_expr(e))
       res |= get_symbols<Add>(e);
@@ -128,6 +215,7 @@ void symex_slicet::slice_assume(symex_target_equationt::SSA_stept &SSA_step)
 void symex_slicet::slice_assignment(symex_target_equationt::SSA_stept &SSA_step)
 {
   assert(is_symbol2t(SSA_step.lhs));
+  // TODO: create an option to ignore nondet symbols (test case generation)
 
   if(!get_symbols<false>(SSA_step.lhs))
   {
@@ -166,6 +254,11 @@ void symex_slicet::slice_renumber(symex_target_equationt::SSA_stept &SSA_step)
   // Don't collect the symbol; this insn has no effect on dependencies.
 }
 
+/**
+ * Naive slicer: slice every step after the last assertion
+ * @param eq symex formula to be sliced
+ * @return number of steps that were ignored
+ */
 static BigInt simple_slice(std::shared_ptr<symex_target_equationt> &eq)
 {
   BigInt ignored = 0;
