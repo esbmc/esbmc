662fa06f2914e300cdbb19c1b226219f3ca5a36e
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-05-20 18:29:40 +0200

    [clang-c-frontend] generate symbols for compound literals, fixes #591

--- src/clang-c-frontend/clang_c_convert.h
+++ src/clang-c-frontend/clang_c_convert.h
@@ -6,7 +6,9 @@
 
 #include <util/context.h>
 #include <util/namespace.h>
+#include <util/std_code.h>
 #include <util/std_types.h>
+#include <util/symbol_generator.h>
 
 // Forward dec, to avoid bringing in clang headers
 namespace clang
@@ -68,8 +70,13 @@ protected:
   namespacet ns;
   std::vector<std::unique_ptr<clang::ASTUnit>> &ASTs;
   const messaget &msg;
+  symbol_generator anon_symbol;
 
   unsigned int current_scope_var_num;
+  /* During get_expr(), which also transforms blocks/scopes, this represents
+   * the latest opened blocks from the top-level. A nullptr 'current_block'
+   * thus means file scope. */
+  code_blockt *current_block;
 
   clang::SourceManager *sm;
 
