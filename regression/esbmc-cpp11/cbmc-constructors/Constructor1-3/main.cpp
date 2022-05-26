d0c0eec4019fdd0259b09e69623411ea2072b78b
kunjsong01 <kunjian90@gmail.com>
2022-05-26 20:43:16 +0100

    [regression] updated constructor test suite

--- regression/esbmc-cpp11/cbmc-constructors/Constructor1-3/main.cpp
+++ regression/esbmc-cpp11/cbmc-constructors/Constructor1-3/main.cpp
@@ -1,32 +1,17 @@
 #include <cassert>
-class t3
+
+class t2
 {
 public:
   int i;
 
-  t3();
+  t2() : i(2)
+  {
+  }
 };
-
-t3::t3() : i(3)
-{
-}
 
 int main()
 {
-  t3 instance3; // struct t3 instance3;
-  assert(instance3.i == 3); // t3(this)(&instance3);
+  t2 instance2;
+  assert(instance2.i == 2);
 }
-
-#if 0
-Symbol......: t3::t3(this)
-Pretty name.:
-Module......: main
-Base name...: t3
-Mode........: cpp
-Type........: auto (struct t3 *) -> constructor
-Value.......: {
-  this->i = 3;
-}
-Flags.......:
-Location....: file Constructor1-3/main.cpp line 7
-#endif
--- regression/esbmc-cpp11/cbmc-constructors/Constructor1-5/main.cpp
+++ regression/esbmc-cpp11/cbmc-constructors/Constructor1-5/main.cpp
@@ -1,15 +1,4 @@
 #include <cassert>
-class t1
-{
-public:
-  int i;
-
-  t1()
-  {
-    i = 1;
-  }
-};
-
 class t2
 {
 public:
@@ -20,50 +9,9 @@ public:
   }
 };
 
-class t3
-{
-public:
-  int i;
-
-  t3(int x);
-
-  int get_i()
-  {
-    return i;
-  }
-};
-
-t3::t3(int x)
-{
-  i = x;
-}
-
 int main()
 {
-  t1 instance1;
-  assert(instance1.i == 1); // public memebr access
-
-  t2 instance2;
-  assert(instance2.i == 2); // different class constructor
-
-  t3 instance3(3);
-  assert(instance3.i == 3);
-
-  t3 instance4(4);
-  assert(instance4.i == 4); // another instantiation of t3 with different ctor param
-
-  t3 instance5(5);
-  assert(instance5.get_i() == 5); // member function access
-
-  t3 instance6(6);
-  assert(instance6.get_i() == 6); // member function access in another object
-
-  /*
   t2 *p = new t2;
   assert(p->i == 2);
   delete p;
-  */
 }
-
-#if 0
-#endif
