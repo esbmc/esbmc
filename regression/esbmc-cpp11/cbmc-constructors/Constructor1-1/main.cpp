26be7bf753b60ebf734acafff619857e77b8eaad
kunjsong01 <kunjian90@gmail.com>
2022-05-26 20:43:16 +0100

    [regression] updated constructor test suite

--- regression/esbmc-cpp11/cbmc-constructors/Constructor1-1/main.cpp
+++ regression/esbmc-cpp11/cbmc-constructors/Constructor1-1/main.cpp
@@ -6,27 +6,11 @@ public:
 
   t1()
   {
-    i = 1;
+    assert(0);
   }
 };
 
 int main()
 {
-  t1 instance1; // struct t1 instance1;
-  assert(instance1.i == 1); // t1(this)(&instance1);
+  t1 instance1;
 }
-
-#if 0
-// user defined constructor
-Symbol......: t1::t1(this)
-Pretty name.:
-Module......: main
-Base name...: t1
-Mode........: cpp
-Type........: auto (struct t1 *) -> constructor
-Value.......: {
-  this->i = 1;
-}
-Flags.......:
-Location....: file Constructor1-1/main.cpp line 7
-#endif
--- regression/esbmc-cpp11/cbmc-constructors/Constructor1-4/main.cpp
+++ regression/esbmc-cpp11/cbmc-constructors/Constructor1-4/main.cpp
@@ -1,31 +1,18 @@
 #include <cassert>
-class t2
+class t3
 {
 public:
   int i;
 
-  t2() : i(2)
-  {
-  }
+  t3();
 };
+
+t3::t3() : i(3)
+{
+}
 
 int main()
 {
-  t2 *p = new t2;
-  assert(p->i == 2);
-  delete p;
+  t3 instance3;
+  assert(instance3.i == 3);
 }
-
-#if 0
-Symbol......: t2::t2(this)
-Pretty name.:
-Module......: main
-Base name...: t2
-Mode........: cpp
-Type........: auto (struct t2 *) -> constructor
-Value.......: {
-  this->i = 2;
-}
-Flags.......:
-Location....: file Constructor1-4/main.cpp line 7
-#endif
