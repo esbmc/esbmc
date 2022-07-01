d0abab8d4e4de6b369ae8abc6264ef631d41bfec
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-07-01 18:05:53 +0200

    [regression] add cases from #775

---
+++ regression/esbmc/github_775-1/gh-775-1.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <assert.h>
+
+void steal_addr_space(size_t n)
+{
+	char *a = malloc(n);
+	free(a);
+}
+
+int main()
+{
+	size_t MiB = 1ULL << 20;
+	/* 16 * 256 MiB = 4 GiB */
+	for (int i=0; i<16; i++)
+		steal_addr_space(256 * MiB);
+	assert(0);
+}
