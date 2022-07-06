00cee32a4af2d07892505e21338ab604c943b6f8
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2022-07-06 12:11:05 +0100

    [message] removed message package

---
+++ src/util/message.h
@@ -0,0 +1,74 @@
+/*******************************************************************\
+
+Module: Message System. This system is used to send messages through
+    ESBMC execution.
+Author: Daniel Kroening, kroening@kroening.com
+
+Maintainers:
+- @2021: Rafael Sá Menezes, rafael.sa.menezes@outlook.com
+
+\*******************************************************************/
+#pragma once
+
+#include <fmt/format.h>
+
+/**
+   * @brief Verbosity refers to the max level
+   * of which inputs are going to be printed out
+   *
+   * The level adds up to the greater level which means
+   * that if the level is set to 3 all messages of value
+   * 0,1,2,3 are going to be printed but 4+ will not be printed
+   *
+   * The number is where it appeared in the definition, in the
+   * implementation below DEBUG is the highest value
+   */
+enum class VerbosityLevel : char
+{
+  None,     // No message output
+  Error,    // fatal errors are printed
+  Warning,  // warnings are printend
+  Result,   // results of the analysis (including CE)
+  Progress, // progress notifications
+  Status,   // all kinds of esbmc is doing that may be useful to the user
+  Debug     // messages that are only useful if you need to debug.
+};
+
+namespace {
+/**
+   * @brief Prints a message
+   * @param message string to be printed
+   */
+void log_print(VerbosityLevel v, const std::string &message) {
+  fmt::print(stdout, "{}\n", message);
+}
+}
+// Macro to generate log functions
+#define log_message(name, verbosity)                                           \
+  template <typename Arg, typename... Args>                                    \
+  static inline void log_##name(Arg &&arg, Args &&...args)                     \
+  {                                                                            \
+    log_print(verbosity, std::forward<Arg>(arg));                              \
+                                                                               \
+    using expander = int[];                                                    \
+    (void)expander{0, (log_print(verbosity, std::forward<Args>(args)), 0)...}; \
+    log_println(verbosity);                                                    \
+  }
+
+log_message(error, VerbosityLevel::Error);
+log_message(result, VerbosityLevel::Result);
+log_message(warning, VerbosityLevel::Warning);
+log_message(progress, VerbosityLevel::Progress);
+log_message(status, VerbosityLevel::Status);
+log_message(debug, VerbosityLevel::Debug);
+
+#undef log_message
+
+#include <stdio.h>
+class messaget_state
+{
+public:
+  static inline auto verbosity = VerbosityLevel::Status;
+  static inline auto error_output = stderr;
+  static inline auto standard_output = stdout;
+};
