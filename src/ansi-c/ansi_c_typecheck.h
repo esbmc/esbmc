7bcf8d706da71406579ed2b49740ed4dc1fa13e8
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-07-09 13:57:02 +0200

    [ansi-c] ansi_c_typecheckt: rm unused ctor and one ansi_c_typecheck() overload

--- src/ansi-c/ansi_c_typecheck.h
+++ src/ansi-c/ansi_c_typecheck.h
@@ -18,11 +18,6 @@ bool ansi_c_typecheck(
   const std::string &module,
   const messaget &message_handler);
 
-bool ansi_c_typecheck(
-  exprt &expr,
-  const messaget &message_handler,
-  const namespacet &ns);
-
 class ansi_c_typecheckt : public c_typecheck_baset
 {
 public:
@@ -32,17 +27,6 @@ public:
     const std::string &_module,
     const messaget &_message_handler)
     : c_typecheck_baset(_context, _module, _message_handler),
-      parse_tree(_parse_tree)
-  {
-  }
-
-  ansi_c_typecheckt(
-    ansi_c_parse_treet &_parse_tree,
-    contextt &_context1,
-    const contextt &_context2,
-    const std::string &_module,
-    const messaget &_message_handler)
-    : c_typecheck_baset(_context1, _context2, _module, _message_handler),
       parse_tree(_parse_tree)
   {
   }
