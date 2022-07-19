02228df0f5a1c4ef72f13cd8d3d648a53e9ee4c9
Fedor Shmarov <shmarovfedor@mail.ru>
2022-07-19 10:00:40 +0100

    [pointer-analysis] reverted dereference to the previous version

--- src/pointer-analysis/dereference.cpp
+++ src/pointer-analysis/dereference.cpp
@@ -2223,8 +2223,7 @@ void dereferencet::bounds_check(
   // Report these as assertions; they'll be simplified away if they're constant
 
   guardt tmp_guard1(guard);
-  if(!options.get_bool_option("symex-ssa-trace-as-c"))
-    tmp_guard1.add(is_in_bounds);
+  tmp_guard1.add(is_in_bounds);
   dereference_failure("array bounds", "array bounds violated", tmp_guard1);
 }
 
@@ -2298,8 +2297,7 @@ void dereferencet::check_data_obj_access(
   if(!options.get_bool_option("no-bounds-check"))
   {
     guardt tmp_guard = guard;
-    if(!options.get_bool_option("symex-ssa-trace-as-c"))
-      tmp_guard.add(gt);
+    tmp_guard.add(gt);
     dereference_failure(
       "pointer dereference", "Access to object out of bounds", tmp_guard);
   }
