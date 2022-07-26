bc764f031a12d783e49d2a01c835d8c0239454f9
kunjsong01 <kunjian90@gmail.com>
2022-07-26 13:52:37 +0100

    [clang-cpp-frontend] fixed comments

--- src/util/destructor.h
+++ src/util/destructor.h
@@ -4,6 +4,10 @@
 #include <util/expr.h>
 #include <util/namespace.h>
 
+/**
+ * Get an expr to represent destructor function call
+ * for cpp delete
+ */
 code_function_callt get_destructor(const namespacet &ns, const typet &type);
 
 #endif
