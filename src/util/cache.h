192f78a084b928a3e54f2687c165603a55ea91a4
Rafael Menezes <rafael.sa.menezes@outlook.com>
2022-07-27 15:30:37 +0000

    applied clang-format

--- src/util/cache.h
+++ src/util/cache.h
@@ -8,18 +8,32 @@ using crc_pair = std::pair<crc_hash, crc_hash>;
 class crc_assert_cache : public ssa_algorithm
 {
 public:
-    explicit crc_assert_cache(symex_target_equationt::SSA_stepst &steps, std::set<crc_pair> &crc_set, bool is_forward_condition) : ssa_algorithm(steps, true), crc_set(crc_set), is_forward_condition(is_forward_condition) {}
+  explicit crc_assert_cache(
+    symex_target_equationt::SSA_stepst &steps,
+    std::set<crc_pair> &crc_set,
+    bool is_forward_condition)
+    : ssa_algorithm(steps, true),
+      crc_set(crc_set),
+      is_forward_condition(is_forward_condition)
+  {
+  }
 
-    void run_on_assert(symex_target_equationt::SSA_stept &) override;
+  void run_on_assert(symex_target_equationt::SSA_stept &) override;
 
-    static std::set<crc_pair> create_empty_set() {
-        return std::set<crc_pair>();
-    }
+  static std::set<crc_pair> create_empty_set()
+  {
+    return std::set<crc_pair>();
+  }
 
-    unsigned get_hits() { return hits; }
+  unsigned get_hits()
+  {
+    return hits;
+  }
+
 protected:
-    std::set<crc_pair>  &crc_set;
-    bool is_forward_condition;
+  std::set<crc_pair> &crc_set;
+  bool is_forward_condition;
+
 private:
-    unsigned hits = 0;
+  unsigned hits = 0;
 };
