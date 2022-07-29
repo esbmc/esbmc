c29eb9a72fc709a7a2789fb7bfc174be5fd31277
Franz Brauße <franz.brausse@manchester.ac.uk>
2022-07-29 11:40:09 +0200

    [slice] document the slice() function and its dependencies

--- src/goto-symex/slice.h
+++ src/goto-symex/slice.h
@@ -3,6 +3,21 @@
 
 #include <goto-symex/symex_target_equation.h>
 
+/**
+ * Marks SSA_steps to be ignored which have no effects on the target equation,
+ * according to the options set in the `config`.
+ *
+ * Notably, this function depends on the global `config`:
+ *  - "no-slice" in `options` -> perform only simple slicing: ignore everything
+ *    after the final assertion
+ *  - "slice-assumes" in `options` -> also perform slicing of assumption steps
+ *  - `config.no_slice_names` and `config.no_slice_ids` -> suppress slicing of
+ *    particular symbols in non-simple slicing mode.
+ *
+ * @param eq The target equation containing the SSA steps to perform program
+ *           slicing on.
+ * @return The number of ignored SSA steps due to this slicing.
+ */
 BigInt slice(std::shared_ptr<symex_target_equationt> &eq);
 
 #endif
