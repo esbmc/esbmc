f2cd39a4108057023fc445ece0abbf3c90a6b9ab
kunjsong01 <kunjian90@gmail.com>
2022-08-04 09:56:07 +0100

    [regression][CI] build all solvers
      - added BENCHBRINGUP in Options.cmake

--- scripts/cmake/Options.cmake
+++ scripts/cmake/Options.cmake
@@ -44,6 +44,7 @@ option(ENABLE_LIBM "Use libm from c2goto (default: ON)" ON)
 option(ENABLE_FUZZER "Add fuzzing targets (default: OFF)" OFF)
 option(ENABLE_CLANG_TIDY "Activate clang tidy analysis (default: OFF)" OFF)
 option(ENABLE_CSMITH "Add csmith Tests (default: OFF) (depends: ENABLE_REGRESSION)" OFF)
+option(BENCHBRINGUP "Run a user-specified benchmark in Github workflow" OFF)
 
 #############################
 # CMake extra Vars
