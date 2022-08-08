8b6ee48b2adbb03a81e7552343798e85d8717928
Mohannad <m.aldughaim@gmail.com>
2022-08-08 17:47:21 +0300

    -Added regression for empty set and last loop.

---
+++ regression/goto-contractor/empty_set_success/main.c
@@ -0,0 +1,15 @@
+#include <assert.h>
+int main()
+{
+  int x = 1;
+  int y = 0;
+
+  while(y < 1000 && __VERIFIER_nondet_int())
+  {
+    x = x + y;
+    y = y + 1;
+  }
+  assert(x>0);
+
+  return 0;
+}
