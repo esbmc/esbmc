75151a55ae7a72fac011a5f5499fd8542aa81a0b
Mohannad <m.aldughaim@gmail.com>
2022-08-08 17:47:21 +0300

    -Added regression for empty set and last loop.

---
+++ regression/goto-contractor/last_loop_success/main.c
@@ -0,0 +1,20 @@
+#include <assert.h>
+int main()
+{
+  int x = 1;
+  int y = 0;
+
+  while(y<10)
+  {
+    y++;
+  }
+
+  __ESBMC_assume(x>0);
+  while(x < 2147483640)
+  {
+    x = x + 1;
+  }
+  assert(x>0);
+
+  return 0;
+}
