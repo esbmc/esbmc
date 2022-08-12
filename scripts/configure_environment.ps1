df9962c2067420d8f834eed4116d1575343bd49f
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2022-08-12 15:19:11 +0100

    Update configure_environment.ps1

--- scripts/configure_environment.ps1
+++ scripts/configure_environment.ps1
@@ -1,4 +1,5 @@
 
 ls C:\vcpkg\installed\x64-windows\bin\
+New-Item C:\Deps -itemtype directory
 Copy-Item C:\vcpkg\installed\x64-windows\bin\boost_filesystem-vc143-mt-x64-1_79.dll C:\Deps
 Copy-Item C:\vcpkg\installed\x64-windows\bin\boost_program_options-vc143-mt-x64-1_79.dll C:\Deps
