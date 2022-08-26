01469dce4f6d43cde0931d96a3bf6f6cc9106849
kunjsong01 <kunjian90@gmail.com>
2022-08-26 14:39:13 +0100

    [scripts] improved comments in flailing

--- scripts/cmake/AppleConfiguration.cmake
+++ scripts/cmake/AppleConfiguration.cmake
@@ -3,11 +3,11 @@
 if (APPLE)
     message(STATUS "Detected APPLE")
     # Suppress superfluous warnings about "*.a" having no symbols on macOS X
-	set(CMAKE_C_ARCHIVE_CREATE   "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
-	set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
-	set(CMAKE_C_ARCHIVE_FINISH   "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
-    set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")   
-        
+    set(CMAKE_C_ARCHIVE_CREATE   "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_CXX_ARCHIVE_CREATE "<CMAKE_AR> Scr <TARGET> <LINK_FLAGS> <OBJECTS>")
+    set(CMAKE_C_ARCHIVE_FINISH   "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
+    set(CMAKE_CXX_ARCHIVE_FINISH "<CMAKE_RANLIB> -no_warning_for_no_symbols -c <TARGET>")
+
     # Note: This should check for the CMake version instead of OS
     cmake_policy(SET CMP0079 NEW)
 
