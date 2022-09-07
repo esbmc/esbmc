ba7734a6371597a4bc6b0db9fb14def53f8999bf
Rafael Menezes <rafael.sa.menezes@outlook.com>
2022-09-07 11:10:37 +0000

    [caching] caching algorithm now uses ssa_step_algorithm as a base

--- src/util/cache.h
+++ src/util/cache.h
@@ -2,44 +2,56 @@
 
 #include <set>
 #include <util/algorithms.h>
-
+#include <util/time_stopping.h>
 // Helper definitions
-using crc_hash = size_t; // from irep2_meta_templates.h
+using hashing_type = std::string; // from irep2_meta_templates.h
 /**
  * @brief This class stores all asserts conditions and guards
  *        for a given SSA, if some pair was already added
  *        then it will convert it to be a trivial case,
  *        e.g. GUARD => COND ----> GUARD => 1
  */
-class crc_assert_cache : public ssa_algorithm
+class assertion_cache : public ssa_step_algorithm
 {
 public:
-  using crc_pair = std::pair<crc_hash, crc_hash>;
-  crc_assert_cache(
-    symex_target_equationt::SSA_stepst &steps,
-    std::set<crc_pair> &crc_set,
-    bool trivial_value)
-    : ssa_algorithm(steps, true), crc_set(crc_set), trivial_value(trivial_value)
+  using assert_pair = std::pair<hashing_type, hashing_type>;
+  assertion_cache(std::set<assert_pair> &assert_set, bool trivial_value)
+    : ssa_step_algorithm(true),
+      assert_set(assert_set),
+      trivial_value(trivial_value)
   {
+  }
+
+  bool run(symex_target_equationt::SSA_stepst &eq) override
+  {
+    fine_timet algorithm_start = current_time();
+    for(auto &step : eq)
+      run_on_step(step);
+    fine_timet algorithm_stop = current_time();
+    log_status(
+      "Caching time: {}s (removed {} assignments)",
+      time2string(algorithm_stop - algorithm_start),
+      hits);
+    return true;
   }
 
   void run_on_assert(symex_target_equationt::SSA_stept &) override;
 
-  static std::set<crc_pair> create_empty_set()
+  static std::set<assert_pair> create_empty_set()
   {
-    return std::set<crc_pair>();
+    return std::set<assert_pair>();
   }
 
-  unsigned get_hits()
+  virtual BigInt ignored() const override
   {
     return hits;
   }
 
 protected:
-  std::set<crc_pair> &crc_set;
+  std::set<assert_pair> &assert_set;
   /// value to be set for  the COND
   bool trivial_value;
 
 private:
-  unsigned hits = 0;
+  BigInt hits = 0;
 };
