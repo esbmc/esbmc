2072d5181c70608928bc82c96391779e05cfa444
Rafael Menezes <rafael.sa.menezes@outlook.com>
2022-09-07 12:33:14 +0000

    [caching] moved cache_db to config

--- src/util/cache.h
+++ src/util/cache.h
@@ -3,8 +3,7 @@
 #include <set>
 #include <util/algorithms.h>
 #include <util/time_stopping.h>
-// Helper definitions
-using hashing_type = std::string; // from irep2_meta_templates.h
+
 /**
  * @brief This class stores all asserts conditions and guards
  *        for a given SSA, if some pair was already added
@@ -14,7 +13,6 @@ using hashing_type = std::string; // from irep2_meta_templates.h
 class assertion_cache : public ssa_step_algorithm
 {
 public:
-  using assert_pair = std::pair<hashing_type, hashing_type>;
   assertion_cache(std::set<assert_pair> &assert_set, bool trivial_value)
     : ssa_step_algorithm(true),
       assert_set(assert_set),
@@ -36,12 +34,6 @@ public:
   }
 
   void run_on_assert(symex_target_equationt::SSA_stept &) override;
-
-  static std::set<assert_pair> create_empty_set()
-  {
-    return std::set<assert_pair>();
-  }
-
   virtual BigInt ignored() const override
   {
     return hits;
