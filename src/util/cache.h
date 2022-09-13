9c79ad45a4f65eb7c3917e289ce52eb389e94b1e
Rafael Menezes <rafael.sa.menezes@outlook.com>
2022-09-13 10:37:09 +0000

    [cache] added support to matching sub-expressions

--- src/util/cache.h
+++ src/util/cache.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "irep2/irep2.h"
 #include <unordered_set>
 
 #include <util/algorithms.h>
@@ -16,8 +17,11 @@
 class assertion_cache : public ssa_step_algorithm
 {
 public:
-  assertion_cache(assert_db &db, bool trivial_value)
-    : ssa_step_algorithm(true), db(db), trivial_value(trivial_value)
+  assertion_cache(assert_db &db, bool trivial_value, bool match_subexpressions)
+    : ssa_step_algorithm(true),
+      db(db),
+      trivial_value(trivial_value),
+      match_subexpressions(match_subexpressions)
   {
   }
 
@@ -26,14 +30,33 @@ public:
   void run_on_assert(symex_target_equationt::SSA_stept &) override;
   virtual BigInt ignored() const override
   {
-    return hits;
+    return hits + sub_hits;
   }
+
+  /**
+   * @brief recursively explore an expression for relations,
+   *        if any operands is in the cache, then replace then with
+   *        `trivial_value`
+   *
+   * Example: If some expression `cond1` is cached, then `cond1 && cond2`
+              can be replaced into `trivial_value && cond2`
+   */
+  bool try_matching_sub_expression(expr2tc &e, const expr2tc &guards);
+
+  inline bool is_in_cache(const expr2tc &e, const expr2tc &guards) const
+  {
+    return db.count(std::make_pair(e, guards)) != 0;
+  }
+
+  bool check_and_add_in_cache(expr2tc &cond, const expr2tc &guards);
 
 protected:
   assert_db &db;
   /// value to be set for  the COND
   bool trivial_value;
+  bool match_subexpressions;
 
 private:
   BigInt hits = 0;
+  BigInt sub_hits = 0;
 };
