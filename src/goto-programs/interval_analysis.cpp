509665c0e843e5c770f7e1681ef8f93af75ba6c3
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2022-11-15 11:48:14 +0000

    [intervals] updated floats to use get next

--- src/goto-programs/interval_analysis.cpp
+++ src/goto-programs/interval_analysis.cpp
@@ -96,7 +96,7 @@ void interval_analysis(goto_functionst &goto_functions, const namespacet &ns)
 
   std::ostringstream oss;
   interval_analysis.output(goto_functions,oss);
-  log_status("{}", oss.str());
+  //log_status("{}", oss.str());
   //  abort();
 
   Forall_goto_functions(f_it, goto_functions)
