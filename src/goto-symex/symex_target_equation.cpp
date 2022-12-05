bc59e0102eb385cc1dada0af18f3913df7d7f656
Rafael Sá Menezes <rafa.sa.xp@gmail.com>
2022-12-05 19:05:00 -0300

    Revert "[goto-symex] more cleanup"
    This reverts commit e30174eec7b49906416ac5fa7a25d6c056e42191.

--- src/goto-symex/symex_target_equation.cpp
+++ src/goto-symex/symex_target_equation.cpp
@@ -356,21 +356,32 @@ static expr2tc flatten_with(const with2t &with)
     assert(0 && "unimplemented");
 }
 
-static void fix_union_type(type2tc &type)
+static void fix_union_type(type2tc &type, bool is_pointer)
 {
-  if(is_union_type(type))
+  if(!is_pointer && is_union_type(type))
   {
     // Replace with byte array.
     type2tc new_type = type;
     auto size = type_byte_size(new_type);
     type = type2tc(
       new array_type2t(get_uint8_type(), gen_ulong(size.to_uint64()), false));
+    return;
+  }
+
+  // Otherwise, recurse, taking care to handle pointers appropriately. All
+  // pointers to unions should remain union types.
+  if(0 && is_pointer_type(type))
+  {
+    fix_union_type(to_pointer_type(type).subtype, /* true */ false);
+  }
+  else if(0 && is_structure_type(type))
+  {
+    for(type2tc &mem : static_cast<struct_union_data *>(type.get())->members)
+      fix_union_type(mem, false);
   }
   else
   {
-    // Otherwise, recurse, taking care to handle pointers appropriately. All
-    // pointers to unions should remain union types.
-    type->Foreach_subtype(fix_union_type);
+    type->Foreach_subtype([](type2tc &ty){ fix_union_type(ty, false); });
   }
 }
 
@@ -397,12 +408,12 @@ static void fix_union_expr(expr2tc &expr)
         member_type, flattened_source, gen_ulong(0), guard);
 
       // Fix type
-      fix_union_type(expr->type);
+      fix_union_type(expr->type, false);
     }
     else
     {
       expr->Foreach_operand(fix_union_expr);
-      fix_union_type(expr->type);
+      fix_union_type(expr->type, false);
     }
   }
   else if(is_union_type(expr))
@@ -410,11 +421,11 @@ static void fix_union_expr(expr2tc &expr)
     if(is_with2t(expr))
     {
       expr = flatten_with(to_with2t(expr));
-      fix_union_type(expr->type); // XXX unnecessary?
+      fix_union_type(expr->type, false); // XXX unnecessary?
     }
     else if(is_symbol2t(expr))
     {
-      fix_union_type(expr->type); // XXX unnecessary?
+      fix_union_type(expr->type, false); // XXX unnecessary?
     }
     else
     {
@@ -428,7 +439,12 @@ static void fix_union_expr(expr2tc &expr)
       if(is_constant_union2t(expr))
         expr = flatten_union(to_constant_union2t(expr));
       else
-        fix_union_type(expr->type);
+        fix_union_type(expr->type, false); // XXX unnecessary?
+      /*
+      expr2tc new_expr;
+      migrate_expr(expr, new_expr);
+      expr = migrate_expr_back(new_expr);
+      */
     }
   }
   else if(is_dereference2t(expr))
@@ -441,13 +457,13 @@ static void fix_union_expr(expr2tc &expr)
     // be transformed into a dereference to one of the fields _within_ the
     // union, so we never up constructing a union reference.
     expr->Foreach_operand(fix_union_expr);
-    fix_union_type(expr->type);
+    fix_union_type(expr->type, /* true */ false);
   }
   else
   {
     // Default action: recurse and beat types.
 
-    fix_union_type(expr->type);
+    fix_union_type(expr->type, false);
 
     expr->Foreach_operand(fix_union_expr);
   }
@@ -459,7 +475,7 @@ static expr2tc flatten_unions(const expr2tc &e)
   if(is_nil_expr(expr))
     return expr;
   fix_union_expr(expr);
-  fix_union_type(expr->type);
+  fix_union_type(expr->type, false);
   return expr;
 }
 
