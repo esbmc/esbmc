4a8331efe9901ecbea799d87d1e41557032a4d94
Mikhail R. Gadelha <mikhail@igalia.com>
2022-12-07 17:09:53 -0300

    Fix location function name of the new models
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- src/clang-c-frontend/clang_c_adjust_expr.cpp
+++ src/clang-c-frontend/clang_c_adjust_expr.cpp
@@ -687,6 +687,7 @@ void clang_c_adjust::adjust_side_effect_function_call(
         }
 
         poly.identifier(identifier_with_type);
+        poly.name(f_op.name());
         poly.location() = expr.location();
 
         symbolt *function_symbol_with_type = context.find_symbol(identifier_with_type);
