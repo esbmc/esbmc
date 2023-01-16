c9e04d0a1c035f81791909f80f25ed906145ed39
kunjsong01 <kunjian90@gmail.com>
2023-01-16 12:36:49 +0000

    [regression] added test cases

---
+++ regression/esbmc-cpp/inheritance_bringup/single_member/main.cpp
@@ -0,0 +1,25 @@
+#include <cassert>
+
+class Vehicle
+{
+public:
+  int data;
+  Vehicle() { data = 1; }
+};
+
+class Motorcycle: public Vehicle
+{
+public:
+  Motorcycle() : Vehicle() {}
+  //Motorcycle() {}
+};
+
+int main()
+{
+  Motorcycle m;
+  bool res1;
+  res1 = (m.data == 1); // PASS
+  assert(res1);
+  return 0;
+}
+
---
+++ regression/esbmc-cpp/inheritance_bringup/single_member_fail/main.cpp
@@ -0,0 +1,25 @@
+#include <cassert>
+
+class Vehicle
+{
+public:
+  int data;
+  Vehicle() { data = 1; }
+};
+
+class Motorcycle: public Vehicle
+{
+public:
+  Motorcycle() : Vehicle() {}
+  //Motorcycle() {}
+};
+
+int main()
+{
+  Motorcycle m;
+  bool res1;
+  res1 = (m.data == 0); // FAIL, should be 1
+  assert(res1);
+  return 0;
+}
+
