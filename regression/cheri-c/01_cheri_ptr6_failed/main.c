628f2474ccc7feed44c724b7b99f7e4dee1e4639
Lucas Cordeiro <lucasccordeiro@gmail.com>
2023-02-07 14:55:57 +0100

    [cheri] added the cap_ptr operational model [tests]

---
+++ regression/cheri-c/01_cheri_ptr6_failed/main.c
@@ -0,0 +1,15 @@
+//#include <cheri/cheric.h>
+#include "cheric.h"
+
+int main(int argc, char *argv[])
+{
+  char array[2];
+  char *__capability arrayp;
+  int ii;
+  arrayp = cheri_ptr(array, sizeof(array));
+  for(ii = 0; ii < sizeof(array) + 1; ii++)
+  {
+    arrayp[ii] = 0;
+  }
+  return (0);
+}
