3906435a2d1dc53c91cc025da1b9d503180c41bb
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-02-07 14:55:57 +0100

    [cheri c2goto/library] fix

--- src/c2goto/library/builtin_libs.c
+++ src/c2goto/library/builtin_libs.c
@@ -13,6 +13,8 @@ int __ESBMC_sync_fetch_and_add(int *ptr, int value)
 // #include <stddef.h>
 // #include <cheri.h>
 
+#if 0
+
 #if !defined(cheri_debug_assert)
 /* Disable cheri-compressed-cap's debug assertions since they assert that
  * base <= top in compute_base_top, which the comment above it admits is not
@@ -26,9 +28,11 @@ int __ESBMC_sync_fetch_and_add(int *ptr, int value)
 #endif
 #include <cheri_compressed_cap.h>
 
+#endif
+
 __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
 {
-#if 1
+#if 0
   union {
     void *__capability cap;
     struct {
@@ -37,63 +41,95 @@ __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
     };
   } u = { cap };
   cc128_cap_t result;
-  cc128_decompress_mem(u.pesbt, u.cursor, false /* TODO: tag bit */, &result);
+  cc128_decompress_mem(u.pesbt, u.cursor, true /* TODO: tag bit */, &result);
   return result.cr_bounds_valid ? result._cr_top - result.cr_base : 0;
 #else
-  union {
+  union u
+  {
     void *__capability cap;
-    struct {
+    struct
+    {
+      uint64_t pesbt;
+      uint64_t cursor;
+    };
+    struct
+    {
+#if __BYTE_ORDER == __LITTLE_ENDIAN
+      uint64_t B_E : 3;
+      uint64_t B_13_3 : 11;
+      uint64_t T_E : 3;
+      uint64_t T_11_3 : 9;
+      uint64_t I_E : 1;
+      uint64_t otype : 18;
+      uint64_t /* reserved */ : 3;
       uint64_t perms : 16;
-      uint64_t : 3;
+#elif __BYTE_ORDER == __BIG_ENDIAN
+      uint64_t perms : 16;
+      uint64_t /* reserved */ : 3;
       uint64_t otype : 18;
       uint64_t I_E : 1;
       uint64_t T_11_3 : 9;
       uint64_t T_E : 3;
       uint64_t B_13_3 : 11;
       uint64_t B_E : 3;
-      uintptr_t a;
+#else
+#error neither big nor little endian, no CHERI-C support for this architecture
+#endif
     };
-  } u = { cap };
+  } u = {cap};
+  u.pesbt ^=
+    (union u){
+      .B_E = 52 & 7,
+      .T_E = 52 >> 3,
+      .I_E = 1,
+      .otype = -1,
+    } /* encoding of the NULL capability, little endian: 0x1ffffc018004 */
+      .pesbt;
   uint64_t E;
   uint64_t T = u.T_11_3 << 3;
   uint64_t B = u.B_13_3 << 3;
   int L_carry_out;
   int L_msb;
-  if (u.I_E) {
+  if(u.I_E)
+  {
     E = u.T_E << 3 | u.B_E;
     L_carry_out = (T & 0x7f8) < (B & 0x7f8);
     L_msb = 1;
-  } else {
+  }
+  else
+  {
     E = 0;
     T |= u.T_E;
     B |= u.B_E;
     L_carry_out = (T & 0x7ff) < (B & 0x7ff);
     L_msb = 0;
   }
-  T |= ((B >> 12) + L_carry_out + L_msb) << 12;
-  uint64_t a_top = u.a & -(1 << (E + 14));
-  uint64_t a_mid = u.a & (-(1 << 13) << E);
-  uint64_t a_low = u.a & ((1 << (E - 1)) - 1);
-  int c_t, c_b;
-  uint64_t A3 = (u.a >> 11) & 0x7;
+  if(E > 52) /* 52 is the max. exponent for this format on non-morello */
+    E = 52;
+  T |= (((B >> 12) + L_carry_out + L_msb) & 3) << 12;
+  uint64_t a_top = E + 14 < 64 ? u.cursor >> (E + 14) : 0;
+  uint64_t one = 1;
+  uint64_t a_mid = (u.cursor >> E) & ((one << 14) - 1);
+  uint64_t a_low = u.cursor & ((one << E) - 1);
+  uint64_t A3 = (u.cursor >> 11) & 0x7;
   uint64_t B3 = (B >> 11) & 0x7;
   uint64_t T3 = (T >> 11) & 0x7;
   uint64_t R = B3 - 1;
-  switch ((A3 < R) << 1 | (T3 < R)) {
-  case 0: c_t = 0; break;
-  case 1: c_t = 1; break;
-  case 2: c_t = -1; break;
-  case 3: c_t = 0; break;
-  }
-  switch ((A3 < R) << 1 | (B3 < R)) {
-  case 0: c_b = 0; break;
-  case 1: c_b = 1; break;
-  case 2: c_b = -1; break;
-  case 3: c_b = 0; break;
-  }
-  uint64_t t = (a_top + c_t) << (E + 14) | T;
-  uint64_t b = (a_top + c_b) << (E + 14) | B;
-  return t - b;
+  int c_t = (T3 < R) - (A3 < R);
+  int c_b = (B3 < R) - (A3 < R);
+  uint64_t t_lo = ((uint64_t)((int64_t)a_top + c_t) << 14 | T) << E;
+  uint64_t t_hi;
+  if(14 + E <= 64)
+    t_hi = (uint64_t)((int64_t)a_top + c_t) >> (64 - (14 + E));
+  else
+    t_hi = (uint64_t)((int64_t)a_top + c_t) << (14 + E - 64) |
+           (E ? T >> (64 - E) : 0);
+  uint64_t b = (E + 14 < 64 ? (a_top + c_b) << (E + 14) : 0) | B << E;
+  if(t_hi > 1)
+    t_hi = 1;
+  if(E < 51 && (int)((t_hi << 1 | t_lo >> 63) & 3) - (int)(b >> 63) > 1)
+    t_hi ^= 1;
+  return t_lo - b;
 #endif
 }
 
