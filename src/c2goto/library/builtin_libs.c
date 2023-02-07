e3987ce213b6957102aba057f8f9e0cd71dd9be0
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-02-07 14:55:57 +0100

    [c2goto/library] remove #include-s <assert.h> and <stdint.h> + only enable cheri-functions for 128-bit mode

--- src/c2goto/library/builtin_libs.c
+++ src/c2goto/library/builtin_libs.c
@@ -7,9 +7,9 @@ int __ESBMC_sync_fetch_and_add(int *ptr, int value)
   return initial;
 }
 
-#ifdef __CHERI__
+#if __ESBMC_CHERI__ == 128
 
-#include <stdint.h>
+// #include <stdint.h>
 // #include <stddef.h>
 // #include <cheri.h>
 
@@ -29,33 +29,39 @@ int __ESBMC_sync_fetch_and_add(int *ptr, int value)
 #include <cheri_compressed_cap.h>
 
 #include <cheri/cheric.h>
-#include <assert.h>
+// #include <assert.h>
 
-void *__capability __esbmc_cheri_bounds_set(void *__capability cap, __SIZE_TYPE__ sz)
+/* modelled after UCAM-CL-TR-951 semantics of CHERI-MIPS instruction CSetBounds */
+void *__capability
+__esbmc_cheri_bounds_set(void *__capability cap, __SIZE_TYPE__ sz)
 {
   __PTRADDR_TYPE__ cursor = (__PTRADDR_TYPE__)cap;
   __PTRADDR_TYPE__ base = cheri_getbase(cap);
   __PTRADDR_TYPE__ top = cheri_gettop(cap);
-  assert(cheri_gettag(cap)); /* else raise_c2_exception(CapEx_TagViolation, cb) */
-  assert(!cheri_getsealed(cap)); /* else raise_c2_exception(CapEx_SealViolation, cb) */
-  assert(base <= cursor); /* else raise_c2_exception(CapEx_LengthViolation, cb) */
+  __ESBMC_assert(cheri_gettag(cap), "tag-violation c2exception");
+  __ESBMC_assert(!cheri_getsealed(cap), "seal-violation c2exception");
+  __ESBMC_assert(base <= cursor, "length-violation c2exception");
   __uint128_t newTop = cursor;
   newTop += sz;
-  assert(newTop <= top); /* else raise_c2_exception(CapEx_LengthViolaton, cb) */
+  __ESBMC_assert(newTop <= top, "length-violation c2exception");
   cc128_cap_t comp;
   bool exact = cc128_setbounds(&comp, cursor, newTop);
   (void)exact; /* ignore */
-  union {
-    struct {
-      uint64_t pesbt;
-      uint64_t cursor;
+  union
+  {
+    struct
+    {
+      __UINT64_TYPE__ pesbt;
+      __UINT64_TYPE__ cursor;
     };
     void *__capability cap;
   } ret;
-  /* TODO: otype, perms, type, ... */
   __ESBMC_assume(ret.cursor == cursor);
   __ESBMC_assume(ret.pesbt == comp.cr_pesbt);
   __ESBMC_assume(cheri_gettag(ret.cap) == cheri_gettag(cap));
+  __ESBMC_assume(cheri_gettype(ret.cap) == cheri_gettype(cap));
+  __ESBMC_assume(cheri_getperm(ret.cap) == cheri_getperm(cap));
+  __ESBMC_assume(cheri_getflags(ret.cap) == cheri_getflags(cap));
   return ret.cap;
 }
 
@@ -70,13 +76,15 @@ void *__capability __esbmc_cheri_bounds_set(void *__capability cap, __SIZE_TYPE_
 __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
 {
 #if 1
-  union {
+  union
+  {
     void *__capability cap;
-    struct {
-      uint64_t pesbt;
-      uint64_t cursor;
+    struct
+    {
+      __UINT64_TYPE__ pesbt;
+      __UINT64_TYPE__ cursor;
     };
-  } u = { cap };
+  } u = {cap};
   cc128_cap_t result;
   cc128_decompress_mem(u.pesbt, u.cursor, true /* TODO: tag bit */, &result);
   return result.cr_bounds_valid ? result._cr_top - result.cr_base : 0;
@@ -86,29 +94,29 @@ __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
     void *__capability cap;
     struct
     {
-      uint64_t pesbt;
-      uint64_t cursor;
+      __UINT64_TYPE__ pesbt;
+      __UINT64_TYPE__ cursor;
     };
     struct
     {
 #if __BYTE_ORDER == __LITTLE_ENDIAN
-      uint64_t B_E : 3;
-      uint64_t B_13_3 : 11;
-      uint64_t T_E : 3;
-      uint64_t T_11_3 : 9;
-      uint64_t I_E : 1;
-      uint64_t otype : 18;
-      uint64_t /* reserved */ : 3;
-      uint64_t perms : 16;
+      __UINT64_TYPE__ B_E : 3;
+      __UINT64_TYPE__ B_13_3 : 11;
+      __UINT64_TYPE__ T_E : 3;
+      __UINT64_TYPE__ T_11_3 : 9;
+      __UINT64_TYPE__ I_E : 1;
+      __UINT64_TYPE__ otype : 18;
+      __UINT64_TYPE__ /* reserved */ : 3;
+      __UINT64_TYPE__ perms : 16;
 #elif __BYTE_ORDER == __BIG_ENDIAN
-      uint64_t perms : 16;
-      uint64_t /* reserved */ : 3;
-      uint64_t otype : 18;
-      uint64_t I_E : 1;
-      uint64_t T_11_3 : 9;
-      uint64_t T_E : 3;
-      uint64_t B_13_3 : 11;
-      uint64_t B_E : 3;
+      __UINT64_TYPE__ perms : 16;
+      __UINT64_TYPE__ /* reserved */ : 3;
+      __UINT64_TYPE__ otype : 18;
+      __UINT64_TYPE__ I_E : 1;
+      __UINT64_TYPE__ T_11_3 : 9;
+      __UINT64_TYPE__ T_E : 3;
+      __UINT64_TYPE__ B_13_3 : 11;
+      __UINT64_TYPE__ B_E : 3;
 #else
 #error neither big nor little endian, no CHERI-C support for this architecture
 #endif
@@ -122,9 +130,9 @@ __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
       .otype = -1,
     } /* encoding of the NULL capability, little endian: 0x1ffffc018004 */
       .pesbt;
-  uint64_t E;
-  uint64_t T = u.T_11_3 << 3;
-  uint64_t B = u.B_13_3 << 3;
+  __UINT64_TYPE__ E;
+  __UINT64_TYPE__ T = u.T_11_3 << 3;
+  __UINT64_TYPE__ B = u.B_13_3 << 3;
   int L_carry_out;
   int L_msb;
   if(u.I_E)
@@ -144,24 +152,25 @@ __SIZE_TYPE__ __esbmc_cheri_length_get(void *__capability cap)
   if(E > 52) /* 52 is the max. exponent for this format on non-morello */
     E = 52;
   T |= (((B >> 12) + L_carry_out + L_msb) & 3) << 12;
-  uint64_t a_top = E + 14 < 64 ? u.cursor >> (E + 14) : 0;
-  uint64_t one = 1;
-  uint64_t a_mid = (u.cursor >> E) & ((one << 14) - 1);
-  uint64_t a_low = u.cursor & ((one << E) - 1);
-  uint64_t A3 = (u.cursor >> 11) & 0x7;
-  uint64_t B3 = (B >> 11) & 0x7;
-  uint64_t T3 = (T >> 11) & 0x7;
-  uint64_t R = B3 - 1;
+  __UINT64_TYPE__ a_top = E + 14 < 64 ? u.cursor >> (E + 14) : 0;
+  __UINT64_TYPE__ one = 1;
+  __UINT64_TYPE__ a_mid = (u.cursor >> E) & ((one << 14) - 1);
+  __UINT64_TYPE__ a_low = u.cursor & ((one << E) - 1);
+  __UINT64_TYPE__ A3 = (u.cursor >> 11) & 0x7;
+  __UINT64_TYPE__ B3 = (B >> 11) & 0x7;
+  __UINT64_TYPE__ T3 = (T >> 11) & 0x7;
+  __UINT64_TYPE__ R = B3 - 1;
   int c_t = (T3 < R) - (A3 < R);
   int c_b = (B3 < R) - (A3 < R);
-  uint64_t t_lo = ((uint64_t)((int64_t)a_top + c_t) << 14 | T) << E;
-  uint64_t t_hi;
+  __UINT64_TYPE__ t_lo =
+    ((__UINT64_TYPE__)((__INT64_TYPE__)a_top + c_t) << 14 | T) << E;
+  __UINT64_TYPE__ t_hi;
   if(14 + E <= 64)
-    t_hi = (uint64_t)((int64_t)a_top + c_t) >> (64 - (14 + E));
+    t_hi = (__UINT64_TYPE__)((__INT64_TYPE__)a_top + c_t) >> (64 - (14 + E));
   else
-    t_hi = (uint64_t)((int64_t)a_top + c_t) << (14 + E - 64) |
+    t_hi = (__UINT64_TYPE__)((__INT64_TYPE__)a_top + c_t) << (14 + E - 64) |
            (E ? T >> (64 - E) : 0);
-  uint64_t b = (E + 14 < 64 ? (a_top + c_b) << (E + 14) : 0) | B << E;
+  __UINT64_TYPE__ b = (E + 14 < 64 ? (a_top + c_b) << (E + 14) : 0) | B << E;
   if(t_hi > 1)
     t_hi = 1;
   if(E < 51 && (int)((t_hi << 1 | t_lo >> 63) & 3) - (int)(b >> 63) > 1)
