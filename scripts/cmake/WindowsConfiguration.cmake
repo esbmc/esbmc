1b1242ed8048335cf9263ec8128583e78d374d8e
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2023-02-20 15:46:39 +0000

    Update WindowsConfiguration.cmake

--- scripts/cmake/WindowsConfiguration.cmake
+++ scripts/cmake/WindowsConfiguration.cmake
@@ -39,10 +39,10 @@ if (WIN32)
 	endif()
 
   if(DOWNLOAD_WINDOWS_DEPENDENCIES)
-    download_zip_and_extract(LLVM https://sourceforge.net/projects/esbmc-deps/files/llvm14-mt/download)
+    download_zip_and_extract(LLVM https://sourceforge.net/projects/esbmc-deps/files/clang.zip/download)
     download_zip_and_extract(Z3 https://github.com/Z3Prover/z3/releases/download/z3-4.11.2/z3-4.11.2-x64-win.zip)
-    set(LLVM_DIR ${CMAKE_BINARY_DIR}/LLVM/llvm+clang+lld-14.0.6-x86_64-windows-msvc-release-mt)
-    set(Clang_DIR ${CMAKE_BINARY_DIR}/LLVM/llvm+clang+lld-14.0.6-x86_64-windows-msvc-release-mt)
+    set(LLVM_DIR ${CMAKE_BINARY_DIR}/LLVM/clang)
+    set(Clang_DIR ${CMAKE_BINARY_DIR}/LLVM/clang)
     set(Z3_DIR ${CMAKE_BINARY_DIR}/Z3/z3-4.11.2-x64-win)
   endif()
 
