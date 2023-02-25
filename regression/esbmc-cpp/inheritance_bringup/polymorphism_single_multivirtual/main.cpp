82a40940eeda660e8ec121050a8c2786ca4b7112
kunjsong01 <kunjian90@gmail.com>
2023-02-25 19:38:14 +0000

    [regression] added new regression tests for polymorphism_single

---
+++ regression/esbmc-cpp/inheritance_bringup/polymorphism_single_multivirtual/main.cpp
@@ -0,0 +1,31 @@
+/*
+ * Polymorphism:
+ *  - derived class contains an overriding method
+ *  - the overriding method has `virtual` keyword
+ */
+#include <cassert>
+
+class Bird {
+  public:
+  virtual int do_something(void) { return 21; }
+  virtual int do_other(void) { return 25; }
+};
+
+class Penguin: public Bird {
+  public:
+    virtual int do_something(void) { return 42; }
+    int do_other(void) { return 50; }
+};
+
+int main(){
+  Bird *b = new Bird();
+  Bird *p = new Penguin();
+  assert(b->do_something() == 21);
+  assert(p->do_something() == 42);
+  assert(b->do_other() == 25);
+  assert(p->do_other() == 50);
+  delete b;
+  delete p;
+  return 0;
+}
+
