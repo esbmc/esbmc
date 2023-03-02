c35b06f4eb6bc862f739c0bc364bd68dfc6b725b
kunjsong01 <kunjian90@gmail.com>
2023-03-02 15:38:24 +0000

    [regression] reviewed polymorphism TCs

--- regression/esbmc-cpp/polymorphism_bringup/2_SI_virtual_tvalCtor/main.cpp
+++ regression/esbmc-cpp/polymorphism_bringup/2_SI_virtual_tvalCtor/main.cpp
@@ -1,12 +1,14 @@
 /*
- * A simple test case of polymorphism
+ * Polymorphism:
+ *  - derived class contains an overriding method
+ *  - the overriding method has `virtual` keyword
+ *  - trival ctor
  */
 #include <cassert>
 
 class Bird {
   public:
   virtual int do_something(void) { return 21; }
-  //virtual ~Bird(){}
 };
 
 class Penguin: public Bird {
@@ -18,7 +20,7 @@ int main(){
   Bird *b = new Bird();
   Bird *p = new Penguin();
   assert(b->do_something() == 21);
-  assert(p->do_something() == 21); // should be 42
+  assert(p->do_something() == 42);
   delete b;
   delete p;
   return 0;
