5b3e1aa9ef381d992502bad71abb1036263ec187
XLIZHI <lxzy@LAPTOP-DIMF6R50>
2023-03-06 17:11:16 +0000

    cuda

---
+++ src/cpp/library/builtin_types.h
@@ -0,0 +1,6 @@
+//#include "device_types.h"
+#include "driver_types.h"
+//#include "surface_types.h"
+//#include "texture_types.h"
+//#include "vector_types.h"
+
---
+++ src/cpp/library_old/definitions.h
@@ -0,0 +1,28 @@
+#ifndef STL_DEFINITIONS
+#define STL_DEFINITIONS
+
+#include "cstddef"
+
+#define SIGINT 2
+
+#define SEEK_SET 0
+#define SEEK_CUR 1
+#define SEEK_END 2
+
+#ifndef __TIMESTAMP__
+#define __TIMESTAMP__ (0)
+#endif
+
+#ifdef _WIN64
+typedef __int64 streamsize;
+#else
+typedef unsigned int streamsize;
+#endif
+
+class smanip
+{
+};
+
+#define _SIZE_T_DEFINED
+
+#endif
