a7ad50ac148d19c7dd190342b36766e56157a3f1
Xianzhiyu <115160284+XLiZHI@users.noreply.github.com>
2023-03-06 21:20:09 +0000

    Delete src/cpp/library directory

--- src/cpp/library/boost/date_time/locale_config.hpp
+++
@@ -1,5 +0,0 @@
-#ifndef DATE_TIME_LOCALE_CONFIG_HPP___
-#define DATE_TIME_LOCALE_CONFIG_HPP___
-
-#endif
-
--- src/cpp/library/builtin_types.h
+++
@@ -1,6 +0,0 @@
-//#include "device_types.h"
-#include "driver_types.h"
-//#include "surface_types.h"
-//#include "texture_types.h"
-//#include "vector_types.h"
-
