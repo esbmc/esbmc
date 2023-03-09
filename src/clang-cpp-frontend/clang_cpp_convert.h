382b63ca65ae4baa95ecdfa495a49c59e5df6391
kunjsong01 <kunjian90@gmail.com>
2023-03-09 15:51:16 +0000

    [clang-cpp-convert] updated dynamic dispatch conditions for dynamic_cast

--- src/clang-cpp-frontend/clang_cpp_convert.h
+++ src/clang-cpp-frontend/clang_cpp_convert.h
@@ -352,6 +352,7 @@ protected:
    * Methods for resolving a clang::MemberExpr to virtual/overriding method
    */
   bool perform_virtual_dispatch(const clang::MemberExpr &member) override;
+  bool is_virtual_or_overriding(const clang::CXXMethodDecl &cxxmd);
   bool get_vft_binding_expr(const clang::MemberExpr &member, exprt &new_expr)
     override;
   /*
