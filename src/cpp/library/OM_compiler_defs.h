756005c1d9e5b117687c3151a84710c2f263f3db
kunjsong01 <kunjian90@gmail.com>
2023-04-03 22:13:45 +0100

    [OM] linting

--- src/cpp/library/OM_compiler_defs.h
+++ src/cpp/library/OM_compiler_defs.h
@@ -5,5 +5,5 @@
 #define CC_DIAGNOSTIC_PUSH() _Pragma("GCC diagnostic push")
 #define CC_DIAGNOSTIC_POP() _Pragma("GCC diagnostic pop")
 
-#define CC_DIAGNOSTIC_IGNORE_OM_LLVM_CHECKS() \
+#define CC_DIAGNOSTIC_IGNORE_OM_LLVM_CHECKS()                                  \
   DO_PRAGMA(GCC diagnostic ignored "-Wreturn-type")
