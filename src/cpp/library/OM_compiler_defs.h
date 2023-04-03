79d2a6f6c1822ad5a931329cb93665bc6302e4e6
kunjsong01 <kunjian90@gmail.com>
2023-04-03 22:00:56 +0100

    [OM] more fixes to streambuf

--- src/cpp/library/OM_compiler_defs.h
+++ src/cpp/library/OM_compiler_defs.h
@@ -1,9 +1,9 @@
+// Header to define some compmiler diagnostic tweaks for OMs
 #pragma once
 
-// Header to define some compmiler diagnostic tweaks for OMs
-
+#define DO_PRAGMA(x) _Pragma(#x)
 #define CC_DIAGNOSTIC_PUSH() _Pragma("GCC diagnostic push")
 #define CC_DIAGNOSTIC_POP() _Pragma("GCC diagnostic pop")
 
-#define CC_DIAGNOSTIC_IGNORE_LLVM_CHECKS()                                     \
+#define CC_DIAGNOSTIC_IGNORE_OM_LLVM_CHECKS() \
   DO_PRAGMA(GCC diagnostic ignored "-Wreturn-type")
