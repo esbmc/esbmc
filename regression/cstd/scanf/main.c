9117b659e61b1eda342c47dd03bd7aa0891d5bab
hejueyun2 <hejueyun@gmail.com>
2023-04-19 11:38:10 +0100

    fix solver build error

--- regression/cstd/scanf/main.c
+++ regression/cstd/scanf/main.c
@@ -3,7 +3,7 @@ int main(int argc, char *argv[])
 {
   char reg_name[12];
   printf("Enter your username:");
-  scanf("%11s", reg_name); //Buffer overflow here
+  scanf("%11s", reg_name); 
   printf("The program is now registered to %s.\n", reg_name);
   return 0;
 }
