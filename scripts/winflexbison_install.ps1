0c63ebefa04678e72bf66c2b08818db110911838
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2023-04-19 11:43:00 +0100

    Create winflexbison_install.ps1

---
+++ scripts/winflexbison_install.ps1
@@ -0,0 +1,12 @@
+$success = $false
+while (-not $success) {
+    Write-Host "Installing nsis.portable and winflexbison..."
+    $output = choco install -y winflexbison --ignore-checksums
+    if ($LASTEXITCODE -eq 0) {
+        $success = $true
+        Write-Host "Installation successful!"
+    } else {
+        Write-Host "Installation failed. Retrying in 10 seconds..."
+        Start-Sleep -Seconds 10
+    }
+}
