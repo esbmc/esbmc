4e6de9e7c39d6d327997262c7c91d7cffe87cb84
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-04-21 18:10:21 +0100

    [forward-slicer] added more operators

--- src/goto-programs/forward_slicer.cpp
+++ src/goto-programs/forward_slicer.cpp
@@ -15,6 +15,19 @@ void get_symbols(const expr2tc &expr, std::set<std::string> &values)
     values.insert(to_symbol2t(expr).get_symbol_name());
     return;
 
+  case expr2t::dynamic_size_id:
+    get_symbols(to_dynamic_size2t(expr).value, values);
+    return;
+
+  case expr2t::address_of_id:
+    get_symbols(to_address_of2t(expr).ptr_obj, values);
+    return;    
+
+  case expr2t::index_id:
+    get_symbols(to_index2t(expr).source_value, values);
+    get_symbols(to_index2t(expr).index, values);
+    return;
+
   case expr2t::with_id:
     return get_symbols(to_with2t(expr).update_value, values);
 
@@ -50,9 +63,15 @@ void get_symbols(const expr2tc &expr, std::set<std::string> &values)
 
   case expr2t::constant_struct_id:
     for(const auto &v : to_constant_struct2t(expr).datatype_members)
-    {
       get_symbols(v, values);
-    }
+    return;
+
+  case expr2t::constant_union_id:
+    for(const auto &v : to_constant_union2t(expr).datatype_members)
+      get_symbols(v, values);
+    return;
+  case expr2t::member_id:
+    get_symbols(to_member2t(expr).source_value, values);
     return;
 
   case expr2t::if_id:
@@ -68,6 +87,23 @@ void get_symbols(const expr2tc &expr, std::set<std::string> &values)
     return;
   }
 
+  case expr2t::bswap_id:
+  {
+    get_symbols(to_bswap2t(expr).value, values);
+    return;
+  }
+
+  case expr2t::ieee_add_id:
+  case expr2t::ieee_div_id:
+  case expr2t::ieee_mul_id:
+  case expr2t::ieee_sub_id:
+  {
+    auto op = std::dynamic_pointer_cast<ieee_arith_2ops>(expr);
+    get_symbols(op->side_1, values);
+    get_symbols(op->side_2, values);
+    return;
+  }
+
   case expr2t::add_id:
   case expr2t::div_id:
   case expr2t::mul_id:
@@ -79,13 +115,63 @@ void get_symbols(const expr2tc &expr, std::set<std::string> &values)
     return;
   }
 
+  case expr2t::and_id:
+  case expr2t::or_id:
+  case expr2t::xor_id:
+  {
+    auto op = std::dynamic_pointer_cast<logic_2ops>(expr);
+    get_symbols(op->side_1, values);
+    get_symbols(op->side_2, values);
+    return;
+  }
+
+  case expr2t::lessthan_id:
+  case expr2t::lessthanequal_id:
+  case expr2t::greaterthan_id:
+  case expr2t::greaterthanequal_id:
+  case expr2t::equality_id:
+  case expr2t::notequal_id:
+  {
+    auto op = std::dynamic_pointer_cast<relation_data>(expr);
+    get_symbols(op->side_1, values);
+    get_symbols(op->side_2, values);
+    return;
+  }
+
+  case expr2t::signbit_id:
+  case expr2t::popcount_id:
+  {
+    auto op = std::dynamic_pointer_cast<overflow_ops>(expr);
+    get_symbols(op->operand, values);
+    return;
+  }
+
+  case expr2t::bitand_id:
+  case expr2t::bitor_id:
+  case expr2t::bitxor_id:
+  case expr2t::bitnand_id:
+  case expr2t::bitnor_id:
+  case expr2t::bitnxor_id:
+  case expr2t::lshr_id:
+  case expr2t::shl_id:
+  case expr2t::ashr_id:
+  case expr2t::concat_id:
+  {
+    auto op = std::dynamic_pointer_cast<bit_2ops>(expr);
+    get_symbols(op->side_1, values);
+    get_symbols(op->side_2, values);
+    return;
+  }
+
   case expr2t::constant_int_id:
+  case expr2t::constant_floatbv_id:
   case expr2t::constant_bool_id:
   case expr2t::constant_array_of_id:
-
+  case expr2t::sideeffect_id:
     // TODO
     return;
   default:
+    log_error("Missing operator {}", *expr);
     throw "TODO: Implement missing operators";
     return;
   }
@@ -124,8 +210,11 @@ void slicer_domaint::assign(const expr2tc &e)
 
   // We do not support pointers
   if(is_pointer_type(assignment.target->type))
+  {
+    log_error("Forward slicer does not support pointers");
     throw "TODO: points-to analysis";
-
+  }
+  
   std::set<std::string> vars;
   get_symbols(assignment.source, vars);
   // Some magic to get the vars
@@ -136,7 +225,10 @@ void slicer_domaint::declaration(const expr2tc &e)
 {
   auto A = to_code_decl2t(e);
   if(is_pointer_type(A.type))
+  {
+    log_error("Forward slicer does not support pointers");
     throw "TODO: points-to analysis";
+  }
 
   std::set<std::string> vars;
   // Some magic to get the vars
@@ -239,7 +331,6 @@ bool forward_slicer::runOnFunction(std::pair<const dstring, goto_functiont> &F)
     return false;
   if(F.second.body_available)
   {
-    log_status("//// Function: {}\n", F.first);
     std::set<std::string> deps;
     for(auto it = (F.second.body).instructions.rbegin();
         it != (F.second.body).instructions.rend();
