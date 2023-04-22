1e14f18d0497a36295eda5225f17f74dc69c47fc
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-04-22 13:48:20 +0100

    [forward-slicer] ignoring atexit symbol

--- src/goto-programs/forward_slicer.cpp
+++ src/goto-programs/forward_slicer.cpp
@@ -1,5 +1,5 @@
 #include <goto-programs/forward_slicer.h>
-
+#include <util/prefix.h>
 namespace
 {
 // Recursively try to extract symbols from an expression
@@ -15,164 +15,9 @@ void get_symbols(const expr2tc &expr, std::set<std::string> &values)
     values.insert(to_symbol2t(expr).get_symbol_name());
     return;
 
-  case expr2t::dynamic_size_id:
-    get_symbols(to_dynamic_size2t(expr).value, values);
-    return;
-
-  case expr2t::address_of_id:
-    get_symbols(to_address_of2t(expr).ptr_obj, values);
-    return;    
-
-  case expr2t::index_id:
-    get_symbols(to_index2t(expr).source_value, values);
-    get_symbols(to_index2t(expr).index, values);
-    return;
-
-  case expr2t::with_id:
-    return get_symbols(to_with2t(expr).update_value, values);
-
-  case expr2t::byte_extract_id:
-    return get_symbols(to_byte_extract2t(expr).source_value, values);
-
-  case expr2t::typecast_id:
-    return get_symbols(to_typecast2t(expr).from, values);
-
-  case expr2t::bitcast_id:
-    return get_symbols(to_bitcast2t(expr).from, values);
-
-  case expr2t::code_assign_id:
-    return get_symbols(to_code_assign2t(expr).target, values);
-
-  case expr2t::code_decl_id:
-    values.insert(to_code_decl2t(expr).value.as_string());
-    return;
-
-  case expr2t::code_dead_id:
-    values.insert(to_code_dead2t(expr).value.as_string());
-    return;
-
-  case expr2t::code_return_id:
-    to_code_return2t(expr).foreach_operand(
-      [&values](auto &e) { get_symbols(e, values); });
-    return;
-
-  case expr2t::code_function_call_id:
-    to_code_function_call2t(expr).foreach_operand(
-      [&values](auto &e) { get_symbols(e, values); });
-    return;
-
-  case expr2t::constant_struct_id:
-    for(const auto &v : to_constant_struct2t(expr).datatype_members)
-      get_symbols(v, values);
-    return;
-
-  case expr2t::constant_union_id:
-    for(const auto &v : to_constant_union2t(expr).datatype_members)
-      get_symbols(v, values);
-    return;
-  case expr2t::member_id:
-    get_symbols(to_member2t(expr).source_value, values);
-    return;
-
-  case expr2t::if_id:
-  {
-    get_symbols(to_if2t(expr).true_value, values);
-    get_symbols(to_if2t(expr).false_value, values);
-    return;
-  }
-
-  case expr2t::not_id:
-  {
-    get_symbols(to_not2t(expr).value, values);
-    return;
-  }
-
-  case expr2t::bswap_id:
-  {
-    get_symbols(to_bswap2t(expr).value, values);
-    return;
-  }
-
-  case expr2t::ieee_add_id:
-  case expr2t::ieee_div_id:
-  case expr2t::ieee_mul_id:
-  case expr2t::ieee_sub_id:
-  {
-    auto op = std::dynamic_pointer_cast<ieee_arith_2ops>(expr);
-    get_symbols(op->side_1, values);
-    get_symbols(op->side_2, values);
-    return;
-  }
-
-  case expr2t::add_id:
-  case expr2t::div_id:
-  case expr2t::mul_id:
-  case expr2t::sub_id:
-  {
-    auto op = std::dynamic_pointer_cast<arith_2ops>(expr);
-    get_symbols(op->side_1, values);
-    get_symbols(op->side_2, values);
-    return;
-  }
-
-  case expr2t::and_id:
-  case expr2t::or_id:
-  case expr2t::xor_id:
-  {
-    auto op = std::dynamic_pointer_cast<logic_2ops>(expr);
-    get_symbols(op->side_1, values);
-    get_symbols(op->side_2, values);
-    return;
-  }
-
-  case expr2t::lessthan_id:
-  case expr2t::lessthanequal_id:
-  case expr2t::greaterthan_id:
-  case expr2t::greaterthanequal_id:
-  case expr2t::equality_id:
-  case expr2t::notequal_id:
-  {
-    auto op = std::dynamic_pointer_cast<relation_data>(expr);
-    get_symbols(op->side_1, values);
-    get_symbols(op->side_2, values);
-    return;
-  }
-
-  case expr2t::signbit_id:
-  case expr2t::popcount_id:
-  {
-    auto op = std::dynamic_pointer_cast<overflow_ops>(expr);
-    get_symbols(op->operand, values);
-    return;
-  }
-
-  case expr2t::bitand_id:
-  case expr2t::bitor_id:
-  case expr2t::bitxor_id:
-  case expr2t::bitnand_id:
-  case expr2t::bitnor_id:
-  case expr2t::bitnxor_id:
-  case expr2t::lshr_id:
-  case expr2t::shl_id:
-  case expr2t::ashr_id:
-  case expr2t::concat_id:
-  {
-    auto op = std::dynamic_pointer_cast<bit_2ops>(expr);
-    get_symbols(op->side_1, values);
-    get_symbols(op->side_2, values);
-    return;
-  }
-
-  case expr2t::constant_int_id:
-  case expr2t::constant_floatbv_id:
-  case expr2t::constant_bool_id:
-  case expr2t::constant_array_of_id:
-  case expr2t::sideeffect_id:
-    // TODO
-    return;
   default:
-    log_error("Missing operator {}", *expr);
-    throw "TODO: Implement missing operators";
+    expr->foreach_operand(
+      [&values](auto &e) { get_symbols(e, values); });
     return;
   }
 }
@@ -208,10 +53,14 @@ void slicer_domaint::assign(const expr2tc &e)
   if(!is_symbol2t(assignment.target))
     return;
 
+  if(has_prefix(to_symbol2t(assignment.target).get_symbol_name(), "c:stdlib.c"))
+     return;
+
   // We do not support pointers
   if(is_pointer_type(assignment.target->type))
   {
     log_error("Forward slicer does not support pointers");
+    e->dump();
     throw "TODO: points-to analysis";
   }
   
@@ -319,7 +168,7 @@ bool forward_slicer::runOnLoop(loopst &loop, goto_programt &)
   }
   return false;
 }
-#include <util/prefix.h>
+
 bool forward_slicer::should_skip_function(const std::string &func)
 {
   return has_prefix(func, "c:@F@__ESBMC_") || has_prefix(func, "c:@F@pthread_");
