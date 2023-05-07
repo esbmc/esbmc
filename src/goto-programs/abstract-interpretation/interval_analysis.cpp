b19ec92822906dc764a33eeef080a342b9eeab71
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-05-07 07:03:21 +0100

    [interval] fixed a bug in checking for membership

--- src/goto-programs/abstract-interpretation/interval_analysis.cpp
+++ src/goto-programs/abstract-interpretation/interval_analysis.cpp
@@ -95,13 +95,6 @@ void interval_analysis(goto_functionst &goto_functions, const namespacet &ns,
   //  interval_analysis.set_options(options);
   interval_analysis(goto_functions, ns);
 
-  if(1)
-  {
-    std::ostringstream oss;
-    interval_analysis.output(goto_functions, oss);
-    log_debug("[interval-analysis: {}", oss.str());
-  }
-
   Forall_goto_functions(f_it, goto_functions)
     instrument_intervals(interval_analysis, f_it->second);
 
