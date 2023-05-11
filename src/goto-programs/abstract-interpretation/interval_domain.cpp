63a091374f578fbfb385a8f1295d63683743aa51
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-05-11 17:26:55 +0100

    [interval] enable arithmetic for wrapped

--- src/goto-programs/abstract-interpretation/interval_domain.cpp
+++ src/goto-programs/abstract-interpretation/interval_domain.cpp
@@ -880,7 +880,7 @@ bool interval_domaint::ai_simplify(expr2tc &condition, const namespacet &ns)
 }
 
 // Options
-bool interval_domaint::enable_interval_arithmetic = false;
+bool interval_domaint::enable_interval_arithmetic = true;
 bool interval_domaint::enable_modular_intervals = false;
 bool interval_domaint::enable_assertion_simplification = false;
 bool interval_domaint::enable_contraction_for_abstract_states = false;
