9d83045cba0436c67b0aaa69263e76dd14c68560
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-05-16 13:52:40 +0200

    [regression] fix test case from #1015 for platform independence

--- regression/esbmc/github_1015/main.c
+++ regression/esbmc/github_1015/main.c
@@ -1,9 +1,11 @@
+#include <stdint.h>
+
 int main() {
   // Defining sorts ...
-  unsigned long mask = (unsigned long)-1 >> (sizeof(unsigned long) * 8 - 64);  
-  unsigned long mask2 = (unsigned long)1 << (64 - 1);
-  __ESBMC_assert(mask == 18446744073709551615UL, "Right shift should hold");
-  __ESBMC_assert(mask2 == 9223372036854775808UL, "Left shift should hold");
+  uint64_t mask = (uint64_t)-1 >> (sizeof(uint64_t) * 8 - 64);
+  uint64_t mask2 = (uint64_t)1 << (64 - 1);
+  __ESBMC_assert(mask == 18446744073709551615ULL, "Right shift should hold");
+  __ESBMC_assert(mask2 == 9223372036854775808ULL, "Left shift should hold");
 
   return 0;
 }
