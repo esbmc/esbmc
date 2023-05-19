c44b512254124ba6336aab6deb060db605705b25
hejueyun2 <hejueyun@gmail.com>
2023-05-19 01:43:17 +0100

    lint code base

--- src/esbmc/bmc.cpp
+++ src/esbmc/bmc.cpp
@@ -741,7 +741,7 @@ smt_convt::resultt bmct::multi_property_check(
     // Just to confirm that things are in parallel
 #ifndef _WIN32
 #ifndef __APPLE__ // sched_getcpu not supported in OS X
-    log_debug("Thread running on Core {}", sched_getcpu());
+      log_debug("Thread running on Core {}", sched_getcpu());
 #endif
 #endif
       // Set up the current claim and slice it!
