462bfcd4ddd2c982572088aebad0059e1855dd2a
kunjsong01 <kunjian90@gmail.com>
2023-05-24 15:10:37 +0100

    [regression] added TC

---
+++ regression/esbmc-cpp/bug_fixes/1006_aggregate_fail/1006.cpp
@@ -0,0 +1,17 @@
+// value-initialize for aggregate
+#include <cassert>
+
+struct uint3
+{
+  unsigned int x, y, z;
+};
+
+struct uint3 indexOfThread[1024];
+
+int main()
+{
+  assert(indexOfThread[0].x == 0);
+  assert(indexOfThread[0].y == 0);
+  assert(indexOfThread[0].z == 1); // should be 0
+  return 0;
+}
