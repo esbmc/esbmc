9e0a0881bec6f056b9bd5bf05a8aa59dd85005e7
kunjsong01 <kunjian90@gmail.com>
2023-05-25 15:13:31 +0100

    [regression] added TC for local storage

---
+++ regression/esbmc-cpp/bug_fixes/1032_POD_function/main.cpp
@@ -0,0 +1,31 @@
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+struct has_bitfield {
+  unsigned int a;
+  unsigned int b : 2;
+  unsigned int c : 2;
+  unsigned int d : 2;
+} beans;
+
+void do_pod()
+{
+  assert(beans.a == 0);
+  assert(beans.b == 0);
+  assert(beans.c == 0);
+  assert(beans.d == 0);
+  beans.d = 1;
+  assert(beans.d == 1);
+  beans.d = 0xFFFFFF;
+  assert(beans.d == 3);
+
+  memset(&beans, 0, sizeof(beans));
+  assert(beans.d == 0);
+}
+
+int main(void)
+{
+  do_pod();
+  return 0;
+}
