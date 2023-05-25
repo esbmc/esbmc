1730e6d92a96b48860e1b6b1ca6a1b295495dc93
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-05-25 12:18:32 +0100

    [cmake] added option to download dependencies

--- scripts/cmake/WindowsConfiguration.cmake
+++ scripts/cmake/WindowsConfiguration.cmake
@@ -38,11 +38,8 @@ if (WIN32)
 	  message(AUTHOR_WARNING "${CMAKE_CXX_COMPILER_ID} is not tested in Windows. You may run into issues.")
 	endif()
 
-  if(DOWNLOAD_WINDOWS_DEPENDENCIES)
-    download_zip_and_extract(LLVM https://gitlab.com/Anthonysdu/llvm11/-/raw/main/llvm+clang+lld-11.0.0-x86_64-windows-msvc-release-mt.zip)
+  if(DOWNLOAD_DEPENDENCIES)
     download_zip_and_extract(Z3 https://github.com/Z3Prover/z3/releases/download/z3-4.11.2/z3-4.11.2-x64-win.zip)
-    set(LLVM_DIR ${CMAKE_BINARY_DIR}/LLVM/llvm+clang+lld-11.0.0-x86_64-windows-msvc-release-mt)
-    set(Clang_DIR ${CMAKE_BINARY_DIR}/LLVM/llvm+clang+lld-11.0.0-x86_64-windows-msvc-release-mt)
     set(Z3_DIR ${CMAKE_BINARY_DIR}/Z3/z3-4.11.2-x64-win)
   endif()
 
