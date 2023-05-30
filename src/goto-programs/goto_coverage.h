c38d87aff9c37426cf42f67d164d0003b6c0fa64
hejueyun2 <hejueyun@gmail.com>
2023-05-30 16:33:34 +0100

    add another 2 options for debug

--- src/goto-programs/goto_coverage.h
+++ src/goto-programs/goto_coverage.h
@@ -1,4 +1,7 @@
 #include <goto-programs/goto_functions.h>
-#include <util/context.h>
 
-void goto_coverage(goto_functionst &goto_functions);
+// convert every assertion to an assert(0)
+void add_false_assert(goto_functionst &goto_functions);
+
+// add an assert(0) before each RETURN/END_FUNCTION statement
+void make_assert_false(goto_functionst &goto_functions);
