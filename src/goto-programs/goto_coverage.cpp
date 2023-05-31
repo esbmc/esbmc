2adfd21d1cfd31b5d863fc6ca6cba5602e982b33
hejueyun2 <hejueyun@gmail.com>
2023-05-31 16:24:09 +0100

    Modified the strategy for inserting assertions

--- src/goto-programs/goto_coverage.cpp
+++ src/goto-programs/goto_coverage.cpp
@@ -1,7 +1,8 @@
 #include <goto-programs/goto_coverage.h>
 
-void make_assert_false(goto_functionst &goto_functions)
+void make_asserts_false(goto_functionst &goto_functions)
 {
+  log_progress("Converting all assertions to false...");
   Forall_goto_functions(f_it, goto_functions)
     if(f_it->second.body_available)
     {
@@ -16,22 +17,39 @@ void make_assert_false(goto_functionst &goto_functions)
     }
 }
 
-void add_false_assert(goto_functionst &goto_functions)
+void add_false_asserts(goto_functionst &goto_functions)
 {
+  log_progress("Adding false assertions...");
   Forall_goto_functions(f_it, goto_functions)
     if(f_it->second.body_available)
     {
       goto_programt &goto_program = f_it->second.body;
       Forall_goto_program_instructions(it, goto_program)
       {
-        if(it->is_return() || it->is_end_function())
+        if(it->is_end_function())
         {
-          goto_programt::targett t = goto_program.insert(it);
-          t->type = ASSERT;
-          t->guard = gen_false_expr();
-          t->location = it->location;
-          it = ++t;
+          insert_false_assert(goto_program, it);
+        }
+
+        if((it->is_goto() && !is_true(it->guard)) || it->is_target())
+        {
+          it++; // add an assertion behind the instruciton
+          insert_false_assert(goto_program, it);
         }
       }
+
+      goto_programt::targett it = goto_program.instructions.begin();
+      insert_false_assert(goto_program, it);
     }
+}
+
+void insert_false_assert(
+  goto_programt &goto_program,
+  goto_programt::targett &it)
+{
+  goto_programt::targett t = goto_program.insert(it);
+  t->type = ASSERT;
+  t->guard = gen_false_expr();
+  t->location = it->location;
+  it = ++t;
 }
