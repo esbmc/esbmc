d2c013d2d87d8202964d7fc442adb3486632e3d7
kunjsong01 <kunjian90@gmail.com>
2023-06-14 15:07:50 +0100

    [clang-cpp-frontend] completed adjust_init for global variable

--- src/clang-cpp-frontend/clang_cpp_main.h
+++ src/clang-cpp-frontend/clang_cpp_main.h
@@ -11,6 +11,7 @@ public:
   // code adjustment for C++, e.g. adding implicit this in ctor when
   // adjusting the object initialization
   void adjust_init(code_assignt &assignment, codet &adjusted) override;
+  void convert_expression_to_code(exprt &expr);
 };
 
 #endif /* CLANG_CPP_FRONTEND_CLANG_CPP_MAIN_H_ */
