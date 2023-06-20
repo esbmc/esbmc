e06d070b4274ff70e9c0851728d4831d50556172
Fedor Shmarov <shmarovfedor@mail.ru>
2023-06-20 12:06:21 +0100

    [util] added c_sizeof method for irep2

--- src/util/c_sizeof.cpp
+++ src/util/c_sizeof.cpp
@@ -9,19 +9,29 @@
 exprt c_sizeof(const typet &src, const namespacet &ns)
 {
   type2tc t = migrate_type(ns.follow(src));
+  return migrate_expr_back(c_sizeof(t, ns));
+}
 
-  // If it is an array of infinite size, we just return
-  // "infinity" expression and multiply it recursively
-  // by the size of the array subtype
+expr2tc c_sizeof(const type2tc &src, const namespacet &ns)
+{
+  type2tc t = ns.follow(src);
+  // A trick to deal with infinitely sized arrays. This only
+  // comes useful when we need to "bound" infinite sizes
+  // (e.g., in GOTO to C translator).
   if(is_array_type(t) && to_array_type(t).size_is_infinite)
-    return mult_exprt(
-      c_sizeof(migrate_type_back(to_array_type(t).subtype), ns),
-      exprt("infinity"));
-
+  {
+    // Right now we just introduce a symbol __ESBMC_INF_SIZE to represent
+    // the "infinity" expression in irep1. (As of now migrating
+    // the latter into irep2 results in an error.)
+    symbol2tc inf_size(get_uint64_type(), "__ESBMC_INF_SIZE");
+    constant_int2tc subtype_size(
+      get_uint64_type(), type_byte_size(to_array_type(t).subtype));
+    return mul2tc(get_uint64_type(), inf_size, subtype_size);
+  }
   // Array size simplification and so forth will have already occurred in
   // migration, but we might still run into a nondeterministically sized
   // array.
-  expr2tc size = type_byte_size_expr(t);
-
-  return migrate_expr_back(size);
+  BigInt size = type_byte_size(t);
+  constant_int2tc res(get_uint64_type(), size);
+  return res;
 }
--- src/util/c_sizeof.h
+++ src/util/c_sizeof.h
@@ -3,3 +3,4 @@
 #include <util/namespace.h>
 
 exprt c_sizeof(const typet &src, const namespacet &ns);
+expr2tc c_sizeof(const type2tc &src, const namespacet &ns);
