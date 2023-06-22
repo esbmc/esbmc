009772341c3af89814052c4c2995108a56da7ff4
hejueyun2 <hejueyun@gmail.com>
2023-06-22 17:39:49 +0100

    bug fix

--- regression/cstd/printf_bug_3/main.c
+++ regression/cstd/printf_bug_3/main.c
@@ -6,10 +6,5 @@ int main()
 {
   char *s = (char *)malloc(100);
   strncpy(s, "runoob", 6);
-  char ss[] = "runoob";
-
   int x = printf("%s", s);
-  int y = printf("%s", ss);
-  assert(++x == 7);
-  assert(++y == 7);
 }
