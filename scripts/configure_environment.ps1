43c04a3080f29e03577b3af02db670099196e0e9
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2023-06-23 13:41:11 +0100

    [scripts] configure_environment.ps1 was renamed to build.ps1

--- scripts/configure_environment.ps1
+++
@@ -1,5 +0,0 @@
-
-ls C:\vcpkg\installed\x64-windows\bin\
-New-Item C:\Deps -itemtype directory
-Copy-Item C:\vcpkg\installed\x64-windows\bin\boost_filesystem-vc143-mt-x64-1_80.dll C:\Deps
-Copy-Item C:\vcpkg\installed\x64-windows\bin\boost_program_options-vc143-mt-x64-1_80.dll C:\Deps
