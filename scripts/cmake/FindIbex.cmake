5e695ec1331caa443449bf5a7c2fb455b1e67715
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2023-06-23 14:07:18 +0100

    [ci] enabled werror

--- scripts/cmake/FindIbex.cmake
+++ scripts/cmake/FindIbex.cmake
@@ -1,6 +1,10 @@
 # Module to find IBEX
 
 if(DOWNLOAD_DEPENDENCIES AND (NOT DEFINED IBEX_DIR))
+   # TODO: there might be a better way of doing this!
+   if(ENABLE_WERROR)
+      add_compile_options(-Wno-error)
+   endif()
    include(CPM)
    CPMAddPackage(
       NAME ibex
@@ -20,6 +24,11 @@ if(DOWNLOAD_DEPENDENCIES AND (NOT DEFINED IBEX_DIR))
       WORKING_DIRECTORY ${ibex_SOURCE_DIR})
 
    set(IBEX_DIR ${ibex_BINARY_DIR})
+
+   if(ENABLE_WERROR)
+      include(WError)
+   endif()
+
 endif()
 
 find_package(PkgConfig)
