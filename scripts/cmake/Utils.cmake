962e5996d4ce8685106cc455d6b99506638cf3f4
Rafael Sá Menezes <rafael.sa.menezes40@gmail.com>
2023-06-29 11:30:14 +0100

    [macos] removed the wrapper

--- scripts/cmake/Utils.cmake
+++ scripts/cmake/Utils.cmake
@@ -4,10 +4,6 @@
 set(ESBMC_BIN "${CMAKE_BINARY_DIR}/src/esbmc/esbmc")
 if(WIN32)
     set(ESBMC_BIN "${CMAKE_INSTALL_PREFIX}/bin/esbmc.exe")
-elseif(APPLE)
-    set(MACOS_ESBMC_WRAPPER "#!/bin/sh\n${ESBMC_BIN} --sysroot ${C2GOTO_SYSROOT} $@")
-    file(WRITE ${CMAKE_CURRENT_BINARY_DIR}/macos-wrapper.sh ${MACOS_ESBMC_WRAPPER})
-    set(ESBMC_BIN "${CMAKE_CURRENT_BINARY_DIR}/macos-wrapper.sh")
 endif()
 
 # Assert that a variable is defined
