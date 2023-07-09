f858980b25a41602343d8aea5d7affd7838c296c
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-07-09 21:46:27 +0100

    [interval] clang-format

--- src/goto-programs/abstract-interpretation/interval_domain.cpp
+++ src/goto-programs/abstract-interpretation/interval_domain.cpp
@@ -687,8 +687,7 @@ expr2tc interval_domaint::make_expression_helper<wrapped_interval>(
     // Interval: [a,b]
     std::vector<expr2tc> disjuncts;
 
-    auto convert = [this, &src, &symbol, &disjuncts](wrapped_interval &w)
-    {
+    auto convert = [this, &src, &symbol, &disjuncts](wrapped_interval &w) {
       assert(w.lower <= w.upper);
 
       std::vector<expr2tc> s_conjuncts;
@@ -728,8 +727,7 @@ expr2tc interval_domaint::make_expression_helper(const expr2tc &symbol) const
     return gen_false_expr();
 
   std::vector<expr2tc> conjuncts;
-  auto typecast = [&symbol](expr2tc v)
-  {
+  auto typecast = [&symbol](expr2tc v) {
     c_implicit_typecast(v, symbol->type, *migrate_namespace_lookup);
     return v;
   };
@@ -806,12 +804,10 @@ bool contains_float(const expr2tc &e)
     return true;
 
   bool inner_float = false;
-  e->foreach_operand(
-    [&inner_float](auto &it)
-    {
-      if(contains_float(it))
-        inner_float = true;
-    });
+  e->foreach_operand([&inner_float](auto &it) {
+    if(contains_float(it))
+      inner_float = true;
+  });
 
   return inner_float;
 }
