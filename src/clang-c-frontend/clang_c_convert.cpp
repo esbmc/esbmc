4c3a1c95ac558fdb2668c5a2496d769f5bae6a71
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-07-09 11:45:46 +0200

    [cheri] rm more commented code

--- src/clang-c-frontend/clang_c_convert.cpp
+++ src/clang-c-frontend/clang_c_convert.cpp
@@ -2613,7 +2613,6 @@ bool clang_c_convertert::get_cast_expr(
     /* An explicit __cheri_tocap means this value might be tagged. */
   case clang::CK_CHERICapabilityToPointer:
     /* both should not be generated in purecap mode */
-    // assert(0);
     break;
 #endif
 
