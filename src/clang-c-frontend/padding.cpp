f8bea59da221e93e5a47681388d45f45c9528df4
kunjsong01 <kunjian90@gmail.com>
2023-07-12 14:14:49 +0100

    [clang-c-frontend] added function to repalce the symbolic type

--- src/clang-c-frontend/padding.cpp
+++ src/clang-c-frontend/padding.cpp
@@ -363,3 +363,17 @@ void add_padding(union_typet &type, const namespacet &ns)
     type.components().push_back(component);
   }
 }
+
+void get_complete_type(struct_typet &type, const namespacet &ns)
+{
+  for(auto &comp : type.components())
+  {
+    typet &it_type = comp.type();
+    if(it_type.is_symbol())
+    {
+      const symbolt *actual_type = ns.lookup(it_type.identifier());
+      assert(actual_type);
+      it_type = actual_type->type;
+    }
+  }
+}
--- src/clang-c-frontend/padding.h
+++ src/clang-c-frontend/padding.h
@@ -12,4 +12,9 @@ BigInt alignment(const typet &type, const namespacet &);
 void add_padding(struct_typet &type, const namespacet &);
 void add_padding(union_typet &type, const namespacet &);
 
+// A type symbol may contain a compoment that has symbolic type,
+// which doesn't work with type_byte_size.
+// This function replace the symbolic type with the actual type.
+void get_complete_type(struct_typet &type, const namespacet &ns);
+
 #endif // CPROVER_ANSI_C_PADDING_H
