1565181d392742b10f5cccf5d2d839b9df8e1daf
kunjsong01 <kunjian90@gmail.com>
2023-07-13 15:51:32 +0100

    [clang-c-frontend] fixed aggregate zero init

--- src/clang-c-frontend/symbolic_types.h
+++ src/clang-c-frontend/symbolic_types.h
@@ -9,9 +9,12 @@
 #include <util/std_types.h>
 #include <util/namespace.h>
 
-// A type symbol may contain a compoment that has symbolic type,
-// which doesn't work with type_byte_size.
-// This function replaces the symbolic type with the complete type.
+// Replaces the symbolic struct type with the complete struct type.
 void get_complete_struct_type(struct_typet &type, const namespacet &ns);
+
+// If an array type's subtype is symbolic struct type, replace it with
+// the complete struct type
+bool array_type_contains_symbolic(const typet &type,
+    typet &complete_type, const namespacet &ns);
 
 #endif // ESBMC_SYMBOLIC_TYPES_H
