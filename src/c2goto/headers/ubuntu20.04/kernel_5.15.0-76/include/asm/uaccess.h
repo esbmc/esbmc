05fd53aa240a35989e82df0198e1f6b2cae7ac1e
Zhicheng Zhou <kai_zhichengzhou@hotmail.com>
2023-07-13 09:56:48 +0100

    build with cmake 3.26, run properly on local test cases, create directory for regression

--- src/c2goto/headers/ubuntu20.04/kernel_5.15.0-76/include/asm/uaccess.h
+++ src/c2goto/headers/ubuntu20.04/kernel_5.15.0-76/include/asm/uaccess.h
@@ -1,17 +1,10 @@
-#include "asm.h"
-#include "extable.h"
-#include "page.h"
-#include "smap.h"
-#include "../linux/compiler.h"
-#include "../linux/string.h"
-#include "../linux/kasan-checks.h"
 
 #define PAGE_SIZE 4096 // page size
-#define USER_MEMORY_SIZE 0x100000000 //only for simulate the user memory
-#define KERNEL_MEMORY_SIZE 0x100000000 // only for simulate the kernel memory
+#define USER_MEMORY_SPACE 10000 //only for simulate the user memory
+#define KERNEL_MEMORY_SPACE 10000// only for simulate the kernel memory
 
-char user_memory[USER_MEMORY_SIZE]; //mock user memory
-char kernel_memory[KERNEL_MEMORY_SIZE];//mock user memory
+char user_memory[USER_MEMORY_SPACE]; //mock user memory
+char kernel_memory[KERNEL_MEMORY_SPACE];//mock user memory
 
 //simulate copy_to_user function in kernel space 
 unsigned long copy_to_user(void* to, void* from, unsigned size);
