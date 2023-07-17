1ea2531a73eb85b363f4de30c083dd88ecf28684
kunjsong01 <kunjian90@gmail.com>
2023-07-17 13:52:39 +0100

    [clang-c-frontend] use complete type for InitListExpr

--- src/clang-c-frontend/clang_c_convert.cpp
+++ src/clang-c-frontend/clang_c_convert.cpp
@@ -489,7 +489,7 @@ bool clang_c_convertert::get_var(const clang::VarDecl &vd, exprt &new_expr)
     // the type might contains symbolic types,
     // replace them with complete types before generating zero initialization
     typet complete_type;
-    bool contains_symbolic = type_contains_symbolic(t, complete_type, ns);
+    bool contains_symbolic = contains_symbolic_struct_types(t, complete_type, ns);
 
     // Initialize with zero value, if the symbol has initial value,
     // it will be added later on in this method
@@ -1967,6 +1967,13 @@ bool clang_c_convertert::get_expr(const clang::Stmt &stmt, exprt &new_expr)
       return true;
 
     exprt inits;
+
+    // the type might contains symbolic types,
+    // replace them with complete types before getting the initializations
+    typet complete_type;
+    bool contains_symbolic = contains_symbolic_struct_types(t, complete_type, ns);
+    if(contains_symbolic)
+      t = complete_type;
 
     // Structs/unions/arrays put the initializer on operands
     if(t.is_struct() || t.is_array() || t.is_vector())
--- src/clang-c-frontend/symbolic_types.h
+++ src/clang-c-frontend/symbolic_types.h
@@ -14,7 +14,7 @@ void get_complete_struct_type(struct_typet &type, const namespacet &ns);
 
 // If a type contains symbolic struct type, replace it with
 // the complete struct type
-bool type_contains_symbolic(
+bool contains_symbolic_struct_types(
   const typet &type,
   typet &complete_type,
   const namespacet &ns);
