c74d495a152691d366354844c41b277f00136d94
kunjsong01 <kunjian90@gmail.com>
2023-07-18 11:05:35 +0100

    [clang-c-frontend] applied get_type patch as suggested in PR 1162

--- src/clang-c-frontend/clang_c_convert.cpp
+++ src/clang-c-frontend/clang_c_convert.cpp
@@ -1003,18 +1003,12 @@ bool clang_c_convertert::get_type(const clang::Type &the_type, typet &new_type)
     std::string id, name;
     get_decl_name(rd, name, id);
 
-    // does symbol already exist before we go through the getter?
-    bool sym_already_exist = context.find_symbol(id);
-
-    // avoid unnecessary conversion if symbol already exists
-    if(!sym_already_exist)
-      if(get_struct_union_class(rd))
-        return true;
-
-    symbolt &s = *context.find_symbol(id);
-    // For the time being we just copy the entire type.
-    // See comment: https://github.com/esbmc/esbmc/issues/991#issuecomment-1535068024
-    new_type = s.type;
+    if(auto *symbol = context.find_symbol(id))
+      // For the time being we just copy the entire type.
+      // See comment: https://github.com/esbmc/esbmc/issues/991#issuecomment-1535068024
+      new_type = symbol->type;
+    else
+      new_type = symbol_typet(id);
 
     break;
   }
