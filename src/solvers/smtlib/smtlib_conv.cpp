6fe5c7668c4767d6148acc94b944f3bd5bc9b3ec
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-08-12 14:25:59 +0200

    [smtlib] also emit (check-sat) to file output

--- src/solvers/smtlib/smtlib_conv.cpp
+++ src/solvers/smtlib/smtlib_conv.cpp
@@ -164,7 +164,11 @@ void smtlib_convt::dump_smt()
 {
   auto path = config.options.get_option("output");
   if(path != "")
+  {
+    assert(emit_opt_output);
+    emit_opt_output.emit("%s\n", "(check-sat)");
     log_status("SMT formula written to output file {}", path);
+  }
 }
 
 smtlib_convt::file_emitter::file_emitter(const std::string &path)
