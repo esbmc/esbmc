0b3949e5cb628af0b753a46374fe17ecb68dfb2f
hejueyun2 <hejueyun@gmail.com>
2023-08-22 15:52:46 +0100

    fix win32

--- regression/cstd/printf_5_bug/main.c
+++ regression/cstd/printf_5_bug/main.c
@@ -3,17 +3,22 @@
 #include <stdlib.h>
 #include <assert.h>
 
-struct D {
-	int dummy;
-	char fmt[16];
+struct D
+{
+  int dummy;
+  char fmt[16];
 };
 
 int main()
 {
-	unsigned n = nondet_uint() % 1024;
-	n++;
-	unsigned i = nondet_uint() % n;
-	struct D data[1];
-	int r = printf("%02X", data[0].fmt);
-	assert(r < 2);
+  unsigned n = nondet_uint() % 1024;
+  n++;
+  unsigned i = nondet_uint() % n;
+  struct D data[1];
+#ifndef _WIN32
+  int r = printf("%02X", data[0].fmt);
+  assert(r < 2);
+#else
+  assert(0);
+#endif
 }
