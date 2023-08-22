eed4d1760bc5fa8c59a42479c2098d0d396ff9de
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-08-22 23:58:22 +0200

    [util] guard: remove declarations of default constructors implicitly there

--- src/util/guard.h
+++ src/util/guard.h
@@ -27,10 +27,6 @@
 class guardt
 {
 public:
-  // Default constructors
-  guardt() = default;
-  guardt(const guardt &ref) = default;
-
   typedef std::vector<expr2tc> guard_listt;
 
   void add(const expr2tc &expr);
