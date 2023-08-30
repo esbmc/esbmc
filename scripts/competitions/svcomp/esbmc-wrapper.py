f5d9248650abf8251225044788db2d801ffb6b87
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-08-30 15:27:36 +0100

    [cse] added gcse in wrapper

--- scripts/competitions/svcomp/esbmc-wrapper.py
+++ scripts/competitions/svcomp/esbmc-wrapper.py
@@ -259,7 +259,7 @@ def get_command_line(strat, prop, arch, benchmark, concurrency, dargs, esbmc_ci)
     command_line += " --no-por --context-bound 2 "
     #command_line += "--no-slice " # TODO: Witness validation is only working without slicing
   else:
-    command_line += "  "
+    command_line += " --gcse "
   # Add witness arg
   witness_name = os.path.basename(benchmark) if esbmc_ci else "witness"
   command_line += "--witness-output " + witness_name + ".graphml "
