1e1147acee450907ec1f510e8bfe9e7ad99ede5c
hejueyun2 <hejueyun@gmail.com>
2023-09-04 02:50:54 +0100

    add regression tests

---
+++ regression/esbmc-solidity/whole_contract_1/contract.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+contract Robot {
+    int x = 0;
+    int y = 0;
+
+    function moveLeftUp() public {
+        --x;
+        ++y;
+    }
+
+    function moveLeftDown() public {
+        --x;
+        --y;
+    }
+
+    function moveRightUp() public {
+        ++x;
+        ++y;
+    }
+
+    function moveRightDown() public {
+        ++x;
+        --y;
+    }
+
+    function test() public view {
+        assert((x + y) % 2 == 0);
+    }
+}
