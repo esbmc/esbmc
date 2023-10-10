1bdcbcb2e257084b173c0d5e282df58c3d011113
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-10-10 20:23:26 +0100

    Handling python dependencies in astgen.py

--- src/python-frontend/astgen.py
+++ src/python-frontend/astgen.py
@@ -1,23 +1,41 @@
 import ast
 import sys
-from ast2json import ast2json
+import importlib.util
 import json
 
-def main():
+def import_module_by_name(module_name):
+    try:
+        module = importlib.import_module(module_name)
+        return module
+    except ImportError:
+        print(f"Error: Module '{module_name}' not found.")
+        print(f"Please install it with: pip3 install {module_name}")
+        sys.exit(1)
+
+
+def check_usage():
     if len(sys.argv) != 2:
         print("Usage: python astgen.py <file path>")
-        return
+        sys.exit(2)
+
+
+def main():
+    ast2json_module = import_module_by_name("ast2json")
+
+    check_usage()
 
     filename = sys.argv[1]
 
     with open(filename, "r") as source:
         tree = ast.parse(source.read())
 
-    ast_json = ast2json(tree)
+
+    ast_json = ast2json_module.ast2json(tree)
     json_filename = "/tmp/ast.json"
 
     with open(json_filename, "w") as json_file:
         json.dump(ast_json, json_file, indent=4)
+
 
 if __name__ == "__main__":
     main()
