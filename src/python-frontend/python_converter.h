ed10a7792b4490166e985d255e4684c31dc7ce5d
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-10-16 16:18:57 +0100

    [python-frontend] Handling if

--- src/python-frontend/python_converter.h
+++ src/python-frontend/python_converter.h
@@ -16,12 +16,17 @@ public:
 
 private:
   void get_var_assign(const nlohmann::json &ast_node, codet& target_block);
+  void get_if_statement(const nlohmann::json &ast_node, codet& target_block);
   exprt get_expr(const nlohmann::json &element);
   exprt get_unary_operator_expr(const nlohmann::json &element);
   exprt get_binary_operator_expr(const nlohmann::json &element);
+  exprt get_block(const nlohmann::json &ast_block);
+
+  const nlohmann::json find_var_decl(const std::string &id);
 
   contextt &context;
   std::string ast_output_dir;
   typet current_element_type;
+  std::string current_function_name;
   nlohmann::json ast_json;
 };
