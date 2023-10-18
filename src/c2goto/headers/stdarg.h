56213043beb90710f3fc3092f8f58bc5243694f5
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-10-18 11:51:20 +0200

    Revert "[c2goto] OM: remove <stdarg.h>"
    This reverts commit 48e41b3e6e768ed9e64f3fa842a1921bd5df216b.

---
+++ src/c2goto/headers/stdarg.h
@@ -0,0 +1,26 @@
+#ifndef __ESBMC_HEADERS_STDARG_H_
+#define __ESBMC_HEADERS_STDARG_H_
+/* Define standard macros; esbmc currently copes with gcc internal forms,
+ * so we'll just replicate those */
+
+#define _VA_LIST
+#define _VA_LIST_DECLARED /* FreeBSD */
+
+#define va_start(v,l)   __ESBMC_va_start(v,l)
+#define va_end(v)       __ESBMC_va_end(v)
+#define va_arg(v,l)     ((l)*(l *)__ESBMC_va_arg(v))
+#define va_copy(d,s)    __ESBMC_va_copy(d,s)
+
+typedef char *va_list;
+
+void __ESBMC_va_start(va_list, ...);
+void __ESBMC_va_end(va_list);
+void * __ESBMC_va_arg(va_list);
+void __ESBMC_va_copy(va_list, va_list);
+
+#ifndef __GNUC_VA_LIST
+#define __GNUC_VA_LIST 1
+typedef va_list __gnuc_va_list;
+#endif
+
+#endif /* __ESBMC_HEADERS_STDARG_H_ */
