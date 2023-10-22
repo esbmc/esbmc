527fa569de1bc16b747c2fae5de2f4513cb6f6a2
hejueyun2 <hejueyun@gmail.com>
2023-10-23 00:37:14 +0100

    add regression tests

---
+++ regression/esbmc/github_1337_5/main.c
@@ -0,0 +1,32 @@
+#include<assert.h>
+
+void loop1()
+{
+   for (int i = 0; i < 2; i++)
+   {
+      assert(1);
+   }
+}
+
+void loop2()
+{
+   for (int i = 0; i < 2; i++)
+   {
+      assert(1);
+   }
+}
+
+int main()
+
+{
+   switch (1)
+   {
+   case 1:
+      loop1();
+   case 2:
+      loop2();
+   default:
+       ;
+   }
+   return 0;
+}
