980c2a220c03b85271b3e82fd9ef4a9700ff305f
Lucas Cordeiro <lucasccordeiro@gmail.com>
2023-10-22 17:57:02 +0100

    [python] Added support for mod operator
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/python-frontend/python_converter.cpp
+++ src/python-frontend/python_converter.cpp
@@ -14,6 +14,7 @@ static const std::unordered_map<std::string, std::string> operator_map = {
   {"Sub", "-"},
   {"Mult", "*"},
   {"Div", "/"},
+  {"Mod", "mod"},
   {"BitOr", "bitor"},
   {"BitAnd", "bitand"},
   {"BitXor", "bitxor"},
