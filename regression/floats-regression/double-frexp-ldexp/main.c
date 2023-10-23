3b75348d8667d61cd6e32b98575711caad1931e9
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-10-23 18:37:52 +0200

    [regression] frexp: test some (finite) constants

--- regression/floats-regression/double-frexp-ldexp/main.c
+++ regression/floats-regression/double-frexp-ldexp/main.c
@@ -48,4 +48,20 @@ int main()
 
 	frexp(DBL_MAX, &ze);
 	assert(ze == DBL_MAX_EXP);
+
+	double r = frexp(0.1234567890123456789, &ze);
+	assert(r == 0x1.f9add3746f65fp-1);
+	assert(ze == -3);
+
+	r = frexp(0.9999999999999999, &ze);
+	assert(r == 0x1.fffffffffffffp-1);
+	assert(ze == 0);
+
+	r = frexp(-1, &ze);
+	assert(r == -.5);
+	assert(ze == 1);
+
+	r = frexp(.5, &ze);
+	assert(r == .5);
+	assert(ze == 0);
 }
