21d9cc98c714b14c7dd8855f601c8ade795b1c52
hejueyun2 <hejueyun@gmail.com>
2023-11-02 00:00:18 +0000

    add regression tests

---
+++ regression/esbmc-solidity/default_value_2/contract.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0;
+
+contract BASE {
+    int a;
+    uint b;
+    uint256 c;
+    bytes1 d;
+    uint[] f = new uint[](2);
+
+    function test() public view {
+        assert(a == 0);
+        assert(b == 0);
+        assert(c == 0);
+        assert(d == 0x00);
+        assert(f[0] == 0);
+        assert(f[1] == 0);
+    }
+}
