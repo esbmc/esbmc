4cb09bcb35a13e1f4206b72b0bbc69796886a3ba
hejueyun2 <hejueyun@gmail.com>
2023-11-03 20:24:23 +0000

    refactoring and fix help info

--- src/esbmc/options.cpp
+++ src/esbmc/options.cpp
@@ -123,7 +123,9 @@ const struct group_opt_templ all_cmd_options[] = {
     {"version", NULL, "show current ESBMC version and exit"},
     {"cex-output",
      boost::program_options::value<std::string>(),
-     "save the counterexample into a file"},
+     "save the counterexample into a file or, "
+     "in multi-property mode, multiple files with name prefix 'N-' "
+     "where 'N' is a decimal increasing from zero"},
     {"file-output",
      boost::program_options::value<std::string>(),
      "redirects every message into a file (no stdout/stderr)"},
@@ -477,7 +479,7 @@ const struct group_opt_templ all_cmd_options[] = {
     {"make-assert-false", NULL, "convert every assertion to false"},
     {"goto-coverage",
      NULL,
-     "this activates --make-assert-false and --multi-property and "
+     "this activates --make-assert-false and --multi-property, "
      "deactivates --keep-verified-claims, and "
      "shows the coverage of assertion instances"},
     {"goto-coverage-claims",
