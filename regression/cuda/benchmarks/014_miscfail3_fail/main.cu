5e034d0440c53aae2859ab9e018a24ff4a7b1a15
XLiZHI <958689657@qq.com>
2023-11-11 01:44:23 +0800

    [regression] add new TCs

--- regression/cuda/benchmarks/014_miscfail3_fail/main.cu
+++ regression/cuda/benchmarks/014_miscfail3_fail/main.cu
@@ -1,18 +1,17 @@
 //xfail:BOOGIE_ERROR
 //main.cu: error: possible read-write race
 //however, this didn't happen in the tests
-// In CUDA providing static and __attribute__((always_inline)) SHOUD NOT
-// keep a copy of inlined function around.
-//ps: the values from A[N-1-offset] to A[N-1] always will receive unpredictable values,
+//altough in CUDA providing the inline keyword should still keep a copy of the function around,
+//this kind of access is considered a error by ESBMC
+//ps: the values from A[N-1-offset) to A[N-1] always will receive unpredictable values,
 //because they acess values because they access memory positions that were not initiated
 
 #include <stdio.h>
 #include <cuda_runtime_api.h>
 #define tid threadIdx.x
-#define N 2//32
+#define N 2//1024
 
-__device__ static __attribute__((always_inline))
-void inlined(int *A, int offset)
+__device__ inline void inlined(int *A, int offset)
 {
    int temp = A[tid + offset];
    A[tid] += temp;
@@ -38,18 +37,13 @@ int main(){
 	cudaMemcpy(dev_a,a,size, cudaMemcpyHostToDevice);
 
 	printf("a:  ");
-	for (int i = 0; i < N; i++)
-		printf("%d	", a[i]);
 
 	//inline_test<<<1,N>>>(dev_a, 2);		//you can change this offset for tests
-	ESBMC_verify_kernel_intt(inline_test, 1, N, dev_a, 2);
-
+	ESBMC_verify_kernel_intt(inline_test, 1, N, dev_a, 1);
+	
 	cudaMemcpy(a,dev_a,size,cudaMemcpyDeviceToHost);
 
 	printf("\nFunction Results:\n   ");
-
-	for (int i = 0; i < N; i++)
-		printf("%d	", a[i]);
 
 	free(a);
 
