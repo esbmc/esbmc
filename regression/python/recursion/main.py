7c24710387eecea60d5555816ccae04c93e126a3
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-11-13 15:52:37 +0000

    Improving regression test

--- regression/python/recursion/main.py
+++ regression/python/recursion/main.py
@@ -1,6 +1,10 @@
-def zero(n:int) -> int:
-    if (n>0):
-        return zero(n-1)
-    return n
+def fib(n:int) -> int:
+  if n <= 0:
+    return 0
+  elif n == 1:
+    return 1
+  else:
+    return fib(n - 1) + fib(n - 2)
 
-result:int = zero(5)
+n:int = 6
+result:int = fib(n)
