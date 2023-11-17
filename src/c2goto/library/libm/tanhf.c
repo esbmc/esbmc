7e4ff35b093882d60f140c874c483b873c5199be
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-11-17 08:18:23 +0100

    [libm] musl implementations of NN functions, based on #1232

--- src/c2goto/library/libm/tanhf.c
+++ src/c2goto/library/libm/tanhf.c
@@ -1,28 +1,39 @@
-#include <math.h>
+#include "libm.h"
 
 float tanhf(float x)
 {
-__ESBMC_HIDE:;
-    /**
-        Highly imprecise implementation of tanhf(x) on floating-point operands
-        which satisfies the following properties:
-        tanhf(x) >= -1
-        tanhf(x) <= 1
-        tanhf(-Inf) = -1
-        tanhf(Inf) = 1
-        tanhf(x) = -tanhf(-x)
-        dtanhf(x)/dx <= 1
-     */
+	union {float f; uint32_t i;} u = {.f = x};
+	uint32_t w;
+	int sign;
+	float t;
 
-    // handle non-finite numbers
-    if(isnan(x))
-        return x;
-    if(isinf(x))
-        return (x > 0)? 1.0f: -1.0f;
+	/* x = |x| */
+	sign = u.i >> 31;
+	u.i &= 0x7fffffff;
+	x = u.f;
+	w = u.i;
 
-    // softsign approximation
-    if(x < 0)
-        return x / (1.0f - x);
-    else
-        return x / (1.0f + x);
+	if (w > 0x3f0c9f54) {
+		/* |x| > log(3)/2 ~= 0.5493 or nan */
+		if (w > 0x41200000) {
+			/* |x| > 10 */
+			t = 1 + 0/x;
+		} else {
+			t = expm1f(2*x);
+			t = 1 - 2/(t+2);
+		}
+	} else if (w > 0x3e82c578) {
+		/* |x| > log(5/3)/2 ~= 0.2554 */
+		t = expm1f(2*x);
+		t = t/(t+2);
+	} else if (w >= 0x00800000) {
+		/* |x| >= 0x1p-126 */
+		t = expm1f(-2*x);
+		t = -t/(t+2);
+	} else {
+		/* |x| is subnormal */
+		FORCE_EVAL(x*x);
+		t = x;
+	}
+	return sign ? -t : t;
 }
