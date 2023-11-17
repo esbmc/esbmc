0ded83f6cf4644bf8e5956f2a35033737affb9ab
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-11-17 08:18:23 +0100

    [libm] musl implementations of NN functions, based on #1232

--- src/c2goto/library/libm/atanhf.c
+++ src/c2goto/library/libm/atanhf.c
@@ -1,12 +1,28 @@
-#include <math.h>
+#include "libm.h"
 
+/* atanh(x) = log((1+x)/(1-x))/2 = log1p(2x/(1-x))/2 ~= x + x^3/3 + o(x^5) */
 float atanhf(float x)
 {
-__ESBMC_HIDE:;
+	union {float f; uint32_t i;} u = {.f = x};
+	unsigned s = u.i >> 31;
+	float_t y;
 
-    // unimplemented operational models
-    // should return VERIFICATION UNKNOWN
-    __ESBMC_unreachable();
+	/* |x| */
+	u.i &= 0x7fffffff;
+	y = u.f;
 
-    return 0.0f;
+	if (u.i < 0x3f800000 - (1<<23)) {
+		if (u.i < 0x3f800000 - (32<<23)) {
+			/* handle underflow */
+			if (u.i < (1<<23))
+				FORCE_EVAL((float)(y*y));
+		} else {
+			/* |x| < 0.5, up to 1.7ulp error */
+			y = 0.5f*log1pf(2*y + 2*y*y/(1-y));
+		}
+	} else {
+		/* avoid overflow */
+		y = 0.5f*log1pf(2*(y/(1-y)));
+	}
+	return s ? -y : y;
 }
--- src/c2goto/library/libm/expf.c
+++ src/c2goto/library/libm/expf.c
@@ -1,42 +1,80 @@
+/*
+ * Single-precision e^x function.
+ *
+ * Copyright (c) 2017-2018, Arm Limited.
+ * SPDX-License-Identifier: MIT
+ */
+
 #include <math.h>
+#include <stdint.h>
+#include "libm.h"
+#include "exp2f_data.h"
+
+/*
+EXP2F_TABLE_BITS = 5
+EXP2F_POLY_ORDER = 3
+
+ULP error: 0.502 (nearest rounding.)
+Relative error: 1.69 * 2^-34 in [-ln2/64, ln2/64] (before rounding.)
+Wrong count: 170635 (all nearest rounding wrong results with fma.)
+Non-nearest ULP error: 1 (rounded ULP error)
+*/
+
+#define N (1 << EXP2F_TABLE_BITS)
+#define InvLn2N __exp2f_data.invln2_scaled
+#define T __exp2f_data.tab
+#define C __exp2f_data.poly_scaled
+
+static inline uint32_t top12(float x)
+{
+	return asuint(x) >> 20;
+}
 
 float expf(float x)
 {
-__ESBMC_HIDE:;
-    /**
-        Highly imprecise implementation of expf(x) on floating-point operands
-        which satisfies the following properties:
-        expf(x) >= 0
-        expf(x) >= 1 + x
-        expf(-Inf) = 0
-        expf(0) = 1
-        expf(+Inf) = Inf
-        dexpf(x=0)/dx = 1
-        logf(expf(x)) = x
-     */
+	uint32_t abstop;
+	uint64_t ki, t;
+	double_t kd, xd, z, r, r2, y, s;
 
-    // handle NaN inputs
-    if(isnan(x))
-        return x;
+	xd = (double_t)x;
+	abstop = top12(x) & 0x7ff;
+	if (predict_false(abstop >= top12(88.0f))) {
+		/* |x| >= 88 or x is nan.  */
+		if (asuint(x) == asuint(-INFINITY))
+			return 0.0f;
+		if (abstop >= top12(INFINITY))
+			return x + x;
+		if (x > 0x1.62e42ep6f) /* x > log(0x1p128) ~= 88.72 */
+			return __math_oflowf(0);
+		if (x < -0x1.9fe368p6f) /* x < log(0x1p-150) ~= -103.97 */
+			return __math_uflowf(0);
+	}
 
-    // handle infinities (x*x will overflow before x)
-    float xx = x * x;
-    if(isinf(xx)) {
-        if(x < 0.0f)
-            return 0.0f; // expf(-Inf) = 0
-        else
-            return 1.0f / 0.0f; // expf(+Inf) = Inf
-    }
+	/* x*N/Ln2 = k + r with r in [-1/2, 1/2] and int k.  */
+	z = InvLn2N * xd;
 
-    // inverse function of the logf approximation
-    // i.e. inverse((x - 1) / sqrt(x))
-    // it is numerically unstable for x < 0
-    // since it relies on x - sqrt(x^2 + 4) cancelling out
-    return 0.5 * x * (x + sqrtf(xx + 4.0f)) + 1.0f;
-}
+	/* Round and convert z to int, the result is in [-150*N, 128*N] and
+	   ideally ties-to-even rule is used, otherwise the magnitude of r
+	   can be bigger which gives larger approximation error.  */
+#if TOINT_INTRINSICS
+	kd = roundtoint(z);
+	ki = converttoint(z);
+#else
+# define SHIFT __exp2f_data.shift
+	kd = eval_as_double(z + SHIFT);
+	ki = asuint64(kd);
+	kd -= SHIFT;
+#endif
+	r = z - kd;
 
-float expm1f(float x)
-{
-__ESBMC_HIDE:;
-    return expf(x) - 1.0f;
+	/* exp(x) = 2^(k/N) * 2^(r/N) ~= s * (C0*r^3 + C1*r^2 + C2*r + 1) */
+	t = T[ki % N];
+	t += ki << (52 - EXP2F_TABLE_BITS);
+	s = asdouble(t);
+	z = C[0] * r + C[1];
+	r2 = r * r;
+	y = C[2] * r + 1;
+	y = z * r2 + y;
+	y = y * s;
+	return eval_as_float(y);
 }
