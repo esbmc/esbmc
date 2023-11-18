6f03ed2e6d43112e11ca682c01afc2788cd297eb
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2023-11-18 15:07:57 +0000

    [setjmp] added _setjmp OM

--- src/c2goto/library/setjmp.c
+++ src/c2goto/library/setjmp.c
@@ -7,6 +7,13 @@ int setjmp(jmp_buf __env)
   __ESBMC_unreachable();
 }
 
+// Due to some macro expansion some programs may have the _setjmp instead
+int _setjmp(jmp_buf __env)
+{
+  setjmp(__env);
+}
+
+
 void longjmp(jmp_buf env, int status)
 {
   __ESBMC_unreachable();
