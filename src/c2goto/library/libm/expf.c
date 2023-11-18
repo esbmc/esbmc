b116b3cb76a2ddf08ce8cc7c75e9586ec6768da1
Edoardo Manino <edoardo.manino@gmail.com>
2023-11-18 15:56:17 +0000

    [libm] Pade approximation of expm1f

--- src/c2goto/library/libm/expf.c
+++ src/c2goto/library/libm/expf.c
@@ -1,5 +1,7 @@
 #include <math.h>
 
+#define ALMOST_ONE 0.9999999f // necessary to ensure expm1f(x) >= x for x <= 0
+
 float expf(float x)
 {
 __ESBMC_HIDE:;
@@ -14,10 +16,22 @@ __ESBMC_HIDE:;
 float expm1f(float x)
 {
 __ESBMC_HIDE:;
-    
-	// unimplemented operational models
-    // should return VERIFICATION UNKNOWN
-    __ESBMC_unreachable();
 
-    return 0.0f;
+    /**
+        Highly imprecise implementation of expm1f(x) on floating-point operands
+        which satisfies the following properties:
+        expm1f(x) >= -1
+        expm1f(x) >= x
+        expm1f(-Inf) = -1
+        expm1f(0) = 0
+        expm1f(+Inf) = Inf
+        dexpm1f(x=0)/dx = 1
+     */
+
+    // handle positive infinity
+    if(x >= 1.0f)
+        return 1.0f / 0.0f;
+
+    // first-order Pade' approximant
+    return 1.0f / (1.0f - x) - ALMOST_ONE;
 }
