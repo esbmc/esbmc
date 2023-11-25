d24356940290bc4699eef7d6430dfeab47a8529f
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-11-25 10:08:22 +0100

    [util] cleanup language_util implementation somewhat

--- src/langapi/language_util.cpp
+++ src/langapi/language_util.cpp
@@ -1,6 +1,29 @@
 #include <langapi/language_util.h>
 #include <langapi/mode.h>
 #include <memory>
+#include <util/message.h>
+
+static int mode_from_symbol(const symbolt *symbol)
+{
+  if(!symbol)
+    return 0;
+
+  if(symbol->mode == "")
+    return 0;
+
+  if(int mode = get_mode(id2string(symbol->mode)); mode >= 0)
+    return mode;
+
+  log_error("symbol '{}' has unknown mode '{}'", symbol->name, symbol->mode);
+  abort();
+}
+
+static std::unique_ptr<languaget>
+language_from_symbol_id(const namespacet &ns, const irep_idt &id)
+{
+  int mode = id == "" ? 0 : mode_from_symbol(ns.lookup(id));
+  return std::unique_ptr<languaget>(mode_table[mode].new_language());
+}
 
 std::string from_expr(
   const namespacet &ns,
@@ -8,28 +31,7 @@ std::string from_expr(
   const exprt &expr,
   presentationt target)
 {
-  int mode;
-
-  if(identifier == "")
-    mode = 0;
-  else
-  {
-    const symbolt *symbol = ns.lookup(identifier);
-
-    if(!symbol)
-      mode = 0;
-    else if(symbol->mode == "")
-      mode = 0;
-    else
-    {
-      mode = get_mode(id2string(symbol->mode));
-      if(mode < 0)
-        throw "symbol " + id2string(symbol->name) + " has unknown mode '" +
-          id2string(symbol->mode) + "'";
-    }
-  }
-
-  std::unique_ptr<languaget> p(mode_table[mode].new_language());
+  std::unique_ptr<languaget> p = language_from_symbol_id(ns, identifier);
   std::string result;
   p->from_expr(expr, result, ns, target);
   return result;
@@ -41,28 +43,7 @@ std::string from_type(
   const typet &type,
   presentationt target)
 {
-  int mode;
-
-  if(identifier == "")
-    mode = 0;
-  else
-  {
-    const symbolt *symbol = ns.lookup(identifier);
-
-    if(!symbol)
-      mode = 0;
-    else if(symbol->mode == "")
-      mode = 0;
-    else
-    {
-      mode = get_mode(id2string(symbol->mode));
-      if(mode < 0)
-        throw "symbol " + id2string(symbol->name) + " has unknown mode '" +
-          id2string(symbol->mode) + "'";
-    }
-  }
-
-  std::unique_ptr<languaget> p(mode_table[mode].new_language());
+  std::unique_ptr<languaget> p = language_from_symbol_id(ns, identifier);
   std::string result;
   p->from_type(type, result, ns, target);
   return result;
