455792a65d7b7b72dd1f2fd989efd5fdf843aa5b
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-12-07 14:16:04 +0000

    [python-frontend] Symbol id generation in a single function

--- src/python-frontend/python_converter.h
+++ src/python-frontend/python_converter.h
@@ -30,8 +30,8 @@ private:
 
   const nlohmann::json
   find_var_decl(const std::string &var_name, const nlohmann::json &json);
-
   void adjust_statement_types(exprt &lhs, exprt &rhs) const;
+  std::string create_symbol_id() const;
 
   contextt &context;
   typet current_element_type;
