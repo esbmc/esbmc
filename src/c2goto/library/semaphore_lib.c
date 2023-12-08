2861fa91abdafdfc8b8f277d3a69f76fa106e418
XLiZHI <958689657@qq.com>
2023-12-08 18:13:50 +0800

    [OM] improve the model implementation

--- src/c2goto/library/semaphore_lib.c
+++ src/c2goto/library/semaphore_lib.c
@@ -1,4 +1,5 @@
 #include <semaphore.h>
+#include <limits.h>
 
 #define __ESBMC_sem_lock_field(a) ((a).__lock)
 #define __ESBMC_sem_count_field(a) ((a).__count)
@@ -10,7 +11,9 @@ int sem_init(sem_t *__sem, int __pshared, unsigned int __value)
 {
 __ESBMC_HIDE:;
   __ESBMC_atomic_begin();
-  __ESBMC_assert(!__pshared, "ESBMC does not check multiprocesses for now");
+  // ESBMC does not support shared memory at the moment
+  // we ignore the __pshared for now
+  __ESBMC_assert(__value <= INT_MAX, "Value has exceeded the maximum");
   __ESBMC_sem_lock_field(*__sem) = 0;
   __ESBMC_sem_count_field(*__sem) = __value;
   __ESBMC_sem_init_field(*__sem) = 1;
@@ -18,7 +21,7 @@ __ESBMC_HIDE:;
   return 0;
 }
 
-int sem_init_check(sem_t *__sem)
+static int sem_init_check(sem_t *__sem)
 {
   // check whether this sem has been initialised
   __ESBMC_atomic_begin();
@@ -42,19 +45,6 @@ __ESBMC_HIDE:;
   return 0;
 }
 
-int sem_wait_noassert(sem_t *__sem)
-{
-__ESBMC_HIDE:;
-  __ESBMC_atomic_begin();
-  sem_init_check(__sem);
-  __ESBMC_sem_count_field(*__sem) -= 1;
-  __ESBMC_assume(!__ESBMC_sem_lock_field(*__sem));
-  if (!__ESBMC_sem_count_field(*__sem))
-    __ESBMC_sem_lock_field(*__sem) = 1;
-  __ESBMC_atomic_end();
-  return 0;
-}
-
 int sem_wait_nocheck(sem_t *__sem)
 {
 __ESBMC_HIDE:;
@@ -68,31 +58,7 @@ __ESBMC_HIDE:;
   return 0;
 }
 
-int sem_post_check(sem_t *__sem)
-{
-__ESBMC_HIDE:;
-  __ESBMC_atomic_begin();
-  sem_init_check(__sem);
-  __ESBMC_sem_count_field(*__sem) += 1;
-  if (__ESBMC_sem_count_field(*__sem))
-    __ESBMC_sem_lock_field(*__sem) = 0;
-  __ESBMC_atomic_end();
-  return 0;
-}
-
-int sem_post_noassert(sem_t *__sem)
-{
-__ESBMC_HIDE:;
-  __ESBMC_atomic_begin();
-  sem_init_check(__sem);
-  __ESBMC_sem_count_field(*__sem) += 1;
-  if (__ESBMC_sem_count_field(*__sem))
-    __ESBMC_sem_lock_field(*__sem) = 0;
-  __ESBMC_atomic_end();
-  return 0;
-}
-
-int sem_post_nocheck(sem_t *__sem)
+int sem_post(sem_t *__sem)
 {
 __ESBMC_HIDE:;
   __ESBMC_atomic_begin();
