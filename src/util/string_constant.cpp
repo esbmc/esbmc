d7d209b05e8f0ce5ff048210105a246adbf8c2cb
Franz Brauße <franz.brausse@manchester.ac.uk>
2023-12-17 13:47:20 +0100

    [util] string constant: remove setting a buggy type - the correct one is already computed by the frontend

--- src/util/string_constant.cpp
+++ src/util/string_constant.cpp
@@ -19,14 +19,5 @@ string_constantt::string_constantt(const irep_idt &value, const typet &type)
 
 void string_constantt::set_value(const irep_idt &value)
 {
-  /* Fails for L"" and other large character types, because the below
-   * computation is buggy for those. See also #1165. */
-  assert(bv_width(type().subtype()) == config.ansi_c.char_width);
-
-  exprt size_expr = constant_exprt(
-    integer2binary(value.size() + 1, bv_width(size_type())),
-    integer2string(value.size() + 1),
-    size_type());
-  type().size(size_expr);
   exprt::value(value);
 }
