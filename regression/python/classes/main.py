50389155e08a93d5d096ad7de75eba67c8d6e39e
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-12-18 22:04:57 +0000

    [python-frontend] Add regression test

---
+++ regression/python/classes/main.py
@@ -0,0 +1,16 @@
+class MyClass:
+    def __init__(self, value: int):
+        self.data:int = value
+
+
+obj1 = MyClass(5)
+#assert(obj1.data == 5)
+obj1.data = 10
+assert(obj1.data == 10)
+obj1.data = 20
+assert(obj1.data == 20)
+
+obj2 = MyClass(30)
+# assert(obj2.data == 30)
+obj2.data = 40
+assert(obj2.data == 40)
