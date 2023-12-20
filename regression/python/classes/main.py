70c5a1d2c3cd7c164daaaf3a7b455620cc0a7613
Bruno Farias <brunocarvalhofarias@gmail.com>
2023-12-20 12:34:12 +0000

    [python-frontend] Fix constructor call and object member access

--- regression/python/classes/main.py
+++ regression/python/classes/main.py
@@ -4,13 +4,13 @@ class MyClass:
 
 
 obj1 = MyClass(5)
-#assert(obj1.data == 5)
+assert(obj1.data == 5)
 obj1.data = 10
 assert(obj1.data == 10)
 obj1.data = 20
 assert(obj1.data == 20)
 
 obj2 = MyClass(30)
-# assert(obj2.data == 30)
+assert(obj2.data == 30)
 obj2.data = 40
 assert(obj2.data == 40)
