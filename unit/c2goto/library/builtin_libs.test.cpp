54b8fdd5dcaa54c120ddd053fe836a2372e60d3a
Mikhail R. Gadelha <mikhail@igalia.com>
2023-12-23 14:24:05 -0400

    Missing header
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- unit/c2goto/library/builtin_libs.test.cpp
+++ unit/c2goto/library/builtin_libs.test.cpp
@@ -13,6 +13,8 @@
 #define CATCH_CONFIG_MAIN // This tells Catch to provide a main() - only do this in one cpp file
 #include <catch2/catch.hpp>
 
+#include <atomic>
+
 void __ESBMC_atomic_begin()
 {
 }
