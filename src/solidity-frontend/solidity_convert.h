52f70ff007b6bbb535bc71f811f56c57921ac204
hejueyun2 <hejueyun@gmail.com>
2023-12-30 03:13:12 +0000

    support struct member call && rewrite

--- src/solidity-frontend/solidity_convert.h
+++ src/solidity-frontend/solidity_convert.h
@@ -48,7 +48,7 @@ protected:
   std::string current_contractName;
   std::string current_fileName;
 
-  // store the ast_node["id"] of contract/struct/function/...
+  // an auxiliary data structure to store the ast_node["id"] of contract/struct/function/...
   std::unordered_map<int, std::string> scope_map;
 
   bool convert_ast_nodes(const nlohmann::json &contract_def);
