c6fa62723f08721b5a0994fccfbec4bbb9791a17
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-01-03 09:55:19 +0100

    [deps] update fmt to v10.2.0 fixing possible dangling reference
    The previous v9.1.0 resulted in these warnings when compiling with GCC 13.2.1:
    
    	In file included from debug/_deps/fmt-src/include/fmt/format.h:48,
    	                 from src/util/message.h:14,
    	                 from src/util/context.h:11,
    	                 from src/util/namespace.h:4,
    	                 from src/goto-symex/witnesses.h:2,
    	                 from src/goto-symex/witnesses.cpp:2:
    	debug/_deps/fmt-src/include/fmt/core.h: In instantiation of 'constexpr fmt::v9::detail::value<Context> fmt::v9::detail::make_value(T&&) [with Context = fmt::v9::basic_format_context<fmt::v9::appender, char>; T = BigInt&]':
    	debug/_deps/fmt-src/include/fmt/core.h:1777:29:   required from 'constexpr fmt::v9::detail::value<Context> fmt::v9::detail: make_arg(T&&) [with bool IS_PACKED = true; Context = fmt::v9::basic_format_context<fmt::v9::appender, char>; type <anonymous> = fmt::v9::detail::type::custom_type; T = BigInt&; typename std::enable_if<IS_PACKED, int>::type <anonymous> = 0]'
    	debug/_deps/fmt-src/include/fmt/core.h:1901:77:   required from 'constexpr fmt::v9::format_arg_store<Context, Args>::format_arg_store(T&& ...) [with T = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&, BigInt&}; Context = fmt::v9::basic_format_context<fmt::v9::appender, char>; Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, BigInt}]'
    	debug/_deps/fmt-src/include/fmt/core.h:1918:31:   required from 'constexpr fmt::v9::format_arg_store<Context, typename std::remove_cv<typename std::remove_reference<Args>::type>::type ...> fmt::v9::make_format_args(Args&& ...) [with Context = basic_format_context<appender, char>; Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >&, BigInt&}]'
    	src/util/message.h:73:52:   required from 'bool messaget::statet::logln(const char*, VerbosityLevel, const char*, int, fmt::v9::format_string<T ...>, Args&& ...) const [with Args = {std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, BigInt&}; fmt::v9::format_string<T ...> = fmt::v9::basic_format_string<char, std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> >, BigInt&>]'
    	src/goto-symex/slice.h:93:5:   required from here
    	debug/_deps/fmt-src/include/fmt/core.h:1735:15: warning: possibly dangling reference to a temporary [-Wdangling-reference]
    	 1735 |   const auto& arg = arg_mapper<Context>().map(FMT_FORWARD(val));
    	      |               ^~~
    	debug/_deps/fmt-src/include/fmt/core.h:1735:46: note: the temporary was destroyed at the end of the full expression 'fmt::v9::detail::arg_mapper<fmt::v9::basic_format_context<fmt::v9::appender, char> >().fmt::v9::detail::arg_mapper<fmt::v9::basic_format_context<fmt::v9::appender, char> >::map<BigInt&>((* & val))'
    	 1735 |   const auto& arg = arg_mapper<Context>().map(FMT_FORWARD(val));
    	      |                     ~~~~~~~~~~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~
    
    This was likely fixed already by fmt-v10.1.1:
    - Disallowed passing temporaries to `make_format_args` to improve API safety by preventing dangling references.

--- scripts/cmake/ExternalDependencies.cmake
+++ scripts/cmake/ExternalDependencies.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 # FMT
 FetchContent_Declare(fmt
   GIT_REPOSITORY https://github.com/fmtlib/fmt.git
-  GIT_TAG 9.1.0)
+  GIT_TAG 10.2.0)
 FetchContent_MakeAvailable(fmt)
 
 #nlohmann json
