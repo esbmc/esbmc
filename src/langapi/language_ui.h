a3219b9eadc448fd0fa316ea39124084442cebbe
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-01-05 07:52:40 +0100

    [langapi] language_uit: make move-ctor/-op protected because it is a base class

--- src/langapi/language_ui.h
+++ src/langapi/language_ui.h
@@ -15,11 +15,6 @@ public:
   language_uit();
   virtual ~language_uit() noexcept = default;
 
-  /* The instance of this class manages the global migrate_namespace_lookup,
-   * thus it cannot be copied. */
-  language_uit(language_uit &&) noexcept;
-  language_uit &operator=(language_uit &&) noexcept;
-
   virtual bool parse(const cmdlinet &cmdline);
   virtual bool parse(const std::string &filename);
   virtual bool typecheck();
@@ -33,6 +28,13 @@ public:
   virtual void show_symbol_table();
   virtual void show_symbol_table_plain(std::ostream &out);
   virtual void show_symbol_table_xml_ui();
+
+protected:
+  /* The instance of this class manages the global migrate_namespace_lookup,
+   * thus it cannot be copied. These functions are protected in order for
+   * derived classes to opt-into move support. */
+  language_uit(language_uit &&) noexcept;
+  language_uit &operator=(language_uit &&) noexcept;
 };
 
 #endif
