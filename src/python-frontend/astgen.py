8cea966d0536b8ec59518d59d209baf1fa32898a
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-02-13 15:26:58 +0000

    [python-frontend] Generate JSON files from imports

--- src/python-frontend/astgen.py
+++ src/python-frontend/astgen.py
@@ -4,6 +4,12 @@ import importlib.util
 import json
 import os
 
+def check_usage():
+    if len(sys.argv) != 3:
+        print("Usage: python astgen.py <file path> <output directory>")
+        sys.exit(2)
+
+
 def import_module_by_name(module_name):
     try:
         module = importlib.import_module(module_name)
@@ -14,15 +20,30 @@ def import_module_by_name(module_name):
         sys.exit(1)
 
 
-def check_usage():
-    if len(sys.argv) != 3:
-        print("Usage: python astgen.py <file path> <output directory>")
-        sys.exit(2)
+def process_imports(node, output_dir):
+    module_name = node.module
+    module = import_module_by_name(module_name)
+    filename = module.__file__
+    if filename.endswith('.pyc'):
+        filename = filename[:-1]  # Remove the 'c' at the end for .pyc files
+    generate_ast_json(filename, output_dir)
+
+
+def generate_ast_json(filename, output_dir):
+    with open(filename, "r") as source:
+        tree = ast.parse(source.read())
+
+    ast2json_module = import_module_by_name("ast2json")
+    ast_json = ast2json_module.ast2json(tree)
+    ast_json["filename"] = filename
+
+    json_filename = os.path.join(output_dir, f"{os.path.basename(filename)}.json")
+
+    with open(json_filename, "w") as json_file:
+        json.dump(ast_json, json_file, indent=4)
 
 
 def main():
-    ast2json_module = import_module_by_name("ast2json")
-
     check_usage()
     filename = sys.argv[1]
     output_dir = sys.argv[2]
@@ -33,16 +54,12 @@ def main():
     with open(filename, "r") as source:
         tree = ast.parse(source.read())
 
-    ast_json = ast2json_module.ast2json(tree)
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Import) or isinstance(node, ast.ImportFrom):
+            process_imports(node, output_dir)
 
-    # Add the filename to the JSON as ast2json does not include it automatically.
-    ast_json["filename"] = filename
-
-    json_filename = os.path.join(output_dir, "ast.json")
-
-    with open(json_filename, "w") as json_file:
-        json.dump(ast_json, json_file, indent=4)
-
+    generate_ast_json(filename, output_dir)
 
 if __name__ == "__main__":
     main()
+
