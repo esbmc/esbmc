3ece90867fc0804f09b7a0be1a4297a471620135
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-02-16 16:17:41 +0100

    [regression] regdb: allow complete formulas in QUERY

--- regression/regdb.py
+++ regression/regdb.py
@@ -7,6 +7,7 @@ import os.path
 import getopt
 import shlex
 import fnmatch
+import operator
 
 SOLVERS = [
     'smtlib',
@@ -59,35 +60,38 @@ FLAG_DESC = {}
 
 class Flag(str):
     def __new__(cls, s):
-        assert(len(s) > 0)
+        assert len(s) > 0
+        assert s[0] not in '+-', "invalid flag: '%s'" % s
+        assert all(c not in '()|&' for c in s), "invalid flag: '%s'" % s
         return super().__new__(cls, s)
 
     @property
-    def value(self):
-        s = super().__str__()
-        return s[s.find('=') + 1:]
+    def value(self) -> str:
+        return self[self.find('=') + 1:]
 
     @property
     def category(self):
-        s = super().__str__()
-        eq = s.find('=')
-        return None if eq < 0 else s[:eq]
+        eq = self.find('=')
+        return None if eq < 0 else self[:eq]
 
-    def matches(self, gpat_flag):
-        smatch = fnmatch.fnmatchcase
+    def matches(self, gpat_flag, smatch = fnmatch.fnmatchcase) -> bool:
         scat = self.category
         pcat = gpat_flag.category
         return ((pcat is None or scat is not None and smatch(scat, pcat)) and
                 smatch(self.value, gpat_flag.value))
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return "Flag(%s)" % super().__repr__()
 
-def list_flags(verbosity : int):
-    # collect
+def known_flags() -> set[Flag]:
     flgs = {BUG} | EXTENSIONS.keys()
     for v in OPT2FLAGS.values():
         flgs |= v
+    return {Flag(f) for f in flgs}
+
+def list_flags(verbosity : int) -> None:
+    # collect
+    flgs = known_flags()
     # output
     if verbosity > 0:
         max_w = max(len(f) for f in flgs)
@@ -96,15 +100,12 @@ def list_flags(verbosity : int):
     else:
         for f in sorted(flgs):
             print(f)
-    # finish
-    sys.exit(0)
 
-def flags(tc : TestCase):
+def flags(tc : TestCase) -> set[Flag]:
     r = set()
     if tc.test_mode in FAIL_MODES:
         r.add(Flag(BUG))
-    opts = tc.generate_run_argument_list('true')[1:]
-    for opt in opts:
+    for opt in tc.generate_run_argument_list('true')[1:]:
         f = set()
         if os.path.exists(opt) and os.path.isfile(os.path.realpath(opt)):
             # probably an input file
@@ -120,34 +121,97 @@ def flags(tc : TestCase):
         r |= f
     return r
 
-def query(qstr, tcs):
-    # parse query
-    pos = set()
-    neg = set()
-    for spec in qstr.split(' '):
-        if spec[0] == '-':
-            neg.add(Flag(spec[1:]))
-        else:
-            pos.add(Flag(spec[1:] if spec[0] == '+' else spec))
+class Predicate:
+    def __init__(self, fmt, op, *args):
+        self.op = op
+        self.args = args
+        self.fmt = fmt
 
-    # process TCs
-    def qualify(flgs):
-        def matches_any(gpat_flag):
-            return any(f.matches(gpat_flag) for f in flgs)
+    def __and__(self, other):
+        # Python operator '&'
+        return Predicate('(%s & %s)', operator.and_, self, other)
 
-        return (all(matches_any(p) for p in pos) and
-                all(not matches_any(n) for n in neg))
+    def __or__(self, other):
+        # Python operator '|'
+        return Predicate('(%s | %s)', operator.or_, self, other)
 
-    return [tc for tc in tcs if qualify(flags(tc))]
+    def __invert__(self):
+        # Python operator '~'
+        return Predicate('!%s', operator.not_, self)
 
-def usage():
+    def __call__(self, variables) -> bool:
+        # Evaluation
+        return self.op(*(arg(variables) for arg in self.args))
+
+    def __str__(self):
+        return self.fmt % tuple(str(arg) for arg in self.args)
+
+class GlobPattern(Predicate):
+    def __init__(self, qstr : str):
+        super().__init__(qstr.replace('%', '%%'), None)
+        self.gpat = Flag(qstr)
+
+    def __call__(self, flgs : set[Flag]) -> bool:
+        return any(f.matches(self.gpat) for f in flgs)
+
+def parse_formula(s : str, debug = False) -> Predicate:
+    def Factor(s):
+        if debug:
+            print("fin '%s'" % s)
+        if s[0] == '(':
+            t, a = Formula(s[1:].strip())
+            assert t and t[0] == ')', "expected ')', but got: '%s'" % t
+            return t[1:].strip(), a
+        if s[0] in '!-':
+            t, a = Factor(s[1:].strip())
+            return t, ~a
+        def efind(s, c):
+            idx = s.find(c)
+            return len(s) if idx < 0 else idx
+        term = min(efind(s, c) for c in ' ()!-&|')
+        if debug:
+            print("fin2 '%s', term: %d" % (s, term))
+        assert term != 0
+        t = s[term:].strip()
+        s = s[:term]
+        if debug:
+            print("fin3 s: '%s', t: '%s'" % (s, t))
+        return t, GlobPattern(s)
+
+    def Product(s):
+        if debug:
+            print("And '%s'" % s)
+        t, a = Factor(s)
+        while t and t[0] not in ')|':
+            if debug:
+                print("And2 '%s'" % t)
+            if t[0] == '&':
+                t = t[1:].strip()
+            t, b = Factor(t)
+            a = a & b
+        return t, a
+
+    def Formula(s):
+        if debug:
+            print("Or '%s'" % s)
+        t, a = Product(s)
+        while t and t[0] == '|':
+            t, b = Product(t[1:].strip())
+            a = a | b
+        return t, a
+
+    t, a = Formula(s.strip())
+    assert not t, "garbage after parsing formula: '%s'" % t
+    return a
+
+def usage() -> None:
     print('usage: %s [-OPTS] [--] [REG_PATH [REG_PATH [...]]]' %
           (sys.argv[0] if len(sys.argv) > 0 else 'regdb.py'))
     print('''
 Options:
   -h          display this help message
   -l          list known flags
-  -q QUERY    output only tests matching QUERY, multiple queries can be
+  -q QUERY    output only test cases satisfying QUERY, multiple queries can be
               performed and each will further restrict the list of results
   -v          increase verbosity (display flags for each test case or
               description for each flag), use twice to show shell-escaped
@@ -157,50 +221,55 @@ Multiple paths can optionally be specified as REG_PATH parameters, which will
 restrict the test cases to process to only those given. If none are specified,
 the default is to search the built-in list of all non-disabled regression tests.
 
-QUERY is a space-separated list of flag patterns of the form:
-  [+]GPAT : match only TCs that have a flag matching the glob pattern GPAT
-    -GPAT : match only TCs that do not have any flag matching GPAT
-All flag patterns are conjunctively connected.
+QUERY is an infix formula (! or - for negation, & or space for conjunction, |
+for disjunction) over glob patterns being matched against flags of test cases.
+QUERY has this grammar (with spaces allowed between any components):
+
+  Formula ::= Product ('|' Product)*
+  Product ::= Factor ('&'? Factor)*
+  Factor  ::= '(' Formula ')'
+            | ('!' | '-') Factor
+            | GlobPattern
 '''[:-1])
-    sys.exit(0)
 
 def main():
     try:
         opts, args = getopt.gnu_getopt(sys.argv[1:], 'hlq:v')
     except getopt.GetoptError as err:
-        print(err, file=sys.stderr)
-        sys.exit(1)
+        return 'error: %s' % err
 
     verbosity = len(tuple(opt for opt, val in opts if opt == '-v'))
 
     if ('-h', '') in opts:
-        usage()
+        return usage()
 
     if ('-l', '') in opts:
-        list_flags(verbosity)
+        return list_flags(verbosity)
 
     tcs = []
     if args:
-        for arg in args:
-            tcs.append(TestCase(arg, os.path.basename(arg)))
+        tcs.extend(TestCase(arg, os.path.basename(arg)) for arg in args)
     else:
         apply_transform_over_tests(tcs.append)
 
-    combined_query = ' '.join(val for opt, val in opts if opt == '-q').strip()
-    if combined_query:
-        tcs = query(combined_query, tcs)
+    queries = [parse_formula(val) for opt, val in opts if opt == '-q']
+    if queries:
+        q = queries[0]
+        for i in range(1, len(queries)):
+            q = q & queries[i]
+        tcs = [tc for tc in tcs if q(flags(tc))]
 
     if len(tcs) == 0 or verbosity == 0:
         # just directory
         for tc in tcs:
             print(tc.test_dir)
     else:
+        # directory + flags or cmdline options
         max_w = max(len(tc.test_dir) for tc in tcs)
         for tc in tcs:
-            # directory + flags or directory + cmdline options
             value = (' '.join(sorted(flags(tc))) if verbosity == 1 else
                      shlex.join(tc.generate_run_argument_list('true')[1:]))
             print('%*s: %s' % (-max_w, tc.test_dir, value))
 
 if __name__ == '__main__':
-    main()
+    sys.exit(main())
