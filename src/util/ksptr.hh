6ef990636fab22e9f6f42a649d54cbf10c2d4da9
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-03-01 12:15:26 +0100

    [util] document ksptr::sptr

--- src/util/ksptr.hh
+++ src/util/ksptr.hh
@@ -6,9 +6,19 @@
 
 namespace ksptr
 {
+/* A type that is large enough to count any number of copies existing in memory
+ * (even on Windows) */
 typedef std::conditional_t<sizeof(void *) >= sizeof(long), long long, long>
   cnt_t;
 
+/**
+ * A somewhat dumbed-down shared_ptr implementation, still missing weak pointers
+ * and reference/array support. The purpose of this class is to provide
+ * shared pointers that
+ * (a) do not have an atomic reference count, and
+ * (b) do not provide for separate storage of control block and actual data,
+ *     which allows to reduce space.
+ */
 template <typename T>
 class sptr
 {
@@ -22,11 +32,11 @@ class sptr
   friend sptr<S> make_shared(Args &&...args);
 
 protected:
-  struct make
+  struct make /* tag type to select correct constructor */
   {
   };
 
-  struct control_block
+  struct control_block /* meta-data together with the concrete data */
   {
     cnt_t use_count;
     void (*dtor)(void *);
@@ -57,20 +67,26 @@ protected:
   }
 
 public:
+  /* constructor and destructor */
+
+  /* default */
   constexpr sptr() noexcept : cb(nullptr)
   {
   }
 
+  /* copy */
   constexpr sptr(const sptr &o) noexcept : cb(o.cb)
   {
     if (cb)
       cb->use_count++;
   }
 
+  /* move */
   constexpr sptr(sptr &&o) noexcept : cb(std::exchange(o.cb, nullptr))
   {
   }
 
+  /* type-cast from an sptr of type convertible to T */
   template <
     typename S,
     typename = std::enable_if_t<std::is_convertible_v<S *, T *>>>
@@ -85,6 +101,8 @@ public:
     if (cb && !--cb->use_count)
       delete cb;
   }
+
+  /* swap & assignment operators */
 
   void swap(sptr &b) noexcept
   {
@@ -102,6 +120,8 @@ public:
     return *this;
   }
 
+  /* observers */
+
   cnt_t use_count() const noexcept
   {
     return cb ? cb->use_count : 0;
@@ -111,6 +131,7 @@ public:
   {
     return *get();
   }
+
   T *operator->() const noexcept
   {
     return get();
@@ -125,6 +146,8 @@ public:
   {
     return cb != nullptr;
   }
+
+  /* mutators */
 
   void reset() noexcept
   {
