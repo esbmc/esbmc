d15d9344f24ab2e406a7bc99933c110a889d0e17
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-03-02 18:52:55 +0000

    [python-frontend] Add support for float numbers

---
+++ regression/python/float/main.py
@@ -0,0 +1,15 @@
+num1 = 1.5
+num2 = 3.0
+
+add = num1 + num2
+assert add == 4.5
+
+sub = num2 - num1
+assert sub == 1.5
+
+mul = num2 * num1
+assert mul == 4.5
+
+div = num2 / num1
+assert div == 2.0
+
