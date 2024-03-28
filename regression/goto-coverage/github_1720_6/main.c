3231768d663e021056209ea9644ee848efd56bcc
hejueyun2 <hejueyun@gmail.com>
2024-03-28 15:55:17 +0000

    [code coverage] algo: update the target num

---
+++ regression/goto-coverage/github_1720_6/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdbool.h>
+
+bool func() { return notdet_bool(); }
+
+int main()
+{
+  bool a = nondet_bool();
+  bool b = true;
+  if (b && (func() || a) && b)
+  {
+    assert(1);
+  }
+  else
+    assert(0);
+}
