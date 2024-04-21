2837bc8576d40aadd420541ac298f53b9a8aa5ea
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-04-21 07:14:37 +0200

    [util] simplify has_(pre|suf)fix()

--- src/util/prefix.h
+++ src/util/prefix.h
@@ -6,14 +6,14 @@
 inline bool has_prefix(std::string_view s, std::string_view prefix)
 {
   size_t n = prefix.size();
-  return s.size() >= n && std::string_view(s.data(), n) == prefix;
+  return s.size() >= n && s.substr(0, n) == prefix;
 }
 
 inline bool has_suffix(std::string_view s, std::string_view suffix)
 {
   size_t n = suffix.size();
   size_t m = s.size();
-  return m >= n && std::string_view(s.data() + (m - n), n) == suffix;
+  return m >= n && s.substr(m - n) == suffix;
 }
 
 #endif
