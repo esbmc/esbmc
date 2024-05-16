jr26271/Finding-

adding support for cvc5

src/esbmc/options.cpp
@@ -179,7 +179,7 @@ const struct group_opt_templ all_cmd_options[] = {
     {"boolector", NULL, "use Boolector (default),"},
     {"z3", NULL, "use Z3"},
     {"mathsat", NULL, "use MathSAT"},
-    {"cvc", NULL, "use CVC4"},
+    {"cvc", NULL, "use CVC5"},
     {"yices", NULL, "use Yices"},
     {"bitwuzla", NULL, "use Bitwuzla"},
     {"bv", NULL, "use solver with bit-vector arithmetic"},

esbmc/esbmc

src/esbmc/options.cpp
@@ -229,6 +229,8 @@ const struct group_opt_templ all_cmd_options[] = {
      "name for Z3 smt2 file"},
     {"mathsat", NULL, "use MathSAT"},
     {"cvc", NULL, "use CVC4"},
+    {"cvc4", NULL, "use CVC4"},
+    {"cvc5", NULL, "use CVC5"},
     {"yices", NULL, "use Yices"},
     {"bitwuzla", NULL, "use Bitwuzla"},
     {"bv", NULL, "use solver with bit-vector arithmetic"},