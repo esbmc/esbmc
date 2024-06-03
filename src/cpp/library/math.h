149d53693a28c11d64b11515ba40d74aa89c0cc8
Franz Brauße <franz.brausse@manchester.ac.uk>
2024-06-03 16:35:02 +0200

    clang-format

--- src/cpp/library/math.h
+++ src/cpp/library/math.h
@@ -6,16 +6,16 @@
 /* Support LLVM's libc++, which defines the float and long double overloads
  * in the top-level :: namespace in <math.h> when __cplusplus is set. These
  * clash with our definitions due to the exception declaration. */
-# pragma push_macro("__cplusplus")
-# undef __cplusplus
+#pragma push_macro("__cplusplus")
+#undef __cplusplus
 extern "C"
 {
-# include_next <math.h>
+#include_next <math.h>
 }
-# pragma pop_macro("__cplusplus") // restore macro
+#pragma pop_macro("__cplusplus") // restore macro
 
 #else
 
-# include_next <math.h>
+#include_next <math.h>
 
 #endif
