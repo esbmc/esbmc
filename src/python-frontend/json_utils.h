d646344e187c53fb1f7fddbfbcb8b8e7c08895f8
Ivo Machado <ivo.machado@outlook.com.br>
2024-07-04 15:07:03 -0400

    [python-frontend] Format with ClangFormat11

--- src/python-frontend/json_utils.h
+++ src/python-frontend/json_utils.h
@@ -8,11 +8,8 @@ namespace json_utils
 template <typename JsonType>
 JsonType find_class(const JsonType &ast_json, const std::string &class_name)
 {
-  auto it = std::find_if(
-    ast_json.begin(),
-    ast_json.end(),
-    [&](const JsonType &obj)
-    {
+  auto it =
+    std::find_if(ast_json.begin(), ast_json.end(), [&](const JsonType &obj) {
       return obj.contains("_type") && obj["_type"] == "ClassDef" &&
              obj["name"] == class_name;
     });
@@ -30,8 +27,8 @@ bool is_class(const std::string &name, const JsonType &ast_json)
   // Find class definition in imported modules
   for (const auto &obj : ast_json["body"])
   {
-    auto is_imported_class = [&ast_json, &name](const std::string &module_name)
-    {
+    auto is_imported_class = [&ast_json,
+                              &name](const std::string &module_name) {
       std::stringstream module_path;
       module_path << ast_json["ast_output_dir"].template get<std::string>()
                   << "/" << module_name << ".json";
