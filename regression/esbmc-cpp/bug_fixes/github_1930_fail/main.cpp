3ddd858be5324c5cef06718c5c4b423e77306f46
XLiZHI <958689657@qq.com>
2024-07-10 13:53:09 +0100

    [regression] added TCs for if string type

---
+++ regression/esbmc-cpp/bug_fixes/github_1930_fail/main.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <cassert>
+
+using namespace std;
+
+int main()
+{
+  for (int i = 0; i < 2; ++i)
+  {
+    string expectedSound = (i == 0) ? "Woof!" : "Meow!";
+    if (i == 0)
+      assert(expectedSound == "Woof!");
+    else
+      assert(expectedSound == "Woof!"); // Meow!
+  }
+
+  return 0;
+}
