0f8ddbf0664f8ca74efe821085feaa94a033e522
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-07-26 16:17:16 +0100

    [regression] Remove explicit type info

--- regression/python/binary-ops/main.py
+++ regression/python/binary-ops/main.py
@@ -1,14 +1,14 @@
-add: int = 1 + 0
-sub: int = 1 - 0
-mul: int = 1 * 0
-div: int = 1 / 1
-idiv:int = -5 // 2
+add = 1 + 0
+sub = 1 - 0
+mul = 1 * 0
+div = 1 / 1
+idiv = -5 // 2
 assert(idiv == -3)
 idiv = 5//2
 assert(idiv == 2)
-mod: int = 2 % 2
-bitor: int = 1 | 1
-bitand: int = 1 & 0
-bitxor: int = 3 ^ 1
-bitlsh: int = 2 << 1
-bitrsh: int = 2 >> 1
+mod = 2 % 2
+bitor = 1 | 1
+bitand = 1 & 0
+bitxor = 3 ^ 1
+bitlsh = 2 << 1
+bitrsh = 2 >> 1
