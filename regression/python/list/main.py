a160bdf968eb497cfa85b976c08092ff9a929283
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-07-28 14:31:29 +0100

    [python-frontend] Add regression test

---
+++ regression/python/list/main.py
@@ -0,0 +1,12 @@
+l1 = [1,2,3]
+assert l1[0] == 1
+assert l1[1] == 2
+assert l1[2] == 3
+
+l2 = [True, False]
+assert l2[0] == True
+assert l2[1] == False
+
+l3 = [1.2, 2.5]
+assert l3[0] == 1.2
+assert l3[1] == 2.5
