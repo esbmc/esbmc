7c8d07fb68214ceb84affeb756b90238a3d44e39
intrigus <abc123zeus@live.de>
2024-08-17 23:19:31 +0200

    [clang-cpp] add overloads to tests

--- regression/esbmc-cpp/polymorphism_bringup_overload/llbmc_multiple_inheritance/main.cpp
+++ regression/esbmc-cpp/polymorphism_bringup_overload/llbmc_multiple_inheritance/main.cpp
@@ -6,30 +6,66 @@
 class Base1
 {
 public:
-    virtual ~Base1() {}
-    virtual int f(void) { return 21; }
+  virtual ~Base1()
+  {
+  }
+  virtual int f(void)
+  {
+    return 21;
+  }
+  virtual int f(int)
+  {
+    return 22;
+  }
 };
 
 class Base2
 {
 public:
-    virtual ~Base2() {}
-    virtual int g(void) { return 21; }
+  virtual ~Base2()
+  {
+  }
+  virtual int g(void)
+  {
+    return 21;
+  }
+  virtual int g(int)
+  {
+    return 22;
+  }
 };
 
-class Derived: public Base1, public Base2
+class Derived : public Base1, public Base2
 {
 public:
-    virtual ~Derived() {}
-    virtual int f(void) { return 42; }
-    virtual int g(void) { return 42; }
+  virtual ~Derived()
+  {
+  }
+  virtual int f(void)
+  {
+    return 42;
+  }
+  virtual int f(int)
+  {
+    return 43;
+  }
+  virtual int g(void)
+  {
+    return 42;
+  }
+  virtual int g(int)
+  {
+    return 43;
+  }
 };
 
 int main()
 {
-    Base2 *o = new Derived();
-    int r = o->g();
-    delete o;
-    assert(r == 42);
-    return r;
+  Base2 *o = new Derived();
+  int r = o->g();
+  int s = o->g(1);
+  delete o;
+  assert(r == 42);
+  assert(s == 43);
+  return r + s;
 }
