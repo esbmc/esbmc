43aced99f45b22ffe9ce0604ece00f3a39e2e56d
intrigus <abc123zeus@live.de>
2024-08-17 23:19:31 +0200

    [clang-cpp] add overloads to tests

--- regression/esbmc-cpp/polymorphism_bringup_overload/2_SI_virtual_tvalCtor_fail/main.cpp
+++ regression/esbmc-cpp/polymorphism_bringup_overload/2_SI_virtual_tvalCtor_fail/main.cpp
@@ -6,23 +6,40 @@
  */
 #include <cassert>
 
-class Bird {
-  public:
-  virtual int do_something(void) { return 21; }
+class Bird
+{
+public:
+  virtual int do_something(void)
+  {
+    return 21;
+  }
+  virtual int do_something(int)
+  {
+    return 22;
+  }
 };
 
-class Penguin: public Bird {
-  public:
-    virtual int do_something(void) { return 42; }
+class Penguin : public Bird
+{
+public:
+  virtual int do_something(void)
+  {
+    return 42;
+  }
+  virtual int do_something(int)
+  {
+    return 43;
+  }
 };
 
-int main(){
+int main()
+{
   Bird *b = new Bird();
   Bird *p = new Penguin();
   assert(b->do_something() == 21);
+  assert(b->do_something(1) == 22);
   assert(p->do_something() == 21); // should be 42
   delete b;
   delete p;
   return 0;
 }
-
