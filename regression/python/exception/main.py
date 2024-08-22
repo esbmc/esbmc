e5f1b36af5c0f24446955e31e7c31e59c1ac6f45
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-08-22 23:29:33 +0100

    [python-frontend] Add regression test

---
+++ regression/python/exception/main.py
@@ -0,0 +1,15 @@
+def foo(value:int) -> int:
+    if value < 0:
+        raise ValueError("Negative value!")
+
+    return value * 2
+
+
+result = 1
+
+try:
+  result = foo(-1)
+except ValueError as e:
+  print(e)
+
+assert result == 1
