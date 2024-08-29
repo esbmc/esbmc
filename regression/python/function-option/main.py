adbb80e53b10ea4cab7626e45fa721ac72db4906
Bruno Farias <brunocarvalhofarias@gmail.com>
2024-08-29 16:11:31 +0100

    [python-frontend] Improving tests

--- regression/python/function-option/main.py
+++ regression/python/function-option/main.py
@@ -1,7 +1,9 @@
-def ok() -> None:
-    assert(1)
+def foo() -> None:
+    pass
 
-def fail() -> None:
-    assert(0)
-
-ok()
+def bar() -> None:
+    foo()
+    x = int(5)
+    bytes_data = b'\x00\x10'
+    y:int = int.from_bytes(bytes_data, 'big', False)
+    z = x.bit_length();
