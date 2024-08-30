a94e4449a2f90a4d093dc14b9234479265aa11ca
Goblin57 <joshua.hines57@gmail.com>
2024-08-30 11:27:52 +0100

    minor symex assign html report change

--- src/goto-symex/symex_function.cpp
+++ src/goto-symex/symex_function.cpp
@@ -117,12 +117,7 @@ unsigned goto_symext::argument_assignments(
 
       // Assign value to function argument
       // TODO: Should we hide it (true means hidden)?
-      if (options.get_bool_option("generate-html-report")) {
-        symex_assign(code_assign2tc(lhs, rhs), false);
-      }
-      else {
-        symex_assign(code_assign2tc(lhs, rhs), true);
-      }
+      symex_assign(code_assign2tc(lhs, rhs), !options.get_bool_option("generate-html-report"));
     }
   }
 
