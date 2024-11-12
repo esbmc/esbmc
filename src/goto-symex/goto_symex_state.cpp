61ee8b670fd8cfac77daba31e68d4e6bb4655c50
XLiZHI <XLiZHI@users.noreply.github.com>
2024-11-12 16:21:39 +0000

    Apply automatic changes

--- src/goto-symex/goto_symex_state.cpp
+++ src/goto-symex/goto_symex_state.cpp
@@ -139,7 +139,7 @@ bool goto_symex_statet::constant_propagation(const expr2tc &expr) const
       is_symbol2t(with.source_value) && is_struct_type(with.source_value) &&
       is_constant_expr(with.update_value))
       return true;
-      
+
     return false;
   }
 
