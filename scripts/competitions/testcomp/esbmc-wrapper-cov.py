0faa543c9928dc998bb0f7fd386a1651415968bf
hejueyun2 <hejueyun@gmail.com>
2024-11-15 13:57:27 +0000

    [test-comp] update esbmc-wrapper-cov

--- scripts/competitions/testcomp/esbmc-wrapper-cov.py
+++ scripts/competitions/testcomp/esbmc-wrapper-cov.py
@@ -190,7 +190,7 @@ def get_command_line(strat, prop, arch, benchmark, concurrency, dargs, coverage)
 
   # Special case for termination, it runs regardless of the strategy
   if prop == Property.coverage:
-    command_line += "--goto-unwind --base-k-step 5 --no-bounds-check --no-pointer-check --quiet --no-standard-checks "
+    command_line += "--unlimited-goto-unwind --base-k-step 5 --quiet --no-standard-checks "
     command_line += "--generate-testcase "
     if coverage == "branch":
       command_line += "--branch-coverage "
