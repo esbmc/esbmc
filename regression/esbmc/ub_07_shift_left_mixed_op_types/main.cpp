907ee4ac505338888299a5f702fc9a883d8f7cc1
intrigus <abc123zeus@live.de>
2024-11-26 17:58:01 +0100

    [TCs] Check ub-shift-check for different widths and signedness

---
+++ regression/esbmc/ub_07_shift_left_mixed_op_types/main.cpp
@@ -0,0 +1,41 @@
+#include <assert.h>
+
+int main()
+{
+  long onel = 1L;
+  unsigned long onelu = 1ul;
+  int onei = 1;
+  unsigned int oneiu = 1u;
+  auto result_i_i = 1 << onei;
+  assert(result_i_i == 2);
+  auto result_i_iu = 1 << oneiu;
+  assert(result_i_iu == 2);
+  auto result_i_l = 1 << onel;
+  assert(result_i_l == 2);
+  auto result_i_lu = 1 << onelu;
+  assert(result_i_lu == 2);
+  auto result_iu_i = 1u << onei;
+  assert(result_iu_i == 2u);
+  auto result_iu_iu = 1u << oneiu;
+  assert(result_iu_iu == 2u);
+  auto result_iu_l = 1u << onel;
+  assert(result_iu_l == 2u);
+  auto result_iu_lu = 1u << onelu;
+  assert(result_iu_lu == 2u);
+  auto result_l_i = 1l << onei;
+  assert(result_l_i == 2l);
+  auto result_l_iu = 1l << oneiu;
+  assert(result_l_iu == 2l);
+  auto result_l_l = 1l << onel;
+  assert(result_l_l == 2l);
+  auto result_l_lu = 1l << onelu;
+  assert(result_l_lu == 2l);
+  auto result_lu_i = 1lu << onei;
+  assert(result_lu_i == 2lu);
+  auto result_lu_iu = 1lu << oneiu;
+  assert(result_lu_iu == 2lu);
+  auto result_lu_l = 1lu << onel;
+  assert(result_lu_l == 2lu);
+  auto result_lu_lu = 1lu << onelu;
+  assert(result_lu_lu == 2lu);
+}
---
+++ regression/esbmc/ub_08_shift_left_mixed_op_types_fail/main.cpp
@@ -0,0 +1,25 @@
+#include <assert.h>
+
+int main()
+{
+  long onel = 64L;
+  unsigned long onelu = 64ul;
+  int onei = 64;
+  unsigned int oneiu = 64u;
+  auto result_i_i = 1 << onei;
+  auto result_i_iu = 1 << oneiu;
+  auto result_i_l = 1 << onel;
+  auto result_i_lu = 1 << onelu;
+  auto result_iu_i = 1u << onei;
+  auto result_iu_iu = 1u << oneiu;
+  auto result_iu_l = 1u << onel;
+  auto result_iu_lu = 1u << onelu;
+  auto result_l_i = 1l << onei;
+  auto result_l_iu = 1l << oneiu;
+  auto result_l_l = 1l << onel;
+  auto result_l_lu = 1l << onelu;
+  auto result_lu_i = 1lu << onei;
+  auto result_lu_iu = 1lu << oneiu;
+  auto result_lu_l = 1lu << onel;
+  auto result_lu_lu = 1lu << onelu;
+}
