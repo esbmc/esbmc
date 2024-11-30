14f42182c6870f4bf0b923bfcd29f48615b25995
intrigus <abc123zeus@live.de>
2024-11-30 12:00:41 +0100

    [TCs] highlight bug in existing lambda captures TCs

--- regression/esbmc-cpp11/lambda/lambda_01_fail/main.cpp
+++ regression/esbmc-cpp11/lambda/lambda_01_fail/main.cpp
@@ -5,35 +5,37 @@ int main()
   int x = 42;
   int y = 1;
 
-  auto lambda_with_capture_1 = [=]()
-  {
+  auto lambda_with_capture_1 = [=]() {
     assert(x == 1); //fail
     assert(y == 1);
   };
 
-  auto lambda_with_capture_2 = [x, y]()
-  {
+  auto lambda_with_capture_2 = [x, y]() {
     assert(x == 42);
     assert(y == 0); //fail
   };
 
-  auto lambda_with_capture_3 = [&]()
-  {
+  auto lambda_with_capture_3 = [&]() {
     y++;
     assert(x == 42);
     assert(y == 2);
   };
 
-  auto lambda_with_capture_4 = [&x, &y]()
-  {
+  auto lambda_with_capture_4 = [&x, &y]() {
     y++;
     assert(x == 42);
     assert(y == 3);
   };
 
   // Call the lambda
+  // The first two lambdas capture by value, so we should be able to freely modify x and y
+  x = 11;
+  y = 11;
   lambda_with_capture_1();
   lambda_with_capture_2();
+  // The last two lambdas capture by reference, so change x and y back
+  x = 42;
+  y = 1;
   lambda_with_capture_3();
   lambda_with_capture_4();
 
