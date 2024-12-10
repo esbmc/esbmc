50b7919397c5e01dbe0cb83a43250b5f01e5fc7f
mikhailramalho <mikhailramalho@users.noreply.github.com>
2024-12-10 23:05:20 +0000

    Apply automatic changes

--- src/util/breakpoint.h
+++ src/util/breakpoint.h
@@ -3,12 +3,12 @@
 static void breakpoint()
 {
 #ifndef _WIN32
-#if !(defined(__arm__) || defined(__aarch64__))
+#  if !(defined(__arm__) || defined(__aarch64__))
   __asm__("int $3");
-#else
+#  else
   log_error("Can't trap on ARM, sorry");
   abort();
-#endif
+#  endif
 #else
   log_error("Can't trap on windows, sorry");
   abort();
