b02c58c92a7ce56606b26ab61b4c3cc40a960783
Mikhail R. Gadelha <mikhail@igalia.com>
2024-12-10 19:34:10 -0300

    No need to guard all kind parallel code
    Signed-off-by: Mikhail R. Gadelha <mikhail@igalia.com>

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -628,7 +628,8 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
 #ifdef _WIN32
   log_error("Windows does not support parallel kind");
   abort();
-#else
+#endif
+
   // Pipes for communication between processes
   int forward_pipe[2], backward_pipe[2];
 
@@ -1189,8 +1190,6 @@ int esbmc_parseoptionst::doit_k_induction_parallel()
   default:
     assert(0 && "Unknown process type.");
   }
-
-#endif
 
   return 0;
 }
