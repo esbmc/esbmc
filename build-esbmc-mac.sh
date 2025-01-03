8b6df7dd9b0a07dba55c72c147c34749d6770dc7
Shivkumar Shivaji <sshivaji@gmail.com>
2025-01-03 12:47:26 -0800

    Added python frontend support

--- build-esbmc-mac.sh
+++ build-esbmc-mac.sh
@@ -9,7 +9,10 @@ if ! command -v brew &> /dev/null; then
     exit 1
 fi
 
-# Ask about Boolector right at the start (Y/yes is default)
+# Ask about Python frontend and Boolector right at the start (Y/yes is default for both)
+read -p "Do you want to build the Python frontend? [Y/n]: " use_python
+use_python=${use_python:-Y}  # Default to Y if user just hits enter
+
 read -p "Do you want to install the recommended Boolector solver? [Y/n]: " use_boolector
 use_boolector=${use_boolector:-Y}  # Default to Y if user just hits enter
 
@@ -20,6 +23,15 @@ cd build
 
 echo "Installing ESBMC dependencies..."
 brew install z3 bison clang llvm
+
+# Set up Python environment if requested
+if [[ $use_python =~ ^[Yy]$ ]]; then
+    echo "Setting up Python environment..."
+    python3 -m venv ../esbmc-venv
+    source ../esbmc-venv/bin/activate
+    pip install ast2json
+    deactivate
+fi
 
 # Get number of CPUs and add 1
 CPU_COUNT=$(($(sysctl -n hw.ncpu) + 1))
@@ -44,26 +56,34 @@ install_boolector() {
     cd ../../build
 }
 
+# Build configuration
+CMAKE_ARGS=(
+    -DCMAKE_PREFIX_PATH="$PATH_LLVM"
+    -DENABLE_Z3=1
+    -DZ3_DIR="$PATH_Z3"
+    -DC2GOTO_SYSROOT="$PATH_SDK"
+    -DClang_DIR="$PATH_LLVM/lib/cmake/clang"
+)
+
+# Add Python frontend if requested
+if [[ $use_python =~ ^[Yy]$ ]]; then
+    CMAKE_ARGS+=(-DENABLE_PYTHON_FRONTEND=On)
+fi
+
+# Add Boolector if requested
 if [[ $use_boolector =~ ^[Yy]$ ]]; then
     install_boolector
     echo "Building ESBMC with Boolector..."
-    cmake .. \
-        -DCMAKE_PREFIX_PATH="$PATH_LLVM" \
-        -DENABLE_Z3=1 \
-        -DZ3_DIR="$PATH_Z3" \
-        -DENABLE_BOOLECTOR=1 \
-        -DBoolector_DIR="$PWD/../boolector-release" \
-        -DC2GOTO_SYSROOT="$PATH_SDK" \
-        -DClang_DIR="$PATH_LLVM/lib/cmake/clang"
+    CMAKE_ARGS+=(
+        -DENABLE_BOOLECTOR=1
+        -DBoolector_DIR="$PWD/../boolector-release"
+    )
 else
     echo "Building ESBMC with Z3 only..."
-    cmake .. \
-        -DCMAKE_PREFIX_PATH="$PATH_LLVM" \
-        -DENABLE_Z3=1 \
-        -DZ3_DIR="$PATH_Z3" \
-        -DC2GOTO_SYSROOT="$PATH_SDK" \
-        -DClang_DIR="$PATH_LLVM/lib/cmake/clang"
 fi
+
+# Run cmake with all arguments
+cmake .. "${CMAKE_ARGS[@]}"
 
 echo "Running make..."
 make -j${CPU_COUNT}
@@ -72,3 +92,10 @@ echo "Installing ESBMC system-wide (requires sudo permission)..."
 sudo make install
 
 echo "Build and installation complete! You can now run 'esbmc' from anywhere."
+
+# Print additional instructions for Python frontend if it was installed
+if [[ $use_python =~ ^[Yy]$ ]]; then
+    echo -e "\nTo use the Python frontend, you need to activate the virtual environment:"
+    echo "source esbmc-venv/bin/activate"
+    echo "Then you can use ESBMC with Python files"
+fi
