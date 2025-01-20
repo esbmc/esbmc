95e6ef6c2543af8af718c9993d35c2e36a0c9361
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-01-20 12:53:27 +0000

    Add documentation

--- src/python-frontend/function_call_expr.h
+++ src/python-frontend/function_call_expr.h
@@ -19,23 +19,50 @@ class function_call_expr
 public:
   function_call_expr(const nlohmann::json &call, python_converter &converter);
 
+  /*
+   * Converts the function from the AST into an exprt.
+   */
   exprt build();
 
 private:
+  /*
+   * Extracts information from the call to populate the function_id attribute.
+   */
   void build_function_id();
 
+  /**
+   * Determines whether a non-deterministic function is being invoked.
+   */
   bool is_nondet_call() const;
 
+  /*
+   * Checks if assume (__ESBMC_assume or __VERIFIER_assume) function is being invoked.
+   */
   bool is_assume_call() const;
 
+  /*
+   * Checks if the Python len() function is being invoked.
+   */
   bool is_len_call() const;
 
+  /*
+   * Creates an expression for a non-deterministic function call.
+   */
   exprt build_nondet_call() const;
 
+  /*
+   * Creates a constant expression from function argument.
+   */
   exprt build_constant_from_arg() const;
 
+  /*
+   * Sets the function_type_ attribute based on the call information.
+   */
   void get_function_type();
 
+  /*
+   * Retrieves the object (caller) name from the AST.
+   */
   std::string get_object_name() const;
 
   const nlohmann::json &call_;
