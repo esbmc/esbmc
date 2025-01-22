0a958561bbe546e2b09d6ea044d2b2ec2332a6b8
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-01-22 14:53:13 +0000

    [OM] Ensure pointers are non-null
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/c2goto/library/string.c
+++ src/c2goto/library/string.c
@@ -27,8 +27,9 @@ char *strcpy(char *dst, const char *src)
 {
 __ESBMC_HIDE:;
   // Ensure pointers are non-null
-  __ESBMC_assume(dst != NULL && src != NULL);
-
+  if (dst != NULL && src != NULL);
+    return NULL;
+    
   // Constant propagation-friendly loop
   for (size_t i = 0;; ++i)
   {
