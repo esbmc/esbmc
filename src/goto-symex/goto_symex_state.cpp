2e7b8a4cf2cbc2d3a3462b7498b19795f93a4de6
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-01-23 10:28:57 +0000

    [symex] propgate constants for array operations
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/goto-symex/goto_symex_state.cpp
+++ src/goto-symex/goto_symex_state.cpp
@@ -137,9 +137,10 @@ bool goto_symex_statet::constant_propagation(const expr2tc &expr) const
     // For now, we focus on propagating constants for structs only.
     // TODO: enable other type will regress performance, need a TC
     // to reproduce
-    if (
-      is_symbol2t(with.source_value) && is_struct_type(with.source_value) &&
-      is_constant_expr(with.update_value))
+    if (is_struct_type(with.source_value))
+      return true;
+
+    if (is_array_type(with.source_value))
       return true;
 
     return false;
@@ -226,14 +227,16 @@ void goto_symex_statet::rename_type(expr2tc &expr)
     if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
       rename(arr_size);
 
-    type->Foreach_subtype([this](type2tc &t) {
-      if (!is_array_type(t))
-        return;
+    type->Foreach_subtype(
+      [this](type2tc &t)
+      {
+        if (!is_array_type(t))
+          return;
 
-      expr2tc &arr_size = to_array_type(t).array_size;
-      if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
-        rename(arr_size);
-    });
+        expr2tc &arr_size = to_array_type(t).array_size;
+        if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
+          rename(arr_size);
+      });
   }
 
   /* All subexpressions' types should also be renamed, this is in line with
