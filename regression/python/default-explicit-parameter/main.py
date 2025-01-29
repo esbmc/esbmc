879cf802b44da296fdc01bea0e470d0a9af7f609
Benjamin Eichhoefer <ben.eichhofer@hotmail.com>
2025-01-29 13:28:07 +0000

    rename some regression tests and add new regression tests \n add more regression tests for function keyword arguments

--- regression/python/default-explicit-parameter/main.py
+++
@@ -1,5 +0,0 @@
-def return_sum(x:int=0,y:int=0) -> int:
-    return x/y
-
-x:int = return_sum(y=1,x=2)
-assert x==2
---
+++ regression/python/keyword-argument/main.py
@@ -0,0 +1,5 @@
+def return_sum(x:int,y:int) -> int:
+    return x+y
+
+x:int = return_sum(y=1,x=2)
+assert x==3
