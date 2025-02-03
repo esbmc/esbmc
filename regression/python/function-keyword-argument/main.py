71864324769e38fbe9560cf8ea1f593ebef2b356
Benjamin Eichhoefer <ben.eichhofer@hotmail.com>
2025-02-03 16:52:56 +0000

    amend keyword-argument test to use non-commutative operator to properly assess correctness rename function test folders to include prefix 'function-'

---
+++ regression/python/function-keyword-argument/main.py
@@ -0,0 +1,5 @@
+def return_sum(x:int,y:int) -> int:
+    return x/y
+
+a:int = return_sum(y=1,x=2)
+assert a==2
--- regression/python/keyword-argument/main.py
+++
@@ -1,5 +0,0 @@
-def return_sum(x:int,y:int) -> int:
-    return x+y
-
-x:int = return_sum(y=1,x=2)
-assert x==3
