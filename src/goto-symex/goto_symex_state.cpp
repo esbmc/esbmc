85938b6d6066c212aa06764eaeaa69de0a8b0fbd
lucasccordeiro <3694109+lucasccordeiro@users.noreply.github.com>
2025-02-04 20:30:52 +0000

    Apply automatic changes

--- src/goto-symex/goto_symex_state.cpp
+++ src/goto-symex/goto_symex_state.cpp
@@ -145,10 +145,9 @@ bool goto_symex_statet::constant_propagation(const expr2tc &expr) const
       is_constant_expr(with.update_value))
       return true;
 
-    if (is_array_type(with.source_value) && 
-      is_constant_expr(with.update_value))
+    if (is_array_type(with.source_value) && is_constant_expr(with.update_value))
       return true;
-    
+
     return false;
   }
 
