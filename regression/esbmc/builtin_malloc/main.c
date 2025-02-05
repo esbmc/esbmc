31bb88a599b94a9160873a612adb0ee426257e17
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2025-02-05 21:33:29 +0100

    Fix #1976: improve __builtin_{memcpy|memmove} (#2133)
    > Implement `__builtin_{memcpy|memmove}` by defining them to be equal to
    `__esbmc_builtin_{memcpy|memmove}`.
    We have to do this, because clang forbids us from directly implementing
    a function called `__builtin_{memcpy|memmove}`.
    `__esbmc_builtin_{memcpy|memmove}` then simply delegates to
    `{memcpy|memmove}`.
    We add the functions to the internal additions header, because they are
    builtins and should be available without
    having to import any headers. (The implementation of string.c is
    available implicitly).
    
    This fixes #1976 and also improves the solution to #569.
    If there is a simpler approach, I'm eager to hear it.
    
    ---------
    
    Co-authored-by: intrigus <abc123zeus@live.de>

---
+++ regression/esbmc/builtin_malloc/main.c
@@ -0,0 +1,21 @@
+#include <assert.h>
+#ifndef __has_builtin        // Optional of course.
+#  define __has_builtin(x) 0 // Compatibility with non-clang compilers.
+#endif
+
+int main()
+{
+#if __has_builtin(__builtin_malloc)
+  char *p = (char *)__builtin_malloc(5);
+  for (int i = 0; i < 5; i++)
+  {
+    p[i] = 'a';
+  }
+  assert(p[0] == 'a');
+  assert(p[1] == 'a');
+  assert(p[2] == 'a');
+  assert(p[3] == 'a');
+  assert(p[4] == 'a');
+  return 0;
+#endif
+}
