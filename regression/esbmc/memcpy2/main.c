8947708598def4cc098a3a73394638b4521352c4
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-02-06 10:51:52 +0000

    [OM] Check for src and dest NULL
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/memcpy2/main.c
+++ regression/esbmc/memcpy2/main.c
@@ -1,16 +1,9 @@
-#include <stdio.h>
 #include <string.h>
 
-void test_memcpy_null() {
-    char *src = "Hello";
-    char dest[5];
-
-     memcpy(dest, src, 5);  
-
-}
-
 int main() {
-    test_memcpy_null();
-    return 0;
+  char *src = "Hello";
+  char dest[5];
+  memcpy(dest, src, 5);
+  return 0;
 }
 
