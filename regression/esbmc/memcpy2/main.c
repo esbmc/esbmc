6a1752ca6470343779cf3e5fa83e511d8f1fa003
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-02-08 12:54:33 +0000

    [regression] check for memcpy output
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/memcpy2/main.c
+++ regression/esbmc/memcpy2/main.c
@@ -1,9 +1,11 @@
 #include <string.h>
+#include <assert.h>
 
 int main() {
   char *src = "Hello";
   char dest[5];
-  memcpy(dest, src, 5);
+  char* result = memcpy(dest, src, 5);
+  assert(result == dest && dest[4] == 'o');
   return 0;
 }
 
