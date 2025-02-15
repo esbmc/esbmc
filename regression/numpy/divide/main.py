0e416a799eef49719261bced4eac9a7f0114e552
sergillam <sgm.oliveira96@gmail.com>
2025-02-15 00:01:16 -0400

    [regression] Fixed test cases for binary functions in the numpy module of the Python frontend

--- regression/numpy/divide/main.py
+++ regression/numpy/divide/main.py
@@ -1,17 +1,48 @@
 import numpy as np
 
-a = np.divide(2.0, 4.0)
-assert a == 0.5
 
-b = np.divide(4.0, 2.0)
-assert b == 2
+result_1 = np.divide(2.0, 4.0)
+assert result_1 == 0.5
 
-c = np.divide([6,8],[2,4])
-assert c[0] == 3
-assert c[1] == 2
+result_2 = np.divide(4.0, 2.0)
+assert result_2 == 2.0
+
+result_3 = np.divide(10, 5)
+assert result_3 == 2.0
+
+result_4 = np.divide(7.5, 2.5)
+assert result_4 == 3.0
+
+result_5 = np.divide(-6, 3)
+assert result_5 == -2.0
+
+result_6 = np.divide(6, -3)
+assert result_6 == -2.0
+
+result_7 = np.divide(-8, -2)
+assert result_7 == 4.0
+
+result_8 = np.divide(0, 5)
+assert result_8 == 0.0
+
+result_9 = np.divide(5, 1)
+assert result_9 == 5.0
+
+result_10 = np.divide(-10.0, 2.0)
+assert result_10 == -5.0
+
+result_11 = np.divide(-50, -2)
+assert result_11 == 25.0
+
+result_12 = np.divide(7.0, -3.5)
+assert result_12 == -2.0
+
+result_13 = np.divide([6,8],[2,4])
+assert result_13[0] == 3
+assert result_13[1] == 2
 
 d = np.array([6,4])
 e = np.array([2,2])
-f = np.divide(d, e)
-assert f[0] == 3
-assert f[1] == 2
+result_14 = np.divide(d, e)
+assert result_14[0] == 3
+assert result_14[1] == 2
--- regression/numpy/power/main.py
+++ regression/numpy/power/main.py
@@ -1,7 +1,13 @@
 import numpy as np
 
-x = np.power(2, 3)
-assert x == 8
+result1 = np.power(2, 3)
+assert result1 == 8
+
+result2= np.power(2, -1)
+assert result2 == 0.5
+
+result3 = np.power(-2, 3)
+assert result3 == -8
 
 y = np.power(2,7, dtype=np.int8)
 assert y == -128
