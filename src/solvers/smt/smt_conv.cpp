28a8d7f2cb6dd158205c593c6ad676f20c70b17f
rafaelsamenezes <8601807+rafaelsamenezes@users.noreply.github.com>
2025-02-18 17:13:04 +0000

    Apply automatic changes

--- src/solvers/smt/smt_conv.cpp
+++ src/solvers/smt/smt_conv.cpp
@@ -320,8 +320,8 @@ smt_astt smt_convt::convert_ast(const expr2tc &expr)
   {
     // Convert all the arguments and store them in 'args'.
     unsigned int i = 0;
-    expr->foreach_operand([this, &args, &i](const expr2tc &e)
-                          { args[i++] = convert_ast(e); });
+    expr->foreach_operand(
+      [this, &args, &i](const expr2tc &e) { args[i++] = convert_ast(e); });
   }
   }
 
@@ -2469,30 +2469,26 @@ expr2tc smt_convt::get(const expr2tc &expr)
     if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
       arr_size = get(arr_size);
 
-    res->type->Foreach_subtype(
-      [this](type2tc &t)
-      {
-        if (!is_array_type(t))
-          return;
+    res->type->Foreach_subtype([this](type2tc &t) {
+      if (!is_array_type(t))
+        return;
 
-        expr2tc &arr_size = to_array_type(t).array_size;
-        if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
-          arr_size = get(arr_size);
-      });
+      expr2tc &arr_size = to_array_type(t).array_size;
+      if (!is_nil_expr(arr_size) && is_symbol2t(arr_size))
+        arr_size = get(arr_size);
+    });
   }
 
   // Recurse on operands
   bool have_all = true;
-  res->Foreach_operand(
-    [this, &have_all](expr2tc &e)
-    {
-      expr2tc new_e;
-      if (e)
-        new_e = get(e);
-      e = new_e;
-      if (!e)
-        have_all = false;
-    });
+  res->Foreach_operand([this, &have_all](expr2tc &e) {
+    expr2tc new_e;
+    if (e)
+      new_e = get(e);
+    e = new_e;
+    if (!e)
+      have_all = false;
+  });
 
   // And simplify
   if (have_all)
