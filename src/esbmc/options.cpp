d2e8799c41dd5c33c489c07a6b03c3e8f762c0ab
hejueyun2 <hejueyun@gmail.com>
2025-02-18 13:12:59 +0000

    [solidity] introduce bccv options

--- src/esbmc/options.cpp
+++ src/esbmc/options.cpp
@@ -88,7 +88,11 @@ const struct group_opt_templ all_cmd_options[] = {
     {"no-visibility",
      NULL,
      "force to verify every function, even it's an unreachable "
-     "internal/private function"}}},
+     "internal/private function"},
+    {"unbound", NULL, "model external function calls as arbitrary behavior"},
+    {"bound",
+     NULL,
+     "model inter-contract function calls within a bounded system"}}},
 #endif
   {"Frontend",
    {{"include,I",
