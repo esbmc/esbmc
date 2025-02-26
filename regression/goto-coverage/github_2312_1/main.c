95478bfa83765289a8d8d885515aeb667e29fe20
hejueyun2 <hejueyun@gmail.com>
2025-02-26 02:14:42 +0000

    [coverage] add regression test

---
+++ regression/goto-coverage/github_2312_1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+  char name[20] = "";
+
+  if (argc > 1 && argv[1] != NULL)
+  {
+    strncpy(name, argv[1], 10);
+  }
+}
--- src/c2goto/library/coverage.c
+++ src/c2goto/library/coverage.c
@@ -7,9 +7,11 @@ char **ESBMC_malloc_argv(int argc)
 {
 __ESBMC_HIDE:;
   char **argv_copy = (char **)malloc((argc + 1) * sizeof(char *));
-  for (int i = 0; i <= argc; i++)
+  for (int i = 0; i < argc; i++)
   {
     argv_copy[i] = __ESBMC_inf_str;
   }
+  argv_copy[argc] = NULL;
+  
   return argv_copy;
 }
