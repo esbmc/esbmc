24b30ab27843306736c0d7cf041603e187447999
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-03-14 21:11:15 +0000

    applied coding style
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/pointer-analysis/dereference.cpp
+++ src/pointer-analysis/dereference.cpp
@@ -2155,7 +2155,7 @@ void dereferencet::bounds_check(
   // Don't bounds check argv; it's always correct,
   // and just adds needless claims.
   if (has_prefix(ns.lookup(to_symbol2t(expr).thename)->id.as_string(), "argv'"))
-    return; 
+    return;
   assert(is_array_type(expr));
   const array_type2t arr_type = to_array_type(expr->type);
 
