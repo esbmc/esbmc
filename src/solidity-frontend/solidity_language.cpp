af61c2a45f59f4b1024db99202cef0177ad4f1d6
intrigus-lgtm <60750685+intrigus-lgtm@users.noreply.github.com>
2025-04-14 13:05:30 +0200

    [solidity] create unique temp file to prevent races (#2396)

--- src/solidity-frontend/solidity_language.cpp
+++ src/solidity-frontend/solidity_language.cpp
@@ -13,6 +13,7 @@ CC_DIAGNOSTIC_POP()
 #include <clang-cpp-frontend/clang_cpp_convert.h>
 #include <c2goto/cprover_library.h>
 #include <util/c_link.h>
+#include "filesystem.h"
 
 languaget *new_solidity_language()
 {
@@ -46,31 +47,25 @@ std::string solidity_languaget::get_temp_file()
 {
   // Create a temp file for clang-tool
   // needed to convert intrinsics
-  auto p = boost::filesystem::temp_directory_path();
-  if (!boost::filesystem::exists(p) || !boost::filesystem::is_directory(p))
-  {
-    log_error("Can't find temporary directory (needed to convert intrinsics)");
-    abort();
-  }
+  static std::once_flag flag;
+  static std::string p;
 
-  // Create temporary directory
-  p += "/esbmc_solidity_temp";
-  boost::filesystem::create_directory(p);
-  if (!boost::filesystem::is_directory(p))
-  {
-    log_error(
-      "Can't create temporary directory (needed to convert intrinsics)");
-    abort();
-  }
+  std::call_once(flag, [&]() {
+    p = file_operations::create_tmp_dir("esbmc_solidity_temp-%%%%-%%%%-%%%%")
+          .path();
+    boost::filesystem::create_directories(p);
+    p += "/libary.cpp";
+    std::ofstream f(p);
+    if (!f)
+    {
+      log_error(
+        "Can't create temporary directory (needed to convert intrinsics)");
+      abort();
+    }
+    f << temp_cpp_file();
+  });
 
-  // populate temp file
-  std::ofstream f;
-  p += "/libary.cpp";
-  f.open(p.string());
-  f << temp_cpp_file();
-  f.close();
-
-  return p.string();
+  return p;
 }
 
 bool solidity_languaget::parse(const std::string &path)
