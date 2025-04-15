446ae0aad75eb521a53a910274de04a7abc3363f
hejueyun2 <hejueyun@gmail.com>
2025-04-15 04:38:29 +0100

    [solidity] fix unit

--- regression/esbmc-solidity/unit_1/contract.sol
+++ regression/esbmc-solidity/unit_1/contract.sol
@@ -1,22 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract TimeUnitTest {
-    function testTimeUnits() public pure returns (bool) {
-        // Solidity time literals
-        uint oneSecond = 1 seconds;
-        uint oneMinute = 1 minutes;
-        uint oneHour = 1 hours;
-        uint oneDay = 1 days;
-        uint oneWeek = 1 weeks;
-
-        // Assert each time unit to its value in seconds
-        assert(oneSecond == 1);          // 1 second = 1 second
-        assert(oneMinute == 60);         // 1 minute = 60 seconds
-        assert(oneHour == 3600);         // 1 hour = 60 * 60 = 3600 seconds
-        assert(oneDay == 86400);         // 1 day = 24 * 3600 = 86400 seconds
-        assert(oneWeek == 604800);       // 1 week = 7 * 86400 = 604800 seconds
-
+contract TestAssertionPass {
+    function testUnits() public pure returns (bool) {
+        // Using solidity units:
+        uint valueWei = 1 wei;       // 1 wei
+        uint valueGwei = 1 gwei;     // 1 gwei
+        
+        // Assertion: 1 gwei should equal 1,000,000,000 wei.
+        assert(valueGwei == 1000000000 * valueWei);
+        
         return true;
     }
 }
