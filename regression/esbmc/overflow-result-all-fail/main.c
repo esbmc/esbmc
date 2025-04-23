99ad02031e993d7fb54dfc99b2a37d62ed3d23c0
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-04-23 14:18:06 +0100

    [regression] improved test case
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/overflow-result-all-fail/main.c
+++ regression/esbmc/overflow-result-all-fail/main.c
@@ -8,9 +8,6 @@ int main() {
   int a = __VERIFIER_nondet_int();
   int b = __VERIFIER_nondet_int();
 
-  //__ESBMC_assume(a >= -1000 && a <= 1000);
-  //__ESBMC_assume(b >= -1000 && b <= 1000);
-
   __ESBMC_overflow_result foo;
 
   // Test addition
