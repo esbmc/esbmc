ef9a11bb5a59822387fa9f3c6d93cbcedcf134a1
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-04-26 12:17:24 +0100

    update test

--- regression/python/slicing/main.py
+++ regression/python/slicing/main.py
@@ -1,5 +1,14 @@
-x = [0,1,2,3,4]
-y = x[0:2]
+a = [0,1,2,3,4]
 
-assert y[0] == 0
-assert y[1] == 1
+b = a[0:2]
+assert b[0] == 0
+assert b[1] == 1
+
+c = a[1:3]
+assert c[0] == 1
+assert c[1] == 2
+
+d = a[2:5]
+assert d[0] == 2
+assert d[1] == 3
+assert d[2] == 4
