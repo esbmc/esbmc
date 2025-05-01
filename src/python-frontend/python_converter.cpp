a5c22051d52ef6026699db195d484f4f31fac6d3
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-05-01 15:07:53 +0100

    [regression] added test case for casting
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/python-frontend/python_converter.cpp
+++ src/python-frontend/python_converter.cpp
@@ -227,27 +227,28 @@ void python_converter::adjust_statement_types(exprt &lhs, exprt &rhs) const
     lhs_type.is_floatbv() && rhs.is_constant() &&
     (rhs_type.is_signedbv() || rhs_type.is_unsignedbv()))
   {
+    // Convert RHS to float
     BigInt value(
       binary2integer(rhs.value().as_string(), rhs_type.is_signedbv()));
     std::string rhs_float = std::to_string(value.to_int64()) + ".0";
     convert_float_literal(rhs_float, rhs);
     update_symbol(rhs);
-    return;
   }
-  // Handle width mismatch for basic types
-  if (lhs_type.width() != rhs_type.width())
+  else if (lhs_type.width() != rhs_type.width())
   {
     auto lhs_type_width = std::stoi(lhs_type.width().c_str());
     auto rhs_type_width = std::stoi(rhs_type.width().c_str());
 
     if (lhs_type_width > rhs_type_width)
     {
+      // Update rhs symbol value to match with new type
       rhs_type = lhs_type;
       if (rhs.is_symbol())
         update_symbol(rhs);
     }
     else
     {
+      // Update lhs symbol value to match with new type
       lhs_type = rhs_type;
       if (lhs.is_symbol())
         update_symbol(lhs);
