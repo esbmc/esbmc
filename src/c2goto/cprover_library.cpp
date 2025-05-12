196d1ac4ad4277293b8ab886e6fb60dd11333dc1
lucasccordeiro <3694109+lucasccordeiro@users.noreply.github.com>
2025-05-12 16:36:13 +0000

    Apply automatic changes

--- src/c2goto/cprover_library.cpp
+++ src/c2goto/cprover_library.cpp
@@ -89,26 +89,10 @@ static const struct buffer
 };
 
 const static std::vector<std::string> python_c_models = {
-  "strncmp",
-  "ceil",
-  "__ceil_array",
-  "fegetround",
-  "fesetround",
-  "rint",
-  "fesetround",
-  "floor",
-  "fabs",
-  "sin",
-  "cos",
-  "exp",
-  "expm1",
-  "expm1_taylor",
-  "fmod",
-  "sqrt",
-  "fmin",
-  "fmax",
-  "trunc",
-  "frexp"};
+  "strncmp", "ceil",       "__ceil_array", "fegetround",   "fesetround",
+  "rint",    "fesetround", "floor",        "fabs",         "sin",
+  "cos",     "exp",        "expm1",        "expm1_taylor", "fmod",
+  "sqrt",    "fmin",       "fmax",         "trunc",        "frexp"};
 
 } // namespace
 
