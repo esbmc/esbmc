50dd7286a81e7c03c8ffcc6c1c4330803602daf1
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-05-12 11:24:43 +0100

    [regression] extended numpy test case for math
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/numpy/math/main.py
+++ regression/numpy/math/main.py
@@ -2,6 +2,8 @@ import numpy as np
 
 # numpy.ceil
 assert np.ceil(4.2) == 5
+ce = np.ceil(10.2)
+assert ce == 11
 assert np.ceil(-1.3) == -1
 assert np.ceil(7.0) == 7
 
@@ -12,6 +14,8 @@ assert result[1] == 3.0
 assert result[2] == -3.0
 
 assert np.floor(2.9) == 2.0
+fl = np.floor(5.1)
+assert fl == 5
 assert np.floor(-2.1) == -3.0
 
 assert np.fabs(-2.1) == 2.1
