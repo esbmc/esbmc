3299c5cb5e806fbe8effd19d1cf09e57fad4de94
XLiZHI <958689657@qq.com>
2025-05-15 14:41:11 +0100

    [cmake] update ibex

--- scripts/cmake/FindIbex.cmake
+++ scripts/cmake/FindIbex.cmake
@@ -9,20 +9,27 @@ if(DOWNLOAD_DEPENDENCIES AND (NOT DEFINED IBEX_DIR))
    CPMAddPackage(
       NAME ibex
       DOWNLOAD_ONLY YES
-      URL https://github.com/ibex-team/ibex-lib/archive/refs/tags/ibex-2.8.9.tar.gz)
+      URL https://github.com/ibex-team/ibex-lib/archive/refs/tags/ibex-2.9.1.tar.gz)
 
    message("[ibex] Setting up ibex")
+   set(_LP_LIB none)
    if(ACADEMIC_BUILD)
-      message(WARNING "the version of ibex you have is ZIB licensed, distribution is impossible.")
-      execute_process(COMMAND ./waf --prefix=${ibex_BINARY_DIR} --lp-lib=soplex  configure
-         WORKING_DIRECTORY ${ibex_SOURCE_DIR})
-   else()
-      execute_process(COMMAND ./waf --prefix=${ibex_BINARY_DIR} configure
-         WORKING_DIRECTORY ${ibex_SOURCE_DIR})
+      message(WARNING "[ibex] Academic build: LP solver SO-Plex required")
+      set(_LP_LIB soplex)
    endif()
+
+   execute_process(
+      COMMAND cmake
+      -DCMAKE_INSTALL_PREFIX=${ibex_BINARY_DIR}
+      -DCMAKE_BUILD_TYPE=Release
+      -DLP_LIB=${_LP_LIB}
+      -B ${ibex_SOURCE_DIR}/build
+      -S ${ibex_SOURCE_DIR})
+
    message("[ibex] Installing ibex")
-   execute_process(COMMAND ./waf -j4 install
-      WORKING_DIRECTORY ${ibex_SOURCE_DIR})
+   execute_process(
+      COMMAND cmake --build . --target install --parallel 4
+      WORKING_DIRECTORY ${ibex_SOURCE_DIR}/build)
 
    set(IBEX_DIR ${ibex_BINARY_DIR})
 
@@ -31,9 +38,7 @@ if(DOWNLOAD_DEPENDENCIES AND (NOT DEFINED IBEX_DIR))
    endif()
 
 endif()
-
 find_package(PkgConfig)
 
 set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:${IBEX_DIR}/share/pkgconfig")
-pkg_search_module(IBEX REQUIRED IMPORTED_TARGET ibex)
-pkg_check_modules(IBEX REQUIRED ibex)
+pkg_check_modules(IBEX REQUIRED IMPORTED_TARGET ibex)
