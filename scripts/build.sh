117ca9018a4d3afca92f046225ff6ae33d1008d0
XLiZHI <958689657@qq.com>
2025-05-15 14:41:11 +0100

    [cmake] update ibex

--- scripts/build.sh
+++ scripts/build.sh
@@ -43,7 +43,7 @@ ubuntu_setup () {
         libgmp-dev cmake bison flex g++-multilib linux-libc-dev \
         libboost-all-dev ninja-build python3-setuptools \
         libtinfo-dev pkg-config python3-pip python3-toml \
-        openjdk-11-jdk \
+        openjdk-11-jdk libbz2-dev liblzma-dev\
     "
     if [ -z "$STATIC" ]; then STATIC=ON; fi
     if [ $STATIC = OFF ]; then
