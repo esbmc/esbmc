6ca699937e21710d8ea94085123908c9ca139d9f
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-05-23 00:08:46 +0100

    [python] Allow variables in NumPy math function calls.

---
+++ src/c2goto/library/numpy/umath.c
@@ -0,0 +1,49 @@
+#include <stdint.h>
+
+void add(int64_t *A, int64_t *B, int64_t *C, int64_t m, int64_t n) {
+    int i = 0;
+    while (i < m) {
+        int j = 0;
+        while (j < n) {
+            *(C + i * n + j) = *(A + i * n + j) + *(B + i * n + j);
+            j++;
+        }
+        i++;
+    }
+}
+
+void subtract(int64_t *A, int64_t *B, int64_t *C, int64_t m, int64_t n) {
+    int i = 0;
+    while (i < m) {
+        int j = 0;
+        while (j < n) {
+            *(C + i * n + j) = *(A + i * n + j) - *(B + i * n + j);
+            j++;
+        }
+        i++;
+    }
+}
+
+void multiply(int64_t *A, int64_t *B, int64_t *C, int64_t m, int64_t n) {
+    int i = 0;
+    while (i < m) {
+        int j = 0;
+        while (j < n) {
+            *(C + i * n + j) = *(A + i * n + j) * *(B + i * n + j);
+            j++;
+        }
+        i++;
+    }
+}
+
+void divide(int64_t *A, int64_t *B, int64_t *C, int64_t m, int64_t n) {
+    int i = 0;
+    while (i < m) {
+        int j = 0;
+        while (j < n) {
+            *(C + i * n + j) = *(A + i * n + j) / *(B + i * n + j);
+            j++;
+        }
+        i++;
+    }
+}
