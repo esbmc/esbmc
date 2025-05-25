1564cd399c57de7f5bae4a81feb9a717e380b685
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2025-05-25 17:22:01 +0100

    [python] improve range() handling (#2473)

--- src/python-frontend/models/range.py
+++ src/python-frontend/models/range.py
@@ -2,4 +2,9 @@ def ESBMC_range_next_(curr: int, step: int) -> int:
   return curr + step
 
 def ESBMC_range_has_next_(curr: int, end: int, step: int) -> bool:
-  return curr + step <= end
+    if step > 0:
+        return curr < end
+    elif step < 0:
+        return curr > end
+    else:
+        return False  # step == 0 is invalid
