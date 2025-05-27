02f1db01a0a03f608005ec606e8d6db084dbf332
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-05-27 15:20:56 +0100

    [migrate] added future comment

--- src/util/migrate.cpp
+++ src/util/migrate.cpp
@@ -1951,6 +1951,7 @@ void migrate_expr(const exprt &expr, expr2tc &new_expr_ref)
   }
   else if (expr.id() == "r_ok")
   {
+    // FUTURE: call __ESBMC_r_ok
     true_exprt t;
     migrate_expr(t, new_expr_ref);
   }
