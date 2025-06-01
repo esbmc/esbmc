cfcdc2c2c29126d9f430b2baf0aba2a8e16fa843
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-06-01 16:34:20 +0100

    [python] added test cases for return type annotation
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/python/return10/main.py
@@ -0,0 +1,16 @@
+def int_annotation() -> "int":
+    return 42
+
+def str_annotation() -> "str":
+    return "hello"
+
+def float_annotation() -> "float":
+    return 3.14
+
+def bool_annotation() -> "bool":
+    return True
+
+result1 = int_annotation()
+result2 = str_annotation()
+result3 = float_annotation()
+result4 = bool_annotation()
