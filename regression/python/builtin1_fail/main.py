07743b24b810052fb347786cc7a1aea3d734bf5a
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2025-06-05 22:11:30 +0100

    Update main.py

--- regression/python/builtin1_fail/main.py
+++ regression/python/builtin1_fail/main.py
@@ -3,4 +3,4 @@ def test_abs(x: int) -> int:
     length = len([1, 2, 3, 10])
     return result + length
 
-assert test_abs(-5) == 8  # abs(-5) + len([1,2,3]) = 5 + 3 = 8
+assert test_abs(-5) == 8  # should fail
