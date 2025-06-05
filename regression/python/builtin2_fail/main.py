6d8b3963ab294379a9d9d89f0333c0c4949c3e09
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2025-06-05 22:11:57 +0100

    Update main.py

--- regression/python/builtin2_fail/main.py
+++ regression/python/builtin2_fail/main.py
@@ -1,5 +1,5 @@
 def test_chr_ord() -> bool:
-    char_code = ord('B')  # Should be 65
+    char_code = ord('B')  # Should be 66
     back_to_char = chr(char_code)
     return back_to_char == 'A' and char_code == 65
 
