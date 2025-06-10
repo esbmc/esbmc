f6046659a0bf611cfa8c1499eb8c9349bd561ded
Xianzhiyu <115160284+XLiZHI@users.noreply.github.com>
2025-06-10 15:56:30 +0100

    [CI] Integrate cheribsd-riscv64 system root (#2447)
    The build on GitHub runner using cheribuild took too long and exceeded
    the runner's hard disk capacity.
    
    Currently I have uploaded the cheribsd SDK built locally and hope it
    works as expected. ;)

--- scripts/build.sh
+++ scripts/build.sh
@@ -38,7 +38,6 @@ error() {
 ubuntu_setup () {
     # Tested on ubuntu 22.04
     PKGS="\
-        clang-$CLANG_VERSION clang-tidy-$CLANG_VERSION \
         python-is-python3 csmith python3 \
         git ccache unzip wget curl libcsmith-dev gperf \
         libgmp-dev cmake bison flex g++-multilib linux-libc-dev \
