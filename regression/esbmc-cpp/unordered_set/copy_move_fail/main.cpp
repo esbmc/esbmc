9aacd7fbd8453c3ee6cfdd8cdcb2fe2017527e38
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2025-06-16 07:47:38 +0100

    [c++] add basic unordered_set support (#2529)

---
+++ regression/esbmc-cpp/unordered_set/copy_move_fail/main.cpp
@@ -0,0 +1,13 @@
+#include <unordered_set>
+#include <cassert>
+
+int main() {
+    std::unordered_set<int> s1;
+    s1.insert(1);
+    std::unordered_set<int> s2(s1);
+    assert(s2.size() == 1);
+    assert(s2.contains(2));
+    assert(s1 == s2);
+    return 0;
+}
+
