da1a26aeab80664c7befefe60acfa983967475ec
hejueyun2 <hejueyun@gmail.com>
2025-07-02 07:28:34 +0100

    [solidity] support selfdestruct

--- src/solidity-frontend/solidity_template.h
+++ src/solidity-frontend/solidity_template.h
@@ -190,8 +190,16 @@ __ESBMC_HIDE:;
 }
 )";
 
+const std::string sol_destruct = R"(
+void selfdestruct()
+{
+__ESBMC_HIDE:;
+  exit(0);
+}
+)";
+
 const std::string sol_funcs =
-  blockhash + gasleft + sol_abi + sol_math + sol_string + sol_byte;
+  blockhash + gasleft + sol_abi + sol_math + sol_string + sol_byte + sol_destruct;
 
 /// data structure
 
