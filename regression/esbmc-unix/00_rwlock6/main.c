be2f3d84aa0a1441191090c61a113210f7971dcc
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2025-07-15 14:31:21 +0100

    [Concurrency] Fix pthread_rwlock opreational model (#2609)
    Co-authored-by: Lucas C. Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/esbmc-unix/00_rwlock6/main.c
@@ -0,0 +1,36 @@
+#include <pthread.h>
+#include <assert.h>
+
+pthread_rwlock_t lock;
+int shared_counter = 0;
+
+void *writer_thread(void *arg)
+{
+    int expected = *(int *)arg;
+    pthread_rwlock_wrlock(&lock);
+    shared_counter++;
+    assert(shared_counter == expected);
+    pthread_rwlock_unlock(&lock);
+    return NULL;
+}
+
+int main()
+{
+    pthread_rwlock_init(&lock, NULL);
+
+    pthread_t t1, t2;
+    int expect1 = 1;
+    int expect2 = 2;
+
+    pthread_create(&t1, NULL, writer_thread, &expect1);
+
+    pthread_create(&t2, NULL, writer_thread, &expect2);
+    pthread_join(t2, NULL);
+
+    pthread_rwlock_wrlock(&lock);
+    shared_counter++;
+    assert(shared_counter == 3);
+    pthread_rwlock_unlock(&lock);
+
+    return 0;
+}
