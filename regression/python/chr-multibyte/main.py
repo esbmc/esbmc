665d0031ca660b73bc90ea2caf965843a3b0bd24
Benjamin Eichhoefer <ben.eichhofer@hotmail.com>
2025-07-23 14:04:18 +0100

    update chr tests

--- regression/python/chr-multibyte/main.py
+++ regression/python/chr-multibyte/main.py
@@ -1,2 +1,15 @@
+# 1-Byte
+a = chr(97)
+assert a == 'a'
+
+# 2-Byte
+a = chr(200)
+assert a == 'È'
+
+# 3-Byte
 a = chr(23383)
-assert a == "字"
+assert a == '字'
+
+# 4-Byte
+a = chr(65536)
+assert a == '𐀀'
--- regression/python/chr-var-multibyte/main.py
+++ regression/python/chr-var-multibyte/main.py
@@ -1,3 +1,19 @@
+# 1-Byte
+a = 97
+b = chr(a)
+assert b == 'a'
+
+# 2-Byte
+a = 200
+b = chr(a)
+assert b == 'È'
+
+# 3-Byte
 a = 23383
 b = chr(a)
-assert b == "字"
+assert b == '字'
+
+# 4-Byte
+a = 65536
+b = chr(a)
+assert b == '𐀀'
