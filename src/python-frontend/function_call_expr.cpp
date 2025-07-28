d293ad2ccd073d91ee32c70e57762148a833c100
Benjamin Eichhoefer <ben.eichhofer@hotmail.com>
2025-07-28 15:00:44 +0100

    remove last trailing whitespace

--- src/python-frontend/function_call_expr.cpp
+++ src/python-frontend/function_call_expr.cpp
@@ -287,7 +287,7 @@ exprt function_call_expr::handle_chr(nlohmann::json &arg) const
   bool null_terminated = int_value > 0x7f;
   // Build and return the string expression
   exprt expr = converter_.get_expr(arg);
-  expr.type() = 
+  expr.type() =
     type_handler_.get_typet("str", utf8_encoded.size() + null_terminated);
   return expr;
 }
