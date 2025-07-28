c914cdbd2deba5660c450ebbc8628d6ed4ee7598
LukeW1999 <lukewang19@outlook.com>
2025-07-28 16:40:53 +0100

    add two simple loop invariants test case

---
+++ regression/loops/loop_invariants/1-loop_invariants_with_no_assigned.c
@@ -0,0 +1,27 @@
+/*
+ * Weiqi Wang
+ * Case for there is a non-assigned variable away from the loop invariant
+ * Should fail line 25 and overflow in line 21
+*/
+
+ #include <assert.h>
+
+ int main() {
+     int i = 0;
+     int sum = 0;
+     int secret = 42; 
+ 
+     /*@ loop invariant i >= 0 && i <= 5000000 && sum == i * 10;
+     @*/
+     
+     while (i < 5000000)
+     {   
+         sum += 10;
+         i++;
+         secret = secret +1;
+     }
+     
+     assert(sum == 50000000);     
+     assert(secret == 5000042); // 42+5000000
+     return 0;
+ }
