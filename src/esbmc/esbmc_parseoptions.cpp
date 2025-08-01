d5735ceca6b79d999faee08bc8fe8fa5efe8bd38
Luke Sanderson <lksndrsn@gmail.com>
2025-08-01 11:37:07 +0100

    Change output for parallel-solving to be inline with multi-property

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -476,7 +476,6 @@ void esbmc_parseoptionst::get_command_line_options(optionst &options)
   // parallel solving activates "--multi-property"
   if (cmdline.isset("parallel-solving"))
   {
-    options.set_option("result-only", true);
     options.set_option("base-case", true);
     options.set_option("multi-property", true);
   }
@@ -1833,7 +1832,7 @@ bool esbmc_parseoptionst::process_goto_program(
   {
     namespacet ns(context);
 
-    bool is_mul = cmdline.isset("multi-property");
+    bool is_mul = cmdline.isset("multi-property") || cmdline.isset("parallel-solving");
     is_coverage = cmdline.isset("assertion-coverage") ||
                   cmdline.isset("assertion-coverage-claims") ||
                   cmdline.isset("condition-coverage") ||
