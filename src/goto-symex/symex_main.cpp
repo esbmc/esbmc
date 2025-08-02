b8640afc205b5c0e6370f57638012981cda210d8
mihaistate05 <mihai.state03@yahoo.com>
2025-08-02 17:33:41 +0300

    Added separate cases for memcpy inside run_builtin() and and symex_step() functions

--- src/goto-symex/symex_main.cpp
+++ src/goto-symex/symex_main.cpp
@@ -324,11 +324,18 @@ void goto_symext::symex_step(reachability_treet &art)
     if (is_symbol2t(call.function))
     {
       const irep_idt &id = to_symbol2t(call.function).thename;
-      if (has_prefix(id.as_string(), "c:@F@__ESBMC") || id == "c:@F@memcpy")
+      if (has_prefix(id.as_string(), "c:@F@__ESBMC"))
       {
         cur_state->source.pc++;
         run_intrinsic(call, art, id.as_string());
         return;
+      }
+
+      if (id.as_string() == "c:@F@memcpy" || has_prefix(id.as_string(), "c:@F@__builtin_memcpy$")) 
+      {
+        cur_state->source.pc++;
+        if(run_builtin(call, id.as_string())) {
+          return; }
       }
 
       if (id == "c:@F@scanf" || id == "c:@F@sscanf" || id == "c:@F@fscanf")
