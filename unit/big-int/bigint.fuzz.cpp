71196f077279504c29a0b8c6821bdb2ff60d89e5
Max Beck-Jones <19396918+DevM-uk@users.noreply.github.com>
2025-08-03 23:33:59 +0100

    Fix bigint fuzz inputs rejection

--- unit/big-int/bigint.fuzz.cpp
+++ unit/big-int/bigint.fuzz.cpp
@@ -14,35 +14,37 @@
 #include <big-int/bigint.hh>
 #include <vector>
 #include <stddef.h>
+#include <iostream>
 
 // Checks whether the input contains digits only
-int is_valid_input(const char *Data, size_t DataSize)
+bool is_valid_input(const char *Data, size_t DataSize)
 {
   const char low_bound = '0';
   const char high_bound = '9';
 
-  for (size_t i = 0; i < DataSize; ++i)
+  if (DataSize == 0) 
+    return false;
+  if (Data[0] == '0')
+    return false;
+  // Last character must be a null terminator
+  if (Data[DataSize-1] != 0)
+    return false;
+  for (size_t i = 0; i < DataSize-1; ++i)
   {
     if (!((Data[i] >= low_bound) && (Data[i] <= high_bound)))
-      return 0;
+      return false;
   }
-  return 1;
+  return true;
 }
 
 void test_construct_bigint(const char *Data, size_t DataSize)
 {
-  if (DataSize == 0)
-    return;
-  if (Data[0] == '0')
-    return;
-  if (!is_valid_input(Data, DataSize))
-    return;
   BigInt obj(Data, 10);
 
   std::vector<char> vec(obj.digits());
   const char *actual = obj.as_string(vec.data(), vec.size());
 
-  for (size_t i = 0; i < DataSize; ++i)
+  for (size_t i = 0; i < DataSize-1; ++i)
   {
     assert(Data[i] == actual[i]);
   }
@@ -50,6 +52,8 @@ void test_construct_bigint(const char *Data, size_t DataSize)
 
 extern "C" int LLVMFuzzerTestOneInput(const char *Data, size_t Size)
 {
+  if (!is_valid_input(Data, Size))
+    return -1;
   test_construct_bigint(Data, Size);
   return 0;
 }
