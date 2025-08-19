f9bee82bd8b82f9de7a3d1877bc0eab50e4b56dd
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-08-19 15:11:18 +0100

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -39,7 +39,7 @@ error() {
 ubuntu_setup () {
     # Tested on ubuntu 22.04
     PKGS="\
-        python-is-python3 csmith python3 \
+        python-is-python3 csmith python3 lcov \
         git ccache unzip wget curl libcsmith-dev gperf \
         cmake bison flex g++-multilib linux-libc-dev \
         libboost-all-dev ninja-build python3-setuptools \
