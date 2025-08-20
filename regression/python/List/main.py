93883f6fc7d25a42ad974aa4508ce5f6db00408d
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-08-20 12:14:45 +0100

    Fix List handling

--- regression/python/List/main.py
+++ regression/python/List/main.py
@@ -6,3 +6,14 @@ assert x[1] == 2
 assert x[2] == 3
 
 assert len(x) == 3
+
+def sum_while(n: List[int]) -> int:
+    l = len(n)
+    i = 0
+    s = 0
+    while i < l:
+        s += n[i]
+        i += 1
+    return s
+
+assert sum_while([1, 2, 3, 4]) == 10
