ca93171564a13971d2969e5b21e78426d3072659
WeiqiW <lukewang19@outlook.com>
2025-08-22 13:42:08 +0100

    add some test cases for nested loop invariant

--- regression/loop-invariants/nested-0-correct_sum/main.c
+++ regression/loop-invariants/nested-0-correct_sum/main.c
@@ -8,17 +8,20 @@ int main()
   int sum = 0;
   int j = 0;
 
-  __ESBMC_loop_invariant(i >= 0 && i <= 10 && sum == i * 10);
+  __ESBMC_loop_invariant(i >= 0 && i <= 10 && sum == i * 11);
   while (i < 10)
   {
-    sum += 10;
-    i++;
-    __ESBMC_loop_invariant(j >= 0 && j <= 10 && sum == i * 10 + j);
+    j = 0;
+    
+    __ESBMC_loop_invariant(j >= 0 && j <= 10 && sum == i * 11 + j);
     while (j < 10)
     {
       sum++;
       j++;
     }
+    
+    sum++;  // Add one more to make total 11 per outer iteration
+    i++;
   }
 
   assert(sum == 110);
