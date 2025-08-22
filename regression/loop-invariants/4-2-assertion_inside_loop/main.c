0d957ae04561c8e98e58e334d83b3ef98e1cb1fe
WeiqiW <lukewang19@outlook.com>
2025-08-22 20:01:33 +0100

    [Invariants checker] Fix the loop invariant affect scope, it should work for nested loop now (#2713)

--- regression/loop-invariants/4-2-assertion_inside_loop/main.c
+++ regression/loop-invariants/4-2-assertion_inside_loop/main.c
@@ -4,7 +4,8 @@ int main()
   unsigned int x, y;
   x = 0U;
   y = 4U;
-  __ESBMC_loop_invariant(y % 4 == 0 && x % 4 == 0);
+  __ESBMC_loop_invariant(x % 4 == 0);
+  //__ESBMC_loop_invariant(y % 4 == 0);
   while (1)
   {
     x = x + y;
