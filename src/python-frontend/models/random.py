41ec62ab8dc4c9385229cded5750a0617ac07657
Bruno Farias <brunocarvalhofarias@gmail.com>
2025-08-23 22:20:50 +0100

    Add Yapf to CI for Python Code Style (#2716)

--- src/python-frontend/models/random.py
+++ src/python-frontend/models/random.py
@@ -1,12 +1,14 @@
 # Stubs for random module.
 # See https://docs.python.org/3/library/random.html
 
-def randint(a:int, b:int) -> int:
-    value:int = nondet_int()
+
+def randint(a: int, b: int) -> int:
+    value: int = nondet_int()
     __ESBMC_assume(value >= a and value <= b)
     return value  # Ensures value is within [a, b]
 
+
 def random() -> float:
-  value:float = nondet_float()
-  __ESBMC_assume(value >= 0.0 and value < 1.0)
-  return value #  Returns a floating number [0,1.0).
+    value: float = nondet_float()
+    __ESBMC_assume(value >= 0.0 and value < 1.0)
+    return value  #  Returns a floating number [0,1.0).
