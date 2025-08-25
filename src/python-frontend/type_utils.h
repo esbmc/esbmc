811003f5a2f7725499504a3420aede1e38eccffb
XLiZHI <958689657@qq.com>
2025-08-25 13:45:21 +0800

    [python-frontend] add exception types

--- src/python-frontend/type_utils.h
+++ src/python-frontend/type_utils.h
@@ -82,7 +82,9 @@ public:
 
   static bool is_python_exceptions(const std::string &name)
   {
-    return (name == "BaseExecption" || name == "ValueError");
+    return (
+      name == "BaseExecption" || name == "ValueError" || name == "TypeError" ||
+      name == "IndexError" || name == "KeyError");
   }
 
   static bool is_c_model_func(const std::string &func_name)
