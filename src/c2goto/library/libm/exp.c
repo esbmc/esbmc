2b1438c8a46aac92925a99befc43f65a89203b19
Sulay Chaudhry <54546648+sulaychaudhry@users.noreply.github.com>
2025-09-02 19:51:49 +0100

    [libm] Correcting +- in ln(1+x) MacLaurin series for exp.c

--- src/c2goto/library/libm/exp.c
+++ src/c2goto/library/libm/exp.c
@@ -12,8 +12,8 @@ static double expm1_taylor(double x)
   acc += (smd *= x / 4);
   acc += (smd *= x / 5);
   acc += (smd *= x / 6);
-  // acc += (smd *= x / 7);
-  // acc += (smd *= x / 8);
+  acc += (smd *= x / 7);
+  acc += (smd *= x / 8);
   return acc;
 }
 
@@ -66,12 +66,12 @@ static double log1p_taylor(double x)
   double acc = x;
   double smd = x;
   acc += (smd *= x) / -2;
-  acc += (smd *= x) / -3;
+  acc += (smd *= x) / 3;
   acc += (smd *= x) / -4;
-  acc += (smd *= x) / -5;
+  acc += (smd *= x) / 5;
   acc += (smd *= x) / -6;
-  // acc += (smd *= x) / -7;
-  // acc += (smd *= x) / -8;
+  acc += (smd *= x) / 7;
+  acc += (smd *= x) / -8;
   return acc;
 }
 
