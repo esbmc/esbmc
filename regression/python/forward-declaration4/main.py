91d1bfb1c67bebc6cb54dec9b6fa3ae7be0e379b
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-09-02 15:34:40 +0100

    [python] added test cases for forward declaration

---
+++ regression/python/forward-declaration4/main.py
@@ -0,0 +1,13 @@
+def is_even(n):
+    if n == 0:
+        return True
+    return is_odd(n - 1)
+
+def is_odd(n):
+    if n == 0:
+        return False
+    return is_even(n - 1)
+
+assert is_even(10)
+assert not is_odd(10)
+
