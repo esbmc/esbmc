caa6e82faea35e8dd798d7c8294411fffcf9ead0
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-09-08 18:17:52 +0100

    [python] added tests for missing return
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

---
+++ regression/python/missing-return10_fail/main.py
@@ -0,0 +1,13 @@
+import random
+
+def foo(x:int, y:int) -> int:
+  if x:
+    if y:
+      return 1
+    else:
+      return None
+  return 2
+
+x = 1# random.randint(0,1)
+y = 0#random.randint(0,1)
+foo(x, y)
