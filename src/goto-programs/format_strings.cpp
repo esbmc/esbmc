0ef061211b7532de09d8eb64d6e5e76353c3fc08
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-09-14 19:40:39 +0100

    [goto-programs] Remove unused tmp variable and simplify iterator loop in format_strings.cpp
    Replace loop that collected unused characters in tmp variable with
    empty loop body that just advances iterator, and use prefix increment.

--- src/goto-programs/format_strings.cpp
+++ src/goto-programs/format_strings.cpp
@@ -163,8 +163,7 @@ void parse_conversion_specifier(
       }
     }
 
-    for (; it != arg_string.end() && *it != ']'; it++)
-      tmp += *it;
+    for (; it != arg_string.end() && *it != ']'; ++it);
 
     break;
   }
