0579f391c7d04b029a98eaf79bd64cd0646cb07c
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-09-22 17:23:35 +0100

    Add interval analysis option for non-concurrent checks

--- scripts/competitions/svcomp/esbmc-wrapper.py
+++ scripts/competitions/svcomp/esbmc-wrapper.py
@@ -287,7 +287,7 @@ def get_command_line(strat, prop, arch, benchmark, concurrency, dargs, esbmc_ci)
     if concurrency:
       command_line += "--no-pointer-check --no-bounds-check "
     else:
-      command_line += "--no-pointer-check --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind "
+      command_line += "--no-pointer-check --interval-analysis --no-bounds-check --error-label ERROR --goto-unwind --unlimited-goto-unwind "
   elif prop == Property.datarace:
     # TODO: can we do better in case 'concurrency == False'?
     command_line += "--no-pointer-check --no-bounds-check --data-races-check --no-assertions "
