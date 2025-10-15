15c7a36a6d2a9e7c7c8b6c00ccf46c1100ca6d78
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-10-15 21:14:25 +0100

    [python] added type annotation to test case
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/str1/main.py
+++ regression/python/str1/main.py
@@ -1,7 +1,10 @@
-def test_no_arguments():
-    """Test str() with no arguments returns """""
-    x = str()
+def test_no_arguments() -> None:
+    """Test that str() with no arguments returns an empty string."""
+    x: str = str()
     assert x == ""
     assert isinstance(x, str)
 
-test_no_arguments()
+
+if __name__ == "__main__":
+    test_no_arguments()
+
