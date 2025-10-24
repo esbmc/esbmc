3dd9f2c60aab8dc0771f07b473b8fe0fc533c231
XLiZHI <958689657@qq.com>
2025-10-24 12:47:20 +0100

    [regression] add TCs for ffs

---
+++ regression/esbmc/ffs_fail/main.c
@@ -0,0 +1,23 @@
+#include <assert.h>
+#include <strings.h>
+
+int main()
+{
+  assert(ffs(0) == 0);
+  assert(ffs(1) == 1);
+  assert(ffs(2) == 2);
+  assert(ffs(4) == 3);
+  assert(ffs(8) == 4);
+
+  assert(ffs(3) == 1);
+  assert(ffs(6) == 2);
+  assert(ffs(12) == 3);
+
+  assert(ffs(0x1000) == 13);
+  assert(ffs(0x80000000) == 32);
+
+  assert(ffs(0b1011000) == 4);
+  assert(ffs(0b01010000) != 5);
+
+  return 0;
+}
