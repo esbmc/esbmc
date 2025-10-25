7c62abb272d3f8417a3529833ea027fe518357d9
lucasccordeiro <3694109+lucasccordeiro@users.noreply.github.com>
2025-10-25 23:13:51 +0000

    style(python): apply YAPF auto-formatting

--- src/python-frontend/models/datetime.py
+++ src/python-frontend/models/datetime.py
@@ -1,10 +1,17 @@
 # Operational model for datetime module
 
+
 class datetime:
     """Represents a date and time"""
-    
-    def __init__(self, year: int, month: int, day: int, 
-                 hour: int = 0, minute: int = 0, second: int = 0, microsecond: int = 0) -> None:
+
+    def __init__(self,
+                 year: int,
+                 month: int,
+                 day: int,
+                 hour: int = 0,
+                 minute: int = 0,
+                 second: int = 0,
+                 microsecond: int = 0) -> None:
         self.year: int = year
         self.month: int = month
         self.day: int = day
