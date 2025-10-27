09a4f26eb34bec94ac8b2d3939ed29f2ca3cae9e
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-10-27 10:13:28 +0000

    Fix formatting in irep2.h

--- src/irep2/irep2.h
+++ src/irep2/irep2.h
@@ -132,7 +132,7 @@
   BOOST_PP_LIST_CONS(forall,                                                   \
   BOOST_PP_LIST_CONS(exists,                                                   \
   BOOST_PP_LIST_NIL)))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))))
-
+// clang-format on
 
 // Even crazier forward decls,
 namespace esbmct
