12184ec903cf2896c4ae4f858bb4824edb91124a
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2025-10-29 08:59:37 +0000

    [python] fix None-pointer comparison and improve string membership handling (#2981)

---
+++ regression/python/github_2980/main.py
@@ -0,0 +1,11 @@
+def foo(s: str | None = None, t: str | None = None) -> None:
+    d: list[str] = []
+    if s is not None:
+        d.append(s)
+    if t is not None:
+        d.append(t)
+    
+    l = len(d)
+    assert l == 1
+
+foo(s="foo")
