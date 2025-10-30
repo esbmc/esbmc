20284006c40d0b13b7934ef69e1c674c81b5eff7
LukeW1999 <lukewang19@outlook.com>
2025-10-30 10:38:09 +0000

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -150,6 +150,7 @@ macos_setup () {
         python3 -m venv ../esbmc-venv&&
         source ../esbmc-venv/bin/activate&&
         pip install ast2json mypy meson&&
+        pip3 install pyparsing toml tomli jira&&
         deactivate&&
     BASE_ARGS="\
         -DLLVM_DIR=/opt/homebrew/opt/llvm@$CLANG_VERSION \
