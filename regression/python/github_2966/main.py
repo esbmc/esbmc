eb18012d4038360c6b4f0917dc859e1e2d14da32
XLiZHI <958689657@qq.com>
2025-10-30 14:44:33 +0000

    Revert "[python] fix #2966: Problem of isinstance handles ternary operators  (#2970)"
    This reverts commit f005902fbbee63e3bbe7b15f2e98998307c7949e.

--- regression/python/github_2966/main.py
+++
@@ -1,8 +0,0 @@
-# Test case for GitHub issue #2966
-# isinstance should work correctly with ternary operators
-from typing import Any
-
-i = 42
-x: Any = "foo" if i > 10 else i + 10
-assert isinstance(x, str)
-
--- regression/python/github_2966_fail/main.py
+++
@@ -1,8 +0,0 @@
-# Test case for GitHub issue #2966 (failure case)
-# isinstance should correctly fail when condition is false
-from typing import Any
-
-i = 5  # i <= 10, so x will be int, not str
-x: Any = "foo" if i > 10 else i + 10
-assert isinstance(x, str)
-
