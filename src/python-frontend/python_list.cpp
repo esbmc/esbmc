1db8204d73d6fe7c7dcd16b70c99de0b00e8f07f
XLiZHI <958689657@qq.com>
2025-11-02 11:22:11 +0000

    Revert "[python] Fix Array Type Dereference Error in Python List Iteration and Class Member Assignment (#3004)"
    This reverts commit c0be65056fa8ee0e682fc2368aef4cf46beba977.

--- src/python-frontend/python_list.cpp
+++ src/python-frontend/python_list.cpp
@@ -882,20 +882,10 @@ exprt python_list::handle_index_access(
       base.swap(deref);
     }
 
-    // Cast from void* to target type pointer
+    // Cast from void* to target type pointer and dereference
     typecast_exprt tc(obj_value, pointer_typet(elem_type));
 
-    // For array types (like strings), apply array-to-pointer decay
-    // Convert pointer-to-array (e.g., char[4]*) to pointer-to-element (e.g., char*)
-    // by casting to the element pointer type
-    if (elem_type.is_array())
-    {
-      const typet &element_type = to_array_type(elem_type).subtype();
-      typecast_exprt array_to_ptr(tc, pointer_typet(element_type));
-      return array_to_ptr;
-    }
-
-    // For non-array types, dereference to get the actual value
+    // Dereference to get the actual value
     dereference_exprt deref(elem_type);
     deref.op0() = tc;
     return deref;
