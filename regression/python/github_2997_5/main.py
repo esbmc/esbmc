2459a1c95e7e0094ec3a729f1ed27b0119ccc45c
LukeW1999 <lukewang19@outlook.com>
2025-11-05 18:21:09 +0000

    Update github_2997_5 test with chained forward references

--- regression/python/github_2997_5/main.py
+++ regression/python/github_2997_5/main.py
@@ -1,9 +1,22 @@
-class Bar:
-    pass
+class A:
+    def __init__(self, val: int) -> None:
+        self.val: int = val
 
-class Foo:
-    def bar(self) -> Bar:  
-        return Bar()
+    def create_b(self) -> 'B':
+        return B(self)
 
-f = Foo()
-b = f.bar()
+class B:
+    def __init__(self, a: A) -> None:
+        self.val: int = a.val
+
+    def create_c(self) -> 'C':
+        return C(self)
+
+class C:
+    def __init__(self, b: B) -> None:
+        self.val: int = b.val
+
+a = A(7)
+b = a.create_b()
+c = b.create_c()
+assert c.val == 7
