4989efe34c31fd350979fe3ae11f01629bb0dbb3
XLiZHI <958689657@qq.com>
2025-11-18 15:52:45 +0000

    [goto-symex] introduce new SSA branching

--- src/goto-symex/symex_goto.cpp
+++ src/goto-symex/symex_goto.cpp
@@ -20,6 +20,16 @@ void goto_symext::symex_goto(const expr2tc &old_guard)
   cur_state->rename(new_guard);
   do_simplify(new_guard);
 
+  if (seen_loop.insert(instruction.loop_number).second)
+  {
+    target->branching(
+      cur_state->guard.as_expr(),
+      new_guard,
+      cur_state->source,
+      cur_state->top().hidden,
+      first_loop);
+  }
+
   bool new_guard_false = (is_false(new_guard) || cur_state->guard.is_false());
   bool new_guard_true = is_true(new_guard);
 
