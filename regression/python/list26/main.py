344b10b004fcf35dad80d970ce0600f83e3baa26
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-11-24 14:45:42 +0000

    [python] added test case for bool
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/list26/main.py
+++ regression/python/list26/main.py
@@ -4,19 +4,9 @@ def test_nondet_list_in_conditional():
     x = []
     x.append(a)
     x.append(b)
-
-    result = []
-
-    if len(x) == 0:
-        result.append(False)
-    elif len(x) == 1:
-        result.append(x[0])
-
     y = False
     x.append(y)
-    result.append(y)
-
-    assert result[len(x)-1] == y
+    assert x[len(x)-1] == y
 
 test_nondet_list_in_conditional()
 
