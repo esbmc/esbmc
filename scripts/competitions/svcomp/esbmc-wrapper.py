c33467d7019946914f0ba59719efa563a6ab79b7
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-12-07 19:37:37 +0000

    [sv-comp] increased k-step to 5
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- scripts/competitions/svcomp/esbmc-wrapper.py
+++ scripts/competitions/svcomp/esbmc-wrapper.py
@@ -227,7 +227,7 @@ esbmc_path = "./esbmc "
 
 # ESBMC default commands: this is the same for every submission
 esbmc_dargs = "--no-div-by-zero-check --force-malloc-success --force-realloc-success --state-hashing --add-symex-value-sets "
-esbmc_dargs += "--no-align-check --k-step 2 --floatbv --unlimited-k-steps "
+esbmc_dargs += "--no-align-check --k-step 5 --floatbv --unlimited-k-steps "
 
 # <https://github.com/esbmc/esbmc/pull/1190#issuecomment-1637047028>
 esbmc_dargs += "--no-vla-size-check "
