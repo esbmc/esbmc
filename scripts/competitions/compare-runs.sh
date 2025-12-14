01375c9b7f40df70062830448d91e749da2fdadb
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-12-14 15:56:09 +0000

    some path fixing

--- scripts/competitions/compare-runs.sh
+++ scripts/competitions/compare-runs.sh
@@ -16,8 +16,8 @@ out=diff-$1-$2
 
 res=()
 mkdir $out &&
-    for c in no-overflow termination unreach-call valid-memcleanup.MemSafety-MemCleanup valid-memsafety; do
-	table-generator -f html -d -o $out/$c {$old,$new}/esbmc-kind.*.results.SV-COMP22_$c.xml.bz2 &&
+    for c in o-data-race no-overflow termination unreach-call valid-memcleanup valid-memsafety; do
+	table-generator -f html -d -o $out/$c {$old,$new}/esbmc-kind.*.results.SV-COMP*_$c.xml.bz2 &&
 	    f=`realpath --relative-to "$(dirname "$0")" $out/$c/results.*.diff.html` &&
 	    res+=( "- [$c](https://fbrausse.github.io/esbmc/svcomp/$f)" )
     done
