aacad71d0f341fcbeb01a177db74cb565cbac09e
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-12-14 15:58:26 +0000

    broken name

--- scripts/competitions/compare-runs.sh
+++ scripts/competitions/compare-runs.sh
@@ -16,7 +16,7 @@ out=diff-$1-$2
 
 res=()
 mkdir $out &&
-    for c in o-data-race no-overflow termination unreach-call valid-memcleanup valid-memsafety; do
+    for c in no-data-race no-overflow termination unreach-call valid-memcleanup valid-memsafety; do
 	table-generator -f html -d -o $out/$c {$old,$new}/esbmc-kind.*.results.SV-COMP*_$c.xml.bz2 &&
 	    f=`realpath --relative-to "$(dirname "$0")" $out/$c/results.*.diff.html` &&
 	    res+=( "- [$c](https://fbrausse.github.io/esbmc/svcomp/$f)" )
