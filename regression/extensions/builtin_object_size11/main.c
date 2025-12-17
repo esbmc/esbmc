574ec842e687852500650a2535f6e11b77701ed1
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2025-12-17 12:33:38 +0000

    moved builtin_object tests to extenssions

--- regression/extensions/builtin_object_size11/main.c
+++ regression/extensions/builtin_object_size11/main.c
@@ -5,7 +5,10 @@ int main(int argc, char **argv) {
     char buffer[100];
     char *ptr = buffer + argc; // symbolic offset
     size_t size = __builtin_object_size(ptr, 1);
-    assert(size == (size_t)-1);
+    if(argc > 100)
+      assert(size == (size_t)0);
+    else
+      Assert((int) size == (100 - argc));      
     return 0;
 }
 
