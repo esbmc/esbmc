97ccb14515e9434cacff1a2ff33a26a9103e3059
LukeW1999 <lukewang19@outlook.com>
2025-12-19 01:40:26 +0100

    add test case

--- regression/python/github_3338/ex.py
+++ regression/python/github_3338/ex.py
@@ -1,5 +1,4 @@
-d: dict[str, list[int]] = {'a': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]}
-l: list[int] = d['a']
-assert len(l) == 10
-for i in d['a']:
-    assert i <= 10
+import ll
+
+s = "foo"
+c = ll.Foo(s)
