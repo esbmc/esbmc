240a8c567a103978740c279ee14fec610daef196
prototypeC14 <caishiyu14luck@gmail.com>
2025-12-28 02:31:48 +0000

    [python] ctest testcase generation version_1

--- src/esbmc/bmc.h
+++ src/esbmc/bmc.h
@@ -7,6 +7,7 @@
 #include <goto-symex/symex_target_equation.h>
 #include <goto-symex/witnesses.h>
 #include <goto-symex/pytest.h>
+#include <goto-symex/ctest.h>
 #include <langapi/language_ui.h>
 #include <list>
 #include <map>
@@ -47,6 +48,7 @@ protected:
   std::unique_ptr<smt_convt> runtime_solver;
   std::unique_ptr<reachability_treet> symex;
   pytest_generator pytest_gen; // For Python pytest test case generation
+  ctest_generator ctest_gen;   // For C CTest test case generation
   mutable std::atomic<bool> keep_alive_running;
   mutable std::atomic<int> keep_alive_interval;
 
@@ -137,6 +139,7 @@ void report_coverage(
   const optionst &options,
   std::unordered_set<std::string> &reached_claims,
   const std::unordered_multiset<std::string> &reached_mul_claims,
-  pytest_generator &pytest_gen);
+  pytest_generator &pytest_gen,
+  ctest_generator &ctest_gen);
 
 #endif
