f8387c90b7b40e36eccddf76e511acd65be3812f
Lucas Cordeiro <lucasccordeiro@gmail.com>
2025-12-31 19:41:39 +0000

    [python] removed test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/indexing1_fail/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    lst = [10, 20, 30]
-    value = lst[5]  # <-- Deliberate out-of-bounds access
-    assert value == 0  # Add an assert to make model checking easier to expose problems
-
-main()
--- regression/python/indexing6_fail/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    lst = [10, 20, 30]
-    value = lst[-5]  # <-- Deliberate negative out-of-bounds access
-    assert value == 0  # Add an assert to make it easier for the model detector to expose problems
-
-main()
