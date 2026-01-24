8fe4bfe9b1fa8350460c527fb9426dc0d2487c6b
lucasccordeiro <3694109+lucasccordeiro@users.noreply.github.com>
2026-01-24 19:41:55 +0000

    Apply automatic changes

--- src/c2goto/library/verifier.c
+++ src/c2goto/library/verifier.c
@@ -1,4 +1,4 @@
-void __VERIFIER_nondet_memory(void *mem, __SIZE_TYPE__ size) 
+void __VERIFIER_nondet_memory(void *mem, __SIZE_TYPE__ size)
 {
   unsigned char *p = (unsigned char *)mem;
   for (__SIZE_TYPE__ i = 0; i < size; i++)
