2d5b44adde0eba55b28349da9eee7db8c728be03
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2026-02-02 05:44:58 +0000

    [python] extend type inference for partial generic container annotations (#3473)
    This PR infers element types for list/dict/set parameters annotated with
    no element type (e.g., x:list -> x:list[int]) by examining variable
    initializations and function call arguments.
    
    ---------
    
    Co-authored-by: lucasccordeiro <3694109+lucasccordeiro@users.noreply.github.com>

--- regression/python/list-var-undef/main.py
+++ regression/python/list-var-undef/main.py
@@ -1,11 +1,14 @@
-def max(x):
+def my_max(x:list):
     i = 1
-    max = x[0]
-    while i < len(l):
-        if x[i] > max:
-            max = x[i]
+    m = x[0]
+
+    while i < len(x):
+        if x[i] > m:
+            m = x[i]
         i = i + 1
-    return max
+
+    return m
+
 
 l = [1, 2, 3]
-assert max(l) == 3
+assert my_max(l) == 3
