83cd370b1a5e8ba831ec25d219d3cca5ef8ec257
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-02-02 05:47:56 +0000

    [python] updated test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/list-var-undef/main.py
+++ regression/python/list-var-undef/main.py
@@ -1,14 +1,11 @@
-def my_max(x:list):
+def max(x):
     i = 1
-    m = x[0]
-
-    while i < len(x):
-        if x[i] > m:
-            m = x[i]
+    max = x[0]
+    while i < len(l):
+        if x[i] > max:
+            max = x[i]
         i = i + 1
-
-    return m
-
+    return max
 
 l = [1, 2, 3]
-assert my_max(l) == 3
+assert max(l) == 3
