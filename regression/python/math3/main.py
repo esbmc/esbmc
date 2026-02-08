2dd45c767308e273c218ff491bc96d5ff1dc6dac
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2026-02-08 09:53:59 +0000

    [python] add support for math.sin() and math.cos() (#3498)

---
+++ regression/python/math3/main.py
@@ -0,0 +1,13 @@
+import math
+import math
+
+a = math.sin(0)
+b = math.sin(0)
+c = math.cos(0)  
+d = math.cos(0)
+e = math.sqrt(2)
+f = math.sqrt(2)
+
+assert a == b        # Both should be 0
+assert c == d        # Both should be 1
+assert e == f        # Both should be sqrt(2)
