232becee7eb11017716f5c3d726d432d614aec1e
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-02-10 21:06:39 +0000

    [python] fix len(range(...)) routing to use get_object_size instead of strlen
    When len() is called directly on a range() expression (e.g., len(range(5))),
    the argument is a Call node rather than a List or Name node. This caused the
    code to incorrectly fall through to strlen instead of using get_object_size.
    
    Added detection for Call nodes invoking range() to properly route to
    kGetObjectSize for correct length computation.

--- src/python-frontend/function_call_builder.cpp
+++ src/python-frontend/function_call_builder.cpp
@@ -280,7 +280,14 @@ symbol_id function_call_builder::build_function_id() const
       }
     }
 
-    if (arg["_type"] == "List")
+    // Handle len(range(...))
+    if (arg["_type"] == "Call")
+    {
+      if (arg.contains("func") && arg["func"].contains("id") &&
+          arg["func"]["id"] == "range")
+        func_name = kGetObjectSize;
+    }
+    else if (arg["_type"] == "List")
       func_name = kGetObjectSize;
     else if (arg["_type"] == "Name")
     {
