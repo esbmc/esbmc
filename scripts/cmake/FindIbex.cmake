1055d3f4e0c37254936c036efaa1d448022a7c1e
Rafael Sá Menezes <rafael.sa.menezes@outlook.com>
2026-02-10 10:19:59 +0000

    [ibex] guarded ibex build

--- scripts/cmake/FindIbex.cmake
+++ scripts/cmake/FindIbex.cmake
@@ -14,35 +14,39 @@ if(DOWNLOAD_DEPENDENCIES AND (NOT DEFINED IBEX_DIR))
    set(_ibex_build_dir "${ibex_BINARY_DIR}/build")
    set(_ibex_install_dir "${ibex_BINARY_DIR}/install")
 
-   message("[ibex] Configuring ibex with CMake")
-   set(_ibex_cmake_args
-      -DCMAKE_INSTALL_PREFIX=${_ibex_install_dir}
-      -DCMAKE_BUILD_TYPE=Release)
-   if(ACADEMIC_BUILD)
-      message(WARNING "the version of ibex you have is ZIB licensed, distribution is impossible.")
-      list(APPEND _ibex_cmake_args -DLP_LIB=soplex)
-   endif()
-   execute_process(
-      COMMAND ${CMAKE_COMMAND} -S ${ibex_SOURCE_DIR} -B ${_ibex_build_dir} ${_ibex_cmake_args}
-      RESULT_VARIABLE _ibex_configure_result)
-   if(NOT _ibex_configure_result EQUAL 0)
-      message(FATAL_ERROR "[ibex] CMake configure failed")
-   endif()
+   if(EXISTS "${_ibex_install_dir}/lib/libibex.a")
+      message("[ibex] Found existing ibex installation, skipping build")
+   else()
+      message("[ibex] Configuring ibex with CMake")
+      set(_ibex_cmake_args
+         -DCMAKE_INSTALL_PREFIX=${_ibex_install_dir}
+         -DCMAKE_BUILD_TYPE=Release)
+      if(ACADEMIC_BUILD)
+         message(WARNING "the version of ibex you have is ZIB licensed, distribution is impossible.")
+         list(APPEND _ibex_cmake_args -DLP_LIB=soplex)
+      endif()
+      execute_process(
+         COMMAND ${CMAKE_COMMAND} -S ${ibex_SOURCE_DIR} -B ${_ibex_build_dir} ${_ibex_cmake_args}
+         RESULT_VARIABLE _ibex_configure_result)
+      if(NOT _ibex_configure_result EQUAL 0)
+         message(FATAL_ERROR "[ibex] CMake configure failed")
+      endif()
 
-   message("[ibex] Building ibex")
-   execute_process(
-      COMMAND ${CMAKE_COMMAND} --build ${_ibex_build_dir} --parallel 4
-      RESULT_VARIABLE _ibex_build_result)
-   if(NOT _ibex_build_result EQUAL 0)
-      message(FATAL_ERROR "[ibex] Build failed")
-   endif()
+      message("[ibex] Building ibex")
+      execute_process(
+         COMMAND ${CMAKE_COMMAND} --build ${_ibex_build_dir} --parallel 4
+         RESULT_VARIABLE _ibex_build_result)
+      if(NOT _ibex_build_result EQUAL 0)
+         message(FATAL_ERROR "[ibex] Build failed")
+      endif()
 
-   message("[ibex] Installing ibex")
-   execute_process(
-      COMMAND ${CMAKE_COMMAND} --install ${_ibex_build_dir}
-      RESULT_VARIABLE _ibex_install_result)
-   if(NOT _ibex_install_result EQUAL 0)
-      message(FATAL_ERROR "[ibex] Install failed")
+      message("[ibex] Installing ibex")
+      execute_process(
+         COMMAND ${CMAKE_COMMAND} --install ${_ibex_build_dir}
+         RESULT_VARIABLE _ibex_install_result)
+      if(NOT _ibex_install_result EQUAL 0)
+         message(FATAL_ERROR "[ibex] Install failed")
+      endif()
    endif()
 
    set(IBEX_DIR ${_ibex_install_dir})
