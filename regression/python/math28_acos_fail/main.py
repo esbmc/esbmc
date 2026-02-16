28b36c77072e5965a3c72fb692cc8d14f8867aff
sergillam.oliveira <sergillam.oliveira@tpv-tech.com>
2026-02-16 12:28:29 -0400

    tests: add math success/failure regressions

---
+++ regression/python/math28_acos_fail/main.py
@@ -0,0 +1,9 @@
+import math
+
+
+def test_acos_fail() -> None:
+    x = 2.0
+    math.acos(x)
+
+
+test_acos_fail()
---
+++ regression/python/math29_fail/main.py
@@ -0,0 +1,12 @@
+import math
+
+
+def test_math29_fail() -> None:
+    # Domain errors
+    math.asin(2.0)
+    math.acos(2.0)
+    math.log2(0.0)
+    math.log10(0.0)
+
+
+test_math29_fail()
