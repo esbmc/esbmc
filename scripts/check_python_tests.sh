eabd9d26919dcb3f9d6cbb39001bd34aa627134f
sergillam.oliveira <sergillam.oliveira@tpv-tech.com>
2026-02-27 12:09:46 -0400

    [python-regression] rename github_3622 tests to nondet and drop ignore-list exceptions

--- scripts/check_python_tests.sh
+++ scripts/check_python_tests.sh
@@ -60,8 +60,6 @@ ignored_dirs=(
   "github_3560_4"
   "github_3563_2"
   "github_3563_3"
-  "github_3622"
-  "github_3622_fail"
   "global"
   "infer-func-no-return_fail"
   "integer_squareroot_fail"
