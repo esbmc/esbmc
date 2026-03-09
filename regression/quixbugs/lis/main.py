c6edc95515e1ddbee3201d85c55fd1298d5e01c7
Lucas C. Cordeiro <lucasccordeiro@gmail.com>
2026-03-09 18:26:36 +0000

    Add function to calculate longest increasing subsequence
    Implement longest increasing subsequence function.

--- regression/quixbugs/lis/main.py
+++ regression/quixbugs/lis/main.py
@@ -15,6 +15,24 @@ def lis(arr):
 
     return longest
 
+"""
+def lis(arr):
+    ends = {}
+    longest = 0
+
+    for i, val in enumerate(arr):
+
+        prefix_lengths = [j for j in range(1, longest + 1) if arr[ends[j]] < val]
+
+        length = max(prefix_lengths) if prefix_lengths else 0
+
+        if length == longest or val < arr[ends[length + 1]]:
+            ends[length + 1] = i
+            longest = max(length + 1, longest)
+
+    return longest
+"""
+
 assert lis([]) == 0
 assert lis([3]) == 1
 #assert lis([10, 20, 11, 32, 22, 48, 43]) == 4
