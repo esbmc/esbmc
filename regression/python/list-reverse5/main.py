dc788549292b8b9d6123bf82322f99c25f5d2a84
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-03-13 08:32:39 +0000

    [python] improved performance of the regression
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/list-reverse5/main.py
+++ regression/python/list-reverse5/main.py
@@ -1,8 +1,8 @@
 def test_reverse_twice_is_identity():
-    l = [1, 2, 3, 4, 5]
+    l = [1, 2, 3]
     l.reverse()
     l.reverse()
-    assert l == [1, 2, 3, 4, 5]
+    assert l == [1, 2, 3]
 
 test_reverse_twice_is_identity()
 
