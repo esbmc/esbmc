3d1197eca95fcc67aabc3e59973dfda0ae7c5c99
Bruno Farias <brunocarvalhofarias@gmail.com>
2026-03-27 21:10:58 +0000

    [python] Add quixbugs buggy tests (#3905)
    Co-authored-by: Lucas C. Cordeiro <lucasccordeiro@gmail.com>

--- regression/quixbugs/pascal_fail/main.py
+++ regression/quixbugs/pascal_fail/main.py
@@ -3,7 +3,7 @@ def pascal(n):
     rows = [[1]]
     for r in range(1, n):
         row = []
-        for c in range(0, r + 1):
+        for c in range(0, r):
             upleft = rows[r - 1][c - 1] if c > 0 else 0
             upright = rows[r - 1][c] if c < r else 0
             row.append(upleft + upright)
@@ -11,9 +11,29 @@ def pascal(n):
 
     return rows
 
-assert pascal(1) == [[1]]
-assert pascal(2) == [[1], [0, 1]]
-#assert pascal(3) == [[1], [1, 1], [1, 2, 1]]
-#assert pascal(4) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
-#assert pascal(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
 
+"""
+Pascal's Triangle
+pascal
+
+
+
+Input:
+    n: The number of rows to return
+
+Precondition:
+    n >= 1
+
+Output:
+    The first n rows of Pascal's triangle as a list of n lists
+
+Example:
+    >>> pascal(5)
+    [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
+"""
+
+assert pascal(1) == [[1]]
+assert pascal(2) == [[1], [1, 1]]
+assert pascal(3) == [[1], [1, 1], [1, 2, 1]]
+assert pascal(4) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+assert pascal(5) == [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1]]
