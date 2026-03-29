ae0054ac76a596e1c4e4a0bebdc9f7f7652fc7e5
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-03-29 16:49:30 +0100

    [python] optmised test cases
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/python/github_3841_6/main.py
+++ regression/python/github_3841_6/main.py
@@ -1,9 +1,7 @@
 from collections import defaultdict as dd
 
-# Alias import form: from collections import defaultdict as dd
 d: dict = dd(int)
 d["a"] += 1
 d["a"] += 1
 v: int = d["missing"]
 assert d["a"] == 2
-assert v == 0
