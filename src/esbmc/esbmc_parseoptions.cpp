f751e0b124a1daa4657ee8834ad021c91979d463
Yiannis Charalambous <yiannis128@hotmail.com>
2026-03-31 16:22:15 +0100

    fix: formatting of logo in -h to be center

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -2981,7 +2981,14 @@ void esbmc_parseoptionst::help()
     isatty(fileno(stderr));
 #endif
 
-  log_status("\n* * *           ESBMC {}          * * *", ESBMC_VERSION);
+  // Print the "* * *     ESBMC x.y.z     * * *"
+  auto const esbmc_string = std::format("ESBMC {}", ESBMC_VERSION);
+  auto const title_start = std::string("* * * ");
+  auto const title_end = std::string(" * * *");
+  auto const inner = 80 - title_start.length() - title_end.length() - esbmc_string.length();
+  auto const left_pad = std::string(inner / 2, ' ');
+  auto const right_pad = std::string(inner - inner / 2, ' ');
+  log_status("\n{}{}{}{}{}", title_start, left_pad, esbmc_string, right_pad, title_end);
 
   std::ostringstream oss;
   oss << cmdline.cmdline_options;
