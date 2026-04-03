a3b77b0ee39072d43d702133747d0d4d347daf08
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-04-03 02:11:54 +0100

    updated test desc
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- regression/esbmc/github_565_1/main.c
+++ regression/esbmc/github_565_1/main.c
@@ -3,7 +3,7 @@ $clang -fsanitize=vla-bound -g main.c -o test
 $./test
 main.c:9:10: runtime error: variable length array bound evaluates to non-positive value 0
 SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior main.c:9:10 
-/*
+*/
 
 #include <assert.h>  // Include the assert library
 
