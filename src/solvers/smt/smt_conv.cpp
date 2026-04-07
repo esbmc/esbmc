a27d5e229beb9969c027953cd621d1cfbfeb8562
Shema <shema.alosaimi@student.manchester.ac.uk>
2026-04-07 16:23:51 +0100

    [ir-ra] simplify nearest-mode RNA dispatch

--- src/solvers/smt/smt_conv.cpp
+++ src/solvers/smt/smt_conv.cpp
@@ -1422,7 +1422,7 @@ smt_astt smt_convt::convert_ast(const expr2tc &expr)
           if (is_nearest_rounding_mode(rounding_mode))
             bounds =
               apply_ieee754_rne_enclosure(real_result, lo_r, hi_r, fbv_type);
-          else if (is_round_to_away(rounding_mode))
+          else
             bounds =
               apply_ieee754_rna_enclosure(real_result, lo_r, hi_r, fbv_type);
           ir_ra_interval_map[real_result] = {bounds.first, bounds.second};
@@ -1493,7 +1493,7 @@ smt_astt smt_convt::convert_ast(const expr2tc &expr)
           if (is_nearest_rounding_mode(rounding_mode))
             bounds =
               apply_ieee754_rne_enclosure(real_result, lo_r, hi_r, fbv_type);
-          else if (is_round_to_away(rounding_mode))
+          else
             bounds =
               apply_ieee754_rna_enclosure(real_result, lo_r, hi_r, fbv_type);
           ir_ra_interval_map[real_result] = {bounds.first, bounds.second};
