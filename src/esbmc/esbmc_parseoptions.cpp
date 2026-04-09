7ada72389deef2f964c31cf316c31f71b91da871
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-04-09 15:50:19 +0100

    do not do interval symex guard pruning for k-induction
    Signed-off-by: Lucas Cordeiro <lucasccordeiro@gmail.com>

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -443,6 +443,9 @@ void esbmc_parseoptionst::get_command_line_options(optionst &options)
   if (cmdline.isset("ub-shift-check"))
     options.set_option("ub-shift-check", true);
 
+  if (cmdline.isset("k-induction"))
+    options.set_option("no-interval-symex-guard", true);
+
   if (cmdline.isset("timeout"))
   {
 #ifdef _WIN32
