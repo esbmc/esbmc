d535999776053e4b94c1c653586f4bfe75b8eb4a
Lucas Cordeiro <lucasccordeiro@gmail.com>
2026-04-30 06:21:15 +0100

    [util] replace C-style cast with static_cast in migrate.cpp:152
    exprt::find() returns const irept &; the cast to (exprt &) dropped const
    and downcast in one step. Using static_cast<const exprt &> makes the
    downcast explicit; the copy into exprt sz does not need a mutable source.

--- src/util/migrate.cpp
+++ src/util/migrate.cpp
@@ -149,7 +149,7 @@ static type2tc migrate_type0(const typet &type)
     }
     else
     {
-      exprt sz = (exprt &)type.find(typet::a_size);
+      exprt sz = static_cast<const exprt &>(type.find(typet::a_size));
       simplify(sz);
       migrate_expr(sz, size);
       size = fixup_containerof_in_sizeof(size);
