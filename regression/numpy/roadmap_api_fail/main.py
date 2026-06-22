aa628d8f9793680087927476a3395aa367d5a1d2
sergillam.oliveira <sergillam.oliveira@tpv-tech.com>
2026-06-22 08:51:46 -0400

    test: isolate numpy roadmap failure coverage

--- regression/numpy/roadmap_api_fail/main.py
+++ regression/numpy/roadmap_api_fail/main.py
@@ -1,10 +1,3 @@
 import numpy as np
 
-
-assert np.sum([1, 2, 3]) == 6
-assert np.prod([2, 3]) == 6
-assert np.min([3, 2, 1]) == 1
-assert np.max([1, 5, 2]) == 5
-assert np.where([True, False], [1, 2], [3, 4]) == [1, 4]
-
-assert np.arange(4) == [0, 1, 2, 4]
+assert np.full(0, 7) == [7]
