a14a913c16d56f0e3c8faa3eae9bb145e5f98e79
sergillam <sgm.oliveira96@gmail.com>
2026-07-30 15:49:34 -0400

    test: add numpy descriptor view metadata regressions

---
+++ regression/numpy/descriptor_view_ndim_success/main.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+a = np.array([[1, 2, 3], [4, 5, 6]])
+row = a[0]
+
+assert row.ndim == 1
+
---
+++ regression/numpy/descriptor_view_unknown_stride_fail/main.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+step = nondet_int()
+a = np.array([1, 2, 3])
+part = a[::step]
+
