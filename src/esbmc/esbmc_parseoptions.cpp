d6453e049f7396f65c92f8cef358b90b76c281ce
Tong Wu <39028016+Anthonysdu@users.noreply.github.com>
2026-08-09 11:24:59 +0100

    Update esbmc_parseoptions.cpp

--- src/esbmc/esbmc_parseoptions.cpp
+++ src/esbmc/esbmc_parseoptions.cpp
@@ -1933,7 +1933,9 @@ bool esbmc_parseoptionst::process_goto_program(
 
     goto_functions.update();
 
-    if (cmdline.isset("data-races-check"))
+    if (
+      cmdline.isset("data-races-check")||
+      cmdline.isset("data-races-check-only"))
     {
       log_status("Adding Data Race Checks");
       options.set_option("no-por", true);
