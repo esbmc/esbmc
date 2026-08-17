6c631129320fbe476aa0022b44759d6dcc5e89c0
Lucas Cordeiro <lucas.cordeiro@manchester.ac.uk>
2026-08-17 18:30:48 +0100

    [c2goto] Restore the filesystem handle the VFS flail blocks use
    PR #6766 added five ESBMC_FLAIL blocks whose bodies call fs.add_bundled(),
    but the declaration of `fs` never came with them, so master has not compiled
    since: each macro expands once per bundled file, turning one missing
    declaration into hundreds of errors. Bind the singleton once, use it in all
    eight blocks, and land the clib_source_from_vfs test #6766 promised.

--- src/c2goto/cprover_libc_sources.cpp
+++ src/c2goto/cprover_libc_sources.cpp
@@ -42,21 +42,20 @@ void register_bundled_libc()
     return;
   done = true;
 
+  file_operations::filesystemt &fs = file_operations::filesystemt::get();
+
 #define ESBMC_FLAIL(body, size, ...)                                           \
-  file_operations::filesystemt::get().add_bundled(                             \
-    vfs_headers + "/" #__VA_ARGS__, body, size);
+  fs.add_bundled(vfs_headers + "/" #__VA_ARGS__, body, size);
 #include <headers/libc_hdr.h>
 #undef ESBMC_FLAIL
 
 #define ESBMC_FLAIL(body, size, ...)                                           \
-  file_operations::filesystemt::get().add_bundled(                             \
-    vfs_library + "/" #__VA_ARGS__, body, size);
+  fs.add_bundled(vfs_library + "/" #__VA_ARGS__, body, size);
 #include <libc.h>
 #undef ESBMC_FLAIL
 
 #define ESBMC_FLAIL(body, size, ...)                                           \
-  file_operations::filesystemt::get().add_bundled(                             \
-    vfs_libm + "/" #__VA_ARGS__, body, size);
+  fs.add_bundled(vfs_libm + "/" #__VA_ARGS__, body, size);
 #include <libm.h>
 #undef ESBMC_FLAIL
 
