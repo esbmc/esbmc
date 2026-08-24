5b75fcebe50483d088ca08f122a3c8c38546f5da
sergillam <sgm.oliveira96@gmail.com>
2026-08-24 17:33:03 -0400

    test: strengthen numpy transpose view regressions

--- regression/numpy/transpose_view_read_success/main.py
+++ regression/numpy/transpose_view_read_success/main.py
@@ -3,7 +3,9 @@ import numpy as np
 a = np.array([[1, 2, 3], [4, 5, 6]])
 t = np.transpose(a)
 
+a[1][0] = 9
+
 assert t[0][0] == 1
-assert t[0][1] == 4
+assert t[0][1] == 9
 assert t[1][0] == 2
 assert t[2][1] == 6
