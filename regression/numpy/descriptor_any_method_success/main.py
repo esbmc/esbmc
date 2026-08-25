da0215f67f24f50316046649a218286cca4a56f8
sergillam <sgm.oliveira96@gmail.com>
2026-08-25 12:06:24 -0400

    test: refine numpy descriptor boolean reducers

--- regression/numpy/descriptor_any_method_success/main.py
+++ regression/numpy/descriptor_any_method_success/main.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-a = np.array([[False, False], [True, False]])
-t = np.transpose(a)
+a = np.array([False, False, True, False])
+t = np.reshape(a, (2, 2))
 
 assert t.any()
--- regression/numpy/descriptor_reducer_bool_edge/main.py
+++ regression/numpy/descriptor_reducer_bool_edge/main.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 a = np.array([[True, False], [False, False]])
-t = np.transpose(a)
+t = np.reshape(a, (4,))
 
 assert t.any()
 assert not t.all()
